54f245bbb4c7d918eb3bb24acb02f43d399ecacc
Vlad Kovalov <vlad.kovalov.92@gmail.com>
2022-07-28 01:59:42 +0300

    update

---
+++ submissions/kovalov/js-pre-oop/index.js
@@ -0,0 +1,46 @@
+const dog = {
+  species: 'dog',
+  name: 'Patron',
+  gender: 'male',
+  legs: 4,
+  hands: 0,
+  saying: 'woof',
+};
+
+const cat = {
+  species: 'cat',
+  name: 'Murzyk',
+  gender: 'male',
+  legs: 4,
+  hands: 0,
+  saying: 'meow',
+};
+
+const woman = {
+  species: 'human',
+  name: 'Anna',
+  gender: 'female',
+  legs: 2,
+  hands: 2,
+  saying: 'hello',
+};
+
+const man = {
+  species: 'human',
+  name: 'Joey',
+  gender: 'male',
+  legs: 2,
+  hands: 2,
+  saying: 'How you doin?',
+};
+
+const catWoman = Object.create(cat);
+catWoman.name = 'Cat-woman';
+catWoman.species = 'human';
+catWoman.gender = 'female';
+catWoman.legs = 2;
+catWoman.hands = 2;
+
+const inhabitants = [dog, cat, woman, man, catWoman];
+
+inhabitants.forEach((item) => print(Object.values(item).join('; ')));
