3cab844c6e9449e15b7b06f372f9328596726774
Vlad Kovalov <vlad.kovalov.92@gmail.com>
2022-07-28 15:34:27 +0300

    update

--- submissions/kovalov/js-pre-oop/index.js
+++ submissions/kovalov/js-pre-oop/index.js
@@ -1,45 +1,52 @@
-function createInhabitant(obj) {
-  return new Map(Object.entries(obj));
-}
-
-const dog = createInhabitant({
+const dog = {
   species: 'dog',
   name: 'Patron',
   gender: 'male',
   legs: 4,
   hands: 0,
   saying: 'woof',
-});
+};
 
-const cat = createInhabitant({
+const cat = {
   species: 'cat',
   name: 'Murzyk',
   gender: 'male',
   legs: 4,
   hands: 0,
   saying: 'meow',
-});
+};
 
-const woman = createInhabitant({
+const woman = {
   species: 'human',
   name: 'Anna',
   gender: 'female',
   legs: 2,
   hands: 2,
   saying: 'hello',
-});
+};
 
-const man = createInhabitant({
+const man = {
   species: 'human',
   name: 'Joey',
   gender: 'male',
   legs: 2,
   hands: 2,
   saying: 'How you doin?',
+};
+
+const catWoman = Object.create(cat);
+catWoman.name = 'Cat-woman';
+catWoman.species = 'human';
+catWoman.gender = 'female';
+catWoman.legs = 2;
+catWoman.hands = 2;
+
+const inhabitants = [dog, cat, woman, man, catWoman];
+
+const keys = ['species', 'name', 'gender', 'legs', 'hands', 'saying'];
+
+const details = inhabitants.map((item) => {
+  return keys.map((key) => item[key]);
 });
 
-const inhabitants = [dog, cat, woman, man];
-
-inhabitants
-  .map((item) => Array.from(item.values()))
-  .forEach((item) => print(item.join('; ')));
+details.forEach((item) => print(item.join('; ')));
