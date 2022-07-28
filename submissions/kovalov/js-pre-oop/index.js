cbee20b7bb00f3edd7bd901471ce7fa3998816a5
Vlad Kovalov <vlad.kovalov.92@gmail.com>
2022-07-28 12:39:28 +0300

    removed comments

--- submissions/kovalov/js-pre-oop/index.js
+++ submissions/kovalov/js-pre-oop/index.js
@@ -1,16 +1,3 @@
-// const catWoman = Object.create(cat);
-// catWoman.name = 'Cat-woman';
-// catWoman.species = 'human';
-// catWoman.gender = 'female';
-// catWoman.legs = 2;
-// catWoman.hands = 2;
-
-// const inhabitants = [dog, cat, woman, man, catWoman];
-
-// inhabitants.forEach((item) => print(Object.values(item).join('; ')));
-
-// const dog = new Map();
-
 function createInhabitant(obj) {
   return new Map(Object.entries(obj));
 }
