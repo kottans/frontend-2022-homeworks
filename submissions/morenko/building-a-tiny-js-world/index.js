75164d6edbc98bdac3c57f2779a9181a9907f845
Maksym Morenko <morenko.maxim@gmail.com>
2022-07-29 14:23:15 +0300

    Building a Tiny JS World

---
+++ submissions/morenko/building-a-tiny-js-world/index.js
@@ -0,0 +1,43 @@
+const woman = {
+    species: 'human',
+    name: 'line',
+    gender: 'female',
+    legs: 2,
+    hands: 2,
+    saying: 'bye',
+  };
+const man = {
+    species: 'human',
+    name: 'Joey',
+    gender: 'male',
+    legs: 2,
+    hands: 2,
+    saying: 'hi',
+  };
+const dog = {
+    species: 'dog',
+    name: 'Dik',
+    gender: 'male',
+    legs: 4,
+    hands: 0,
+    saying: 'woof',
+  };
+const cat = {
+    species: 'cat',
+    name: 'Tom',
+    gender: 'male',
+    legs: 4,
+    hands: 0,
+    saying: 'mau',
+  };
+  
+  
+  const inhabitants = [dog, cat, woman, man,];
+  
+  const keys = ['species', 'name', 'gender', 'legs', 'hands', 'saying'];
+  
+  const description = inhabitants.map((el) => {
+    return keys.map(key => el[key]);
+  });
+  
+  description.map(el => print(el.join('; ')));
