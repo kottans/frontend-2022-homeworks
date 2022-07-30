bae5aaf140c8e20376a22c611c358ab76117c584
William <j.umvangeli@gmail.com>
2022-07-31 00:33:10 +0300

    add missed semicolon

--- submissions/sadoharu/task_js_dom_/scripts.js
+++ submissions/sadoharu/task_js_dom_/scripts.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     const pages = document.querySelectorAll('.section__page');
-    const navList = document.querySelector('.nav-bar__list')
+    const navList = document.querySelector('.nav-bar__list');
 
     const navListItem = [
         {title: 'Overview', icon: 'icon-browser'},
@@ -8,46 +8,46 @@ window.addEventListener('DOMContentLoaded', () => {
         {title: 'Achievement', icon: 'icon-award'},
         {title: 'Projects', icon: 'icon-files-empty'},
         {title: 'Reviews', icon: 'icon-rate_review'}
-    ]
+    ];
 
     navListItem.forEach((item, i) => {
         const liItem = document.createElement('li');
         liItem.classList.add('nav-bar__list_item');
         liItem.tabIndex = 0;
-        liItem.setAttribute('data-index', i)
+        liItem.setAttribute('data-index', i);
         liItem.innerHTML = `
             <span class="nav-bar__list_icon ${item.icon}"></span>
             <span class="nav-bar__list_title">${item.title}</span>
-            `
-        navList.append(liItem)
-    })
+            `;
+        navList.append(liItem);
+    });
 
     const hideShowPage = (show = 0) => {
         pages.forEach(item => {
             item.style.display = "none";
-        })
+        });
         pages[show].style.display = "block";
-    }
+    };
 
     hideShowPage();
 
     const togglePage = (e, node) => {
         hideShowPage(node.getAttribute('data-index'));
-        e.currentTarget.querySelectorAll('.nav-bar__list_item').forEach(item => item.classList.remove('active'))
-        node.classList.add('active')
-    }
+        e.currentTarget.querySelectorAll('.nav-bar__list_item').forEach(item => item.classList.remove('active'));
+        node.classList.add('active');
+    };
 
     navList.addEventListener('click', e => {
         if (e.target.getAttribute('data-index')) {
-            togglePage(e, e.target)
+            togglePage(e, e.target);
         } else if (e.target.parentNode.getAttribute('data-index')) {
-            togglePage(e, e.target.parentNode)
+            togglePage(e, e.target.parentNode);
         }
-    })
+    });
 
       document.addEventListener('keyup', e => {
         if (e.code === 'Space') {
-            togglePage(e, e.target)
+            togglePage(e, e.target);
         }
-      })
+      });
 });
