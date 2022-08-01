fdf6599049c125b97fa26ec39f69eeb4f6e71391
Maksim <60241469+maximmorenko@users.noreply.github.com>
2022-08-01 05:47:11 +0300

    пофиксил отступы

--- submissions/morenko/oop-exercise/index.js
+++ submissions/morenko/oop-exercise/index.js
@@ -1,42 +1,39 @@
-// создадим родительский класс World
+// создадим родительский класс WorldInhabitant
 class WorldInhabitant {
-  // объявляем метод конструктор (ожидаем на входе значения)
-  constructor(name, species, gender, saying){
-  // инициализация полей
-  this.name = name;
-  this.species = species;
-  this.gender = gender;
-  this.saying = saying;
+  // объявляем метод конструктор (ожидаем на входе значения, список свойств жителя)
+  constructor(name, species, gender, saying) {
+    // инициализация полей
+    this.name = name;
+    this.species = species;
+    this.gender = gender;
+    this.saying = saying;
   }
-  
   // объявление метода toString()
   // toString() возвращает строку, представляющую объект
   // обращение к собственному атрибуту через this
   toString() {
-     return `${this.name}; ${this.species}; ${this.gender}; ${this.saying}; `;
+    return `${this.name}; ${this.species}; ${this.gender}; ${this.saying}; `;
   }
 }
 
 // создадим два класса наследующие от родительского часть свойств
 class Human extends WorldInhabitant {
   constructor(name, species, gender, saying, legs, hands) {
-     super(name, species, gender, saying); //наследуем поля от родителя, и добавляем еще два поля
-     this.legs = 2 || legs;
-     this.hands = 2 || hands;
+    super(name, species, gender, saying); //наследуем поля от родителя, и добавляем еще два поля
+    this.legs = 2 || legs;
+    this.hands = 2 || hands;
   } 
-  
   toString() {
-  return super.toString() + `${this.legs}; ${this.hands}; `
+    return super.toString() + `${this.legs}; ${this.hands};`
   }
 }
 
 class Animal extends WorldInhabitant {
   // наследуем четыре свойства от родителя и добавляем лапы для животных
   constructor(name, species, gender, saying, paws) {
-     super(name, species, gender, saying) //наследуем поля от родителя, и добавляем еще два поля
-     this.paws = 4 || paws;
+    super(name, species, gender, saying) //наследуем поля от родителя, и добавляем еще два поля
+    this.paws = 4 || paws;
   }
-
   toString(){
     return super.toString() + `${this.paws}; `
   }
@@ -45,34 +42,34 @@ class Animal extends WorldInhabitant {
 // создаем экземпляр жителя и передаем параметр
 // этот экземпляр с собственным состоянием свойств класса
 const man = new Human(
-'human',
-'Joey',
-'male',
-'Hi'
+  'human',
+  'Joey',
+  'male',
+  'Hi',
 );
 
 // создаем новый экземпляр жителя
 const woman = new Human(
-'human',
-'Rey',
-'female',
-'Bye'
+  'human',
+  'Rey',
+  'female',
+  'Bye',
 );
 
 // создаем новый экземпляр жителя
 const cat = new Animal(
-'cat',
-'Allen',
-'female',
-'Mau'
+  'cat',
+  'Allen',
+  'female',
+  'Mau',
 );
   
 // создаем новый экземпляр жителя
 const dog = new Animal(
-'dog',
-'Johnny',
-'male',
-'Woof'
+  'dog',
+  'Johnny',
+  'male',
+  'Woof',
 );
 
 [man, woman, cat, dog].map(el => print(el));
