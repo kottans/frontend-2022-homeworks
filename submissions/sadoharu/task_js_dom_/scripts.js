5dab45d70eb08893690fafdad6ed2d5e5dfd5c82
William <j.umvangeli@gmail.com>
2022-08-04 00:05:26 +0300

    fix js file

--- submissions/sadoharu/task_js_dom_/scripts.js
+++ submissions/sadoharu/task_js_dom_/scripts.js
@@ -1,73 +1,60 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const pages = document.querySelectorAll(".section__page");
-    const navList = document.querySelector(".nav-bar__list");
-  
-    const navListItem = [
-      {
-        title: "Overview",
-        icon: "icon-browser",
-      },
-      {
-        title: "About",
-        icon: "icon-about-dot-me",
-      },
-      {
-        title: "Achievement",
-        icon: "icon-award",
-      },
-      {
-        title: "Projects",
-        icon: "icon-files-empty",
-      },
-      {
-        title: "Reviews",
-        icon: "icon-rate_review",
-      },
-    ];
-  
-    navListItem.forEach((item, i) => {
-      const liItem = document.createElement("li");
-      liItem.classList.add("nav-bar__list_item");
-      liItem.tabIndex = 0;
-      liItem.setAttribute("data-index", i);
-      liItem.innerHTML = `
-              <span class="nav-bar__list_icon ${item.icon}"></span>
-              <span class="nav-bar__list_title">${item.title}</span>
-              `;
-      navList.append(liItem);
-    });
-  
-    const togglePageVisibility = (show = 0) => {
-      pages.forEach((item) => {
-        item.classList.remove("visible");
-      });
-      pages[show].classList.add("visible");
-    };
-  
-    togglePageVisibility();
-  
-    const togglePage = (e, node) => {
-      togglePageVisibility(node.getAttribute("data-index"));
-      e.currentTarget
-        .querySelectorAll(".nav-bar__list_item")
-        .forEach((item) => item.classList.remove("active"));
-      node.classList.add("active");
-    };
-  
-    navList.addEventListener("click", (e) => {
-      if (e.target.getAttribute("data-index")) {
-        togglePage(e, e.target);
-      } else if (
-        e.target.closest(".nav-bar__list_item").getAttribute("data-index")
-      ) {
-        togglePage(e, e.target.closest(".nav-bar__list_item"));
-      }
-    });
-  
-    document.addEventListener("keyup", (e) => {
-      if (e.code === "Space") {
-        togglePage(e, e.target);
-      }
-    });
+  const pages = document.querySelectorAll(".section__page");
+  const navList = document.querySelector(".nav-bar__list");
+
+  const navListItem = [
+    {
+      title: "Overview",
+      icon: "icon-browser",
+    },
+    {
+      title: "About",
+      icon: "icon-about-dot-me",
+    },
+    {
+      title: "Achievement",
+      icon: "icon-award",
+    },
+    {
+      title: "Projects",
+      icon: "icon-files-empty",
+    },
+    {
+      title: "Reviews",
+      icon: "icon-rate_review",
+    },
+  ];
+
+  navListItem.forEach((item, i) => {
+    const liItem = document.createElement("li");
+    liItem.classList.add("nav-bar__list_item");
+    liItem.setAttribute("data-index", i);
+    liItem.innerHTML = `
+      <button class="nav-bar__list_button">
+        <span class="${item.icon}">
+        </span>
+        ${item.title}
+      </button>
+    `;
+    navList.append(liItem);
   });
-  
+
+  navList.querySelector(".nav-bar__list_button").classList.add("active");
+
+  const togglePageVisibility = (show = 0) => {
+    pages.forEach((item) => {
+      item.classList.remove("visible");
+    });
+    pages[show].classList.add("visible");
+  };
+
+  togglePageVisibility();
+
+  navList.addEventListener("click", (e) => {
+    togglePageVisibility(
+      e.target.closest(".nav-bar__list_item").getAttribute("data-index")
+    );
+    navList.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+  });
+});
