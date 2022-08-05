8e1f427637fd8c052daa89b5a7cdbab866e6f96d
Olstani <olstaniev@gmail.com>
2022-08-05 17:29:38 +0200

    tiny js

---
+++ submissions/OlStaniev/tiny-js/index.js
@@ -0,0 +1,36 @@
+const dog = {
+   name: 'Bob',
+   gender: 'male',
+   legs: 4,
+   hands: 0,
+   saying: 'woof!'
+}
+
+const cat = {
+   name: 'Kitty',
+   gender: 'female',
+   legs: 4,
+   hands: 0,
+   saying: 'meow!'
+}
+
+const woman = {
+   name: 'Sara',
+   gender: 'male',
+   legs: 2,
+   hands: 2,
+   saying: 'Hello!'
+}
+
+const man = {
+   name: 'Mario',
+   gender: 'male',
+   legs: 2,
+   hands: 2,
+   saying: 'Hi!'
+}
+
+const inhabitants = [cat, dog, woman, man]
+const arrForPrint = inhabitants.map(({name, gender, legs, hands,saying}) => [`${name}; ${gender}; ${legs}; ${hands}; ${saying}`])
+
+arrForPrint.forEach(e=> print(e))
