1e2a0ebda512c491cf7d1b201c5e3707b4081e57
Vlad Kovalov <vlad.kovalov.92@gmail.com>
2022-08-05 13:54:27 +0300

    small fix

--- submissions/kovalov/memory-pair-game/main.js
+++ submissions/kovalov/memory-pair-game/main.js
@@ -1,93 +1,88 @@
 const imageNames = [
-	'img-1.png',
-	'img-2.png',
-	'img-3.png',
-	'img-4.png',
-	'img-5.png',
-	'img-6.png',
-	'img-7.png',
-	'img-8.png',
- ];
+  'img-1.png',
+  'img-2.png',
+  'img-3.png',
+  'img-4.png',
+  'img-5.png',
+  'img-6.png',
+  'img-7.png',
+  'img-8.png',
+];
 
- let currentCard = '';
- let previousCard = '';
- let isActive = true;
- let timesClicked = 0;
- let timesMatched = 0;
+let currentCard = '';
+let previousCard = '';
+let isActive = true;
+let timesClicked = 0;
+let timesMatched = 0;
 
- const gameContainer = document.querySelector('[data-game]');
- const gameMenu = document.querySelector('[data-menu]');
- const cardGrid = document.querySelector('[data-grid]');
+const gameContainer = document.querySelector('[data-game]');
+const gameMenu = document.querySelector('[data-menu]');
+const cardGrid = document.querySelector('[data-grid]');
 
- function getShuffledImages(imageNames, filePath) {
-	return [...imageNames, ...imageNames]
-	  .sort(() => 0.5 - Math.random())
-	  .map((imageName) => ({
-		 imageName,
-		 src: `${filePath}/${imageName}`,
-	  }));
- }
+function getShuffledImages(imageNames, filePath) {
+  return [...imageNames, ...imageNames]
+    .sort(() => 0.5 - Math.random())
+    .map((imageName) => ({
+      imageName,
+      src: `${filePath}/${imageName}`,
+    }));
+}
 
- function createCard(cardDetails) {
-	const cardElement = document.createElement('div');
-	cardElement.innerHTML = `
-	  <div class="game__card"
-		 data-is-opened="false"
-		 data-name="${cardDetails.imageName}"
-		  >
-			 <div class="game__card-flipper">
-				<div class="game__card-front">
-				  <span class="game__card-icon">?</span>
-				</div>
-			<div class="game__card-back">
-			  <img src="${cardDetails.src}" alt="" class="game__card-image"
-			  />
-			</div>
-			 </div>
-	  </div>
-		`;
-	return cardElement;
- }
+function createCard(cardDetails) {
+  const cardElement = document.createElement('div');
+  cardElement.innerHTML = `
+	<div class="game__card" data-is-opened="false" data-name="${cardDetails.imageName}">
+	 <div class="game__card-flipper">
+		 <div class="game__card-front">
+			 <span class="game__card-icon">?</span>
+		 </div>
+		 <div class="game__card-back">
+			 <img src="${cardDetails.src}" alt="" class="game__card-image"/>
+		 </div>
+	 </div>
+	</div>`;
+  return cardElement;
+}
 
- function initGame(imageNames, filePath, container) {
-	const shuffledImages = getShuffledImages(imageNames, filePath);
-	shuffledImages
-	  .map(createCard)
-	  .forEach((card) => addCard(card, container));
- }
+function initGame(imageNames, filePath, container) {
+  const shuffledImages = getShuffledImages(imageNames, filePath);
+  shuffledImages
+    .map(createCard)
+    .forEach((card) => addCard(card, container));
+}
 
- function addCard(cardElement, container) {
-	container.appendChild(cardElement);
- }
+function addCard(cardElement, container) {
+  container.appendChild(cardElement);
+}
 
- gameMenu.addEventListener('click', (event) => {
-	if (event.target.dataset.button !== 'start') return;
+gameMenu.addEventListener('click', (event) => {
+  if (event.target.dataset.button !== 'start') return;
 
-	cardGrid.innerHTML = '';
+  cardGrid.innerHTML = '';
 
-	currentCard = '';
-	previousCard = '';
+  currentCard = '';
+  previousCard = '';
 
-	timesClicked = 0;
-	timesMatched = 0;
+  timesClicked = 0;
+  timesMatched = 0;
 
-	gameContainer.dataset.isActive = true;
-	initGame(imageNames, './assets/images', cardGrid);
- });
+  gameContainer.dataset.isActive = true;
+  initGame(imageNames, './assets/images', cardGrid);
+});
 
- function clearVariables() {
-	currentCard = '';
-	previousCard = '';
- }
+function clearVariables() {
+  currentCard = '';
+  previousCard = '';
+}
 
- function checkWin() {
-	if (timesMatched !== 8) return;
+function checkWin() {
+  if (timesMatched !== 8) return;
 
-	isActive = false;
-	gameContainer.dataset.isActive = false;
+  isActive = false;
+  gameContainer.dataset.isActive = false;
 
-	gameMenu.innerHTML = '';
-	const successMessage = `
+  gameMenu.innerHTML = '';
+  const successMessage = `
 	 <h1 class="game__menu-title">You win!</h1>
 	 <p class="game__menu-description">
 		You have made ${timesClicked} clicks.
@@ -97,46 +92,46 @@ const imageNames = [
 	 </button>
 	 `;
 
-	gameMenu.innerHTML = successMessage;
- }
+  gameMenu.innerHTML = successMessage;
+}
 
- function handleClick(event) {
-	if (isActive) {
-	  if (!event.target.closest('.game__card')) return;
+function handleClick(event) {
+  if (isActive) {
+    if (!event.target.closest('.game__card')) return;
 
-	  if (!currentCard) {
-		 timesClicked++;
-		 currentCard = event.target.closest('.game__card');
-		 currentCard.dataset.isOpened = true;
-		 return;
-	  }
+    if (!currentCard) {
+      timesClicked++;
+      currentCard = event.target.closest('.game__card');
+      currentCard.dataset.isOpened = true;
+      return;
+    }
 
-	  if (currentCard) {
-		 previousCard = event.target.closest('.game__card');
-		 previousCard.dataset.isOpened = true;
-		 isActive = false;
+    if (currentCard) {
+      previousCard = event.target.closest('.game__card');
+      previousCard.dataset.isOpened = true;
+      isActive = false;
 
-		 if (currentCard.dataset.name === previousCard.dataset.name) {
-			timesMatched++;
-			checkWin();
-			currentCard = '';
-			previousCard = '';
-			isActive = true;
-			return;
-		 }
+      if (currentCard.dataset.name === previousCard.dataset.name) {
+        timesMatched++;
+        checkWin();
+        currentCard = '';
+        previousCard = '';
+        isActive = true;
+        return;
+      }
 
-		 if (currentCard.dataset.name !== previousCard.dataset.name) {
-			setTimeout(() => {
-			  currentCard.dataset.isOpened = false;
-			  previousCard.dataset.isOpened = false;
-			  currentCard = '';
-			  previousCard = '';
-			  isActive = true;
-			  return;
-			}, 1000);
-		 }
-	  }
-	}
- }
+      if (currentCard.dataset.name !== previousCard.dataset.name) {
+        setTimeout(() => {
+          currentCard.dataset.isOpened = false;
+          previousCard.dataset.isOpened = false;
+          currentCard = '';
+          previousCard = '';
+          isActive = true;
+          return;
+        }, 1000);
+      }
+    }
+  }
+}
 
- cardGrid.addEventListener('click', handleClick);
+cardGrid.addEventListener('click', handleClick);
