09a98b3febca4af5a575b4a336dd9d446fde09b5
Pozzitive11 <palaga2442@gmail.com>
2022-08-06 18:24:36 +0300

    world fix

--- submissions/pozzitive11/js-world-oop/index.js
+++ submissions/pozzitive11/js-world-oop/index.js
@@ -7,71 +7,68 @@ class Inhabitant {
   }
 
   getProperties() {
-    return `${this.species}; My name ${this.name}; ${this.gender}; I have ${this.legs} legs and ${this.hands} hands; ${this.saying};` ;
+    return `${this.species}; My name ${this.name}; ${this.gender}; ${this.saying}`;
   }
 }
-
-class Animals extends Inhabitant {
+class Animal extends Inhabitant {
   constructor(species, name, gender, paw, saying) {
     super(species, name, gender, saying);
     this.paw = paw;
   }
   getProperties() {
-    return `${this.species}; ${this.name}; ${this.gender}; I have ${this.paw} paws; ${this.saying};`;
+    return super.getProperties() + `; I have ${this.paw} paws;`;
   }
 }
 
-class Humans extends Inhabitant {
-  constructor(species, name, gender, legs, hands, saying){
+class Human extends Inhabitant {
+  constructor(species, name, gender, saying) {
     super(species, name, gender, saying);
-    this.legs = legs;
-    this.hands = hands;
+    this.legs = 2;
+    this.hands = 2;
+  }
+
+  getProperties() {
+    return (
+      super.getProperties() +
+      `; I have ${this.legs} legs and ${this.hands} hands;`
+    );
   }
 }
 
-class Dog extends Animals {
+class Dog extends Animal {
   constructor(species, name, gender, saying, paw) {
     super(species, name, gender, paw, saying);
   }
 }
 
-class Cat extends Animals {
+class Cat extends Animal {
   constructor(species, name, gender, paw) {
-    super(species, name, gender, paw, "mew");
+    super(species, name, gender, paw, "meow-meow");
   }
 }
-
 class Catwoman extends Cat {
   constructor(species, name, gender, paws, saying) {
     super(species, name, gender, paws, saying);
-  } 
-}
-
-class Man extends Humans {
-  constructor(species, name, gender, legs, hands, saying) {
-    super(species, name, gender, legs, hands, saying);
   }
 }
 
-class Woman extends Humans {
-  constructor(species, name, gender, legs, hands, saying) {
-    super(species, name, gender, legs, hands, saying);
+class Man extends Human {
+  constructor(name, gender, saying) {
+    super("man", name, gender, saying);
+  }
+}
+
+class Woman extends Human {
+  constructor(name, gender, saying) {
+    super("woman", name, gender, saying);
   }
 }
 
 const dog = new Dog("dog", "Dick", "male", "woof-woof!", 4);
 const cat = new Cat("cat", "Kitty", "female", 5);
 const catwoman = new Catwoman("catwoman", "Nazar", "female", 2);
-console.log(catwoman);
-const woman = new Woman(
-  "woman",
-  "Dazdraperma",
-  "female",
-  2,
-  2,
-  "It used to be better"
-);
-const man = new Man("man", "Mike", "male", 2, 2, "Hello World!");
+const woman = new Woman("Dazdraperma", "female", "It used to be better");
+const man = new Man("Mike", "male", "Hello World!");
 
 const inhabitants = [dog, cat, catwoman, woman, man];
 
