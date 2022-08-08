704c778d007b3da6fa0c9b590fc1f6ba13a844a2
Olstani <olstaniev@gmail.com>
2022-08-08 13:06:35 +0200

    refactor

--- submissions/OlStaniev/DOM/script/script.js
+++ submissions/OlStaniev/DOM/script/script.js
@@ -1,46 +1,50 @@
 import episodes from './text-episode.js';
 
 const menuList = document.querySelector('.nav__list');
-const introText = document.querySelector('.introText')
-const burgerIcon = document.querySelector('.header__burgerMenu')
+const introText = document.querySelector('.introText');
+const burgerBtn = document.querySelector('.header__burgerMenu');
 
 document.addEventListener('click', (event) => {
     const episodeName = event.target.closest('.nav__list_item')
     if (episodeName) {
         const content = generateText(episodeName.dataset.id)
-        addContent(content,introText)
+        addContent(content, introText)
     }
     burgerMenuOpen(event)
-})
+});
 
 function burgerMenuOpen(event) {
     if (event.target.closest('.header__burgerMenu')) {
         menuList.classList.toggle('nav__listOpen')
-        burgerIcon.classList.toggle('burgerCross')
+        burgerBtn.classList.toggle('burgerCross')
     }
     if (!event.target.closest('.header__burgerMenu')) {
         menuList.classList.remove('nav__listOpen')
-        burgerIcon.classList.remove('burgerCross')
+        burgerBtn.classList.remove('burgerCross')
     }
-}
+};
 
 function generateText(episodeId) {
-    const currentEpisode = episodes.find((episodeNumber) => episodeId === episodeNumber.id)
-    const storyText = `<div class="introText_wrapper"><h2>${currentEpisode.title}</h2><h3>${currentEpisode.subTitle}</h3><p>${currentEpisode.p1}</p><p>${currentEpisode.p2}</p><p>${currentEpisode.p3}</p></div>`
+    const currentEpisode = episodes.find(({ id }) => episodeId === id)
+    const storyText = `<div class="introText_wrapper"><h2>${currentEpisode.title}</h2>
+                        <h3>${currentEpisode.subTitle}</h3>
+                        <p>${currentEpisode.p1}</p>
+                        <p>${currentEpisode.p2}</p>
+                        <p>${currentEpisode.p3}</p></div>`
     return storyText
-}
+};
 
-const navList = episodes.reduce((list, {id, title, subTitle}) =>{
+const navList = episodes.reduce((list, { id, title, subTitle }) => {
     return list +
-    `<li class="listItem">
+        `<li class="listItem">
         <button class="nav__list_item" data-id="${id}">
             <span class="listTitle">${title}</span>
             <span class="listSubTitle">${subTitle}</span>
         </button>
     </li>`
-}, '')
+}, '');
 
-function addContent(content, target){
+function addContent(content, target) {
     target.innerHTML = content
-}
-addContent(navList,menuList)
+};
+addContent(navList, menuList);
