988bd0414e6a1b3c0af00e70855e3fce9aeda9ab
AsaMitaka <cepelits@gmail.com>
2022-08-11 16:26:11 +0300

    fixed iframe in arr-obj, fixed tables responsive, fixed padding in mobile

--- submissions/asaMitaka/dom/script.js
+++ submissions/asaMitaka/dom/script.js
@@ -9,7 +9,6 @@ main.classList.add('mainBlock');
 content.append(main);
 
 let arrOfDatas = [
-    
     {
         title: 'The College Dropout',
         dataName: '1',
@@ -20,9 +19,7 @@ let arrOfDatas = [
         ],
         img: 'https://upload.wikimedia.org/wikipedia/en/a/a3/Kanyewest_collegedropout.jpg',
         alt: 'The College Dropout',
-        source: `
-            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/4Uv86qWpGTxf7fU7lG5X6F?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-        `,
+        source: `https://open.spotify.com/embed/album/4Uv86qWpGTxf7fU7lG5X6F?utm_source=generator`,
         class: 'colledge'
     },
     {
@@ -35,9 +32,7 @@ let arrOfDatas = [
         ],
         img: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f4/Late_registration_cd_cover.jpg/220px-Late_registration_cd_cover.jpg',
         alt: 'Late Registration',
-        source: `
-            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/5ll74bqtkcXlKE7wwkMq4g?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-        `,
+        source: `https://open.spotify.com/embed/album/5ll74bqtkcXlKE7wwkMq4g?utm_source=generator`,
         class: 'late'
     },
     {
@@ -49,9 +44,7 @@ let arrOfDatas = [
         ],
         img: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Graduation_%28album%29.jpg/220px-Graduation_%28album%29.jpg',
         alt: 'Graduation',
-        source: `
-            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/4SZko61aMnmgvNhfhgTuD3?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-        `,
+        source: `https://open.spotify.com/embed/album/4SZko61aMnmgvNhfhgTuD3?utm_source=generator`,
         class: 'graduation'
     },
     {
@@ -63,9 +56,7 @@ let arrOfDatas = [
         ],
         img: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f1/808s_%26_Heartbreak.png/220px-808s_%26_Heartbreak.png',
         alt: '808andHeartbreak',
-        source: `
-            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/3WFTGIO6E3Xh4paEOBY9OU?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-        `,
+        source: `https://open.spotify.com/embed/album/3WFTGIO6E3Xh4paEOBY9OU?utm_source=generator `,
 
         class: 'heartbreak'
     },
@@ -78,9 +69,7 @@ let arrOfDatas = [
         ],
         img: 'https://upload.wikimedia.org/wikipedia/en/b/be/MBDTF_ALT.jpg',
         alt: 'MBDTF',
-        source: `
-            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/20r762YmB5HeofjMCiPMLv?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-        `,
+        source: `https://open.spotify.com/embed/album/20r762YmB5HeofjMCiPMLv?utm_source=generator`,
         class: 'mbdtf'
     },
     {
@@ -91,9 +80,7 @@ let arrOfDatas = [
         ],
         img: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Yeezus_album_cover.png/220px-Yeezus_album_cover.png',
         alt: 'Yeezus',
-        source: `
-            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/7D2NdGvBHIavgLhmcwhluK?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-        `,
+        source: `https://open.spotify.com/embed/album/7D2NdGvBHIavgLhmcwhluK?utm_source=generator`,
         class: 'yeezus'
     },
     {
@@ -105,9 +92,7 @@ let arrOfDatas = [
         ],
         img: 'https://upload.wikimedia.org/wikipedia/en/4/4d/The_life_of_pablo_alternate.jpg',
         alt: 'TLOP',
-        source: `
-            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/7gsWAHLeT0w7es6FofOXk1?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-        `,
+        source: `https://open.spotify.com/embed/album/7gsWAHLeT0w7es6FofOXk1?utm_source=generator`,
         class: 'tlop'
     },
     {
@@ -118,9 +103,7 @@ let arrOfDatas = [
         ],
         img: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Ye_album_cover.jpg/220px-Ye_album_cover.jpg',
         alt: 'Ye',
-        source: `
-            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/2Ek1q2haOnxVqhvVKqMvJe?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-        `,
+        source: `https://open.spotify.com/embed/album/2Ek1q2haOnxVqhvVKqMvJe?utm_source=generator`,
         class: 'ye'
     },
     {
@@ -131,9 +114,7 @@ let arrOfDatas = [
         ],
         img: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Kanye_West_-_Jesus_Is_King.png/220px-Kanye_West_-_Jesus_Is_King.png',
         alt: 'JiK',
-        source: `
-            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/0FgZKfoU2Br5sHOfvZKTI9?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-        `,
+        source: `https://open.spotify.com/embed/album/0FgZKfoU2Br5sHOfvZKTI9?utm_source=generator`,
         class: 'jis'
     },
     {
@@ -146,9 +127,7 @@ let arrOfDatas = [
         ],
         img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Almost_black_square_020305.png/640px-Almost_black_square_020305.png',
         alt: 'Donda',
-        source: `
-            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/5CnpZV3q5BcESefcB3WJmz?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-        `,
+        source: `https://open.spotify.com/embed/album/5CnpZV3q5BcESefcB3WJmz?utm_source=generator`,
         class: 'donda'
     }
 ];
@@ -158,27 +137,25 @@ arrOfDatas.forEach(item => {
 });
 
 function renderAsideItems(item) {
-    aside.innerHTML += `
-        <button class='asideBtn' data-attribute='${item.dataName}'>${item.title}</button>
-    `;
+    let button = document.createElement('button');
+    button.classList.add('asideBtn');
+    button.dataset.attribute = `${item.dataName}`;
+    button.textContent = `${item.title}`;
+    aside.append(button);
 }
 
 let asideBtn = document.querySelectorAll('.asideBtn');
+asideBtn[0].classList.add('clicked');
+addToMain(arrOfDatas[0]);
+
 document.querySelector('.asideBlock').addEventListener('click', function(e) {
-    if (e.target && e.target.classList == 'asideBtn') {
+    if (e.target && e.target.classList.contains('asideBtn')) {
         renderMain(e);
     }
-})
+});
 
 
 function renderMain(event) {
-    if (!event) {
-        asideBtn[0].classList.add('clicked');
-        addToMain(arrOfDatas[0]);
-
-        return;
-    }
-
     document.querySelector('.asideBtn.clicked').classList.remove('clicked');
     event.target.classList.add('clicked');
     let thatTarget = event.target.dataset.attribute;
@@ -190,8 +167,6 @@ function renderMain(event) {
 
     addToMain(mainItem);
 }
-renderMain();
-
 
 function addToMain(item) {
     content.classList = "";
@@ -203,7 +178,9 @@ function addToMain(item) {
             <div class='mainDescr'></div>
             <img class='mainImg' src='${item.img}' alt='${item.alt}' />
         </div>
-        <div class='mainSource'>${item.source}</div>
+        <div class='mainSource'>
+            <iframe style="border-radius:12px" src=${item.source} width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+        </div>
     `;
 
     document.querySelector('.mainDescr').append(createOl(item));
@@ -212,6 +189,7 @@ function addToMain(item) {
 function createOl(item) {
     let ol = document.createElement('ol');
     ol.classList.add('mainDescrOl');
+
     item.description.forEach(el => {
         let li = document.createElement('li');
         li.classList.add('mainDescrLi');
@@ -221,4 +199,3 @@ function createOl(item) {
 
     return ol;
 }
-
