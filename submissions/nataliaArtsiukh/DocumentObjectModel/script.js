f79c0574a35b7d3f2235639702f39b3334e82e93
Natalia Artsiukh <nataliaprocessmail@gmail.com>
2022-08-13 21:28:19 +0300

    Image optimization. Init function picked out. Console.log deleted. Again.

--- submissions/nataliaArtsiukh/DocumentObjectModel/script.js
+++ submissions/nataliaArtsiukh/DocumentObjectModel/script.js
@@ -13,7 +13,8 @@ const monsters = {
         height: '1145px'
     },
     'Azathoth': {
-        img: './img/azathoth3.png',
+        img: './img/azathoth.png',
+        imgSet: './img/azathoth.png 1025w, ./img/azathoth1024.png 1024w, ./img/azathoth540.png 540w',
         text: 'Azathoth is the supreme among the Other Gods, the "formless sultan of demons" who swirls in the very center of infinity. This is the blind and insane ruler of all things, forever surrounded by the maddening howl of flutes and the roar of drums.',
         class: 'azathoth',
         width: '1102px',
@@ -21,6 +22,7 @@ const monsters = {
     },
     'Yog-Sothoth': {
         img: './img/yog-sothoth.png',
+        imgSet: './img/yog-sothoth.png 1025w, ./img/yog-sothoth1024.png 1024w, ./img/yog-sothoth540.png 540w',
         text: 'Yog-Sothoth the All-Containing, for example, is a hodgepodge of thousands of star-like spheres (or eyes?), hanging outside the universe and commanding time.',
         class: 'yog-sothoth',
         width: '705px',
@@ -28,6 +30,7 @@ const monsters = {
     }, 
     'Shub-Niggurath': {
         img: './img/shub-niggurath.png',
+        imgSet: './img/shub-niggurath.png 1025w, ./img/shub-niggurath1024.png 1024w, ./img/shub-niggurath540.png 540w',
         text: 'Shub-Niggurath, which can be attributed with some success to the female gender, is considered a symbol of perverted fertility, therefore this slimy flying mass with limbs sticking out in all directions, including goat limbs, rarely appears without its brood.',
         class: 'shub-niggurath',
         width: '1574px',
@@ -35,6 +38,7 @@ const monsters = {
     },
     'Nyarlathotep': {
         img: './img/nyarlathotep.png',
+        imgSet: './img/nyarlathotep.png 1025w, ./img/nyarlathotep1024.png 1024w, ./img/nyarlathotep540.png 540w',
         text: 'Nyarlathotep is one of the few beings who is not in cosmic exile or a sleep like death. Moreover, this is perhaps the only deity capable of understanding people, communicating with them in their language and taking on a human form. His true appearance is perfectly described by Nyarlathotep\'s middle name - Creeping Chaos. That is, it is not described in any way.',
         class: 'nyarlathotep',
         width: '923px',
@@ -42,6 +46,7 @@ const monsters = {
     },
     'Cthulhu': {
         img: './img/Cthulhu.png',
+        imgSet: './img/Cthulhu.png 1025w, ./img/Cthulhu1024.png 1024w, ./img/Cthulhu540.png 540w',
         text: 'Cthulhu is a giant with the head of an octopus. From his underwater R\'Lyeh, he involuntarily emits radiation, muffled by the titanic water column. As R\'lyeh rises from the bottom, this influence intensifies, and waves of tantrums, suicides, and blood orgies spread across the Earth. When Cthulhu finally throws off the shackles of sleep, like death, humanity will probably complete its journey and perish.',
         class: 'cthulhu',
         width: '2219px',
@@ -49,25 +54,21 @@ const monsters = {
     }
 };
 
-createNavigation();
-createContent('Howard Phillips Lovecraft');
-
-navigation.addEventListener('click', function (e) {
+function navigationHandler(e) {
     if (!e.target.classList.contains('nav-list')) {
         navigation.classList.remove('nav-shown');
         createContent(e.target.textContent);
     }
-});
+}
 
-burgerButton.addEventListener('click', function () {
+function burgerHandler() {
     navigation.classList.add('nav-shown');
-});
+}
 
 function createNavigation() {
     for (item in monsters) {
         let navItem = document.createElement('li');
         navItem.classList.add('nav-item');
-        console.log(item);
         navItem.textContent = item;
         navList.append(navItem);
     }
@@ -78,6 +79,9 @@ function createContent(monster) {
     const img = document.createElement('img');
     img.classList.add(monsters[monster].class);
     img.setAttribute('src', monsters[monster].img);
+    if (monsters[monster].imgSet) {
+        img.setAttribute('srcset', monsters[monster].imgSet);
+    } 
     img.setAttribute('width', monsters[monster].width);
     img.setAttribute('height', monsters[monster].height);
     mainContent.append(img);
@@ -90,6 +94,16 @@ function createContent(monster) {
     text.textContent = monsters[monster].text;
     mainContent.append(text);
 }
+
+function main() {
+    createNavigation();
+    createContent('Howard Phillips Lovecraft');
+    navigation.addEventListener('click', navigationHandler);
+    burgerButton.addEventListener('click', burgerHandler);
+}
+
+main();
+
 
 
 
