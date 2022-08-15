f139a9fa75d61189564e0ba02aa7bb8d432b6a47
Vladimir <75982068+vovan-zt@users.noreply.github.com>
2022-08-16 01:38:08 +0300

    Update index.js

--- submissions/vovan-zt/Tiny_JS_World/index.js
+++ submissions/vovan-zt/Tiny_JS_World/index.js
@@ -8,18 +8,18 @@ const dog = {
    legs: 4,
    hands: 0,
    saying: "woof-woof!",
-   };
+};
    
-   const cat = {
+const cat = {
    species: "cat",
    name: "Luna",
    gender: "female",
    legs: 4,
    hands: 0,
    saying: "mur",
-   };
+};
    
-   const woman = {
+const woman = {
    species: "woman",
    name: "Lena",
    gender: "female",
@@ -27,9 +27,9 @@ const dog = {
    hands: 2,
    saying: "Hello",
    friends: 'Alex, Lena',
-   };
+};
    
-   const man = {
+const man = {
    species: "man",
    name: "Alex",
    gender: "male",
@@ -37,29 +37,28 @@ const dog = {
    hands: 2,
    saying: "I want to sleep",
    friends: 'Leo, Ivan',
-   };
+};
    
-   const catWoman = {
+const catWoman = {
    species: "catwoman",
    name: "Selina",
    gender: "female",
    legs: 2,
    hands: 2,
    saying: cat.saying,
-   };
+};
 
-   const objName = [dog, cat, woman, man, catWoman];
+const objName = [dog, cat, woman, man, catWoman];
 
-   const objPrint = objName.map((obj) => {
-      let arr=[]
-      for (let key in obj) { 
-         arr.push(obj[key])
-      }
-      return arr
-   });
+const objPrint = objName.map((obj) => {
+   let arr=[]
+   for (let key in obj) { 
+      arr.push(obj[key])
+   }
+   return arr
+});
 
-
-   objPrint.forEach(item => {
-      print(item.join('; '))
-   })
+objPrint.forEach(item => {
+   print(item.join('; '))
+})
 
