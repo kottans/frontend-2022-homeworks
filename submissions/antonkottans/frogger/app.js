1ab950b61b73101977cb43dd34c4cdbbb0818ce0
toha <toha@anton.com>
2022-08-17 08:59:18 +0300

    brand new movement and collision

--- submissions/antonkottans/frogger/app.js
+++ submissions/antonkottans/frogger/app.js
@@ -1,7 +1,7 @@
 class Field {
     constructor() {
         this.topIndent = 57;
-        this.squareWidth = 101;
+        this.squareWidth = 100;
         this.squareHeight = 85;
         this.maxreachableXCoord = 4 * this.squareWidth;
         this.maxreachableYCoord = 4 * this.squareHeight;
@@ -15,14 +15,20 @@ class Enemy {
         this.y = y;
         this.speed = speed;
         this.sprite = "images/enemy-bug.png";
+        (this.width = 99), (this.height = 77);
     }
+
     checkCollision() {
-        const distanceOfXCollision = field.squareWidth / 1.3;
-        const distanceOfYColision = 1;
-        const sameRow = () => Math.abs(player.y - this.y) < distanceOfYColision;
-        const bugTooClose = () =>
-            Math.abs(player.x - this.x) < distanceOfXCollision;
-        if (sameRow() && bugTooClose()) {
+        if (
+            (player.x >= this.x &&
+                player.x <= this.x + this.width &&
+                player.y >= this.y &&
+                player.y <= this.y + this.height) ||
+            (this.y >= player.y &&
+                this.y <= player.y + player.height &&
+                this.x >= player.x &&
+                this.x <= player.x + player.width)
+        ) {
             player.toStart();
         }
     }
@@ -46,25 +52,72 @@ class Player {
         this.start = { x: 2, y: 4 };
         this.x = this.start.x * field.squareWidth;
         this.y = this.start.y * field.squareHeight + field.topIndent;
+        (this.width = 67),
+            (this.height = 84),
+            (this.speed = 5),
+            (this.moving = false),
+            (this.movingCountDown = 0),
+            (this.movingDirection = null);
     }
-    update() {}
+
+    update() {
+        if (this.movingCountDown === 0) this.moving = false;
+        if (this.y < 0) {
+            this.toStart();
+        }
+        if (this.moving) {
+            if (this.movingDirection === "right") {
+                this.x += this.speed;
+                this.movingCountDown -= this.speed;
+            }
+            if (this.movingDirection === "left") {
+                this.x -= this.speed;
+                this.movingCountDown -= this.speed;
+            }
+            if (this.movingDirection === "up") {
+                this.y -= this.speed;
+                this.movingCountDown -= this.speed;
+            }
+            if (this.movingDirection === "down") {
+                this.y += this.speed;
+                this.movingCountDown -= this.speed;
+            }
+        }
+    }
+
     toStart() {
+        this.moving = false;
+        this.movingCountDown = 0;
         this.x = this.start.x * field.squareWidth;
         this.y = this.start.y * field.squareHeight + field.topIndent;
     }
+
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
+
     handleInput(key) {
-        if (key === "up") this.y -= field.squareHeight;
-        else if (key === "right" && this.x < field.maxreachableXCoord)
-            this.x += field.squareWidth;
-        else if (key === "down" && this.y < field.maxreachableYCoord)
-            this.y += field.squareHeight;
-        else if (key === "left" && this.x > 0) this.x -= field.squareWidth;
-        if (this.y < 0) this.toStart();
+        if (this.moving) return null;
+        if (key === "up") {
+            this.moving = true;
+            this.movingDirection = "up";
+            this.movingCountDown = field.squareHeight;
+        } else if (key === "right" && this.x < field.maxreachableXCoord) {
+            this.moving = true;
+            this.movingDirection = "right";
+            this.movingCountDown = field.squareWidth;
+        } else if (key === "down" && this.y < field.maxreachableYCoord) {
+            this.moving = true;
+            this.movingDirection = "down";
+            this.movingCountDown = field.squareHeight;
+        } else if (key === "left" && this.x > 0) {
+            this.moving = true;
+            this.movingDirection = "left";
+            this.movingCountDown = field.squareWidth;
+        }
     }
 }
+
 const player = new Player();
 
 const allEnemies = (() => {
