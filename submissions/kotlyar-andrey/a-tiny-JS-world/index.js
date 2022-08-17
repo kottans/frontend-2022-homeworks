aa32959fff09a4ae6ba0b71b9f464d2a2f25871d
Andrey Kotlyar <kotlyar562@gmail.com>
2022-08-17 13:58:29 +0300

    change 'the world' according to comments

--- submissions/kotlyar-andrey/a-tiny-JS-world/index.js
+++ submissions/kotlyar-andrey/a-tiny-JS-world/index.js
@@ -5,45 +5,73 @@
    Web app: https://kotlyar-andrey.github.io/a-tiny-JS-world/
    */
 
-function Creature(species, name, gender, legs, hands, saying) {
-  this.species = species;
-  this.name = name;
-  this.gender = gender;
-  this.legs = legs;
-  this.hands = hands;
-  this.saying = saying;
-
-  this.properties = [
-    "species",
-    "name",
-    "gender",
-    "legs",
-    "hands",
-    "saying",
-    "friendsNames",
-  ];
-
-  this.display = function () {
-    if (this.friends) {
-      this.friendsNames = this.friends.map((friend) => friend.name).join(", ");
-    } else {
-      this.friendsNames = "undefined :(";
+   class Creature {
+    constructor(species, name, gender, legs, hands, phrase, linkTo = null) {
+      this._species = species;
+      this._name = name;
+      this._gender = gender;
+      this._legs = legs;
+      this._hands = hands;
+      this._phrase = phrase;
+      this._friends = [];
+      this._link = linkTo;
+      this._properties = [
+        "species",
+        "_name",
+        "_gender",
+        "_legs",
+        "_hands",
+        "saying",
+        "friends",
+      ];
     }
-    print(this.properties.map((prop) => this[prop]).join("; "));
-  };
-}
-
-const cat = new Creature("cat", "Jerry", "male", 4, 0, "Myau");
-const dog = new Creature("dog", "Bars", "male", 4, 0, "Woof");
-const woman = new Creature("human", "Maria", "female", 2, 2, "Hi");
-const man = new Creature("human", "Ivan", "male", 2, 2, "Hello");
-
-man.friends = [woman, cat, dog];
-woman.friends = [man, cat];
-dog.friends = [man];
-
-const inhabitants = [man, woman, dog, cat];
-
-inhabitants.forEach((creature) => {
-  creature.display();
-});
+  
+    get species() {
+      return this._link
+        ? `${this._species}-${this._link._species}`
+        : this._species;
+    }
+  
+    get friends() {
+      return this._friends && this._friends.length > 0
+        ? this._friends.map(friend => friend._name).join(", ")
+        : "no friends";
+    }
+  
+    set friends(newFriends) {
+      this._friends = [...newFriends];
+    }
+  
+    get saying() {
+      return this._link ? this._link._phrase : this._phrase;
+    }
+  
+    set saying(newFrase) {
+      this._phrase = newFrase;
+    }
+  
+    display() {
+      print(this._properties.map(prop => this[prop]).join("; "));
+    }
+  }
+  
+  const cat = new Creature("cat", "Jerry", "male", 4, 0, "Myau");
+  const dog = new Creature("dog", "Bars", "male", 4, 0, "Woof");
+  const woman = new Creature("human", "Maria", "female", 2, 2, "Hi");
+  const man = new Creature("human", "Ivan", "male", 2, 2, "Hello");
+  const woman_cat = new Creature("human", "Marta", "female", 2, 2, "", cat);
+  const werewolf = new Creature("human", "Petr", "male", 4, 2, "", dog);
+  
+  man.friends = [woman, cat, dog];
+  woman.friends = [man, cat];
+  dog.friends = [cat];
+  woman_cat.friends = [man, cat];
+  
+  cat.saying = "new message for test woman-cats";
+  
+  const inhabitants = [man, woman, dog, cat, woman_cat, werewolf];
+  
+  inhabitants.forEach(creature => {
+    creature.display();
+  });
+  
