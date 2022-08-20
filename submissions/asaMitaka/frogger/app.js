d2be854cfbb554de00f5ff1bdeb2542695638e58
AsaMitaka <cepelits@gmail.com>
2022-08-20 15:56:09 +0300

    fixed key down, key left, replaced var to const and let variables

--- submissions/asaMitaka/frogger/app.js
+++ submissions/asaMitaka/frogger/app.js
@@ -1,54 +1,43 @@
-// Enemies our player must avoid
-var newP = document.createElement('p');
+const newP = document.createElement('p');
 newP.textContent = `Your score: 0 `;
 document.body.append(newP);
 
-var enemyStat = {
+const enemyStat = {
     width: 98,
     height: 56,
     sprite: "images/enemy-bug.png",
 };
 
-var field = {
+const field = {
     min: -50,
     borderX: 400,
     borderY: 450,
     waterCoord: 50,
 }
 
-var playerStat = {
-    width: 80,
-    height: 80,
+const playerStat = {
     startPositionX: 200,
     startPositionY: 400
 }
 
-var xColDistance = 80;
-var yColDistance = 60;
+let xColDistance = 80;
+let yColDistance = 60;
 
-var Enemy = function(posX, posY, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+const Enemy = function(posX, posY, speed) {
     this.posX = posX;
     this.posY = posY;
     this.speed = speed;
-    this.width = enemyStat.width;
-    this.height = enemyStat.height;
-    this.sprite = enemyStat.sprite;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    this.width = 100;
+    this.height = 56;
+    this.sprite = "images/enemy-bug.png";
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     this.posX += this.speed * dt;
     if (this.posX > ctx.canvas.width) {
-        this.posX = -enemyStat.width;
+        this.posX = -100;
     }
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
     this.collision();
 };
 
@@ -58,12 +47,11 @@ Enemy.prototype.collision = function() {
     }
 }
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.posX, this.posY);
 };
 
-var allEnemyVal = [
+const allEnemyVal = [
     {
         x: -120,
         y: 200,
@@ -81,22 +69,18 @@ var allEnemyVal = [
     }
 ];
 
-var allEnemies = allEnemyVal.map(
+let allEnemies = allEnemyVal.map(
     ({x, y, speed}) => new Enemy(x, y, speed)
 )
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-var Player = function(posX, posY) {
+const Player = function(posX, posY) {
     this.posX = posX;
     this.posY = posY;
     this.stat = 0;
-    this.step = 75;
+    this.step = 100;
 
-    this.width = playerStat.width;
-    this.height = playerStat.height;
+    this.width = 80;
+    this.height = 80;
 
     this.sprite = 'images/char-boy.png';
 }
@@ -108,7 +92,6 @@ Player.prototype.update = function() {
 
 Player.prototype.win = function() {
     if (field.min >= this.posY) {
-        console.log(field.min, this.posY);
         this.stat += 1;
         newP.textContent = `Your score: ${this.stat}`;
         this.goToStartPosition();
@@ -123,9 +106,6 @@ Player.prototype.goToStartPosition = function() {
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.posX, this.posY);
 }
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
 Player.prototype.handleInput = function(key) {
     switch(key) {
@@ -135,13 +115,13 @@ Player.prototype.handleInput = function(key) {
         case 'down':
             this.posY += this.step;
             if (this.posY > field.borderY) {
-                this.posY = field.borderY;
+                this.posY = field.borderY - 50;
             }
             break;
         case 'left':
             this.posX -= this.step;
             if (this.posX < this.step) {
-                this.posX = this.step;
+                this.posX = 0;
             }
             break;
         case 'right':
@@ -153,12 +133,10 @@ Player.prototype.handleInput = function(key) {
     }
 }
 
-var player = new Player(playerStat.startPositionX, playerStat.startPositionY);
+let player = new Player(playerStat.startPositionX, playerStat.startPositionY);
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
