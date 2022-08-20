a40c18ca3c4911c6975fa77cd524dd21e3ed4aa9
AsaMitaka <cepelits@gmail.com>
2022-08-20 13:07:56 +0300

    fixed func name, renamed let to const, rewrite classes

--- submissions/asaMitaka/OOP exercise/indexClass.js
+++ submissions/asaMitaka/OOP exercise/indexClass.js
@@ -1,96 +1,70 @@
-/* Main Class */
 class Mammal  {
-    constructor (name, gender, legs, hands, wings, saying) {
+    constructor (name, gender, legs, saying) {
         this.species = 'mammal';
         this.name = name;
         this.gender = gender;
         this.legs = legs;
-        this.hands = hands;
-        this.wings = wings;
         this.saying = saying;
     }
 
-    aboutMe() {
-        return `${this.saying} My name is ${this.name}. I'm ${this.species} ${this.gender}. I have ${this.legs} legs, ${this.wings} wings, ${this.hands} hands.`;
+    tellAboutClass() {
+        return `${this.saying} My name is ${this.name}. I'm ${this.species} ${this.gender}. I have ${this.legs} legs.`;
     }
 }
 
-/* Human */
 class Human extends Mammal{
-    constructor (name, gender, saying = 'Hello') {
-        super();
+    constructor (name, gender, saying) {
+        super(name, gender);
         this.species = 'human';
-        this.name = name;
-        this.gender = gender;
         this.legs = 2;
         this.hands = 2;
         this.saying = saying;
     }
 
-    aboutMe() {
-        return `${this.saying} My name is ${this.name}. I'm ${this.species} ${this.gender}. I have ${this.legs} legs, ${this.hands} hands.`;
+    tellAboutClass() {
+        return `${super.tellAboutClass()} I have ${this.hands} hands.`;
     }
 }
 
 class Man extends Human {
-    constructor (name, saying = 'hi', species, legs, hands) {
-        super(species, legs, hands);
-        this.name = name;
+    constructor (name, saying = 'Hello.') {
+        super(name, saying);
         this.gender = 'male';
         this.saying = saying;
     }
 }
 
 class Woman extends Human {
-    constructor (name, saying = 'hello', species, legs, hands) {
-        super(species, legs, hands);
-        this.name = name;
+    constructor (name, saying = 'Hi') {
+        super(name, saying);
         this.gender = 'female';
         this.saying = saying;
     }
 }
 
-/* Dog */
 class Dog extends Mammal {
-    constructor (name, gender, saying = 'Woof-Woof', ) {
-        super();
+    constructor (name, gender, saying = 'woof') {
+        super(name, gender);
         this.species = 'dog';
-        this.name = name;
-        this.gender = gender;
         this.legs = 4;
-        this.hands = 0;
         this.saying = saying;
-    }
-
-    aboutMe() {
-        return `${this.saying} My name is ${this.name}. I'm ${this.species} ${this.gender}. I have ${this.legs} legs.`;
     }
 }
 
-/* Cat */
 class Cat extends Mammal {
-    constructor (name, gender, saying='Meow') {
-        super();
+    constructor (name, gender, saying = 'Meow') {
+        super(name, gender);
         this.species = 'cat';
-        this.name = name;
-        this.gender = gender;
         this.legs = 4;
-        this.hands = 0;
         this.saying = saying;
-    }
-
-    aboutMe() {
-        return `${this.saying} My name is ${this.name}. I'm ${this.species} ${this.gender}. I have ${this.legs} legs.`;
     }
 }
 
-let john = new Man('John', 'Hello');
-let cassy = new Woman('Cassy', 'Hi!');
-let ralph = new Dog('Ralph', 'male', 'Woooooof');
-let aimi = new Cat('Aimi', 'female', 'meeeeeeoooooooow');
+const john = new Man('John');
+const cassy = new Woman('Cassy', 'Hi!');
+const ralph = new Dog('Ralph', 'male', 'Woooooof');
+const aimi = new Cat('Aimi', 'female', 'Meeeeeeoooooooow');
 
-const inhabitants = [
-    john, cassy, ralph, aimi
-];
+const inhabitants = [john, cassy, ralph, aimi];
 
-inhabitants.forEach( item => print(item.aboutMe()));
+inhabitants.forEach( item => print(item.tellAboutClass()));
