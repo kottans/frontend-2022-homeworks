0f0bace6ecca1c33c82ec9c99fb0a367de029fc3
Yulia Horodechna <horodechnajulia@gmail.com>
2022-08-21 15:49:50 +0300

    Added DOM task

---
+++ submissions/yhorodechna/side-menu-dom/js/script.js
@@ -0,0 +1,164 @@
+const DATA = [
+    {
+        title: 'The Houses of Hogwarts',
+        bgImg: '../img/hogwarts.jpeg',
+        bgLocation: '-1100px',
+        img: '',
+        desc: '',
+        quote: '',
+        author: '',
+        id: 'hogwarts'
+    },
+    {
+        title: 'Gryffindor',
+        img: 'img/gryffindor_logo.png',
+        bgImg: 'img/gryffindor-dungeon.jpeg',
+        bgLocation: '0',
+        desc: 'The Gryffindor house is composed of  some of the most well known, and loved, characters from the Wizarding World such as Harry Potter, Ron Weasley  — the entire Weasley family for that matter , Hermione Granger, Professor Minerva McGonagall, Albus Dumbledore, Sirius Black and Remos Lupin.  The house founded by Godric Gryffindor values bravery, daring, nerve, and chivalry. To express those characteristics, the lion was chosen as the houses symbol, with the vibrant house colors being red and gold.  Every Hogwarts house has its own ghost. The ghost that haunts the Gryffindor Tower is Sir Nicholas de Mimsy-Porpington, or as the students at Hogwarts like to call him Nearly Headless Nick  Nearly Headless Nick was a wizard part of Henry VIIs royal court, and in love with Lady Grieve. The Gryffindor ghost was sentenced to death after casting a spell to try to fix Lady Grieves crooked teeth. The spell backfired and Lady Grieve grew tusks. He was sentenced to be executed with a dull ax, taking several strikes to cut his head off, but the beheading was never completed. Hence the name "nearly headless."',
+        quote: '"You might belong in Gryffindor, where dwell the brave of heart. Their daring, nerve, and chivalry set Gryffindors apart."',
+        author: '— J.K Rowling, Harry Potter and the Philosophers Stone',
+        id: 'gryffindor'
+    },
+    {
+        title: 'Hufflepuff',
+        img: 'img/hufflepuff_crest.png',
+        bgImg: 'img/hufflepuff-dungeon.jpeg',
+        bgLocation: '0',
+        desc: 'The Hufflepuff house founded by Helga Hufflepuff is the house for those wizards and witches that value hard work, dedication, patience, loyalty, and fair play.  The house mascot is a badger, and its colors are yellow and black. Some of the most famous characters in the Harry Potter series that was part of the Hufflepuff house were Newt Scamander (from Fantastic Beasts series), Cedric Diggory, Nymphadora Tonks, and Hogwarts herbology professor, Pomona Sprout.  Like the other houses, Hufflepuff is haunted by the ghost of The Fat Friar. According to the wizarding world website, and originally published on Pottermore, the Friar was a jolly fellow that like to help muggles, especially by curing pox. His use of magic to cure the pox aroused suspicions from the senior churchmen, which lead to his death.',
+        quote: '"You might belong in Hufflepuff, where they are just and loyal. Those patient Hufflepuffs are true and unafraid of toil."',
+        author: '— J.K Rowling, Harry Potter and the Philosophers Stone',
+        id: 'hufflepuff'
+    },
+    {
+        title: 'Ravenclaw',
+        img: 'img/ravenclaw_crest.png',
+        bgImg: 'img/ravenclaw-dungeon.jpeg',
+        bgLocation: '0',
+        desc: 'Its founder, Rowena Ravenclaw valued intelligence, knowledge, curiosity, creativity, and wit in her students. The house symbol is an Eagle, despite the misconception that it is a raven. The name Ravenclaw means "black claw" which is the color of an Eagles claws. The eagle also represents boldness and wisdom reflecting the houses values.  The house colors are blue and copper, but in the Harry Potter movies, Ravenclaw is portrayed with blue and silver colors.  Some of the most well-known wizards and witches that were members of the Ravenclaw House while attending Hogwarts were Luna Lovegood, Gilderoy Lockhart, Professor Filius Flitwick, and Garrick Ollivander (yes, the owner of the wand shop Ollivanders in Diagon Alley)  The ghost that haunts Ravenclaw House is no other than its founders daughter Helena Ravenclaw, or The Grey Lady. Helena Ravenclaw met her end at the hands of "The Baron" (who later would become Slytherins ghost) when she stole her mothers diadem and ran away. The Baron was sent by Rowena to retrieve, not only the diadem but her daughter. When The Baron found Helena in a forest in Albania (the same forest Lord Voldemort kept himself hidden for several years), he was jealous of her freedom and he ended up stabbing her when she refused to go back with him. But before she died, Helena hid her mothers tiara, to be found hundreds of years later by Voldemort.',
+        quote: '"Or yet in wise old Ravenclaw, if youve a ready mind, Where those of wit and learning can always find their kind."',
+        author: '— J.K Rowling, Harry Potter and the Philosophers Stone',
+        id: 'ravenclaw'
+    },
+    {
+        title: 'Slytherin',
+        img: 'img/slytherin_crest.png',
+        bgImg: 'img/slytherin-dungeon.png',
+        bgLocation: '0',
+        desc: 'The Slytherin house is the Hogwarts house that has sprung the darkest wizards. However, that does not mean that all Slytherins are dark wizards or evil, which will vary on a person-to-person basis. Slytherins house personality traits are ambition, leadership, self-preservation, cunning, and resourcefulness. Its founder, Salazar Slytherin, had a special hereditary trait, he was a Parselmouth. Parseltongue is the language of the serpents, and a wizard or witch that is a parselmouth is able to communicate with magical — yes, a basilisk — and no magical serpents.  Salazars connection to snakes was probably what influenced the Slytherin House symbol to be a serpent.  As mentioned in Helena Ravenclaws (aka. The Grey Lady) story above, The Baron was the one responsible for her death. Yet, what has not mentioned in the Ravenclaw portion of this article is that The Baron was a hot-tempered sort man madly in love with Helena Ravenclaw. After being asked by Rowena to go find her daughter and bring her back to Hogwarts, The Baron found Helena, but she refused to return to Hogwarts. In an episode of rage that ended up killing his loved one, and filled with remorse The Baron used the same weapon to take his own life. After his death, the Slytherin ghost was title The Bloody Baron since they were soaked in blood (his own blood, and Helena Ravenclaws).',
+        quote: '"Or perhaps in Slytherin youll make your real friends. Those cunning folk use any means to achieve their ends."',
+        author: '— J.K Rowling, Harry Potter and the Philosophers Stone',
+        id: 'slytherin'
+    }
+];
+
+const NAV_BAR_ITEMS = DATA.map(item => {
+    return {
+        title: item.title,
+        id: item.id
+    }
+});
+
+function createNavList(obj) {
+    let activeElId;
+
+    const { items,
+        parentEl,
+        defaultId,
+        navClick } = obj;
+
+    activeElId = defaultId;
+
+    const navElements = items.map((item) =>
+        `<li class="nav__item">
+            <a id="${item.id}" onclick2="navLinkClick(this,event)" href="#" class="nav__link ${defaultId === item.id ? 'active' : ''}">
+                <span class="nav__text">${item.title}</span> 
+            </a>
+        </li>`
+    );
+
+    const html = `
+        <header class="header">
+            <a class="header__link" onclick2="headerLinkClick()" ><img src="img/logo.png" alt="" class="img__btn"></a>
+            <h2 class="header__title">The Houses of Hogwarts</h2>
+        </header>
+        <nav class="nav"><ul class="nav__list">${navElements.join('')}</ul></nav>
+                `;
+
+    const div = document.createElement("div");
+    div.className = 'container';
+    div.innerHTML = html;
+
+    div.addEventListener("click", (event) => {
+        const mainEl = document.querySelector('.main');
+        const navEl = div.querySelector('.nav');
+
+        let current = event.target;
+        while (current) {
+            if (current.className && current.className.includes('nav__link')) {
+                console.log('navLink')
+                if (activeElId !== current.id) {
+                    if (activeElId) {
+                        div.querySelector('#' + activeElId).classList.remove('active')
+                    }
+                    activeElId = current.id;
+                    current.classList.add('active')
+                    const item = items.find(f => f.id === current.id);
+                    navClick(current, item)
+                }
+                console.log(activeElId)
+                navEl.classList.remove('nav__full');
+                break;
+            }
+            else if (current.className && current.className.includes('header__link')) {
+                console.log('header link')
+                navEl.classList.add('nav__full');
+                mainEl.style.cssText = `display:none;`
+                break;
+            }
+            else if (current.className && current.className.includes('container')) {
+                console.log('end')
+                break;
+            }
+            console.log('inside event listener', current)
+            current = current.parentNode;
+        }
+    })
+
+    parentEl.appendChild(div);
+};
+
+function navClick(current, navItem) {
+    console.log('linkClick', current, navItem)
+    const mainEl = document.querySelector('.main');
+    const item = DATA.find(f => f.id === current.id);
+    const wrapperEl = document.querySelector('.wrapper');
+    const titleEl = document.querySelector('.header__title');
+    const mainHtml = `
+        <article class="main__article">
+                <img class="main__img" src="${item.img}" alt="">
+                <p class="main-description">${item.desc}</p>
+                <p class="main__quote">${item.quote} </p>
+                <p class="main__author">${item.author}</p>
+        </article>
+        `;
+
+    mainEl.innerHTML = mainHtml;
+
+    wrapperEl.style.cssText = `background: url("${item.bgImg}") ${item.bgLocation}px 0 / cover no-repeat;`;
+
+    titleEl.innerHTML = item.title;
+    if (DATA[0].id === item.id) {
+        mainEl.style.cssText = `display:none`;
+    } else {
+        mainEl.style.cssText = `display:block;`;
+    }
+}
+createNavList({
+    items: NAV_BAR_ITEMS,
+    parentEl: document.getElementById('navDivContainer'),
+    defaultId: NAV_BAR_ITEMS[0].id,
+    navClick
+});
+
+
+
