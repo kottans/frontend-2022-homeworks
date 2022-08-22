448fcc3f190859f7526923f0ba656d57e57066dd
Tedzury <asdrogachev@gmail.com>
2022-08-22 15:54:34 +0300

    Deleted unneeded comments

--- submissions/Oleksii_Drohachov/Tiny_JS_world/index.js
+++ submissions/Oleksii_Drohachov/Tiny_JS_world/index.js
@@ -1,12 +1,4 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
 
-   Code repository: https://github.com/Tedzury/Tiny_JS_world
-   Web app: https://tedzury.github.io/Tiny_JS_world/
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
 catsPhrase = 'meow-meow skinbag';
 
 const tinyWorldInhabitants = [
@@ -55,29 +47,8 @@ const tinyWorldInhabitants = [
       saying : catsPhrase,
       friends: ['definitely nobody except her own slaves, she\'s a cat, you know...']
    }
-
 ]
 
-
-
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-   
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
 
    function formOutput(obj) {
 
@@ -94,7 +65,7 @@ const tinyWorldInhabitants = [
    }
    
    tinyWorldInhabitants.forEach( item => print(formOutput(item)));
-   
+
 
 
 
