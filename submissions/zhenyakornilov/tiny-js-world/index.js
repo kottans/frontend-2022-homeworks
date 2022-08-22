e8cf5c2ae63d056728f036eb39dc6ce1d6648774
Yevhen Kornilov <81849412+zhenyakornilov@users.noreply.github.com>
2022-08-22 17:43:52 +0300

    OOP exercise - Tiny JS World (#238)
    * OOP exercise
    
    * Change index.js
    
    * New changes

--- submissions/zhenyakornilov/tiny-js-world/index.js
+++ submissions/zhenyakornilov/tiny-js-world/index.js
@@ -1,77 +1,95 @@
-const dog = {
-  species: "dog",
-  name: "Ronnie",
-  gender: "male",
-  paws: 4,
-  legs: 0,
-  hands: 0,
-  saying: "Woof!",
-};
+class Inhabitant {
+  constructor(species, name, gender, saying) {
+    this.species = species;
+    this.name = name;
+    this.gender = gender;
+    this.friends = [];
+    this.saying = saying;
+    this.props = ["species", "name", "gender", "friends", "saying"];
+  }
 
-const cat = {
-  species: "cat",
-  name: "Reggie",
-  gender: "male",
-  paws: 4,
-  legs: 0,
-  hands: 0,
-  saying: "Meoooow!",
-};
-
-const woman = {
-  species: "human",
-  name: "Agnes",
-  gender: "female",
-  paws: 0,
-  legs: 2,
-  hands: 2,
-  saying: "Hi, my name is Agnes",
-};
-
-const man = {
-  species: "human",
-  name: "Mark",
-  gender: "male",
-  paws: 0,
-  legs: 2,
-  hands: 2,
-  saying: `Hello, I am Mark`,
-  friends: [cat, dog],
-};
-
-const catWoman = Object.create(cat);
-
-catWoman.species = "catwoman";
-catWoman.name = "Selina";
-catWoman.gender = "female";
-catWoman.paws = 0;
-catWoman.legs = 2;
-catWoman.hands = 2;
-
-const inhabitants = [dog, cat, woman, man, catWoman];
-const properties = [
-  "species",
-  "name",
-  "gender",
-  "paws",
-  "legs",
-  "hands",
-  "friends",
-  "saying",
-];
-
-for (let inhabitant of inhabitants) {
-  print(
-    properties
+  toString() {
+    return this.props
       .map((key) => {
         if (key === "friends") {
-          if (key in inhabitant) {
-            return inhabitant[key].map((friend) => friend.name).join(", ");
-          }           
-          return "No friends";
+          if (this[key].length === 0) {
+            return "No friends";
+          }
+          return this[key].map((friend) => friend.name).join(", ");
         }
-        return inhabitant[key];
+        return this[key];
       })
-      .join("; ")
-  );
+      .join("; ");
+  }
+}
+
+class Animal extends Inhabitant {
+  constructor(species, name, gender, saying) {
+    super(species, name, gender, saying);
+    this.paws = 4;
+  }
+
+  toString() {
+    return super.toString() + `; ${this.paws} paws`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, gender) {
+    super("dog", name, gender, "Woof-woof");
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, gender) {
+    super("cat", name, gender, "Meoooow");
+  }
+}
+
+class Human extends Inhabitant {
+  constructor(name, gender, saying) {
+    super("human", name, gender, saying);
+    this.legs = 2;
+    this.hands = 2;
+  }
+
+  toString() {
+    return super.toString() + `; ${this.legs} legs, ${this.hands} hands`;
+  }
+}
+
+class Woman extends Human {
+  constructor(name, saying) {
+    super(name, "female");
+    this.saying = saying;
+  }
+}
+
+class Man extends Human {
+  constructor(name, saying, friends = []) {
+    super(name, "male");
+    this.saying = saying;
+    this.friends = friends;
+  }
+}
+
+class CatWoman extends Human {
+  constructor(catRef) {
+    super("Selina", "female");
+    this.cat = catRef;
+    this.saying = this.cat.saying;
+    this.species = "catwoman";
+  }
+}
+
+const dog = new Dog("Ronnie", "male");
+const cat = new Cat("Reggie", "male");
+const woman = new Woman("Agnes", "Hello");
+const man = new Man("Mark", "Hi", [cat, dog]);
+const catWoman = new CatWoman(cat);
+
+const allInhabitants = [dog, cat, woman, man, catWoman];
+
+for (let inhabitant of allInhabitants) {
+  print(inhabitant);
 }
