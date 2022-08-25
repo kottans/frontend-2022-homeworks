d60385018793b4893e35a53a53ee282f9faba466
LuckyDnepr <LuckyDnepr@gmail.com>
2022-08-25 15:34:50 +0300

    Do self-checks, add some changes. Ready for review.

--- submissions/LuckyDnepr/task-js-oop/app.js
+++ submissions/LuckyDnepr/task-js-oop/app.js
@@ -1,89 +1,119 @@
 "use strict";
 
-const cellX = 101, //gamefield sizes
-    cellY = 83,
-    height = cellX * 6,
-    width = cellY * 5;
+const blockWidth = 101, //gamefield sizes
+    blockHeight = 83,
+    nBlocksWidth = 5,
+    nBlocksHeight = 6,
+    fieldWidth = blockWidth * nBlocksWidth,
+    fieldHeight = blockHeight * nBlocksHeight,
+    enemyTracksHeight = [62, 145, 228], //coordinates of enemy tracks
+    playerStartPoint = [200, 405],
+    hardestLevel = 4;
 
 let winPoints = 0,
 failPoints = 0;
 
-class Enemy {
-    constructor(coordX, coordY, startSpeed) {
+class Unit {
+    constructor (coordX, coordY, avatar) {
         this.x = coordX,
-            this.y = coordY,
-            this.speed = startSpeed,
-            this.sprite = "images/enemy-bug.png"
+        this.y = coordY;
+        this.sprite = avatar;
     }
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
+    update() {}
+    setAvatar (avatar) {
+        this.sprite = avatar;
+    }
+}
+
+class Enemy extends Unit {
+    constructor (coordX, coordY, startSpeed, avatar) {
+        super(coordX, coordY, avatar);
+        this.speed = startSpeed;
+    }
     update(dt) {
         this.x += this.speed * dt;
-        if (this.x > width + cellY) { 
+        this.moveEnemyToStart ();
+        this.checkCollisions();
+    }
+    checkCollisions () {
+        if (this.x > player.x - blockWidth + 20 && //+20 - correction for avatar, for clearer collision
+            this.x < player.x + blockWidth - 15 && //-15 - correction for avatar, for clearer collision
+            this.y < player.y &&
+            this.y + blockHeight > player.y) {
+            endGame(false); //false - you lose the game
+        }
+    }
+    moveEnemyToStart () {
+        if (this.x > fieldWidth + blockWidth) {
             this.x = Math.random() * -300; //change enemy start point before new appearance
             this.speed = Math.random() * 150 + Math.random() * 150; //change enemy speed before new appearance
-        }
-        if (this.x > player.x - cellX + 20 && //+20 - correction for avatar, for clearer collision
-            this.x < player.x + cellX - 15 && //-15 - correction for avatar, for clearer collision
-            this.y < player.y &&
-            this.y + cellY > player.y) {
-            endGame(false); //false - you lose the game
         }
     }
 }
 
-class Player {
-    constructor(coordX, coordY) {
-        this.x = coordX,
-            this.y = coordY,
-            this.freeze = true,
-            this.sprite = "images/chibi-1.png";
-    }
-    update() {}
-    render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+class Player extends Unit {
+    constructor (coordX, coordY, avatar) {
+        super(coordX, coordY, avatar);
+        this._freeze = true; //the player does not respond to the keyboard if _freeze === true
     }
     freezing() {
-        this.freeze = true; //player freezed until the win/lose banner showing
+        this._freeze = true; //player freezed until the win/lose banner showing
     }
     unfreezing() {
-        this.freeze = false; //player unfreezed when gameplay start
+        this._freeze = false; //player unfreezed when gameplay start
     }
-    goToStart() { //randomize player start position after win/lose
-        this.x = Math.floor(Math.random() * 4) * cellX;
-        this.y = (Math.random() > 0.5) ? (cellY * 5 - 10) : (cellY * 4 - 10);
+    movePlayerToStart() { //randomize player start position after win/lose
+        this.x = Math.floor(Math.random() * 4) * blockWidth;
+        this.y = (Math.random() > 0.5) ? (blockHeight * 5 - 10) : (blockHeight * 4 - 10);
     }
-    handleInput(direction) {
-        if (!this.freeze) {
-            switch (direction) { //move player
+    handleInput(key) {
+        if (!this._freeze) {
+            switch (key) { //choose direction depending on the key pressed
                 case "ArrowLeft":
-                    if (this.x > 0) {
-                        this.x -= cellX;
-                    }
+                    this.stepLeft();
                     break;
                 case "ArrowRight":
-                    if (this.x < width - cellX) {
-                        this.x += cellX;
-                    }
+                    this.stepRight();
                     break;
                 case "ArrowUp":
-                    if (this.y > 0) {
-                        this.y -= cellY;
-                    }
+                    this.stepUp();
                     break;
                 case "ArrowDown":
-                    if (this.y < height - 3 * cellY) {
-                        this.y += cellY;
-                    }
+                    this.stepDown();
                     break;
                 default:
                     break;
             }
-            if (this.y < 0) { //player on the water => you win
-                this.freeze = true;
-                endGame(true); //win the game
-            }
+            this.checkWin(); //check win after move player
+        }
+    }
+    stepLeft () { //player do step left
+        if (this.x > 0) {
+            this.x -= blockWidth;
+        }
+    }
+    stepRight () { //player do step right
+        if (this.x < fieldWidth - 1.5 * blockWidth) {
+            this.x += blockWidth;
+        }
+    }
+    stepUp () { //player do step up
+        if (this.y > 0) {
+            this.y -= blockHeight;
+        }
+    }
+    stepDown () { //player do step down
+        if (this.y < fieldHeight - 1.5 * blockHeight) {
+            this.y += blockHeight;
+        }
+    }
+    checkWin () { //player on the water => you win
+        if (this.y < 0) { 
+            this._freeze = true;
+            endGame(true); //win the game
         }
     }
 }
@@ -103,30 +133,27 @@ function endGame(win) { //end gameset
 function restartGame(winLose) { //restart gameset - winLose - true->win; false->fail
     winLoseBanners (winLose);
     player.freezing();
-    player.goToStart();
+    player.movePlayerToStart();
     setTimeout(() => {
         winLoseBanners ();
         player.unfreezing();
     }, 750);
 }
 
-function generateEnemies(n) { //n - enemies per track
-    const enemyTracksY = [62, 145, 228]; //coordinates of enemy tracks
+function generateEnemies(n) { //n - enemies per track - this is equivalent difficulty
     let enemies = [];
     for (let i = 1; i <= n; i++) {
-        enemies = [
-            ...enemies,
-            ...enemyTracksY.map((trackY) => {
-                const startEnemySpeed = Math.random() * 350, //randomize start enemy speed for first appearance
-                    startEnemyX = Math.random() * -300; //randomize start enemy point for first appearance
-                return new Enemy(startEnemyX, trackY, startEnemySpeed);
-            }),
-        ];
+        enemies
+            .push(...enemyTracksHeight
+                .map((trackHeight) => {
+                    return new Enemy(Math.random() * -300, trackHeight, Math.random() * 350); //randomise start point and speed
+                })
+            );
     }
     return enemies;
 }
 
-function updatePoints () {
+function updatePoints () { //update win/lode points
     document.querySelector("#win-points").innerHTML = winPoints;
     document.querySelector("#fail-points").innerHTML = failPoints;
 }
@@ -140,8 +167,7 @@ function winLoseBanners (winLose = -1) { //show/hide win/lose banners
             ? document.querySelector(".win").classList.add("show")
             : document.querySelector(".lose").classList.add("show");
     }
-    
 }
 
-var allEnemies = generateEnemies(4), //"4" - the hardest level (12 enemies)
-player = new Player(200, 405); //create player
+var allEnemies = generateEnemies(hardestLevel), //"4" - the hardest level (12 enemies)
+player = new Player(...playerStartPoint); //create player
--- submissions/LuckyDnepr/task-js-oop/main.js
+++ submissions/LuckyDnepr/task-js-oop/main.js
@@ -1,33 +1,33 @@
 "use strict";
 
-import {
-    Engine
-} from "./engine.js";
+import { Engine } from "./engine.js";
 
 const doc = window.document,
-    avatars = [
-        "images/chibi-1.png",
-        "images/chibi-2.png",
-        "images/chibi-3.png",
-        "images/chibi-4.png",
-        "images/chibi-5.png",
-        "images/chibi-6.png",
-        "images/chibi-7.png",
-        "images/chibi-8.png",
-    ];
+  enemyAvatar = "images/enemy-bug.png",
+  playerAvatars = [
+    "images/chibi-1.png",
+    "images/chibi-2.png",
+    "images/chibi-3.png",
+    "images/chibi-4.png",
+    "images/chibi-5.png",
+    "images/chibi-6.png",
+    "images/chibi-7.png",
+    "images/chibi-8.png",
+  ];
 
 let difficulty = 2;
 
 (function main() {
-    renderWelcomeInterface();
-    addWelcomeButtonsLiteners();
-    addUserControlsListeners();
+  renderWelcomeInterface();
+  addWelcomeEventsLiteners();
+  addSwipeEventListener();
+  addKeyUpEventListener();
 })();
 
 function renderWelcomeInterface() {
-    const mainDiv = doc.createElement("div");
-    mainDiv.classList.add("main");
-    mainDiv.innerHTML = `
+  const mainDiv = doc.createElement("div");
+  mainDiv.classList.add("main");
+  mainDiv.innerHTML = `
       <div class="player-change">
         <h1 class="title">Classic Frogger Game</h1>
         <div class="avatar-diff">
@@ -46,148 +46,151 @@ function renderWelcomeInterface() {
       </div>
       `;
 
-    doc.body.appendChild(mainDiv);
+  doc.body.appendChild(mainDiv);
 }
 
-function addWelcomeButtonsLiteners() {
-    doc
-        .querySelector("#btn-next")
-        .addEventListener("click", (e) => nextAvatar(e));
+function addWelcomeEventsLiteners() {
+  doc
+    .querySelector("#btn-next")
+    .addEventListener("click", (e) => nextAvatar(e));
 
-    doc
-        .querySelector("#btn-prev")
-        .addEventListener("click", (e) => prevAvatar(e));
+  doc
+    .querySelector("#btn-prev")
+    .addEventListener("click", (e) => prevAvatar(e));
 
-    doc
-        .querySelector("#diff-input")
-        .addEventListener("input", (e) => changeDifficulty(e));
+  doc
+    .querySelector("#diff-input")
+    .addEventListener("input", (e) => changeDifficulty(e));
 
-    doc
-        .querySelector("#btn-start")
-        .addEventListener("click", (e) => startGame(e));
+  doc
+    .querySelector("#btn-start")
+    .addEventListener("click", (e) => startGame(e));
 }
 
-function addUserControlsListeners() {
-    doc.addEventListener("keyup", function (e) { //keyboard handler for move player
-        const allowedKeys = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"];
-        if (allowedKeys.includes(e.code)) {
-            player.handleInput(e.code);
-        }
-    });
+function addKeyUpEventListener() {
+  doc.addEventListener("keyup", function (e) {
+    //keyboard handler for move player
+    const allowedKeys = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"];
+    if (allowedKeys.includes(e.code)) {
+      player.handleInput(e.code);
+    }
+  });
+}
 
-    doc.addEventListener("touchstart", handleTouchStart, false); //swipes handler for move player
-    doc.addEventListener("touchmove", handleTouchMove, false); //swipes handler for move player
+function addSwipeEventListener() {
+  doc.addEventListener("touchstart", handleTouchStart, false); //swipes handler for move player
+  doc.addEventListener("touchmove", handleTouchMove, false); //swipes handler for move player
 
-    var xDown = null;
-    var yDown = null;
+  var xDown = null;
+  var yDown = null;
 
-    function handleTouchStart(evt) {
-        const firstTouch = evt.touches[0];
-        xDown = firstTouch.clientX; //finger down
-        yDown = firstTouch.clientY; //finger down
+  function handleTouchStart(evt) {
+    const firstTouch = evt.touches[0];
+    xDown = firstTouch.clientX; //finger down
+    yDown = firstTouch.clientY; //finger down
+  }
+
+  function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+      return;
     }
 
-    function handleTouchMove(evt) {
-        if (!xDown || !yDown) {
-            return;
-        }
+    var xUp = evt.touches[0].clientX; //finger up
+    var yUp = evt.touches[0].clientY; //finger up
 
-        var xUp = evt.touches[0].clientX; //finger up
-        var yUp = evt.touches[0].clientY; //finger up
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
 
-        var xDiff = xDown - xUp;
-        var yDiff = yDown - yUp;
-
-        if (Math.abs(xDiff) > Math.abs(yDiff)) {
-            /*most significant*/
-            if (xDiff > 0) {
-                /* left swipe */
-                player.handleInput("ArrowLeft");
-            } else {
-                /* right swipe */
-                player.handleInput("ArrowRight");
-            }
-        } else {
-            if (yDiff > 0) {
-                /* up swipe */
-                player.handleInput("ArrowUp");
-            } else {
-                /* down swipe */
-                player.handleInput("ArrowDown");
-            }
-        }
-        /* reset values */
-        xDown = null;
-        yDown = null;
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+      /*most significant*/
+      if (xDiff > 0) {
+        /* left swipe */
+        player.handleInput("ArrowLeft");
+      } else {
+        /* right swipe */
+        player.handleInput("ArrowRight");
+      }
+    } else {
+      if (yDiff > 0) {
+        /* up swipe */
+        player.handleInput("ArrowUp");
+      } else {
+        /* down swipe */
+        player.handleInput("ArrowDown");
+      }
     }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+  }
 }
 
 function nextAvatar(e) {
-    e.preventDefault();
-    const avatarPicture = doc.querySelector(".avatar"),
-        n = avatarPicture.dataset.avatar;
-    if (n == avatars.length - 1) {
-        avatarPicture.src = avatars[0];
-        avatarPicture.dataset.avatar = 0;
-    } else {
-        avatarPicture.src = avatars[+n + 1];
-        avatarPicture.dataset.avatar = +n + 1;
-    }
+  e.preventDefault();
+  const avatarPicture = doc.querySelector(".avatar"),
+    n = avatarPicture.dataset.avatar;
+  if (n == playerAvatars.length - 1) {
+    avatarPicture.src = playerAvatars[0];
+    avatarPicture.dataset.avatar = 0;
+  } else {
+    avatarPicture.src = playerAvatars[+n + 1];
+    avatarPicture.dataset.avatar = +n + 1;
+  }
 }
 
 function prevAvatar(e) {
-    e.preventDefault();
-    const avatarPicture = doc.querySelector(".avatar"),
-        n = avatarPicture.dataset.avatar;
-    if (n == 0) {
-        avatarPicture.src = avatars[avatars.length - 1];
-        avatarPicture.dataset.avatar = avatars.length - 1;
-    } else {
-        avatarPicture.src = avatars[+n - 1];
-        avatarPicture.dataset.avatar = +n - 1;
-    }
+  e.preventDefault();
+  const avatarPicture = doc.querySelector(".avatar"),
+    n = avatarPicture.dataset.avatar;
+  if (n == 0) {
+    avatarPicture.src = playerAvatars[playerAvatars.length - 1];
+    avatarPicture.dataset.avatar = playerAvatars.length - 1;
+  } else {
+    avatarPicture.src = playerAvatars[+n - 1];
+    avatarPicture.dataset.avatar = +n - 1;
+  }
 }
 
-function changeDifficulty (e) {
-    const btnStart = doc.querySelector("#btn-start");
-    switch (e.target.value) {
-        case "1":
-            difficulty = 1;
-            btnStart.innerHTML = "Play game EASY";
-            break;
-        case "2":
-            difficulty = 2;
-            btnStart.innerHTML = "Play game NORMAL";
-            break;
-        case "3":
-            difficulty = 3;
-            btnStart.innerHTML = "Play game HARD";
-            break;
-        case "4":
-            difficulty = 4;
-            btnStart.innerHTML = "Play game HARDEST";
-            break;
-        default:
-            break;
-    }
+function changeDifficulty(e) {
+  switch (e.target.value) {
+    case "1":
+      difficulty = 1;
+      doc.querySelector("#btn-start").innerHTML = "Play game EASY";
+      break;
+    case "2":
+      difficulty = 2;
+      doc.querySelector("#btn-start").innerHTML = "Play game NORMAL";
+      break;
+    case "3":
+      difficulty = 3;
+      doc.querySelector("#btn-start").innerHTML = "Play game HARD";
+      break;
+    case "4":
+      difficulty = 4;
+      doc.querySelector("#btn-start").innerHTML = "Play game HARDEST";
+      break;
+    default:
+      break;
+  }
 }
 
 function startGame(e) {
-    e.preventDefault();
-    allEnemies.length = difficulty * 3;
-    player.sprite = avatars[+doc.querySelector(".avatar").dataset.avatar];
-    doc.querySelector(".player-change").remove();
-    renderGamePlayDOM(window);
-    Engine(window);
-    player.unfreezing();
+  e.preventDefault();
+  allEnemies.length = difficulty * 3; //change quantity of enemies
+  player.setAvatar(playerAvatars[+doc.querySelector(".avatar").dataset.avatar]); //assign player avatar
+  allEnemies.forEach((enemy) => enemy.setAvatar(enemyAvatar)); //assign enemies avatar
+  doc.querySelector(".player-change").remove(); //remove welcome interface
+  renderGamePlayDOM(window); //render game field and etc.
+  Engine(window); //run game engine
+  player.unfreezing(); //unfreezing player
 }
 
 function renderGamePlayDOM() {
-    const mainDiv = doc.createElement("div");
-    mainDiv.classList.add("main");
-    mainDiv.innerHTML = `
+  const mainDiv = doc.createElement("div");
+  mainDiv.classList.add("main");
+  mainDiv.innerHTML = `
           <div class="container" id="game-container">
-          <button id="goFS">GO to Full Screen Mode</button>
+          <button id="goFullScreen">GO to Full Screen Mode</button>
             <div class="points">
               <p class="points-show">Wins: <span id="win-points">${winPoints}</span></p>
               <p class="points-show">Fails: <span id="fail-points">${failPoints}</span></p>
@@ -197,28 +200,29 @@ function renderGamePlayDOM() {
             <canvas class="game-field" width="505" height="606">
           </div>
           `;
-    doc.querySelector(".main").replaceWith(mainDiv);
-    
-    doc.querySelector("#goFS").addEventListener("click", (e) => {
-        e.preventDefault();
-        if (!doc.fullscreenElement) {
-            e.target.innerText = "EXIT from Full Screen Mode";
-            e.target.style.marginTop = "0.5em";
-            doc.querySelector(".main").requestFullscreen();
-            e.target.style.fontSize = "12px";
-            doc.querySelector(".points").style.fontSize = "18px";
-            doc.querySelector("#win-points").style.fontSize = "18px";
-            doc.querySelector("#fail-points").style.fontSize = "18px";
+  doc.querySelector(".main").replaceWith(mainDiv);
+  goFullScreenButtonListener();
+}
 
-        } else {
-            e.target.innerText = "GO to Full Screen Mode";
-            doc.exitFullscreen();
-            e.target.style.fontSize = "";
-            e.target.style.marginTop = "";
-            doc.querySelector(".points").style.fontSize = "";
-            doc.querySelector("#win-points").style.fontSize = "";
-            doc.querySelector("#fail-points").style.fontSize = "";
-        }
-        
-    });
+function goFullScreenButtonListener() {
+  doc.querySelector("#goFullScreen").addEventListener("click", (e) => {
+    e.preventDefault();
+    if (!doc.fullscreenElement) {
+      e.target.innerText = "EXIT from Full Screen Mode";
+      e.target.style.marginTop = "0.5em";
+      doc.querySelector(".main").requestFullscreen();
+      e.target.style.fontSize = "12px";
+      doc.querySelector(".points").style.fontSize = "18px";
+      doc.querySelector("#win-points").style.fontSize = "18px";
+      doc.querySelector("#fail-points").style.fontSize = "18px";
+    } else {
+      e.target.innerText = "GO to Full Screen Mode";
+      doc.exitFullscreen();
+      e.target.style.fontSize = "";
+      e.target.style.marginTop = "";
+      doc.querySelector(".points").style.fontSize = "";
+      doc.querySelector("#win-points").style.fontSize = "";
+      doc.querySelector("#fail-points").style.fontSize = "";
+    }
+  });
 }
