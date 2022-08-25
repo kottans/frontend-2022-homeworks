9c98b37d10d72ba1f22e1a7f5450842802fae6fd
Tedzury <asdrogachev@gmail.com>
2022-08-25 19:37:11 +0300

    slightly refactored array of characters, used ternary operator in main func, set cat as a prototype to catwoman

--- submissions/Oleksii_Drohachov/Tiny_JS_world/index.js
+++ submissions/Oleksii_Drohachov/Tiny_JS_world/index.js
@@ -1,82 +1,58 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
+const dog = {
+  species: "dog",
+  name: "Barbos",
+  gender: "male",
+  legs: 4,
+  hands: 0,
+  saying: "gav-gav kaje pes",
+  friends: ["definitely everybody in the world !"],
+};
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
+const cat = {
+  species: "cat",
+  name: "Sonya",
+  gender: "female",
+  legs: 4,
+  hands: 0,
+  saying: "meow-meow, skinbag...",
+  friends: ["definitely nobody except her own slaves"],
+};
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-catsPhrase = "meow-meow skinbag";
+const man = {
+  species: "human",
+  name: "Oleksii",
+  gender: "male",
+  legs: 2,
+  hands: 2,
+  saying: "Wanna be frontend ninja in future!",
+  friends: ["Alex", "Victoria", "every lovely kottan on the course"],
+};
 
-const tinyWorldInhabitants = [
-  {
-    species: "dog",
-    name: "Barbos",
-    gender: "male",
-    legs: 4,
-    hands: 0,
-    saying: "gav-gav kaje pes",
-    friends: ["definitely everybody in the world !"],
-  },
-  {
-    species: "cat",
-    name: "Sonya",
-    gender: "female",
-    legs: 4,
-    hands: 0,
-    saying: catsPhrase,
-    friends: ["definitely nobody except her own slaves"],
-  },
-  {
-    species: "human",
-    name: "Oleksii",
-    gender: "male",
-    legs: 2,
-    hands: 2,
-    saying: "Wanna be frontend ninja in future!",
-    friends: ["Alex", "Victoria", "every lovely kottan on the course"],
-  },
-  {
-    species: "human",
-    name: "Victoria",
-    gender: "female",
-    legs: 2,
-    hands: 2,
-    saying: "When will you bring the money to home, honey?",
-    friends: ["Natasha", "Katya", "Lada"],
-  },
-  {
-    species: "catWoman",
-    name: "Anjela",
-    gender: "female",
-    legs: 2,
-    hands: 2,
-    saying: catsPhrase,
-    friends: [
-      "definitely nobody except her own slaves, she's a cat, you know...",
-    ],
-  },
-];
+const woman = {
+  species: "human",
+  name: "Victoria",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  saying: "When will you bring the money to home, honey?",
+  friends: ["Natasha", "Katya", "Lada"],
+};
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-   
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+const catWoman = {
+  species: "catWoman",
+  name: "Anjela",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  friends: [
+    "definitely nobody except her own slaves, she's a cat, you know...",
+  ],
+};
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
+catWoman.__proto__ = cat;
 
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+const tinyWorldInhabitants = [dog, cat, man, woman, catWoman];
+
 const keys = [
   "species",
   "name",
@@ -90,12 +66,9 @@ const keys = [
 tinyWorldInhabitants.forEach((person) => {
   print(
     keys
-      .map((key) => {
-        if (Array.isArray(person[key])) {
-          person[key] = person[key].join(", ");
-        }
-        return person[key];
-      })
-      .join("; ")
+      .map((key) =>
+        Array.isArray(person[key]) ? person[key].join(", ") : person[key]
+      )
+      .join(" ; ")
   );
 });
