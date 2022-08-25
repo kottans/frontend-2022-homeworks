b174217e1ac135e883a654c5a9ec0aee8eac9813
Kostiantyn <konstantinokhorzin@gmail.com>
2022-08-25 16:02:39 +0300

    global update

--- submissions/KostiantynOkhorzin/oop-exercise/index.js
+++ submissions/KostiantynOkhorzin/oop-exercise/index.js
@@ -1,42 +1,46 @@
+
 class Inhabitan {
    constructor(species, name, gender, saying) {
       this.species = species;
       this.name = name;
       this.gender = gender;
       this.saying = saying;
-   }
-};
-
-class Human extends Inhabitan {
-   constructor(species, name, gender, saying, legs = 2, hands = 2) {
-       super(species, name, gender, saying);
-       this.legs = legs;
-       this.hands = hands;
+      this.keys = ['species', 'name', 'gender', 'saying'];
    }
 
    showPropsToPrint() {
-      print([this.species, this.name, this.gender, this.legs, this.hands, this.saying].join('; '));
+      let props = [];
+      this.keys.forEach((key) => {
+         props.push(this[key]);
+      })
+      print(props.join('; '));
+   }
+};
+
+
+class Human extends Inhabitan {
+   constructor(name, gender, saying, legs = 2, hands = 2) {
+      super('human', name, gender, saying);
+      this.legs = legs;
+      this.hands = hands;
+      this.keys = ['species', 'name', 'gender', 'legs', 'hands', 'saying'];
    }
 };
 
 class Animal extends Inhabitan {
    constructor(species, name, gender, saying, legs = 4) {
-       super(species, name, gender, saying);
-       this.legs = legs;
-   }
-
-   showPropsToPrint() {
-      print([this.species, this.name, this.gender, this.legs, this.saying].join('; '));
+      super(species, name, gender, saying);
+      this.legs = legs;
+      this.keys = ['species', 'name', 'gender', 'legs', 'saying'];
    }
 };
 
-const dogToby = new Animal('dog', 'Toby', 'male', 'woof-woof!');
-const catTom = new Animal('cat', 'Tom', 'male', 'hello Jerry!');
-const womanMonica = new Human('woman', 'Monica', 'female', 'hello Joe!');
-const manJoe = new Human('man', 'Joe', 'male', 'one hundred himars to these brave Ukrainians!');
+const dog = new Animal('dog', 'Toby', 'male', 'woof-woof!');
+const cat = new Animal('cat', 'Tom', 'male', 'hello Jerry!');
+const woman = new Human('Monica', 'female', 'hello Joe!');
+const man = new Human('Joe', 'male', 'one hundred himars to these brave Ukrainians!');
 
-const inhabitans = [dogToby, catTom, womanMonica, manJoe];
+const inhabitans = [dog, cat, woman, man];
 
 inhabitans.forEach(inhabitan => inhabitan.showPropsToPrint());
-
 
