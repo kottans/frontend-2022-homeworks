0ee23a8dff537c27f627f3c84a0899a0f758f1df
VladimirRutskiy <vovarutskiy@gmail.com>
2022-08-25 16:41:14 +0300

    Fixed some mistakes

--- submissions/VolodymyrRutskyi/task_js_dom/main.js
+++ submissions/VolodymyrRutskyi/task_js_dom/main.js
@@ -1,169 +1,154 @@
-let data = [
+const dataList = [
     {
         name: 'Concat',
         image: 'img/concat.jpg',
         description: [
-            `<p>Метод concat создаёт новый массив, состоящий
+            `Метод concat создаёт новый массив, состоящий
             из элементов в объекте, на котором он был вызван, 
             за которыми по порядку следуют, для каждого аргумента, 
             все его элементы (если аргумент является массивом), 
-            либо сам аргумент (если он массивом не является).</p>
-            <p>Метод concat не изменяет данный массив</p>
-            `
+            либо сам аргумент (если он массивом не является).`,
+            `Метод concat не изменяет данный массив`
         ]
     },
     {
         name: 'CopyWithin',
         image: 'img/copywithin.jpg',
         description: [
-            `<p>Метод copyWithin() копирует последовательность
+            `Метод copyWithin() копирует последовательность
             элементов массива внутри него в позицию, начинающуюся 
-            по индексу target.</p>
-        `]
+            по индексу target.`
+        ]
     },
     {
         name: 'Every',
         image: 'img/every.jpg',
         description: [
-            `<p>Метод every() проверяет, удовлетворяют ли все элементы
-            массива условию, заданному в передаваемой функции.
-            </p>
-            `
+            `Метод every() проверяет, удовлетворяют ли все элементы
+            массива условию, заданному в передаваемой функции.`
         ]
     },
     {
         name: 'Fill',
         image: 'img/fill.jpg',
         description: [
-            `<p>Метод fill() заполняет все элементы массива от начального
-            до конечного индексов одним значением.</p>
-            `
+            `Метод fill() заполняет все элементы массива от начального
+            до конечного индексов одним значением.`
         ]
     },
     {
         name: 'Filter',
         image: 'img/filter.jpg',
         description: [
-            `<p>Метод filter() создаёт новый массив со всеми элементами, 
-            прошедшими проверку, задаваемую в передаваемой функции.</p>
-            `
+            `Метод filter() создаёт новый массив со всеми элементами, 
+            прошедшими проверку, задаваемую в передаваемой функции.`
         ]
     },
     {
         name: 'Find',
         image: 'img/find.jpg',
         description: [
-            `<p>Метод find() возвращает значение первого найденного
+            `Метод find() возвращает значение первого найденного
             в массиве элемента, которое удовлетворяет условию переданному 
-            в callback функции. В противном случае возвращается undefined.</p>
-            `
+            в callback функции. В противном случае возвращается undefined.`
         ]
     },
     {
         name: 'Findindex',
         image: 'img/findindex.jpg',
         description: [
-            `<p>Метод findIndex() возвращает индекс в массиве, 
+            `Метод findIndex() возвращает индекс в массиве, 
             если элемент удовлетворяет условию проверяющей 
-            функции. В противном случае возвращается -1.</p>
-            `
+            функции. В противном случае возвращается -1.`
         ]
     },
     {
         name: 'forEach',
         image: 'img/foreach.jpg',
         description: [
-            `<p>Метод forEach() выполняет указанную функцию
-             один раз для каждого элемента в массиве.</p>
-            `
+            `Метод forEach() выполняет указанную функцию
+             один раз для каждого элемента в массиве.`
         ]
     },
     {
         name: 'Includes',
         image: 'img/includes.jpg',
         description: [
-            `<p>Метод includes() определяет, содержит
+            `Метод includes() определяет, содержит
             ли массив определённый элемент, возвращая в 
-            зависимости от этого true или false.</p>
-            `
+            зависимости от этого true или false.`
         ]
     },
     {
         name: 'Join',
         image: 'img/join.jpg',
         description: [
-            `<p>Метод join() объединяет все элементы 
-            массива (или массивоподобного объекта) в строку.</p>
-            `
+            `Метод join() объединяет все элементы 
+            массива (или массивоподобного объекта) в строку.`
         ]
     },
     {
         name: 'Map',
         image: 'img/map.jpg',
         description: [
-            `<p>Метод map() создаёт новый массив с результатом 
-            вызова указанной функции для каждого элемента массива.</p>
-            <p>Метод map() не изменяет данный массив, создает копию</p>
-            `
+            `Метод map() создаёт новый массив с результатом 
+            вызова указанной функции для каждого элемента массива.`,
+            `Метод map() не изменяет данный массив, создает копию.`
         ]
     },
     {
         name: 'Push',
         image: 'img/push.jpg',
         description: [
-            `<p>Метод push() добавляет один или более элементов 
-            в конец массива и возвращает новую длину массива.</p>
-            `
+            `Метод push() добавляет один или более элементов 
+            в конец массива и возвращает новую длину массива.`
         ]
     },
     {
         name: 'Reverse',
         image: 'img/reverse.jpg',
         description: [
-            `<p>Метод reverse() на месте обращает порядок следования
+            `Метод reverse() на месте обращает порядок следования
             элементов массива. Первый элемент массива становится
-            последним, а последний — первым.</p>
-            `
+            последним, а последний — первым.`
         ]
     },
     {
         name: 'Slice',
         image: 'img/slice.jpg',
         description: [
-            `<p>Метод slice() возвращает новый массив, 
-            содержащий копию части исходного массива.</p>
-            <p>Метод slice() не изменяет данный массив, создает копию</p>
-            `
+            `Метод slice() возвращает новый массив, 
+            содержащий копию части исходного массива.`,
+            `Метод slice() не изменяет данный массив, создает копию.`
         ]
     },
     {
         name: 'Some',
         image: 'img/some.jpg',
         description: [
-            `<p>Метод some() проверяет, удовлетворяет ли 
+            `Метод some() проверяет, удовлетворяет ли 
             какой-либо элемент массива условию, заданному 
-            в передаваемой функции.</p>
-            `
+            в передаваемой функции.`
         ]
     }
 ];
 
-let main = document.querySelector('.main');
-let navList = document.querySelector('.nav__list');
-let content = document.querySelector('.content');
+const main = document.querySelector('.main');
+const navList = document.querySelector('.nav__list');
+const content = document.querySelector('.content');
 
-document.addEventListener('load', nav());
+document.addEventListener('DOMContentLoaded', contentDisplay);
 
-function nav() {
-    data.forEach((item) =>{
+function contentDisplay() {
+    dataList.map((item) =>{
         navList.innerHTML += `<li class='nav__item'>${item.name}</li>`;
-    });
+    })
     main.prepend(navList);
 }
 
 navList.onclick = function(event){
     let btns= document.querySelectorAll('.nav__item');
-    data.forEach((item) => {
+    dataList.map((item) => {
         if (event.target.innerHTML === item.name) { 
             content.innerHTML = `
                 <header class="main__header">
@@ -171,9 +156,11 @@ navList.onclick = function(event){
                 </header>
                 <article class="main__content">
                     <img src="${item.image}" alt="${item.name}">
-                    <p>${item.description}</p>
                 </article>
             `;
+            item.description.map((text) => {
+                document.querySelector('.main__content').innerHTML += `<p>${text}</p>`;
+            });
         } 
     });
     btns.forEach((item) =>{
@@ -183,4 +170,5 @@ navList.onclick = function(event){
             item.classList.remove('active'); 
         }
     });
+    
 };
