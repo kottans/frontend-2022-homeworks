a673c68bedab7a5f60f860378029c332d3b7c12d
unknown <dikun.yulia@gmail.com>
2022-08-25 19:13:45 +0300

    change const to uppercase, use original inheritance pattern, use new keyword

--- submissions/dikun/frogger-game/app.js
+++ submissions/dikun/frogger-game/app.js
@@ -1,113 +1,103 @@
 "use srtict";
 
-const cell = {
+const CELL = {
 	width: 100,
 	height: 85
 };
 
-const gameField = {
+const GAME_FIELD = {
 	top: 0,
-	right: cell.width * 5,
-	bottom: cell.height * 5,
+	right: CELL.width * 5,
+	bottom: CELL.height * 5,
 	left: 0
 };
 
-const playerStart = {
+const PLAYER_START = {
 	x: 202,
 	y: 400
 };
 
-const enemySpeed = {
+const ENEMY_SPEED = {
 	min: 90,
 	max: 250 
 }; 
 
-const sprite = {
+const SPRITE = {
 	enemy: 'images/enemy-bug.png',
 	player: 'images/char-boy.png'
 };
 
-const speed = () => enemySpeed.min + Math.floor(Math.random() * enemySpeed.max);
-const crash = 70;
-const border = 20;
+const SPEED = () => ENEMY_SPEED.min + Math.floor(Math.random() * ENEMY_SPEED.max);
+const OVERLAP_AMOUNT = 70;
+const BORDER = 20;
 
-const Enemy = function (x, y, speed, player) {
+const Enemy = function (x, y, SPEED, player) {
 	this.x = x;
 	this.y = y;
-	this.speed = speed();
+	this.SPEED = SPEED();
 	this.player = player;
-	this.sprite = sprite.enemy;	
+	this.SPRITE = SPRITE.enemy;	
+};
 
-	this.checkColission = function () {
-		if(this.player.x + crash > this.x &&
-			this.player.x < this.x + crash &&
-			this.player.y + crash > this.y &&
-			this.player.y < this.y + crash) {
-			this.player.x = playerStart.x;
-			this.player.y = playerStart.y;
+Enemy.prototype.checkColission = function () {
+	if(this.player.x + OVERLAP_AMOUNT > this.x &&
+	this.player.x < this.x + OVERLAP_AMOUNT &&
+	this.player.y + OVERLAP_AMOUNT > this.y &&
+	this.player.y < this.y + OVERLAP_AMOUNT) {
+	this.player.x = PLAYER_START.x;
+	this.player.y = PLAYER_START.y;
+	}
+};
+
+Enemy.prototype.update = function (dt) {
+	if (this.x > GAME_FIELD.right) {
+		this.x -= GAME_FIELD.right;
+		this.SPEED = SPEED();
 		}
-	};
+	this.x += this.SPEED * dt;
+	this.checkColission();
+};
 
-	this.update = function (dt) {
-		if (this.x > gameField.right) {
-			this.x -= gameField.right;
-			this.speed = speed();
-			}
-		this.x += this.speed * dt;
-		this.checkColission();
-	};
-
-	this.render = function () {ctx.drawImage(Resources.get(this.sprite), this.x, this.y);};
+Enemy.prototype.render = function () {
+	ctx.drawImage(Resources.get(this.SPRITE), this.x, this.y);
 };
 
 const Player = function () {
-	this.x = playerStart.x;
-	this.y = playerStart.y;
-	this.sprite = sprite.player;	
+	this.x = PLAYER_START.x;
+	this.y = PLAYER_START.y;
+	this.SPRITE = SPRITE.player;	
+};
 
-	this.update = function () {
-		if (this.y  < gameField.top) {			
-		his.x = playerStart.x;
-		this.y = playerStart.y;	
+Player.prototype.update = function (dt) {};
+
+Player.prototype.handleInput = function (keyPress) {
+	if (keyPress == 'left' && this.x > GAME_FIELD.left + CELL.width) {
+		this.x -= CELL.width;
+		}
+	if (keyPress == 'right' && this.x < GAME_FIELD.right - CELL.width) {
+		this.x += CELL.width;
+	}
+	if (keyPress == 'up' && this.y > GAME_FIELD.top) {
+		this.y -= CELL.height;
+	}
+	if (keyPress == 'down' && this.y < GAME_FIELD.bottom - CELL.height) {
+		this.y += CELL.height;
+	}	
+	else if (this.y  < GAME_FIELD.top) {			
+		this.x = PLAYER_START.x;
+		this.y = PLAYER_START.y;	
 		alert('You win!');	
-	}};
-
-	this.render = function () {ctx.drawImage(Resources.get(this.sprite), this.x, this.y);};
-
-	this.handleInput = function (keyPress) {
-		if (keyPress == 'left' && this.x > gameField.left + cell.width) {
-			this.x -= cell.width;
-			}
-		if (keyPress == 'right' && this.x < gameField.right - cell.width) {
-			this.x += cell.width;
-		}
-		if (keyPress == 'up' && this.y > gameField.top) {
-			this.y -= cell.height;
-		}
-		if (keyPress == 'down' && this.y < gameField.bottom - cell.height) {
-			this.y += cell.height;
-		}	
-		if (this.y  < gameField.top) {			
-			this.x = playerStart.x;
-			this.y = playerStart.y;	
-			alert('You win!');	
-		}			
-	};
+	}		
 };
 
-Function.prototype.construct = function (position) {
-	let gameEnteties = Object.create(this.prototype);
-	this.apply(gameEnteties, position);
-	return gameEnteties;
+Player.prototype.render = function () {
+	ctx.drawImage(Resources.get(this.SPRITE), this.x, this.y);
 };
 
-const playerPosition = [playerStart.x, playerStart.y];
-const player = Player.construct(playerPosition);
+const player = new Player(PLAYER_START.x, PLAYER_START.y);
 
-const allEnemies = [cell.height - border, cell.height*2 - border, cell.height*3 - border].map((value) => {
-	const enemyPosition = [gameField.left, value, speed, player];
-	return Enemy.construct(enemyPosition);
-});
+const allEnemies = [CELL.height - BORDER, CELL.height*2 - BORDER, CELL.height*3 - BORDER]
+				   .map(value => {return new Enemy(GAME_FIELD.left, value, SPEED, player);});
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
