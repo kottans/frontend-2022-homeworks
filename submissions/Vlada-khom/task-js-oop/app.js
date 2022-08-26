57bc3eff6007cfe692681973e331d1c0c88a0697
Vlada-khom <vvlada2610@gmail.com>
2022-08-26 17:38:13 +0300

    add changes

--- submissions/Vlada-khom/task-js-oop/app.js
+++ submissions/Vlada-khom/task-js-oop/app.js
@@ -1,92 +1,100 @@
-const xStartPlayer = 200;
-const yStartPlayer = 400;
-const yLineforVictory = 0;
-const cell = {
+const X_START_PLAYER = 200;
+const Y_START_PLAYER = 400;
+const Y_LINE_VICTORY = 0;
+const CELL = {
   width: 100,
-  height: 90,
+  height: 85,
 };
-const visibleBorder = {
+const VISIBLE_BORDER = {
   width: 500,
   height: 400,
 };
 
-const enemySpeed = {
+const ENEMY_SPEED = {
   minSpeed: 60,
   maxSpeed: 200,
 };
 
-let counter = document.createElement("div");
+const ROW_FOR_ENEMIES = {
+  one: 230,
+  two: 145,
+  three: 60
+}
+
 let count = 0;
-counter.innerHTML = `Counter: ${count}`;
+let counter = document.createElement("div");
 document.body.append(counter);
 
-const Player = function (x, y) {
+const Player = function (x, y, count) {
   this.x = x;
   this.y = y;
   this.sprite = "images/char-boy.png";
+  this.count = count;
 };
 
 Player.prototype.update = function () {
-  if (this.y < yLineforVictory) {
+  if (this.y < Y_LINE_VICTORY) {
     alert("You WINNER!");
-    this.x = xStartPlayer;
-    this.y = yStartPlayer;
+    this.x = X_START_PLAYER;
+    this.y = Y_START_PLAYER;
     allEnemies.forEach((el) => (el.xMove = el.x));
-    count++;
-    counter.innerHTML = `Counter: ${count}`;
+    this.count++;
+    counter.innerHTML = `Counter: ${this.count}`;
   }
 };
 
 Player.prototype.render = function () {
+  counter.innerHTML = `Counter: ${this.count}`;
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 Player.prototype.handleInput = function (key) {
   if (key === "left" && this.x > 0) {
-    this.x -= 100;
+    this.x -= CELL.width;
   }
-  if (key === "right" && this.x < visibleBorder.width - cell.width) {
-    this.x += 100;
+  if (key === "right" && this.x < VISIBLE_BORDER.width - CELL.width) {
+    this.x += CELL.width;
   }
   if (key === "up" && this.y > 0) {
-    this.y -= 85;
+    this.y -= CELL.height;
   }
-  if (key === "down" && this.y < visibleBorder.height) {
-    this.y += 85;
+  if (key === "down" && this.y < VISIBLE_BORDER.height) {
+    this.y += CELL.height;
   }
 };
 
-const Enemy = function (x, y, speed) {
+const Enemy = function (x, y, speed, player) {
   this.x = x;
   this.xMove = this.x;
   this.y = y;
   this.sprite = "images/enemy-bug.png";
   this.speed = speed;
+  this.player = player;
 };
 
-let player = new Player(xStartPlayer, yStartPlayer);
+let player = new Player(X_START_PLAYER, Y_START_PLAYER, count);
 
 Enemy.prototype.update = function (dt) {
   this.xMove += this.speed * dt;
 
-  if (this.xMove > visibleBorder.width) {
-    this.xMove = this.xMove - visibleBorder.width - cell.width;
-    this.speed = this.randomSpeed(enemySpeed.minSpeed, enemySpeed.maxSpeed);
+  if (this.xMove > VISIBLE_BORDER.width) {
+    this.xMove = this.xMove - VISIBLE_BORDER.width - CELL.width;
+    this.speed = this.randomSpeed(ENEMY_SPEED.minSpeed, ENEMY_SPEED.maxSpeed);
   }
-  this.collision(player);
+  this.collision();
 };
 
-Enemy.prototype.collision = function (player) {
+Enemy.prototype.collision = function () {
   if (
-    player.x < this.xMove + cell.width &&
-    this.xMove - cell.width < player.x &&
-    this.y + cell.height / 2 > player.y &&
-    this.y - cell.height / 2 < player.y
+    this.player.x < this.xMove + CELL.width &&
+    this.xMove - CELL.width < this.player.x &&
+    this.y + CELL.height / 2 > this.player.y &&
+    this.y - CELL.height / 2 < this.player.y
   ) {
     alert("Game over!");
     this.xMove = this.x;
-    player.x = xStartPlayer;
-    player.y = yStartPlayer;
+    this.player.x = X_START_PLAYER;
+    this.player.y = Y_START_PLAYER;
   }
 };
 
@@ -104,8 +112,8 @@ function randomX(min = -150, max = 0) {
   return Math.floor(randomX);
 }
 
-let allEnemies = [230, 145, 60].map(
-  (enemyLine) => new Enemy(randomX(), enemyLine, enemySpeed.minSpeed)
+let allEnemies = [ROW_FOR_ENEMIES.one, ROW_FOR_ENEMIES.two, ROW_FOR_ENEMIES.three].map(
+  (enemyLine) => new Enemy(randomX(), enemyLine, ENEMY_SPEED.minSpeed, player)
 );
 
 document.addEventListener("keyup", function (e) {
