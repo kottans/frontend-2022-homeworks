8cc66e939c0261c6fa7b8457ff6a2728e143ab53
LuckyDnepr <LuckyDnepr@gmail.com>
2022-08-26 22:51:33 +0300

    Add changes based on comments from @OleksiyRudenko

--- submissions/LuckyDnepr/task-js-oop/app.js
+++ submissions/LuckyDnepr/task-js-oop/app.js
@@ -1,77 +1,101 @@
 "use strict";
 
-const blockWidth = 101, //gamefield sizes
-    blockHeight = 83,
-    nBlocksWidth = 5,
-    nBlocksHeight = 6,
-    fieldWidth = blockWidth * nBlocksWidth,
-    fieldHeight = blockHeight * nBlocksHeight,
-    enemyTracksHeight = [62, 145, 228], //coordinates of enemy tracks
-    playerStartPoint = [200, 405],
+const widthOfBlock = 101,
+    heightOfBlock = 83,
+    blocksByWidth = 5,
+    blocksByHeight = 6,
+    paddingEnemyAvatar = {
+        bottom: 20,
+        left: 0,
+        right: 0
+    },
+    paddingPlayerAvatar = {
+        bottom: 10,
+        left: 20,
+        right: 15,
+    },
+    widthOfField = widthOfBlock * blocksByWidth,
+    heightOfField = heightOfBlock * blocksByHeight,
+    enemyTracksYCoord = [
+        heightOfBlock - paddingEnemyAvatar.bottom,
+        2 * heightOfBlock - paddingEnemyAvatar.bottom,
+        3 * heightOfBlock - paddingEnemyAvatar.bottom,
+    ],
+    enemyBasicSpeed = 350,
+    playersStartPoint = [
+        Math.floor(blocksByWidth / 2) * widthOfBlock,
+        (blocksByHeight - 1) * heightOfBlock - paddingPlayerAvatar.bottom,
+    ],
     hardestLevel = 4;
 
 let winPoints = 0,
-failPoints = 0;
+    failPoints = 0;
 
 class Unit {
-    constructor (coordX, coordY, avatar) {
-        this.x = coordX,
-        this.y = coordY;
+    constructor(coordX, coordY, avatar) {
+        (this.x = coordX), (this.y = coordY);
         this.sprite = avatar;
     }
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
     update() {}
-    setAvatar (avatar) {
+    setAvatar(avatar) {
         this.sprite = avatar;
     }
 }
 
 class Enemy extends Unit {
-    constructor (coordX, coordY, startSpeed, avatar) {
+    constructor(coordX, coordY, startSpeed, avatar) {
         super(coordX, coordY, avatar);
         this.speed = startSpeed;
     }
     update(dt) {
         this.x += this.speed * dt;
-        this.moveEnemyToStart ();
+        this.moveToStart();
         this.checkCollisions();
     }
-    checkCollisions () {
-        if (this.x > player.x - blockWidth + 20 && //+20 - correction for avatar, for clearer collision
-            this.x < player.x + blockWidth - 15 && //-15 - correction for avatar, for clearer collision
+    checkCollisions() {
+        if (
+            this.x - paddingEnemyAvatar.left > player.x - widthOfBlock + paddingPlayerAvatar.left &&
+            this.x + paddingEnemyAvatar.right < player.x + widthOfBlock - paddingPlayerAvatar.right &&
             this.y < player.y &&
-            this.y + blockHeight > player.y) {
-            endGame(false); //false - you lose the game
+            this.y + heightOfBlock > player.y
+        ) {
+            endGame("fail");
         }
     }
-    moveEnemyToStart () {
-        if (this.x > fieldWidth + blockWidth) {
-            this.x = Math.random() * -300; //change enemy start point before new appearance
-            this.speed = Math.random() * 150 + Math.random() * 150; //change enemy speed before new appearance
+    moveToStart() {
+        if (this.x > widthOfField + widthOfBlock) {
+            this.x = -Math.random() * (3 * widthOfBlock);
+            this.speed = Math.random() * enemyBasicSpeed;
         }
     }
 }
 
 class Player extends Unit {
-    constructor (coordX, coordY, avatar) {
+    constructor(coordX, coordY, avatar) {
         super(coordX, coordY, avatar);
-        this._freeze = true; //the player does not respond to the keyboard if _freeze === true
+        this._isFrozen = true;
     }
-    freezing() {
-        this._freeze = true; //player freezed until the win/lose banner showing
+    freeze() {
+        this._isFrozen = true;
     }
-    unfreezing() {
-        this._freeze = false; //player unfreezed when gameplay start
+    unfreeze() {
+        this._isFrozen = false;
     }
-    movePlayerToStart() { //randomize player start position after win/lose
-        this.x = Math.floor(Math.random() * 4) * blockWidth;
-        this.y = (Math.random() > 0.5) ? (blockHeight * 5 - 10) : (blockHeight * 4 - 10);
+    moveToStart() {
+        this.x = Math.floor(Math.random() * (blocksByWidth - 1)) * widthOfBlock;
+        this.y =
+            Math.random() > 0.5
+                ? heightOfBlock * (blocksByHeight - 1) -
+                  paddingPlayerAvatar.bottom
+                : heightOfBlock * (blocksByHeight - 2) -
+                  paddingPlayerAvatar.bottom;
     }
     handleInput(key) {
-        if (!this._freeze) {
-            switch (key) { //choose direction depending on the key pressed
+        if (!this._isFrozen) {
+            switch (key) {
                 case "ArrowLeft":
                     this.stepLeft();
                     break;
@@ -87,87 +111,91 @@ class Player extends Unit {
                 default:
                     break;
             }
-            this.checkWin(); //check win after move player
+            this.checkWin();
         }
     }
-    stepLeft () { //player do step left
+    stepLeft() {
         if (this.x > 0) {
-            this.x -= blockWidth;
+            this.x -= widthOfBlock;
         }
     }
-    stepRight () { //player do step right
-        if (this.x < fieldWidth - 1.5 * blockWidth) {
-            this.x += blockWidth;
+    stepRight() {
+        if (this.x < widthOfField - 1.5 * widthOfBlock) {
+            this.x += widthOfBlock;
         }
     }
-    stepUp () { //player do step up
+    stepUp() {
         if (this.y > 0) {
-            this.y -= blockHeight;
+            this.y -= heightOfBlock;
         }
     }
-    stepDown () { //player do step down
-        if (this.y < fieldHeight - 1.5 * blockHeight) {
-            this.y += blockHeight;
+    stepDown() {
+        if (this.y < heightOfField - 1.5 * heightOfBlock) {
+            this.y += heightOfBlock;
         }
     }
-    checkWin () { //player on the water => you win
-        if (this.y < 0) { 
-            this._freeze = true;
-            endGame(true); //win the game
+    checkWin() {
+        if (this.y < 0) {
+            this._isFrozen = true;
+            endGame("win");
         }
     }
 }
 
-function endGame(win) { //end gameset
-    if (win) {
+function endGame(status) {
+    if (status === "win") {
         winPoints += 1;
         setTimeout(() => restartGame(true), 400);
         updatePoints();
-    } else {
+    } else if (status === "fail") {
         failPoints += 1;
         setTimeout(() => restartGame(false), 0);
         updatePoints();
     }
 }
 
-function restartGame(winLose) { //restart gameset - winLose - true->win; false->fail
-    winLoseBanners (winLose);
-    player.freezing();
-    player.movePlayerToStart();
+function restartGame(winOrLose) {
+    showWinOrLoseBanners(winOrLose);
+    player.freeze();
+    player.moveToStart();
     setTimeout(() => {
-        winLoseBanners ();
-        player.unfreezing();
+        showWinOrLoseBanners();
+        player.unfreeze();
     }, 750);
 }
 
-function generateEnemies(n) { //n - enemies per track - this is equivalent difficulty
+function generateAllEnemies(difficultyLevel) {
     let enemies = [];
-    for (let i = 1; i <= n; i++) {
-        enemies
-            .push(...enemyTracksHeight
-                .map((trackHeight) => {
-                    return new Enemy(Math.random() * -300, trackHeight, Math.random() * 350); //randomise start point and speed
-                })
-            );
+    for (let i = 1; i <= difficultyLevel; i++) {
+        enemies.push(
+            ...enemyTracksYCoord.map(
+                (trackYCoord) =>
+                    new Enemy(
+                        -Math.random() * (3 * widthOfBlock),
+                        trackYCoord,
+                        Math.random() * enemyBasicSpeed
+                    )
+            )
+        );
     }
     return enemies;
 }
 
-function updatePoints () { //update win/lode points
+function updatePoints() {
     document.querySelector("#win-points").innerHTML = winPoints;
     document.querySelector("#fail-points").innerHTML = failPoints;
 }
 
-function winLoseBanners (winLose = -1) { //show/hide win/lose banners
-    if (winLose == -1) {
+function showWinOrLoseBanners(winOrLose = -1) {
+    if (winOrLose == -1) {
         document.querySelector(".win").classList.remove("show");
         document.querySelector(".lose").classList.remove("show");
     } else {
-        (winLose)
+        winOrLose
             ? document.querySelector(".win").classList.add("show")
             : document.querySelector(".lose").classList.add("show");
     }
 }
 
-var allEnemies = generateEnemies(hardestLevel), //"4" - the hardest level (12 enemies)
-player = new Player(...playerStartPoint); //create player
+var allEnemies = generateAllEnemies(hardestLevel),
+    player = new Player(...playersStartPoint);
