2ab07f780b952516bae2a4a23ef0d2668ef397e0
Tomash <virelesez@gmail.com>
2022-08-26 23:16:40 +0300

    Add some files

---
+++ submissions/EnF1NeeT/DOM-task/script.js
@@ -0,0 +1,156 @@
+const postsLst = [
+    {
+        id: '1',
+        topzoneImg:'./img/2.png',
+        p:'@Jonnie',
+        imgSrc: './img/post1.jpg',
+        imgSrc2: './img/post2.jpg',
+        imgSrc3: './img/post3.jpg',
+        imgSrc4: './img/post4.jpg'
+    },
+    {
+        id: '2',
+        topzoneImg:'./img/3.png',
+        p:'@Mike',
+        imgSrc: './img/post5.jpg',
+        imgSrc2: './img/post6.jpg',
+        imgSrc3: './img/post7.jpg',
+        imgSrc4: './img/post8.jpg'
+    },
+    {
+        id: '3',
+        topzoneImg:'./img/1.png',
+        p:'@Kate',
+        imgSrc: './img/post9.jpg',
+        imgSrc2: './img/post10.jpg',
+        imgSrc3: './img/post11.jpg',
+        imgSrc4: './img/post12.jpg'
+    },
+    {
+        id: '4',
+        topzoneImg:'./img/4.png',
+        p:'@Jack',
+        imgSrc: './img/post13.jpg',
+        imgSrc2: './img/post14.jpg',
+        imgSrc3: './img/post15.jpg',
+        imgSrc4: './img/post16.jpg'
+    }
+
+];
+
+const btnContainer = document.querySelector('.friends');
+const mainContainer = document.querySelector('.posts');
+
+btnContainer.addEventListener('click', addContent);
+
+function addContent({ target }) {
+    if (target.id) {
+        const contant = postsLst.find((pos) => pos.id === target.id);
+        mainContainer.innerHTML = post(contant);
+    }
+};
+
+function post({ imgSrc, imgSrc2, imgSrc3, imgSrc4, p, topzoneImg}) {
+
+    return (
+        `<article class="post">
+            <div class="topzone">
+                <img class="topzone-img" src="${topzoneImg}" alt="post-img">
+                <p>${p}</p>
+            </div>
+            <div class="post-img">
+                <img src="${imgSrc}" class="post-img" alt="post-img">
+            </div>
+            <div class="action-zone">
+                <button class="btn">
+                    <img src="./img/favorite_FILL0_wght400_GRAD0_opsz48.png" alt="" class="btn-img">
+                    Like
+                </button>
+                <button class="btn">
+                    <img src="./img/forum_FILL0_wght400_GRAD0_opsz48.png" alt="" class="btn-img">
+                    Comment
+                </button>
+                <button class="btn">
+                    <img src="./img/send_FILL0_wght400_GRAD0_opsz48.png" alt="" class="btn-img">
+                    Share
+                </button>
+            </div>
+        <div class="post-time">posted <time datetime="24.5.3222">posted 5h ago</time></div>
+        </article>
+        <article class="post">
+            <div class="topzone">
+                <img class="topzone-img" src="${topzoneImg}" alt="post-img">
+                <p>${p}</p>
+            </div>
+            <div class="post-img">
+                <img src="${imgSrc2}" class="post-img" alt="post-img">
+            </div>
+            <div class="action-zone">
+                <button class="btn">
+                    <img src="./img/favorite_FILL0_wght400_GRAD0_opsz48.png" alt="" class="btn-img">
+                    Like
+                </button>
+                <button class="btn">
+                    <img src="./img/forum_FILL0_wght400_GRAD0_opsz48.png" alt="" class="btn-img">
+                    Comment
+                </button>
+                <button class="btn">
+                    <img src="./img/send_FILL0_wght400_GRAD0_opsz48.png" alt="" class="btn-img">
+                    Share
+                </button>
+            </div>
+        <div class="post-time">posted <time datetime="24.5.3222">posted 5h ago</time></div>
+        </article>
+        <article class="post">
+            <div class="topzone">
+                <img class="topzone-img" src="${topzoneImg}" alt="post-img">
+                <p>${p}</p>
+            </div>
+            <div class="post-img">
+                <img src="${imgSrc3}" class="post-img" alt="post-img">
+            </div>
+            <div class="action-zone">
+                <button class="btn">
+                    <img src="./img/favorite_FILL0_wght400_GRAD0_opsz48.png" alt="" class="btn-img">
+                    Like
+                </button>
+                <button class="btn">
+                    <img src="./img/forum_FILL0_wght400_GRAD0_opsz48.png" alt="" class="btn-img">
+                    Comment
+                </button>
+                <button class="btn">
+                    <img src="./img/send_FILL0_wght400_GRAD0_opsz48.png" alt="" class="btn-img">
+                    Share
+                </button>
+            </div>
+        <div class="post-time">posted <time datetime="24.5.3222">posted 5h ago</time></div>
+        </article>
+        </article>
+        <article class="post">
+            <div class="topzone">
+                <img class="topzone-img" src="${topzoneImg}" alt="post-img">
+                <p>${p}</p>
+            </div>
+            <div class="post-img">
+                <img src="${imgSrc4}" class="post-img" alt="post-img">
+            </div>
+            <div class="action-zone">
+                <button class="btn">
+                    <img src="./img/favorite_FILL0_wght400_GRAD0_opsz48.png" alt="" class="btn-img">
+                    Like
+                </button>
+                <button class="btn">
+                    <img src="./img/forum_FILL0_wght400_GRAD0_opsz48.png" alt="" class="btn-img">
+                    Comment
+                </button>
+                <button class="btn">
+                    <img src="./img/send_FILL0_wght400_GRAD0_opsz48.png" alt="" class="btn-img">
+                    Share
+                </button>
+            </div>
+        <div class="post-time">posted <time datetime="24.5.3222">posted 5h ago</time></div>
+        </article>`
+        
+    );
+};
+
