bbcc04909fba956f5549771b58bc582cf23a1169
unknown <dikun.yulia@gmail.com>
2022-08-26 22:21:27 +0300

    change cell prop, create enemies using map

--- submissions/dikun/frogger-game/app.js
+++ submissions/dikun/frogger-game/app.js
@@ -1,20 +1,20 @@
 "use srtict";
 
 const CELL = {
-	width: 100,
-	height: 85
+	width: 101,
+	height: 80
 };
 
 const GAME_FIELD = {
 	top: 0,
 	right: CELL.width * 5,
-	bottom: CELL.height * 5,
+	bottom: CELL.height * 6,
 	left: 0
 };
 
 const PLAYER_START = {
-	x: CELL.width * 2.02,
-	y: CELL.height * 4.7
+	x: CELL.width * 2,
+	y: CELL.height * 5 
 };
 
 const ENEMY_SPEED = {
@@ -30,12 +30,6 @@ const SPRITE = {
 const speed = () => ENEMY_SPEED.min + Math.floor(Math.random() * ENEMY_SPEED.max);
 const OVERLAP_AMOUNT = 70;
 const BORDER = 20;
-
-const LINE = {
-	1: CELL.height - BORDER,
-	2: CELL.height * 2 - BORDER,
-	3: CELL.height * 3- BORDER
-};
 
 const Enemy = function (x, y, speed, player) {
 	this.x = x;
@@ -77,7 +71,7 @@ const Player = function () {
 Player.prototype.update = function (dt) {};
 
 Player.prototype.handleInput = function (keyPress) {
-	if (keyPress == 'left' && this.x > GAME_FIELD.left + CELL.width) {
+	if (keyPress == 'left' && this.x > GAME_FIELD.left) {
 		this.x -= CELL.width;
 		}
 	if (keyPress == 'right' && this.x < GAME_FIELD.right - CELL.width) {
@@ -89,7 +83,7 @@ Player.prototype.handleInput = function (keyPress) {
 	if (keyPress == 'down' && this.y < GAME_FIELD.bottom - CELL.height) {
 		this.y += CELL.height;
 	}	
-	else if (this.y  < GAME_FIELD.top) {			
+	else if (this.y  < GAME_FIELD.top + CELL.height) {			
 		this.x = PLAYER_START.x;
 		this.y = PLAYER_START.y;	
 		alert('You win!');	
@@ -102,17 +96,9 @@ Player.prototype.render = function () {
 
 const player = new Player(PLAYER_START.x, PLAYER_START.y);
 
-const allEnemies = [];
-
-function createEnemyPerLine (line, numberOfEnemies) {	
-	for (let i = 0; i < numberOfEnemies; i++) {		
-		allEnemies.push(new Enemy((GAME_FIELD.left - CELL.width), LINE[line], speed, player));		
-	}
-}
-
-createEnemyPerLine(1, 1);
-createEnemyPerLine(2, 2);
-createEnemyPerLine(3, 1);
+const allEnemies = [1, 2, 2, 3].
+				   map(trackNumber => CELL.height*trackNumber - BORDER).
+				   map(enemyPositionY => new Enemy((GAME_FIELD.left - CELL.width), enemyPositionY, speed, player));
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
