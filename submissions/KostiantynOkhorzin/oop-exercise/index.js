f432fa3dbe02c85af6e1af816b4a8da45bf7b687
Kostiantyn <konstantinokhorzin@gmail.com>
2022-08-26 17:26:46 +0300

    added classes Dog and Cat

--- submissions/KostiantynOkhorzin/oop-exercise/index.js
+++ submissions/KostiantynOkhorzin/oop-exercise/index.js
@@ -1,57 +1,44 @@
 
 class Inhabitan {
-   constructor(species, name, gender, saying) {
+   constructor(species, name, gender, legs, saying) {
       this.species = species;
       this.name = name;
       this.gender = gender;
+      this.legs = legs;
       this.saying = saying;
-      this.keys = ['species', 'name', 'gender', 'saying'];
    }
 
    showProps() {
-      return ['species', 'name', 'gender', 'saying'].map(key => this[key]).join('; ');
+      return ['species', 'name', 'gender', 'legs', 'saying'].map(key => this[key]).join('; ');
    }
 };
 
 class Human extends Inhabitan {
    constructor(name, gender, saying, legs = 2, hands = 2) {
-      super('human', name, gender, saying);
-      this.legs = legs;
+      super('human', name, gender, legs, saying);
       this.hands = hands;
    }
 
-   connectsProps() {
-      const props = super.showProps().split('; ');
-      const propsStart = props.slice(0, props.indexOf('gender'));
-      const propsEnd = props.slice(props.indexOf('gender'));
-      return [...propsStart, this.legs, this.hands, ...propsEnd].join('; ');
-   }
-
    showProps() {
-      return this.connectsProps();
+      return `${super.showProps()} ${this.hands};`
    }
 };
 
-class Animal extends Inhabitan {
-   constructor(species, name, gender, saying, legs = 4) {
-      super(species, name, gender, saying);
+class Dog extends Inhabitan {
+   constructor(name, gender, saying, legs = 4) {
+      super('dog', name, gender, saying);
       this.legs = legs;
    }
+};
 
-   connectsProps() {
-      const props = super.showProps().split('; ');
-      const propsStart = props.slice(0, props.indexOf('gender'));
-      const propsEnd = props.slice(props.indexOf('gender'));
-      return [...propsStart, this.legs, ...propsEnd].join('; ');
-   }
-
-   showProps() {
-      return this.connectsProps();
+class Cat extends Inhabitan {
+   constructor(name, gender, saying, legs = 4) {
+      super('cat', name, gender, legs, saying);
    }
 };
 
-const dog = new Animal('dog', 'Toby', 'male', 'woof-woof!');
-const cat = new Animal('cat', 'Tom', 'male', 'hello Jerry!');
+const dog = new Dog('Toby', 'male', 'woof-woof!');
+const cat = new Cat('Tom', 'male', 'hello Jerry!');
 const woman = new Human('Monica', 'female', 'hello Joe!');
 const man = new Human('Joe', 'male', 'one hundred himars to these brave Ukrainians!');
 
