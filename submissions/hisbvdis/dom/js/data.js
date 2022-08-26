5408ec939b467f5b2f515a5e41fb70633b2c95fe
hisbvdis <hisbvdis@gmail.com>
2022-08-26 14:33:33 +0300

    Task 'DOM'

---
+++ submissions/hisbvdis/dom/js/data.js
@@ -0,0 +1,75 @@
+export const data = [
+  {
+    id: 0,
+    name: "Mercury",
+    imgUrl: "mercury.jpg",
+    url: "mercury",
+    desc: [
+      "Mercury (0.4 AU (60 million km; 37 million mi) from the Sun) is the closest planet to the Sun. The smallest planet in the Solar System (0.055 MEarth), Mercury has no natural satellites. The dominant geological features are impact craters or basins with ejecta blankets, the remains of early volcanic activity including magma flows, and lobed ridges or rupes that were probably produced by a period of contraction early in the planet's history. Mercury's very tenuous atmosphere consists of solar-wind particles trapped by Mercury's magnetic field, as well as atoms blasted off its surface by the solar wind. Its relatively large iron core and thin mantle have not yet been adequately explained. Hypotheses include that its outer layers were stripped off by a giant impact, or that it was prevented from fully accreting by the young Sun's energy.",
+      "There have been searches for 'Vulcanoids', asteroids in stable orbits between Mercury and the Sun, but none have been discovered"
+    ]
+  },
+  {
+    id: 1,
+    name: "Venus",
+    imgUrl: "venus.jpg",
+    url: "venus",
+    desc: [
+      "Venus (0.7 AU (100 million km; 65 million mi) from the Sun) is close in size to Earth (0.815 MEarth) and, like Earth, has a thick silicate mantle around an iron core, a substantial atmosphere, and evidence of internal geological activity. It is much drier than Earth, and its atmosphere is ninety times as dense. Venus has no natural satellites. It is the hottest planet, with surface temperatures over 400 °C (752 °F), mainly due to the amount of greenhouse gases in the atmosphere. The planet has no magnetic field that would prevent depletion of its substantial atmosphere, which suggests that its atmosphere is being replenished by volcanic eruptions. A relatively young planetary surface displays extensive evidence of volcanic activity, but is devoid of plate tectonics. It may undergo resurfacing episodes on a time scale of 700 million years."
+    ]
+  },
+  {
+    id: 2,
+    name: "Earth",
+    imgUrl: "earth.jpg",
+    url: "earth",
+    desc: [
+      "Earth (1 AU (150 million km; 93 million mi) from the Sun) is the largest and densest of the inner planets, the only one known to have current geological activity, and the only place where life is known to exist. Its liquid hydrosphere is unique among the terrestrial planets, and it is the only planet where plate tectonics has been observed. Earth's atmosphere is radically different from those of the other planets, having been altered by the presence of life to contain 21% free oxygen. The planetary magnetosphere shields the surface from solar and cosmic radiation, limiting atmospheric stripping and maintaining habitability. It has one natural satellite, the Moon, the only large satellite of a terrestrial planet in the Solar System."
+    ]
+  },
+  {
+    id: 3,
+    name: "Mars",
+    imgUrl: "mars.jpg",
+    url: "mars",
+    desc: [
+      "Mars (1.5 AU (220 million km; 140 million mi) from the Sun) is smaller than Earth and Venus (0.107 MEarth). It has an atmosphere of mostly carbon dioxide with a surface pressure of 6.1 millibars (0.088 psi; 0.18 inHg); roughly 0.6% of that of Earth but sufficient to support weather phenomena. Its surface, peppered with volcanoes, such as Olympus Mons, and rift valleys, such as Valles Marineris, shows geological activity that may have persisted until as recently as 2 million years ago. Its red colour comes from iron oxide (rust) in its soil. Mars has two tiny natural satellites (Deimos and Phobos) thought to be either captured asteroids, or ejected debris from a massive impact early in Mars's history."
+    ]
+  },
+  {
+    id: 4,
+    name: "Jupiter",
+    imgUrl: "jupiter.jpg",
+    url: "jupiter",
+    desc: [
+      "Jupiter (5.2 AU (780 million km; 480 million mi) from the Sun), at 318 MEarth, is 2.5 times the mass of all the other planets put together. It is composed largely of hydrogen and helium. Jupiter's strong internal heat creates semi-permanent features in its atmosphere, such as cloud bands and the Great Red Spot. The planet possesses a 4.2–14 Gauss strength magnetosphere that spans 22–29 million km, making it, in certain respects, the largest object in the Solar System. Jupiter has 80 known satellites. The four largest, Ganymede, Callisto, Io, and Europa, are called the Galilean moons: they show similarities to the terrestrial planets, such as volcanism and internal heating. Ganymede, the largest satellite in the Solar System, is larger than Mercury; Callisto is almost as large."
+    ]
+  },
+  {
+    id: 5,
+    name: "Saturn",
+    imgUrl: "saturn.jpg",
+    url: "saturn",
+    desc: [
+      "Saturn (9.5 AU (1.42 billion km; 880 million mi) from the Sun), distinguished by its extensive ring system, has several similarities to Jupiter, such as its atmospheric composition and magnetosphere. Although Saturn has 60% of Jupiter's volume, it is less than a third as massive, at 95 MEarth. Saturn is the only planet of the Solar System that is less dense than water. The rings of Saturn are made up of small ice and rock particles. Saturn has 83 confirmed satellites composed largely of ice. Two of these, Titan and Enceladus, show signs of geological activity; they, as well as five other Saturnian moons (Iapetus, Rhea, Dione, Tethys, and Mimas), are large enough to be round. Titan, the second-largest moon in the Solar System, is bigger than Mercury and the only satellite in the Solar System to have a substantial atmosphere."
+    ]
+  },
+  {
+    id: 6,
+    name: "Uranus",
+    imgUrl: "uranus.jpg",
+    url: "uranus",
+    desc: [
+      "Uranus (19.2 AU (2.87 billion km; 1.78 billion mi) from the Sun), at 14 MEarth, has the lowest mass of the outer planets. Uniquely among the planets, it orbits the Sun on its side; its axial tilt is over ninety degrees to the ecliptic. This gives the planet extreme seasonal variation as each pole points toward and then away from the Sun. It has a much colder core than the other giant planets and radiates very little heat into space. As a consequence, it has the coldest planetary atmosphere in the Solar System. Uranus has 27 known satellites, the largest ones being Titania, Oberon, Umbriel, Ariel, and Miranda. Like the other giant planets, it possesses a ring system and magnetosphere."
+    ]
+  },
+  {
+    id: 7,
+    name: "Neptune",
+    imgUrl: "neptune.jpg",
+    url: "neptune",
+    desc: [
+      "Neptune (30.1 AU (4.50 billion km; 2.80 billion mi) from the Sun), though slightly smaller than Uranus, is more massive (17 MEarth) and hence more dense. It radiates more internal heat than Uranus, but not as much as Jupiter or Saturn. Neptune has 14 known satellites. The largest, Triton, is geologically active, with geysers of liquid nitrogen. Triton is the only large satellite with a retrograde orbit, which indicates that it did not form with Neptune, but was probably captured from the Kuiper belt. Neptune is accompanied in its orbit by several minor planets, termed Neptune trojans, that either lead or trail the planet by about one-sixth of the way around the Sun, positions known as Lagrange points."
+    ]
+  },
+]
---
+++ submissions/hisbvdis/dom/js/script.js
@@ -0,0 +1,122 @@
+import { data } from "./data.js";
+const navBtn = document.querySelector("[js-navBtn]");
+const nav = document.querySelector("[js-nav]");
+const navList = document.querySelector("[js-navList]");
+const navCloseBtn = document.querySelector("[nav-closeBtn]");
+const mainHeader = document.querySelector("[main-header]");
+const mainContent = document.querySelector("[main-content]");
+let activeLink = null;
+
+window.addEventListener("load", onWindow_Load_Handler);
+navBtn.addEventListener("click", onNavBtn_Click_Handler);
+
+
+function openNav() {
+  nav.classList.add("nav--visible");
+}
+
+
+function closeNav() {
+  nav.classList.remove("nav--visible");
+}
+
+
+function renderNav() {
+  data.forEach(({ id, name, url }) => {
+    const navItem = document.createElement("li");
+    navItem.classList.add("nav__item");
+
+    const navLink = document.createElement("a");
+    navLink.classList.add("nav__link");
+    navLink.href = "";
+    navLink.dataset.url = url;
+    navLink.dataset.navLink = true;
+    navLink.dataset.navId = id;
+    navLink.textContent = name;
+
+    navItem.append(navLink);
+    navList.append(navItem);
+  });
+
+  nav.addEventListener("click", forNavLinks_onNavList_Click_Handler);
+  navCloseBtn.addEventListener("click", onNavCloseBtn_Click_Handler);
+}
+
+
+function renderMain(planetId=null) {
+  mainContent.innerHTML = "";
+  
+  if (planetId === null) {
+    mainHeader.textContent = "Planets of Solar System";
+
+    const planetsList = document.createElement("ul");
+    planetsList.classList.add("planetsList");
+
+    data.forEach(({name, imgUrl}) => {
+      const item = document.createElement("li");
+      item.classList.add("planetsList__item");
+
+      const img = document.createElement("img");
+      img.classList.add("planetsList__img");
+      img.src = `./img/${imgUrl}`;
+      img.width = "150";
+      img.height = "150";
+      img.alt = name;
+
+      item.append(img);
+      planetsList.append(item);
+    })
+
+    mainContent.append(planetsList);
+  } 
+  
+  else {
+    const {name, imgUrl, desc} = data.find(({id}) => id === Number(planetId));
+    
+    mainHeader.textContent = name;
+
+    const planet = document.createElement("img");
+    planet.classList.add("main__planet");
+    planet.src=`./img/${imgUrl}`;
+    planet.width = "800";
+    planet.height = "400";
+    planet.alt = name;
+    mainContent.append(planet);
+
+    desc.forEach((par) => {
+      const paragraph = document.createElement("p");
+      paragraph.classList.add("main__desc");
+      paragraph.textContent = par;
+      mainContent.append(paragraph);
+    })
+  }
+}
+
+
+function onWindow_Load_Handler() {
+  renderNav();
+  renderMain();
+}
+
+
+function onNavBtn_Click_Handler() {
+  openNav();
+}
+
+
+function onNavCloseBtn_Click_Handler() {
+  closeNav();
+}
+
+
+function forNavLinks_onNavList_Click_Handler(evt) {
+  if (!evt.target.dataset.navLink) return;
+  evt.preventDefault();
+
+  renderMain(evt.target.dataset.navId);
+  closeNav();
+
+  activeLink && activeLink.classList.remove("nav__link--active");
+  activeLink = evt.target;
+  activeLink.classList.add("nav__link--active");
+}
