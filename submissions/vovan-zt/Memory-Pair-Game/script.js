292fae270b16adaf467f048f4e3a4ca91a9fa609
vovan-zt <predator_vovan@ukr.net>
2022-08-27 23:48:09 +0300

    improved code and formatted with prettier

--- submissions/vovan-zt/Memory-Pair-Game/script.js
+++ submissions/vovan-zt/Memory-Pair-Game/script.js
@@ -1,84 +1,91 @@
-window.addEventListener('DOMContentLoaded', () => {
+﻿'use strict'
 
-    const cards = [
-        { name: 'naruto', image: 'img/1.png'},
-        { name: 'uchiha', image: 'img/2.png'},
-        { name: 'sai', image: 'img/3.png'},
-        { name: 'kakashi', image: 'img/4.png'},
-        { name: 'minato', image: 'img/5.png'},
-        { name: 'zetsu', image: 'img/6.png'}, 
-    ];
+const cards = [
+    { name: 'naruto', image: 'img/1.png' },
+    { name: 'uchiha', image: 'img/2.png' },
+    { name: 'sai', image: 'img/3.png' },
+    { name: 'kakashi', image: 'img/4.png' },
+    { name: 'minato', image: 'img/5.png' },
+    { name: 'zetsu', image: 'img/6.png' },
+]
 
-    cards.push(...cards);
-    cards.sort(() => { return 0.5 - Math.random() });
-      
-    const container = document.querySelector('.container');
+cards.push(...cards)
+cards.sort(() => {
+    return 0.5 - Math.random()
+})
 
-    const generateCards = (name, image, alt) => {
-        return `
+const container = document.querySelector('.container')
+
+const generateCards = (name, image) => {
+    return `
             <div class ="hide-swap">
                 <div class="c1" data-card='${name}'></div>
                 <img class="c2" src="${image}" alt="${name}">
             </div>
-        `;
-    }
+        `
+}
 
-    const cardsDiv = document.createElement('div');
-    cardsDiv.classList.add('wrapper'); 
+const cardsDiv = document.createElement('div')
+cardsDiv.classList.add('wrapper')
 
-    const cardsHTML = cards.map(card => {
-        return generateCards(card.name, card.image, card.alt);
-    }).join('');
-
-    cardsDiv.innerHTML = cardsHTML
-    container.append(cardsDiv);
- 
-    const cardsList = document.querySelectorAll('.hide-swap');
-    const wrapper = document.querySelector('.wrapper');
-
-    let cardFirst = [];
-
-    function cancelCardActive() {
-        cardsList.forEach(card => { 
-            card.classList.remove('active');
-        })
-    }
-
-    function removeClassVisible() {
-        cardsList.forEach(card => { 
-            card.classList.remove('visible');
-        })
-    }
-    
-    function resetCards () {  
-        const cardsLength = document.querySelectorAll('.visible').length;
-        if (cardsLength == 12) {
-            setTimeout(() => alert('You win') , 400);
-            setTimeout(() => removeClassVisible() , 500);
-            setTimeout(() => cancelCardActive() , 600);
-            cards.sort(() => (0.5 - Math.random()), 700);
-        }    
-    }
-
-    function changeCard() {
-        if (cardFirst.length == 2 && cardFirst[0]==cardFirst[1]) {
-            cardsList.forEach(item => { 
-                if (item.classList.contains('active')) {
-                    item.classList.add('visible');
-                }
-            })
-        } else if (cardFirst.length > 2) {
-            cardFirst = [];
-            cancelCardActive();
-        } 
-    }
-
-    wrapper.addEventListener('click', ({target}) => {
-        if (!target.parentElement.classList.contains('container')) {
-        target.parentElement.classList.toggle('active');
-        cardFirst.push(target.dataset.card);
-        changeCard();
-        resetCards();
-        }
+const cardsHTML = cards
+    .map((card) => {
+        return generateCards(card.name, card.image, card.alt)
     })
-});
+    .join('')
+
+cardsDiv.innerHTML = cardsHTML
+container.append(cardsDiv)
+
+const cardsList = document.querySelectorAll('.hide-swap')
+const wrapper = document.querySelector('.wrapper')
+
+let cardFirst = []
+
+function cancelCardActive() {
+    cardsList.forEach((card) => {
+        card.classList.remove('active')
+    })
+}
+
+function removeClassVisible() {
+    cardsList.forEach((card) => {
+        card.classList.remove('visible')
+    })
+}
+
+function resetCards() {
+    const cardsLength = document.querySelectorAll('.visible').length
+    if (cardsLength == 12) {
+        setTimeout(() => alert('You win'), 400)
+        setTimeout(() => removeClassVisible(), 500)
+        setTimeout(() => cancelCardActive(), 600)
+        cards.sort(() => 0.5 - Math.random(), 700)
+    }
+}
+
+function changeCard() {
+    if (cardFirst.length == 2 && cardFirst[0] == cardFirst[1]) {
+        cardsList.forEach((item) => {
+            if (item.classList.contains('active')) {
+                item.classList.add('visible')
+            }
+        })
+    } else if (cardFirst.length > 2) {
+        cardFirst = []
+        cancelCardActive()
+    }
+}
+
+function showContent({ target }) {
+    if (!target.parentElement.classList.contains('container')) {
+        target.parentElement.classList.toggle('active')
+        cardFirst.push(target.dataset.card)
+        changeCard()
+        resetCards()
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    wrapper.addEventListener('click', showContent)
+})
