f99c6cd2828a3b32992f2e0ac608f3b1fbb252cf
vasilpaliy <vasilpaliy96@gmail.com>
2022-08-28 18:52:20 +0200

    tiny-js-world

--- submissions/vasylpaliy/pre-OOP/index.js
+++ submissions/vasylpaliy/pre-OOP/index.js
@@ -1,31 +1,63 @@
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
+   Code repository: https://github.com/vasylpaliy/a-tiny-JS-world
+   Web app: https://vasylpaliy.github.io/a-tiny-JS-world/
    */
 
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+const dog = {
+  species: 'dog',
+  name: 'Toby',
+  gender: 'male',
+  legs: 4,
+  hands: 0,
+  saying: 'woof-woof!'
+};
+const cat = {
+  species: 'cat',
+  name: 'Garfield',
+  gender: 'female',
+  legs: 4,
+  hands: 0,
+  saying: 'meow'
+};
+
+const man = {
+  species: 'human',
+  name: 'Ted',
+  gender: 'male',
+  legs: 2,
+  hands: 2,
+  saying: 'My code has no errors'
+};
+
+const woman = {
+  species: 'human',
+  name: 'Jennifer',
+  gender: 'female',
+  legs: 2,
+  hands: 2,
+  saying: 'I found errors in your code'
+};
+
 
 
 // ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+let myCreatures = [dog, cat, man, woman];
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
+let printObject = function (obj) {
+  let values = "";
+  for (let i in obj) {
+    values += obj[i] + "; ";
+  }
+  return values;
+}
+for (let i in myCreatures) {
+  print(printObject(myCreatures[i]));
+}
 
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+
 
 
