df64870898b1af29cc395a26e7dff730d7e2eb24
hisbvdis <hisbvdis@gmail.com>
2022-08-29 14:41:11 +0300

    Class for 'Field'

--- submissions/hisbvdis/task_js_oop/app.js
+++ submissions/hisbvdis/task_js_oop/app.js
@@ -1,4 +1,12 @@
-const Enemy = function(initialX, initialY, speed, player){
+const Field = function() {
+  this.width = 400;
+  this.height = 375;
+}
+
+const field = new Field();
+
+
+const Enemy = function(field, initialX, initialY, speed, player){
   this.sprite = "images/enemy-bug.png";
   this.initialX = initialX;
   this.initialY = initialY;
@@ -8,12 +16,11 @@ const Enemy = function(initialX, initialY, speed, player){
   this.player = player;
   this.enemyHeight = 40;
   this.enemyWidth = 83;
-  this.fieldWidth = 400;
-  this.fieldHeight = 375;
+  this.field = field;
 }
 
 Enemy.prototype.update = function(dt) {
-  if (this.x >= this.fieldHeight + this.enemyWidth) {
+  if (this.x >= this.field.height + this.enemyWidth) {
     this.x = this.initialX;
   }
 
@@ -44,12 +51,11 @@ Enemy.prototype.checkCollision = function() {
 }
 
 
-const Player = function() {
+const Player = function(field) {
   this.sprite = "images/char-boy.png";
-  this.fieldWidth = 400;
-  this.fieldHeight = 375;
-  this.initialX = this.fieldWidth / 2;
-  this.initialY = this.fieldHeight;
+  this.field = field;
+  this.initialX = this.field.width / 2;
+  this.initialY = this.field.height;
   this.y = this.initialY;
   this.x = this.initialX;
   this.speed = 100;
@@ -69,7 +75,7 @@ Player.prototype.handleInput = function (key) {
   const actions = {
     up: () => this.y -= 80,
     down: () => {
-      if (this.y < this.fieldHeight) {
+      if (this.y < this.field.height) {
         this.y += 80
       }
     },
@@ -79,7 +85,7 @@ Player.prototype.handleInput = function (key) {
       }
     },
     right: () => {
-      if (this.x < this.fieldWidth) {
+      if (this.x < this.field.width) {
         this.x += 100
       }
     },
@@ -93,12 +99,12 @@ Player.prototype.reset = function () {
   this.y = this.initialY;
 };
 
-const player = new Player();
+const player = new Player(field);
 
 const allEnemies = [
-  new Enemy(-100, 60, 200, player),
-  new Enemy(-100, 140, 250, player),
-  new Enemy(-100, 225, 300, player),
+  new Enemy(field, -100, 60, 200, player),
+  new Enemy(field, -100, 140, 250, player),
+  new Enemy(field, -100, 225, 300, player),
 ];
 
 document.addEventListener("keydown", (evt) => {
