ecea4ccbd01ce931bd39adcc9d8551acf00bbf50
hisbvdis <hisbvdis@gmail.com>
2022-08-29 12:37:21 +0300

    ES6 => Prototypes

--- submissions/hisbvdis/task_js_oop/app.js
+++ submissions/hisbvdis/task_js_oop/app.js
@@ -7,82 +7,79 @@ const SIZES = {
   playerWidth: 40,
 };
 
-class Enemy {
-  constructor(initialX, initialY, speed) {
-    this.sprite = "images/enemy-bug.png";
-    this.initialX = initialX;
-    this.initialY = initialY;
-    this.x = initialX;
-    this.y = initialY;
-    this.speed = speed;
+const Enemy = function(initialX, initialY, speed){
+  this.sprite = "images/enemy-bug.png";
+  this.initialX = initialX;
+  this.initialY = initialY;
+  this.x = initialX;
+  this.y = initialY;
+  this.speed = speed;
+}
+
+Enemy.prototype.update = function(dt) {
+  if (this.x >= SIZES.fieldHeight + SIZES.enemyWidth) {
+    this.x = this.initialX;
   }
 
-  update(dt) {
-    if (this.x >= SIZES.fieldHeight + SIZES.enemyWidth) {
-      this.x = this.initialX;
-    }
+  this.x = this.x + this.speed * dt;
 
-    this.x = this.x + this.speed * dt;
+  this.checkCollision();
+}
 
-    this.checkCollision();
-  }
+Enemy.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
-  render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  }
-
-  checkCollision() {
-    const playerZone = {
-      fromX: player.x,
-      toX: player.x + SIZES.playerWidth,
-      fromY: player.y,
-      toY: player.y + SIZES.playerHeight,
-    };
-    if (
-      this.x >= playerZone.fromX &&
-      this.x <= playerZone.toX &&
-      this.y >= playerZone.fromY &&
-      this.y <= playerZone.toY
-    ) {
-      player.reset();
-    }
+Enemy.prototype.checkCollision = function() {
+  const playerZone = {
+    fromX: player.x,
+    toX: player.x + SIZES.playerWidth,
+    fromY: player.y,
+    toY: player.y + SIZES.playerHeight,
+  };
+  if (
+    this.x >= playerZone.fromX &&
+    this.x <= playerZone.toX &&
+    this.y >= playerZone.fromY &&
+    this.y <= playerZone.toY
+  ) {
+    player.reset();
   }
 }
 
-class Player {
-  constructor() {
-    this.sprite = "images/char-boy.png";
-    this.initialX = SIZES.fieldWidth / 2;
-    this.initialY = SIZES.fieldHeight;
-    this.x = this.initialX;
-    this.y = this.initialY;
-    this.speed = 100;
-  }
 
-  update(dt) {
-    if (this.y < 0) this.reset();
-  }
-
-  render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  }
-
-  handleInput(key) {
-    const actions = {
-      up: () => (this.y -= 80),
-      down: () => (this.y += 80),
-      left: () => (this.x -= 100),
-      right: () => (this.x += 100),
-    };
-
-    actions[key]();
-  }
-
-  reset() {
-    this.x = this.initialX;
-    this.y = this.initialY;
-  }
+const Player = function() {
+  this.sprite = "images/char-boy.png";
+  this.initialX = SIZES.fieldWidth / 2;
+  this.initialY = SIZES.fieldHeight;
+  this.x = this.initialX;
+  this.y = this.initialY;
+  this.speed = 100;
 }
+
+Player.prototype.update = function (dt) {
+  if (this.y < 0) this.reset();
+};
+
+Player.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function (key) {
+  const actions = {
+    up: () => (this.y -= 80),
+    down: () => (this.y += 80),
+    left: () => (this.x -= 100),
+    right: () => (this.x += 100),
+  };
+
+  actions[key]();
+};
+
+Player.prototype.reset = function () {
+  this.x = this.initialX;
+  this.y = this.initialY;
+};
 
 const allEnemies = [
   new Enemy(-100, 60, 200),
