ca13b1e1fde744937cf8da89692ac19dc0a6fa5e
hisbvdis <hisbvdis@gmail.com>
2022-08-29 13:05:02 +0300

    Refactoring

--- submissions/hisbvdis/task_js_oop/app.js
+++ submissions/hisbvdis/task_js_oop/app.js
@@ -1,23 +1,19 @@
-const SIZES = {
-  fieldWidth: 400,
-  fieldHeight: 375,
-  enemyHeight: 40,
-  enemyWidth: 83,
-  playerHeight: 40,
-  playerWidth: 40,
-};
-
-const Enemy = function(initialX, initialY, speed){
+const Enemy = function(initialX, initialY, speed, player){
   this.sprite = "images/enemy-bug.png";
   this.initialX = initialX;
   this.initialY = initialY;
   this.x = initialX;
   this.y = initialY;
   this.speed = speed;
+  this.player = player;
+  this.enemyHeight = 40;
+  this.enemyWidth = 83;
+  this.fieldWidth = 400;
+  this.fieldHeight = 375;
 }
 
 Enemy.prototype.update = function(dt) {
-  if (this.x >= SIZES.fieldHeight + SIZES.enemyWidth) {
+  if (this.x >= this.fieldHeight + this.enemyWidth) {
     this.x = this.initialX;
   }
 
@@ -32,10 +28,10 @@ Enemy.prototype.render = function() {
 
 Enemy.prototype.checkCollision = function() {
   const playerZone = {
-    fromX: player.x,
-    toX: player.x + SIZES.playerWidth,
-    fromY: player.y,
-    toY: player.y + SIZES.playerHeight,
+    fromX: this.player.x,
+    toX: this.player.x + this.player.width,
+    fromY: this.player.y,
+    toY: this.player.y + this.player.height,
   };
   if (
     this.x >= playerZone.fromX &&
@@ -43,18 +39,22 @@ Enemy.prototype.checkCollision = function() {
     this.y >= playerZone.fromY &&
     this.y <= playerZone.toY
   ) {
-    player.reset();
+    this.player.reset();
   }
 }
 
 
 const Player = function() {
   this.sprite = "images/char-boy.png";
-  this.initialX = SIZES.fieldWidth / 2;
-  this.initialY = SIZES.fieldHeight;
-  this.x = this.initialX;
+  this.fieldWidth = 400;
+  this.fieldHeight = 375;
+  this.initialX = this.fieldWidth / 2;
+  this.initialY = this.fieldHeight;
   this.y = this.initialY;
+  this.x = this.initialX;
   this.speed = 100;
+  this.width = 40;
+  this.height = 40;
 }
 
 Player.prototype.update = function (dt) {
@@ -67,10 +67,22 @@ Player.prototype.render = function () {
 
 Player.prototype.handleInput = function (key) {
   const actions = {
-    up: () => (this.y -= 80),
-    down: () => (this.y += 80),
-    left: () => (this.x -= 100),
-    right: () => (this.x += 100),
+    up: () => this.y -= 80,
+    down: () => {
+      if (this.y < this.fieldHeight) {
+        this.y += 80
+      }
+    },
+    left: () => {
+      if (this.x > 0) {
+        this.x -= 100
+      }
+    },
+    right: () => {
+      if (this.x < this.fieldWidth) {
+        this.x += 100
+      }
+    },
   };
 
   actions[key]();
@@ -81,13 +93,13 @@ Player.prototype.reset = function () {
   this.y = this.initialY;
 };
 
-const allEnemies = [
-  new Enemy(-100, 60, 200),
-  new Enemy(-100, 140, 250),
-  new Enemy(-100, 225, 300),
-];
-
 const player = new Player();
+
+const allEnemies = [
+  new Enemy(-100, 60, 200, player),
+  new Enemy(-100, 140, 250, player),
+  new Enemy(-100, 225, 300, player),
+];
 
 document.addEventListener("keydown", (evt) => {
   const allowedCodes = {
