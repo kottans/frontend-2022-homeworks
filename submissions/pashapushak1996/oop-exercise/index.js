db2ba3e4826c089a887ae8ca83406f66304be4a2
pashapushak1996 <pashapushak1996@gmail.com>
2022-08-29 22:02:31 +0300

    refactoring according to requirements

--- submissions/pashapushak1996/oop-exercise/index.js
+++ submissions/pashapushak1996/oop-exercise/index.js
@@ -1,36 +1,55 @@
 class Inhabitant {
-    constructor(species, name, gender, greeting, legs) {
-        this.species = species;
+    constructor(name, gender, greeting) {
+        this.species = this.constructor.name.toLowerCase();
         this.name = name;
         this.gender = gender;
         this.greeting = greeting;
-        this.legs = legs;
     }
 }
 
 class Human extends Inhabitant {
-    constructor(name, gender, greeting, legs, hands) {
-        super(name, gender, greeting, legs);
-        this.species = 'human';
-        this.hands = hands;
+    constructor(name, gender, greeting) {
+        super(name, gender, greeting);
+        this.legs = 2;
+        this.hands = 2;
     }
 }
 
-const dog = new Inhabitant('dog', 'Tom', 'male', 'Howl!', 4);
-const cat = new Inhabitant('cat', 'Kitty', 'female', 'Meow!', 4);
-const woman = new Human('Jessy', 'female', 'Hello!', 2, 2);
-const man = new Human('Ted', 'male', 'Hello my dear!', 2, 2);
+class Animal extends Inhabitant {
+    constructor(name, gender, greeting) {
+        super(name, gender, greeting);
+        this.paws = 4;
+    }
+}
+
+class Cat extends Animal {
+    constructor(name, gender, greeting) {
+        super(name, gender, greeting);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, gender, greeting) {
+        super(name, gender, greeting);
+    }
+}
+
+const dog = new Dog('Tom', 'male', 'Howl!');
+const cat = new Cat('Kitty', 'female', 'Meow!');
+const woman = new Human('Jessy', 'female', 'Hello!');
+const man = new Human('Ted', 'male', 'Hello my dear!');
 
 const arrayOfInhabitants = [dog, cat, woman, man];
 
-const arrayOfKeys = ['species', 'name', 'gender', 'greeting', 'legs', 'hands'];
+const arrayOfKeys = ['species', 'name', 'gender', 'greeting', 'legs', 'hands', 'paws'];
 
 const pullValuesOfObj = (obj) => {
     const array = [];
 
     arrayOfKeys.forEach((key) => {
-        obj[key] && array.push(obj[key])
+        obj[key] && array.push(obj[key]);
     });
+
 
     return array;
 };
