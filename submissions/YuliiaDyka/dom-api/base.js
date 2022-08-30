22a6ad488a03e6923ea924881e9cff6a7ea21799
Yuliia <yuliiadd@gmail.com>
2022-08-30 09:59:03 +0300

    added DOM-API homework

---
+++ submissions/YuliiaDyka/dom-api/base.js
@@ -0,0 +1,220 @@
+"use strict"
+
+
+const dataObj = {
+    'Git Basics':`<ol class="task">
+    <li class="task__item">
+        Прослухайте тижні 1 і 2 курсу <a class="link tomato" href="https://www.coursera.org/learn/introduction-git-github">Introduction to Git and GitHub</a> 
+    </li>
+    <li class="task__item">
+        Пройдіть наступні рівні тут <a class="link tomato" href="https://learngitbranching.js.org/">learngitbranching.js.org:</a>
+        <ul>
+            <li class="task__item">Основи: Introduction Sequence, Ramping Up</li>
+            <li class="task__item">Віддалені репозиторії: Push & Pull -- віддалені репозиторії в Git!</li>
+        </ul>
+    </li>
+    <li class="task__item">
+        Створіть репозиторій та назвіть його kottans-frontend.
+    </li>
+    <li class="task__item">
+        Створіть README.md для репозиторію.
+    </li>
+    <li class="task__item">
+        Опишіть свої враження від вивченого матеріалу.
+    </li>
+    <li class="task__item">
+        Надішліть pull-request сюди Kottans/mock-repo пропонуючи зміни.
+    </li>
+ </ol>`,
+
+    'Linux CLI and Networking': `<ol class="task">
+    <li class="task__item">
+        <a class="link tomato" href="https://linuxsurvival.com/linux-tutorial-introduction/">Linux Survival (4 modules)</a> 
+    </li>
+    <li class="task__item">
+       <a class="link tomato" href="https://code.tutsplus.com/uk/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177">HTTP: Протокол, який повинен розуміти кожний веб-розробник - Частина 1</a>
+    </li>
+    <li class="task__item">
+       <a class="link tomato" href="https://code.tutsplus.com/uk/tutorials/http-the-protocol-every-web-developer-must-know-part-2--net-31155">HTTP: Протокол, який повинен розуміти кожний веб-розробник - Частина 2</a>
+    </li>
+ </ol>`,
+
+    'VCS (hello gitty), GitHub and Collaboration':`<ol class="task">
+    <li class="task__item">
+        Прослухайте тижні 3 і 4 курсу <a class="link tomato" href="https://www.coursera.org/learn/introduction-git-github">Introduction to Git and GitHub</a> 
+    </li>
+    <li class="task__item">
+        Пройдіть наступні рівні тут <a class="link tomato" href="https://learngitbranching.js.org/">learngitbranching.js.org:</a>
+        <ul>
+            <li class="task__item">Основи: Переміщуємо роботу туди-сюди</li>
+            <li class="task__item">Через origin – до зірок. Прогресивне використання Git Remotes</li>
+        </ul>
+    </li>
+ </ol>`,
+ 
+    'Intro to HTML & CSS':`<ol class="task">
+    <li class="task__item">
+        Прослухайте тижні 1 і 2 (до Introduction to Responsive Design) курсу <a class="link tomato" href="https://www.coursera.org/learn/html-css-javascript-for-web-developers">Intro to HTML & CSS</a> 
+    </li>
+    <li class="task__item">
+        <a class="link tomato" href="https://www.codecademy.com/learn/learn-html">Learn HTML(Eng)</a> 
+    </li>
+    <li class="task__item">
+        <a class="link tomato" href="https://www.codecademy.com/learn/learn-css">Learn CSS(Eng)</a> 
+    </li>
+ </ol>`,
+
+    'Responsive Web Design':`<ol class="task">
+    <li class="task__item">
+        <a class="link tomato" href="https://web.dev/i18n/en/responsive-web-design-basics/">Responsive web design basics</a> 
+    </li>
+    <li class="task__item">
+        <a class="link tomato" href="https://www.youtube.com/playlist?list=PLM6XATa8CAG5mPV60dMmjMRrHVW4LmV2x">FLEXBOX. Вчимося верстати на флексах</a> 
+    </li>
+    <li class="task__item">
+        <a class="link tomato" href="http://flexboxfroggy.com/">Flexbox Froggy</a> 
+    </li>
+    <li class="task__item">
+        <a class="link tomato" href="https://www.youtube.com/watch?v=GV92IdMGFfA&list=PLM6XATa8CAG5pXQrW_kDaeZb_uIAMNZIm">CSS Grid Layout</a> 
+    </li>
+    <li class="task__item">
+        <a class="link tomato" href="http://cssgridgarden.com/">Grid Garden</a> 
+    </li>
+ </ol>`,
+
+    'HTML & CSS Practice': `<ol class="task">
+    <li class="task__item">
+         Розробити випадаюче контекстне меню 
+    </li>
+    <li class="task__item">
+        Ніякого JavaScript, лише HTML/CSS, дозволено використання препроцесорів
+    </li>
+    <li class="task__item">
+        Ніяких зовнішніх бібліотек чи фреймворків
+    </li>
+    <li class="task__item">
+        Підтримка веббраузера: лише Chrome (використовуйте найновіші фічі)
+    </li>
+    <li class="task__item">
+        Очікується, що попап матиме три стани залежно від дій користувача:
+        <ul>
+            <li class="task__item">Початковий стан: попап не видно</li>
+            <li class="task__item">Після натискання на кнопку popup-button попап стає видимим або прихованим, якщо воно вже відкрите</li>
+            <li class="task__item">Натисніть кнопку "More", щоб додати ще від 3 до 10 іконок і робить вміст попапу доступним для скролу</li>
+        </ul>
+    </li>
+ </ol>`,
+ 
+    'JavaScript Basics': `<ol class="task">
+    <li class="task__item">
+        <a class="link tomato" href="https://www.coursera.org/learn/html-css-javascript-for-web-developers/home/week/4">Вступ до JS</a> 
+    </li>
+    <li class="task__item">
+        <a class="link tomato" href="https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/basic-javascript/">Basic JavaScript </a> 
+    </li>
+    <li class="task__item">
+        <a class="link tomato" href="https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/es6/">ES6 Challenges </a> 
+    </li>
+    <li class="task__item">
+        <a class="link tomato" href="https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/basic-data-structures/">Basic Data Structures</a> 
+    </li>
+    <li class="task__item">
+        <a class="link tomato" href="https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/basic-algorithm-scripting/">Basic Algorithm Scripting</a> 
+    </li>
+    <li class="task__item">
+        <a class="link tomato" href="https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/functional-programming/">Functional Programming</a> 
+    </li>
+    <li class="task__item">
+        <a class="link tomato" href="https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting">Algorithm Scripting Challenges </a> 
+    </li>
+ </ol>`,
+
+    'Document Object Model - practice':`<ol class="task">
+    <li class="task__item">
+        <a class="link tomato" href="https://www.coursera.org/learn/html-css-javascript-for-web-developers/home/week/5">Document Object Model Manipulation</a> 
+    </li>
+    <li class="task__item">
+        <a class="link tomato" href="https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting">freecodecamp Algorithm Scripting Challenges</a> 
+    </li>
+    <li class="task__item">
+    Практичне завдання: Впровадити інтерактивне side-menu без перезавантаження сторінки. Ви можете вибрати будь-яку тему: покемони, телефони, тощо.
+    </li>
+ </ol>`,
+
+    'Building a Tiny JS World (pre-OOP) - practice':`<ol class="task">
+    <li class="task__item">
+        Створіть a tiny JS world model, дотримуючись інструкцій <a class="link tomato" href="https://github.com/OleksiyRudenko/a-tiny-JS-world">тут</a> 
+    </li>
+    <li class="task__item">
+    На цьому етапі вам потрібно виконати лише частину First approach. Ви повернетеся до свого Tiny JS World, щоб покращити його пізніше. Отже, поки що обмежтеся засобами, у яких ви відчуваєте себе впевнено.
+    </li>
+ </ol>`,
+
+    'Object oriented JS - practice':`<ol class="task">
+    <li class="task__item">
+        <a class="link tomato" href="https://www.youtube.com/watch?v=jgCiWIdUZ-s&list=PLM7wFzahDYnEltE-aVGhRHYPwIJn0Xquu&index=40">ООП 1 частина - Класи</a> 
+    </li>
+    <li class="task__item">
+        <a class="link tomato" href="https://www.youtube.com/watch?v=e-3GS5-rak8&list=PLM7wFzahDYnEltE-aVGhRHYPwIJn0Xquu&index=46">ООП 2 частина - Прототипи</a> 
+    </li>
+    <li class="task__item">
+         <a class="link tomato" href="https://github.com/kottans/frontend/blob/2022_UA/tasks/js-oop-frogger.md">Практика - Classic Frogger Game</a> 
+    </li>
+    <li class="task__item">
+        Зареєструйтеся на Codewars, приєднайтеся до клану Kottans і досягніть 7 kyu.
+    </li>
+ </ol>`,
+
+    'OOP exercise - practice':`<ol class="task">
+    <li class="task__item">
+        Удосконалюйте свій tiny JS world model, яку ви форкнули <a class="link tomato" href="https://github.com/OleksiyRudenko/a-tiny-JS-world">звідси</a> 
+    </li>
+    <li class="task__item">
+         На цей раз вам не потрібен форк, працюйте з наявним репо. Git зберігає історію для вас.
+    </li>
+    <li class="task__item">
+    Ці матеріали будуть корисні - <a class="link tomato" href="https://github.com/kottans/frontend/blob/2022_UA/tasks/js-oop-frogger.md">link</a> 
+    </li>
+ </ol>`,
+
+    'Offline Web Applications - optional': `<ol class="task">
+    <li class="task__item">
+        Удосконалюйте свій tiny JS world model, яку ви форкнули <a class="link tomato" href="https://learn.udacity.com/courses/ud899">Offline Web Applications</a> 
+    </li>
+ </ol>`,
+
+    'Memory pair game — real project!':`<ol class="task">
+    <li class="task__item">
+         У цьому розділі ви створите Memory - Pair Game — карткову гру, в якій всі картки кладуться на поверхню лицьовою стороною вниз, і кожен раз перевертаються по дві картки. Мета гри - знайти пари відповідних карток. Гра закінчується, коли буде підібрана остання пара.
+    </li>
+    <li class="task__item">
+    Деякі з наведених нижче посилань можуть бути корисними:
+        <ul> 
+            <li>
+                <a class="link tomato" href="https://css-tricks.com/snippets/javascript/shuffle-array/">Shuffle Array</a>
+            </li>
+            <li>
+                <a class="link tomato" href="https://www.w3schools.com/css/css3_transitions.asp">CSS3 Transitions</a>
+            </li>
+            <li>
+                <a class="link tomato" href="https://davidwalsh.name/css-flip">Flip Animation</a>
+            </li>
+        </ul>
+    </li>
+
+ </ol>`,
+    'Website Performance Optimization - optional':`<ol class="task">
+    <li class="task__item">
+        Прочитайте це - <a class="link tomato" href="https://www.smashingmagazine.com/2021/01/front-end-performance-2021-free-pdf-checklist/">Front-End Performance Checklist 2021</a> 
+    </li>
+ </ol>`,
+
+    'Friends App - real project!': `<ol class="task">
+    <li class="task__item">
+        Створіть невеличку сторінку пошуку друзів у соціальних мережах з картками користувачів, пошуком, сортуванням та фільтрацією їх за віком, прізвищем, ім'ям, або чим завгодно, за допомогою <a class="link tomato" href="https://randomuser.me/">Random User API </a> 
+    </li>
+ </ol>`,
+ 
+ }
+
+ export default dataObj;
---
+++ submissions/YuliiaDyka/dom-api/main.js
@@ -0,0 +1,87 @@
+'use strict'
+
+import dataObj from "./base.js";
+
+const title = document.querySelector('.title__chapter');
+const description = document.querySelector('.description');
+const nav_items = document.querySelectorAll('.nav__item');
+const nav = document.querySelector('.nav');
+const btn = document.querySelector('.btn');
+const buttonsDiv = document.querySelector('.buttons');
+const btn__home = document.querySelector('.home__btn');
+const checkboxes = document.querySelectorAll('.checkbox');
+const progressDig = document.querySelector('.progress__dig');
+const progressLine = document.querySelector('.progress');
+
+let currentDesc = description.textContent;
+let currentTitle = title.textContent;
+let progressMax = checkboxes.length;
+ 
+// event listeners
+
+nav.addEventListener("click", getCurrrentContent);
+btn__home.addEventListener('click', goToHomePage);
+
+// functions declaration
+
+function getCurrrentContent(e) {
+    nav_items.forEach(item => { item.classList.remove('selected')}); 
+    let target = e.target;
+    switch (target.tagName) {
+        case "LI": 
+        target.classList.add('selected');
+        currentTitle = e.target.innerText;
+        console.log(e.target.innerText);
+        currentDesc = dataObj[currentTitle];
+        changePage(currentDesc, currentTitle);
+        break; 
+
+        case "A":
+        target.parentElement.classList.add('selected'); 
+        currentTitle = e.target.innerText;
+        console.log(e.target.innerText);
+        currentDesc = dataObj[currentTitle];
+        changePage(currentDesc, currentTitle);
+        break; 
+
+        case "INPUT":
+        let currentProgress = 0;  
+        checkboxes.forEach(item => { if (item.checked) { currentProgress++;}});
+        progressDig.textContent = Math.floor((currentProgress / progressMax * 100)) + "%";
+        progressLine.style.width = Math.floor((currentProgress / progressMax * 100)) + "%";  
+        
+        default:
+        return;
+    };
+};
+
+function changePage(currentDesc, currentTitle) {
+
+const title = document.querySelector('.title__chapter');
+const description = document.querySelector('.description');
+
+let newTitle = document.createElement("h4");
+newTitle.classList.add('title__chapter');
+newTitle.textContent = currentTitle;
+title.replaceWith(newTitle);
+
+let newDesc = document.createElement('div');
+newDesc.classList.add('description');
+newDesc.innerHTML = currentDesc;
+description.replaceWith(newDesc);
+
+btn.style.display = "none";
+buttonsDiv.style.display = 'flex';
+
+};
+
+function goToHomePage() {
+    nav_items.forEach(item => {
+        item.classList.remove('selected');
+    }); 
+    changePage("We are community of developers who love to code and help each other.The course contains basics of front-end development: HTML, CSS, JavaScript, TypeScript and front-end frameworks. We plan to decrease the amount of lectures in favor of collective discussions, team projects & collective work. Nevertheless, doing that on one's own can be challenging when it comes to continuing without support from surrounding people, such situation really beats the motivation, especially when encountering either boring or impossible to do (at first glance) task or illogical aspect of programming language you can't just grasp.", "Welcome to Kottans!")
+    btn.style.display = "flex";
+    buttonsDiv.style.display = 'none';
+};
+
+
