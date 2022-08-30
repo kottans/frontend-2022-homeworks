fcc70610e96ffe522628b091b0343e2574c67035
Eugene-CG <evhenisssss@gmail.com>
2022-08-30 20:13:47 +0300

    something not good

--- submissions/Eugene-CG/OOP-exercise/index.js
+++ submissions/Eugene-CG/OOP-exercise/index.js
@@ -12,42 +12,78 @@ class Inhabitant {
     this.saying = saying;
     this.gender = gender;
   }
-  getProperties() {
-    return `species:${this.species}; name:${this.name}; gender:${this.gender}; legs:${this.legs}; hands:${this.hands}; saying:${this.saying};`;
+  getName() {
+    return `name:${this.name}; `;
+  }
+  getSaying() {
+    return `saying:${this.saying}; `;
+  }
+  getGender() {
+    return `gender:${this.gender}; `;
+  }
+  getSpecies() {
+    return `species:${this.species}; `;
+  }
+  getLegs() {
+    return `legs:${this.legs}; `;
+  }
+  getHands() {
+    return `hands:${this.hands}; `;
+  }
+  getPaws() {
+    return `paws:${this.paws}; `;
   }
 }
 class Animal extends Inhabitant {
   constructor(name, saying, gender) {
     super(name, saying, gender);
-    this.paws = 4;
-    this.species = "animal";
+    // this.paws = 4;
+    // this.species = "animal";
   }
   getProperties() {
-    return `species:${this.species}; name:${this.name}; gender:${this.gender}; paws:${this.paws}; saying:${this.saying};`;
+    return (
+      super.getName() +
+      super.getSaying() +
+      super.getGender() +
+      super.getSpecies() +
+      super.getPaws()
+    );
   }
 }
 class Human extends Inhabitant {
-  constructor(name, saying) {
-    super(name, saying);
+  constructor(name, saying, gender) {
+    super(name, saying, gender);
     this.legs = 2;
     this.hands = 2;
-    this.species = "human";
+    this.species = "Homo sapiens";
+  }
+  getProperties() {
+    return (
+      super.getName() +
+      super.getSaying() +
+      super.getGender() +
+      super.getSpecies() +
+      super.getLegs() +
+      super.getHands()
+    );
   }
 }
 class Cat extends Animal {
-  constructor(name) {
-    super(name);
-    this.saying = "Meow";
-    this.gender = "male";
-    this.species = "cat";
+  constructor(name, saying = "Meow", gender) {
+    super(name, saying, gender);
+    this.paws = 4;
+    this.species = "Felis catus";
   }
 }
 class Dog extends Animal {
-  constructor(name, saying, gender) {
+  constructor(
+    name,
+    saying = "By default Im sating helo guyc my name dog  im e dog nice to meet me ",
+    gender
+  ) {
     super(name, saying, gender);
-    this.saying = "Woof-Woof";
     this.paws = 4;
-    this.species = "dog";
+    this.species = "Canis familiaris";
   }
 }
 class Man extends Human {
@@ -62,18 +98,27 @@ class Woman extends Human {
     this.gender = "female";
   }
 }
-class CatWoman extends Human {
+class CatWoman extends Cat {
   constructor(name) {
     super(name);
     this.legs = 2;
     this.hands = 2;
     this.species = "сatwoman";
     this.gender = "female";
-    this.saying = "Meow";
+  }
+  getProperties() {
+    return (
+      super.getName() +
+      super.getSaying() +
+      super.getGender() +
+      super.getSpecies() +
+      super.getLegs() +
+      super.getHands()
+    );
   }
 }
 const inhabitants = [
-  new Cat("Eugene", "Hey Fellas!"),
+  new Cat("Eugene", "Meow", "male"),
   new Dog("Toby", "Woof-Woof", "male"),
   new Man("John", "I am a man"),
   new Woman("Yennefer", "Nice to meet you"),
