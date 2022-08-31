a3c8058eb33feee48bee6dbc222c6541efd60c13
Ivan Chukhalo <ivanchukhalo@gmail.com>
2022-08-31 15:42:05 +0300

    refactor: get rid of 'magic numbers', make them constants

--- submissions/ivan-chukhalo/frogger-game/app.js
+++ submissions/ivan-chukhalo/frogger-game/app.js
@@ -1,6 +1,15 @@
 const AVAILABLE_ROWS_FOR_ENEMIES = [63, 146, 229]; 
 const allEnemies = [];
 
+const WIDTH_OF_GAME_SPRITES = 100;
+const COLUMNS_ON_GAME_FIELD = 5;
+const HEIGH_OF_GAME_FIELD_CELL = 83;
+const PLAYER_BUG_CENTERS_DISTANCE_OF_TOUCHING = 80;
+const Y_OF_GAME_FIELD_ROWS = [63, 146, 229, 312, 395];
+const Y_OF_LOWEST_GAME_FIELD_ROW = Y_OF_GAME_FIELD_ROWS[4];
+const MIN_SPEED_MULTIPLIER_FOR_BUG = 200;
+const MAX_SPEED_MULTIPLIER_FOR_BUG = 350;
+
 function resetPlayer(){
     player.x = player.xInitial;
     player.y = player.yInitial;
@@ -9,7 +18,7 @@ function resetPlayer(){
 function resetEnemies(){
     allEnemies.forEach((el)=>{        
         el.x = el.xInitial;
-        el.speedKoef = Math.floor(Math.random() * 10) * 25 + 75;  
+        el.speedKoef = Math.floor(Math.random() * MAX_SPEED_MULTIPLIER_FOR_BUG - MIN_SPEED_MULTIPLIER_FOR_BUG) + MIN_SPEED_MULTIPLIER_FOR_BUG;  
     })
 }
 
@@ -23,17 +32,16 @@ function isOnWater(){
 
 var Enemy = function(y) {                    
     this.sprite = 'images/enemy-bug.png';
-    this.speedKoef = Math.floor(Math.random() * 10) * 25 + 75;
-    this.widthOfBugSprite = 98;
-    this.xInitial = -this.widthOfBugSprite;
+    this.speedKoef = Math.floor(Math.random() * MAX_SPEED_MULTIPLIER_FOR_BUG - MIN_SPEED_MULTIPLIER_FOR_BUG) + MIN_SPEED_MULTIPLIER_FOR_BUG;  
+    this.xInitial = -WIDTH_OF_GAME_SPRITES;
     this.xAxisMin = this.xInitial;
     this.x = this.xInitial;
-    this.xAxisMax = 101 * 4 + this.widthOfBugSprite;
+    this.xAxisMax = WIDTH_OF_GAME_SPRITES * COLUMNS_ON_GAME_FIELD;
     this.y = y;
 };
 
 Enemy.prototype.doesHitPlayer = function (){
-    if (Math.abs(this.x - player.x) <= 75 && this.y === player.y){   
+    if (Math.abs(this.x - player.x) <= PLAYER_BUG_CENTERS_DISTANCE_OF_TOUCHING && this.y === player.y){   
         resetPlayer();
         resetEnemies();
     }
@@ -52,16 +60,16 @@ Enemy.prototype.render = function() {
 
 var Player = function (){
     this.sprite = 'images/char-boy.png';
-    this.xInitial = 202; 
-    this.yInitial = 395;
-    this.xStep = 101; 
-    this.yStep = 83;
+    this.xInitial = WIDTH_OF_GAME_SPRITES * 2; 
+    this.yInitial = Y_OF_LOWEST_GAME_FIELD_ROW;
+    this.xStep = WIDTH_OF_GAME_SPRITES; 
+    this.yStep = HEIGH_OF_GAME_FIELD_CELL;
     this.x = this.xInitial;
     this.y = this.yInitial;
     this.xAxisMin = 0;
-    this.xAxisMax = this.xAxisMin + this.xStep * 4;             
-    this.yAxisMin = this.yInitial - this.yStep * 5;             
-    this.yAxisMax = this.yAxisMin + this.yStep * 5;             
+    this.xAxisMax = this.xAxisMin + this.xStep * (COLUMNS_ON_GAME_FIELD - 1);             
+    this.yAxisMin = this.yInitial - this.yStep * COLUMNS_ON_GAME_FIELD;             
+    this.yAxisMax = this.yAxisMin + this.yStep * COLUMNS_ON_GAME_FIELD;             
 }
 
 Player.prototype.update = function (moveDirection){             
