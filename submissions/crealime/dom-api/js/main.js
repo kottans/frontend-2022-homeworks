c8229bcb7a9e25f053b09201972e3a48da5bdef2
Serhii Kornilov <crealime@gmail.com>
2022-09-01 20:27:13 +0300

    Fixes after review

--- submissions/crealime/dom-api/js/main.js
+++ submissions/crealime/dom-api/js/main.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  const url = 'json/movies.json' // Json file with movies data
-  let moviesData = [] // Storage for movies data
+  const url = 'json/movies.json'
+  let moviesData = []
   const menuUl = document.querySelector('.menu__ul')
   const menu = document.querySelector('.menu')
   const movieContent = document.querySelector('.main')
   const movieRandomize = document.querySelector('.menu__random')
   const menuShowButton = document.querySelector('.hamburger')
 
-  // Fill menu template with data
-  function menuTemplate({id, title}) {
+  function getMenuTemplate({id, title}) {
     return `
 			<div class="menu__li">
 				<a class="menu__link" href="#" title="Seven Random Marvel Movies" data-id="${id}">${title}</a>
@@ -17,8 +16,7 @@ document.addEventListener("DOMContentLoaded", function() {
 		`
   }
 
-  // Fill movie template with data
-  function movieTemplate({title, description, release, box, runtime, picture}) {
+  function getMovieTemplate({title, description, release, box, runtime, picture}) {
 
     return `
 		<section class="movie">
@@ -26,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
 				<div class="preloader">
 					<div class="spinner"></div>
 				</div>
-				<img class="movie__img" src="${picture}" alt="Captain America: The First Avenger">
+				<img class="movie__img" src="${picture}" alt="${title}">
 			</div>
 			<div class="movie__center">
 				<h2 class="movie__title">${title}</h2>
@@ -59,37 +57,30 @@ document.addEventListener("DOMContentLoaded", function() {
     })
   }
 
-  // Find selected movie and paste it into the main
   function renderActualMovie(movies, id = '01') {
 
     movieContent.innerHTML = ''
 
     const activeMovie = movies.find(el => el.id === id)
-    const activeMovieHTML = movieTemplate(activeMovie)
+    const activeMovieHTML = getMovieTemplate(activeMovie)
 
     movieContent.insertAdjacentHTML('afterbegin', activeMovieHTML)
 
     addActiveClassToMenuItem(id)
   }
 
-  // Loop through random movies and paste them into the menu
   function renderMoviesMenu(movies) {
 
     menuUl.innerHTML = ''
-    let fragment = ''
 
-    movies.forEach(el => {
-      const movie = menuTemplate(el)
-      fragment += movie
-    })
+    const fragment = movies.reduce((acc, el) => {
+      acc += getMenuTemplate(el)
+      return acc
+    }, '')
 
     menuUl.insertAdjacentHTML('afterbegin', fragment)
-
-    const actualMovieId = movies[0].id
-    renderActualMovie(movies, actualMovieId)
   }
 
-  // Get seven random movies
   function randomMovies(movies) {
     const arrOfRandomMovies = []
 
@@ -100,9 +91,9 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     renderMoviesMenu(arrOfRandomMovies)
+    renderActualMovie(arrOfRandomMovies, arrOfRandomMovies[0].id)
   }
 
-  // Get data from json
   async function getMoviesData(url) {
     return await fetch(url).then(response => response.json())
   }
@@ -114,20 +105,17 @@ document.addEventListener("DOMContentLoaded", function() {
     })
     .catch(error => console.log(error))
 
-  // Click to select some movie
   menuUl.addEventListener('click', function (event) {
     event.preventDefault()
     const movieId = event.target.dataset.id
     renderActualMovie(moviesData, movieId)
   })
 
-  // Click to randomize movies
   movieRandomize.addEventListener('click', function (event) {
     event.preventDefault()
     randomMovies(moviesData)
   })
 
-  // Hide / show menu
   function toggleMenuClass() {
     menuShowButton.classList.toggle('active')
     menu.classList.toggle('active')
