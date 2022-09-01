321fb6b6e55a68e4a110311db508c84c966edcb1
toha <toha@anton.com>
2022-09-01 18:28:23 +0300

    fixed createArrayOfProps

--- submissions/antonkottans/building-a-tiny-js-world/index.js
+++ submissions/antonkottans/building-a-tiny-js-world/index.js
@@ -1,109 +1,185 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
+const KEYS_VALUE_SEPARATOR = ": ";
+const PROPS_SEPARATOR = "; ";
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
+class Creature {
+    constructor({ name, gender, friends = [], species, saying }) {
+        this.name = name;
+        this.species = species;
+        this.gender = gender;
+        this.friends = friends;
+        this.saying = saying;
+    }
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-const man = {
-    species: "human",
-    name: "Tom",
-    gender: "male",
-    legs: 2,
-    hands: 2,
-    paws: 0,
-    saying: "Hello Jenny!",
-    friends: ["Jenny", "Felix"],
-};
-const woman = {
-    species: "human",
-    name: "Jenny",
-    gender: "female",
-    legs: 2,
-    hands: 2,
-    paws: 0,
-    saying: "Hello Tom!",
-    friends: ["Tom", "Felix"],
-};
-const dog = {
-    species: "dog",
-    name: "Rex",
-    gender: "male",
-    legs: 0,
-    hands: 0,
-    paws: 4,
-    tail: 'tail',
-    saying: "woof-woof!",
-};
-const cat = {
-    species: "cat",
-    name: "Felix",
-    gender: "male",
-    legs: 0,
-    hands: 0,
-    paws: 4,
-    tail: "long fluffy tail",
-    saying: "meow!",
-    friends: ["Tom", "Jenny"],
-};
+    createArrayOfProps() {
+        return ["name", "species", "gender", "saying", "friends"]
+            .map((key) => {
+                if (key != "friends")
+                    return key + KEYS_VALUE_SEPARATOR + this[key];
+                return this.friends.length
+                    ? "friends: " +
+                          this.friends.map((friend) => friend.name).join(", ")
+                    : "";
+            })
+            .filter((_) => _);
+    }
 
-const catWoman = {
-    species: "human",
+    addFriends(...newFriends) {
+        if (newFriends.length > 0)
+            this.friends = [...this.friends, ...newFriends];
+    }
+}
+
+class Animal extends Creature {
+    constructor({ name, gender, friends, paws, species, saying }) {
+        super({ name, gender, friends, species, saying });
+        this.paws = paws;
+    }
+
+    createArrayOfProps() {
+        return [
+            ...super.createArrayOfProps(),
+            "paws" + KEYS_VALUE_SEPARATOR + this["paws"],
+        ];
+    }
+}
+
+class Human extends Creature {
+    constructor({ name, gender, friends, legs, hands, saying }) {
+        super({ name, gender, friends, species: "human", saying });
+        this.legs = legs;
+        this.hands = hands;
+    }
+
+    createArrayOfProps() {
+        return [
+            ...super.createArrayOfProps(),
+            ...["legs", "hands"].map(
+                (key) => key + KEYS_VALUE_SEPARATOR + this[key]
+            ),
+        ];
+    }
+}
+
+class Dog extends Animal {
+    constructor({
+        name,
+        gender,
+        friends,
+        paws,
+        tail = "ordinary tail",
+        saying,
+    }) {
+        super({ name, gender, friends, paws, tail, species: "dog", saying });
+        this.tail = tail;
+    }
+
+    createArrayOfProps() {
+        return [
+            ...super.createArrayOfProps(),
+            "tail" + KEYS_VALUE_SEPARATOR + this.tail,
+        ];
+    }
+}
+
+class Cat extends Animal {
+    constructor({
+        name,
+        gender,
+        friends,
+        paws,
+        tail = "ordinary tail",
+        saying = "meow!",
+    }) {
+        super({ name, gender, friends, paws, tail, species: "cat", saying });
+        this.saying = saying;
+        this.tail = tail;
+    }
+
+    createArrayOfProps() {
+        return [
+            ...super.createArrayOfProps(),
+            "tail" + KEYS_VALUE_SEPARATOR + this.tail,
+        ];
+    }
+}
+
+class CatWoman extends Human {
+    constructor({
+        name,
+        gender,
+        friends,
+        legs,
+        hands,
+        tail = "ordinary Cat-woman tail",
+        saying = "meow!",
+    }) {
+        super({
+            name,
+            gender,
+            friends,
+            legs,
+            hands,
+            species: "cat-woman",
+        });
+        this.tail = tail;
+        this.saying = saying;
+    }
+
+    createArrayOfProps() {
+        return [
+            ...super.createArrayOfProps(),
+            "tail" + KEYS_VALUE_SEPARATOR + this.tail,
+        ];
+    }
+}
+
+const Halle = new CatWoman({
     name: "Halle",
     gender: "female",
     legs: 2,
     hands: 2,
-    paws: 0,
-    tail: "long not fluffy tail",
-    get saying(){
-        return cat.saying
-    },
-    friends: ["Felix"],
-};
-const inhabitants = [dog, cat, woman, man, catWoman];
-
-const keys = [
-    "species",
-    "name",
-    "gender",
-    "legs",
-    "hands",
-    "paws",
-    "tail",
-    "saying",
-    "friends",
-];
-
-inhabitants.forEach((inhabitant) => {
-    print(
-        keys
-            .reduce(
-                (output, key) =>
-                    Object.hasOwn(inhabitant, key)
-                        ? output.concat(inhabitant[key])
-                        : output,
-                []
-            )
-            .join(", ")
-    );
+    tail: "long, not fluffy tail",
+    saying: Cat.saying,
 });
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+const Felix = new Cat({
+    name: "Felix",
+    gender: "male",
+    paws: 4,
+    tail: "long, fluffy tail",
+});
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
+const Rex = new Dog({
+    name: "Rex",
+    gender: "male",
+    paws: 4,
+    tail: "small tail",
+    saying: "woof-woof!",
+});
 
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+const Tom = new Human({
+    name: "Tom",
+    gender: "male",
+    legs: 2,
+    hands: 2,
+    saying: "Hello Jenny!",
+});
+
+const Jenny = new Human({
+    name: "Jenny",
+    gender: "female",
+    legs: 2,
+    hands: 2,
+    saying: "Hello Tom!",
+});
+
+Halle.addFriends(Felix);
+Felix.addFriends(Tom, Jenny);
+Tom.addFriends(Jenny, Felix);
+Jenny.addFriends(Tom, Felix);
+
+const inhabitants = [Rex, Felix, Jenny, Tom, Halle];
+
+inhabitants.forEach((inhabitant) =>
+    print(inhabitant.createArrayOfProps().join(PROPS_SEPARATOR))
+);
