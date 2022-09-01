0c58e7cc3f65cb437014c61124170f77934490d1
Ihor <goshus@ukr.net>
2022-09-01 17:15:06 +0300

    delete all comments from index.js

--- submissions/IhorOzerov/a-tiny-JS-world/index.js
+++ submissions/IhorOzerov/a-tiny-JS-world/index.js
@@ -1,12 +1,3 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
-
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-
 const man = {
    creature: 'human',
    name: "Gregory",
@@ -51,7 +42,6 @@ catWoman.legs = 2;
 catWoman.hands = 2;
 catWoman.friends =  ["Yohanna", "Gangster"];
 
-// ======== OUTPUT ========
 const keys = {
    creature: "I am ",
    name: "My name is ",
@@ -71,25 +61,3 @@ members.forEach(member =>
    )
 );
  
-/*
-print("Hi, I am a " + man.creature + ';' + man.name + ';' + man.gender + ';' + 
-  man.hands + ';' +man.legs + ';' + man.say + ';' + man.friends);
-print(dog.creature + ';' + dog.name + ';' + dog.gender + ';' + 
-  dog.paws + ';' + dog.say + ';' + dog.friends);*/
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
