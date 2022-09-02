0807d764013829eb633dc0668428c39bce2126d8
Andrii Konovalov <andy.smokk@gmail.com>
2022-09-02 18:30:12 +0200

    fix tamplate func

--- submissions/andysmokk/dom-js/js/script.js
+++ submissions/andysmokk/dom-js/js/script.js
@@ -3,23 +3,23 @@ import movies from "../data/data.json" assert { type: "json" };
 const movieList = document.querySelector(".list-movie");
 const movieBox = document.querySelector(".movie-box");
 
-const template = (arr) => {
-  return `<img class="img-poster" src=${arr[0].Poster} alt="poster">
+const template = (obj) => {
+  return `<img class="img-poster" src=${obj.Poster} alt="poster">
           <div class="description-box">
-            <h2 class="description-text description-title"> ${arr[0].Title}</h2>
-            <p class="description-text"><span class="description-text-span">Genre:</span> ${arr[0].Genre}</p>
-            <p class="description-text"><span class="description-text-span">IMDB:</span> ${arr[0].imdbRating}</p>
-            <p class="description-text description-text__hidden-laptop"><span class="description-text-span">Year:</span> ${arr[0].Year}</p>
-            <p class="description-text"><span class="description-text-span">Released:</span> ${arr[0].Released}</p>
-            <p class="description-text description-text__hidden-tablet"><span class="description-text-span">Runtime:</span> ${arr[0].Runtime}</p>
-            <p class="description-text description-text__hidden-tablet"><span class="description-text-span">Awards:</span> ${arr[0].Awards}</p>
-            <p class="description-text description-text__hidden-laptop"><span class="description-text-span">IMDB Votes:</span> ${arr[0].imdbVotes}</p>
-            <p class="description-text description-text__hidden-tablet"><span class="description-text-span">Country:</span> ${arr[0].Country}</p>
-            <p class="description-text"><span class="description-text-span">Box Office:</span> ${arr[0].BoxOffice}</p>
+            <h2 class="description-text description-title"> ${obj.Title}</h2>
+            <p class="description-text"><span class="description-text-span">Genre:</span> ${obj.Genre}</p>
+            <p class="description-text"><span class="description-text-span">IMDB:</span> ${obj.imdbRating}</p>
+            <p class="description-text description-text__hidden-laptop"><span class="description-text-span">Year:</span> ${obj.Year}</p>
+            <p class="description-text"><span class="description-text-span">Released:</span> ${obj.Released}</p>
+            <p class="description-text description-text__hidden-tablet"><span class="description-text-span">Runtime:</span> ${obj.Runtime}</p>
+            <p class="description-text description-text__hidden-tablet"><span class="description-text-span">Awards:</span> ${obj.Awards}</p>
+            <p class="description-text description-text__hidden-laptop"><span class="description-text-span">IMDB Votes:</span> ${obj.imdbVotes}</p>
+            <p class="description-text description-text__hidden-tablet"><span class="description-text-span">Country:</span> ${obj.Country}</p>
+            <p class="description-text"><span class="description-text-span">Box Office:</span> ${obj.BoxOffice}</p>
           </div>
           <div class="plot-box">
             <h3 class="description-plot-title">Plot</h3>
-            <p class="description-plot-text">${arr[0].Plot}</p>
+            <p class="description-plot-text">${obj.Plot}</p>
           </div>`;
 };
 
@@ -28,7 +28,7 @@ const onClick = ({ target }) => {
     return;
   }
 
-  const currentMovie = movies.filter((el) => target.id === el.imdbID);
+  const [currentMovie] = movies.filter((el) => target.id === el.imdbID);
   movieBox.innerHTML = template(currentMovie);
 };
 
@@ -36,11 +36,13 @@ const markupMoviesList = (arr) => {
   return arr
     .map(
       ({ Title, imdbID }) =>
-        `<li class="list-movie-item"><button id=${imdbID} class="list-movie-btn" onclick="() => onClick()"><span class="list-movie-btn-text">${Title}</span></button></li>`
+        `<li class="list-movie-item"><button id=${imdbID} class="list-movie-btn"><span class="list-movie-btn-text">${Title}</span></button></li>`
     )
     .join("");
 };
 
 movieList.addEventListener("click", onClick);
 movieList.innerHTML = markupMoviesList(movies);
-movieBox.innerHTML = template(movies);
+
+const randomInteger = () => Math.floor(0 + Math.random() * (9 + 1 - 0));
+movieBox.innerHTML = template(movies[randomInteger()]);
