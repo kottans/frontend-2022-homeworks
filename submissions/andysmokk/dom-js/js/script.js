fcc2791e4e9116720cf12c405e72dfbabf9cf481
Andrii Konovalov <andy.smokk@gmail.com>
2022-09-02 21:37:02 +0200

    renamed some functions and variables

--- submissions/andysmokk/dom-js/js/script.js
+++ submissions/andysmokk/dom-js/js/script.js
@@ -3,23 +3,23 @@ import movies from "../data/data.json" assert { type: "json" };
 const movieList = document.querySelector(".list-movie");
 const movieBox = document.querySelector(".movie-box");
 
-const template = (obj) => {
-  return `<img class="img-poster" src=${obj.Poster} alt="poster">
+const getMarkupTemplate = (movie) => {
+  return `<img class="img-poster" src=${movie.Poster} alt="poster">
           <div class="description-box">
-            <h2 class="description-text description-title"> ${obj.Title}</h2>
-            <p class="description-text"><span class="description-text-span">Genre:</span> ${obj.Genre}</p>
-            <p class="description-text"><span class="description-text-span">IMDB:</span> ${obj.imdbRating}</p>
-            <p class="description-text description-text__hidden-laptop"><span class="description-text-span">Year:</span> ${obj.Year}</p>
-            <p class="description-text"><span class="description-text-span">Released:</span> ${obj.Released}</p>
-            <p class="description-text description-text__hidden-tablet"><span class="description-text-span">Runtime:</span> ${obj.Runtime}</p>
-            <p class="description-text description-text__hidden-tablet"><span class="description-text-span">Awards:</span> ${obj.Awards}</p>
-            <p class="description-text description-text__hidden-laptop"><span class="description-text-span">IMDB Votes:</span> ${obj.imdbVotes}</p>
-            <p class="description-text description-text__hidden-tablet"><span class="description-text-span">Country:</span> ${obj.Country}</p>
-            <p class="description-text"><span class="description-text-span">Box Office:</span> ${obj.BoxOffice}</p>
+            <h2 class="description-text description-title"> ${movie.Title}</h2>
+            <p class="description-text"><span class="description-text-span">Genre:</span> ${movie.Genre}</p>
+            <p class="description-text"><span class="description-text-span">IMDB:</span> ${movie.imdbRating}</p>
+            <p class="description-text description-text__hidden-laptop"><span class="description-text-span">Year:</span> ${movie.Year}</p>
+            <p class="description-text"><span class="description-text-span">Released:</span> ${movie.Released}</p>
+            <p class="description-text description-text__hidden-tablet"><span class="description-text-span">Runtime:</span> ${movie.Runtime}</p>
+            <p class="description-text description-text__hidden-tablet"><span class="description-text-span">Awards:</span> ${movie.Awards}</p>
+            <p class="description-text description-text__hidden-laptop"><span class="description-text-span">IMDB Votes:</span> ${movie.imdbVotes}</p>
+            <p class="description-text description-text__hidden-tablet"><span class="description-text-span">Country:</span> ${movie.Country}</p>
+            <p class="description-text"><span class="description-text-span">Box Office:</span> ${movie.BoxOffice}</p>
           </div>
           <div class="plot-box">
             <h3 class="description-plot-title">Plot</h3>
-            <p class="description-plot-text">${obj.Plot}</p>
+            <p class="description-plot-text">${movie.Plot}</p>
           </div>`;
 };
 
@@ -29,10 +29,10 @@ const onClick = ({ target }) => {
   }
 
   const [currentMovie] = movies.filter((el) => target.id === el.imdbID);
-  movieBox.innerHTML = template(currentMovie);
+  movieBox.innerHTML = getMarkupTemplate(currentMovie);
 };
 
-const markupMoviesList = (arr) => {
+const getMarkupMoviesList = (arr) => {
   return arr
     .map(
       ({ Title, imdbID }) =>
@@ -42,7 +42,7 @@ const markupMoviesList = (arr) => {
 };
 
 movieList.addEventListener("click", onClick);
-movieList.innerHTML = markupMoviesList(movies);
+movieList.innerHTML = getMarkupMoviesList(movies);
 
-const randomInteger = () => Math.floor(0 + Math.random() * (9 + 1 - 0));
-movieBox.innerHTML = template(movies[randomInteger()]);
+const getRandomInteger = () => Math.floor(0 + Math.random() * (9 + 1 - 0));
+movieBox.innerHTML = getMarkupTemplate(movies[getRandomInteger()]);
