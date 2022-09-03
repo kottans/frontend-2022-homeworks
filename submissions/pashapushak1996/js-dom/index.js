a78ec1024a1f15ce4a489359feb32e2135c13e34
pashapushak1996 <pashapushak1996@gmail.com>
2022-09-03 18:41:03 +0300

    refactoring according to mentor comment

--- submissions/pashapushak1996/js-dom/index.js
+++ submissions/pashapushak1996/js-dom/index.js
@@ -144,24 +144,22 @@ const menuLinkOnClick = () => {
     const sideMenuItems = document.querySelectorAll('.side-menu__item');
 
     const characterItems = document.querySelectorAll('.character');
-    sideMenuItems.forEach((item, index) => {
-        item.addEventListener('click', toggleClass(characterItems, index, 'character_hide'));
 
-        item.addEventListener('click', toggleClass(sideMenuItems, index, 'active', 'add'));
+    sideMenuItems.forEach((menuLink) => {
+
+        menuLink.addEventListener('click', ({ target }) => {
+            characterItems.forEach((character, index) => {
+                if (target.id === character.dataset['id']) {
+                    target.parentNode.classList.add('active');
+                    character.classList.remove('character_hide');
+                } else {
+                    character.classList.add('character_hide');
+                    sideMenuItems[index].classList.remove('active');
+                }
+            });
+        });
     });
 }
-
-const toggleClass = (arrayToChange, index, className, method = 'remove') => () => {
-    for (let i = 0; i < arrayToChange.length; i++) {
-        const item = arrayToChange[i];
-
-        if (i === index) {
-            method === 'add' ? item.classList.add(className) : item.classList.remove(className);
-        } else {
-            method === 'add' ? item.classList.remove(className) : item.classList.add(className);
-        }
-    }
-};
 
 const loadMainContent = () => {
     const characterBlocks = characters.map((character) => {
