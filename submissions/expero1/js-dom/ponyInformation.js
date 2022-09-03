c6d710ec58d1df88a985efcdee80f2ad75a772e6
eugene <eu.emets@gmail.com>
2022-09-03 18:30:28 +0300

    rename data.js to ponyInformation.js with data for loading to page

---
+++ submissions/expero1/js-dom/ponyInformation.js
@@ -0,0 +1,38 @@
+export const ponyInformation = [
+  {
+    name: "Twilight Sparkle",
+    description:
+      "<p><b>Twilight Sparkle</b> — is the central main character of My Little Pony Friendship is Magic. She is a female unicorn pony who transforms into an Alicorn and becomes a princess in Magical Mystery Cure. She is also the daughter of Twilight Velvet and Night Light, the younger sister of Shining Armor, sister-in-law to Princess Cadance, and paternal aunt to Flurry Heart.</p> <p>At the beginning of the series, she and Spike, her best friend and assistant, move from Canterlot to Golden Oak Library in Ponyville to study the magic of friendship, and in Twilight's Kingdom - Part 2, she gains her own castle—the Castle of Friendship—and gains the title Princess of Friendship. Twilight Sparkle represents the element of magic.</p>",
+    image: "img/Princess_Twilight_Sparkle_ID_S4E26.webp",
+  },
+  {
+    name: "Rainbow Dash",
+    description:
+      "<p><b>Rainbow Dash</b> — is a female Pegasus pony and one of the main characters in My Little Pony Friendship is Magic. </p><p>She maintains the weather and clears the skies in Ponyville. As a huge fan of the Wonderbolts, she becomes a reservist member of the elite flying group in Testing Testing 1, 2, 3 and a full member in Newbie Dash. In Sonic Rainboom, Rarity and Princess Celestia both declare that she is the best flier in all of Equestria. Rainbow Dash has a pet tortoise named Tank, whom she chooses out of Fluttershy's offered animals in the episode May the Best Pet Win! She represents the element of loyalty.</p>",
+    image: "img/Rainbow_Dash_Wonderbolt_fantasy_cropped_S1E3.webp",
+  },
+  {
+    name: "Rarity",
+    description:
+      "<p><b>Rarity</b> — is a female unicorn pony and one of the main characters of My Little Pony Friendship is Magic.</p><p> She is Sweetie Belle's older sister and the subject of Spike's long-term crush. Rarity works as both a fashion designer and a seamstress at her own shop in Ponyville, the Carousel Boutique. She has a white Persian cat named Opalescence. She represents the element of generosity.</p>",
+    image: "img/Rarity_ID_card.webp",
+  },
+  {
+    name: "Applejack",
+    description:
+      "<p><b><i>Applejack</i></b> — is a female Earth pony and one of the main characters of My Little Pony Friendship is Magic. She lives and works at Sweet Apple Acres with her grandmother Granny Smith, her older brother Big McIntosh, her younger sister Apple Bloom, and her dog Winona. She represents the element of honesty.</p><p>Applejack is one of the only two members of the main cast who were part of the original My Little Pony cast in the 1980s animated series, the other member being Spike. Lauren Faust, the show's developer, was inspired to create Applejack by her childhood toy G1 pony Applejack</p>",
+    image: "img/Applejack_S01E13_cropped.webp",
+  },
+  {
+    name: "Sunny Starscout",
+    description:
+      "<h3>Person</h3> <p>Sunny Starscout is the central main character of the My Little Pony Generation 5 film My Little Pony: A New Generation. She is a female Earth pony resident of Maretime Bay who dreams of making the world a better place and bringing magic and harmony back to Equestria after it disappeared a long time ago.</p><p>In Izzy's hometown of Bridlewood, Sunny and her friends acquire the Unicorn Crystal from Alphabittle, but nothing happens when the two crystals are joined together.Returning to Maretime Bay in despair, Sunny discovers the existence of a third crystal among her father's old belongings, and in her efforts to rejoin her friends, she gets swept up in a destructive plot to wage war against unicorns and Pegasi orchestrated by Hitch's overzealous deputy Sprout.</p > ",
+    image: "img/Sunny_Starscout.webp",
+  },
+  {
+    name: "Izzy Moonbow",
+    description:
+      " <h3>Person</h3 > <p><b>Izzy Moonbow</b> is a female unicorn pony and one of the main characters of My Little Pony: A New Generation and My Little Pony Generation 5. She is an energetic and optimistic resident of Bridlewood Forest and Sunny Starscout's first non-Earth pony friend who specializes in arts and crafts.</p> <p>Upon arriving in Zephyr Heights, Izzy and Sunny are imprisoned by the equally prejudiced Pegasi but soon freed by crown princess Zipp Storm, who reveals Pegasi do not have magic either and believes the reason for this to be the separation of two magical crystals (one possessed by the Pegasi, another by the unicorns). With Zipp's help, Izzy and Sunny steal the Pegasus Crystal from Zipp's mother Queen Haven and in the process end up recruiting Zipp's sister Pipp Petals as well as the pursuant Hitch Trailblazer in their quest to bring magic back to Equestria. During this time, she reveals that she came to Maretime Bay because she had discovered the invitation Sunny and her father had sent out years ago.</p>",
+    image: "img/IzzycgiID.webp",
+  },
+];
