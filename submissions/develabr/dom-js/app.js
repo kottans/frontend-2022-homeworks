b04e5b9092d74bf0763a63d8547c3b064411bd21
develabr <develabr@gmail.com>
2022-09-04 13:20:09 +0300

    Fix issues from code review

--- submissions/develabr/dom-js/app.js
+++ submissions/develabr/dom-js/app.js
@@ -15,45 +15,58 @@ iconMenu.addEventListener('click', function (e) {
 const listBtn = document.querySelector('.navbar-list');
 const main = document.querySelector('.main');
 
-let [kyiv, lviv, mykolaiv, odesa, ternopil, kharkiv, kherson, chernivtsi] =
+const [kyiv, lviv, mykolaiv, odesa, ternopil, kharkiv, kherson, chernivtsi] =
   cities;
 
-listBtn.onclick = function (event) {
-  let target = event.target;
-  if (target && target.classList.contains('navbar-link')) {
+listBtn.onclick = function ({ target }) {
+  if (target.classList.contains('navbar-link')) {
     const cityItem = cities.find(({ id }) => id === target.dataset.city);
     addContent(cityItem);
   }
 };
 
-const addContent = (item) => {
+const addContent = (city) => {
+  const {
+    id,
+    name,
+    state,
+    img,
+    emblem,
+    source,
+    population,
+    area,
+    founded,
+    code,
+    registration,
+    cityday,
+  } = city;
   main.innerHTML = `
 	<section class="main-section">
 	<header class="header-content">
-		<img class="emblem" src="${item.emblem}" alt="Герб ${item.name}" />
-		<span class="state">${item.name}, ${item.state}</span>
+		<img class="emblem" src="${emblem}" alt="Герб ${name}" />
+		<span class="state">${name}, ${state}</span>
 	</header>
 	<article class="content">
 		<figure class="image-wrap">
-			<img class="image" src="${item.img}" alt="Київ" />
+			<img class="image" src="${img}" alt="Київ" />
 			<figcaption class="img-caption">
-			${item.source}
+			${source}
 			</figcaption>
 		</figure>
 		<div class="description">
 			<dl class="char-wrap">
 				<dt class="char-wrap-atrr">Засноване</dt>
-				<dd class="char-wrap-value">${item.founded}</dd>
+				<dd class="char-wrap-value">${founded}</dd>
 				<dt class="char-wrap-atrr">Населення</dt>
-				<dd class="char-wrap-value">${item.population}</dd>
+				<dd class="char-wrap-value">${population}</dd>
 				<dt class="char-wrap-atrr">Площа</dt>
-				<dd class="char-wrap-value">${item.area}</dd>
+				<dd class="char-wrap-value">${area}</dd>
 				<dt class="char-wrap-atrr">Телефоний код</dt>
-				<dd class="char-wrap-value">${item.code}</dd>
+				<dd class="char-wrap-value">${code}</dd>
 				<dt class="char-wrap-atrr">Номери автомобілів</dt>
-				<dd class="char-wrap-value">${item.registration}</dd>
+				<dd class="char-wrap-value">${registration}</dd>
 				<dt class="char-wrap-atrr">День міста</dt>
-				<dd class="char-wrap-value">${item.cityday}</dd>
+				<dd class="char-wrap-value">${cityday}</dd>
 			</dl>
 		</div>
 	</article>
