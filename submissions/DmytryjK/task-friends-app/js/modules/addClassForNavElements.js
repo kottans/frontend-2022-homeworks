37382783ef4232f18fff7bf0cfddcd145a1b666d
Dmytro <dmytryj.kyzyma@gmail.com>
2022-09-04 18:45:37 +0300

    add more ui functional for adaptive menu

--- submissions/DmytryjK/task-friends-app/js/modules/addClassForNavElements.js
+++ submissions/DmytryjK/task-friends-app/js/modules/addClassForNavElements.js
@@ -3,6 +3,8 @@ function addActiveClass() {
     const burgerBtn = document.querySelector('.navigation-top__list-adaptive-burger');
     const filterMenu = document.querySelector('.navigation-left');
     const resetFiltersBtn = document.querySelector('.navigation-left__reset-btn');
+    const applyFiltersBtn = document.querySelector('.navigation-left__apply-btn');
+    const closeNavigation = document.querySelector('.navigation-left__closebtn');
 
     window.addEventListener('scroll', event => {
         if (window.scrollY > '0') {
@@ -16,11 +18,15 @@ function addActiveClass() {
         burgerBtn.classList.toggle('navigation-top__list-adaptive-burger-active');
         filterMenu.classList.toggle('navigation-left-active');
 
-        resetFiltersBtn.addEventListener('click', event => {
-            filterMenu.classList.remove('navigation-left-active');
-            burgerBtn.classList.remove('navigation-top__list-adaptive-burger-active');
-        });
+        resetFiltersBtn.addEventListener('click', closeMenu);
+        applyFiltersBtn.addEventListener('click', closeMenu);
+        closeNavigation.addEventListener('click', closeMenu);
     });
+
+    function closeMenu() {
+        filterMenu.classList.remove('navigation-left-active');
+        burgerBtn.classList.remove('navigation-top__list-adaptive-burger-active');
+    }
 }
 
 export default addActiveClass;
