3c6dc96f2d44c54d8e80e97146c4ae823fd8be62
Yevhen Kornilov <81849412+zhenyakornilov@users.noreply.github.com>
2022-09-05 22:11:56 +0300

    Document Object Model - practice (#159)
    Document Object Model - zhenyakornilov

---
+++ submissions/zhenyakornilov/js-dom/js/data.js
@@ -0,0 +1,92 @@
+const museums = [
+  {
+    id: 0,
+    name: 'National Museum "Chornobyl"',
+    btnName: "Chornobyl",
+    description:
+      "The National Museum “Chornobyl” is a multifunctional institution combining scientific, cultural and educational " +
+      "activity with a modern museum and archive, documenting,preservingand conveying the history of the Chornobyl " +
+      "nuclear disaster as the most severe radioecological disaster of the XX century. The after-effects of it have " +
+      "no analogy and differ from other natural or manmade catastrophes. These are still the issues of contemporaneity " +
+      "and, unfortunately, of the generation to come." +
+      "The Museum opened to the public on April 26, 1992,in Kiev, the capital of Ukraine, from a hundred " +
+      "kilometers of the epicenter of the disaster.",
+    imageSrc: "images/chernobyl.webp",
+    imageAlt: "photo of Chornobyl museum",
+    url: "http://chornobylmuseum.kiev.ua/uk/",
+  },
+  {
+    id: 1,
+    name: 'The first state "Science Museum" of the Small Academy of Sciences of Ukraine',
+    btnName: "Science",
+    description:
+      "The Museum of Science of the National Academy of Sciences is the first state museum where it is forbidden " +
+      "NOT to touch the exhibits. The visitor will find himself in a space where the most important scientific " +
+      "achievements of mankind, their laws and interaction are demonstrated in an understandable and accessible " +
+      "form. Here you can ride a bicycle with square wheels, go on a space trip, see a two-meter hologram and a " +
+      "multi-kilogram crystal, learn how the human body is built, and hold a model of the human brain and organs " +
+      "in your hands. In addition, many discoveries and impressions await visitors: from controlling a robot to being " +
+      'able to see your own voice and "metal memory", get inside a giant shell and into a mirror tunnel with an optical ' +
+      "illusion. Learn how colors are formed and create a rainbow. The Science Museum's largest interactive exhibit " +
+      'is a glass ceiling that changes color."',
+    imageSrc: "images/science.webp",
+    imageAlt: "photo of Science museum",
+    url: "https://sciencemuseum.com.ua/",
+  },
+  {
+    id: 2,
+    name: "Museum of the History of Ukraine in the Second World War",
+    btnName: "Ukraine in WWII",
+    description:
+      "With the change of name (before the museum was called the Museum of the Great Patriotic War), an exposition " +
+      "on the participation of the Ukrainian military in military conflicts in the world was added to the main " +
+      "exposition (which was created in Soviet times, but looks quite modern). There are also exhibitions dedicated " +
+      "to the liberation struggle of the UPA and the war in Eastern Ukraine. Many samples of military equipment are " +
+      'exhibited on the territory of the open-air museum. The most famous object of the museum is the "Motherland" ' +
+      'monument. There are two viewing platforms: "Landscape" at a height of 36.6 m, under the feet of a giant woman, ' +
+      'and "Extreme" - at a height of 91 m (on the hand with which she holds a shield). Only visitors over the age ' +
+      "of 18 are allowed on this site. The ascent is accompanied by an instructor and can be carried out only in good " +
+      "weather conditions (t °С from +5 to +30), without precipitation and strong gusts of wind.",
+    imageSrc: "images/ukraine_ww2.webp",
+    imageAlt: "photo of Ukraine in WWII museum",
+    url: "https://www.warmuseum.kiev.ua/",
+  },
+  {
+    id: 3,
+    name: "The National Museum of Natural History of the National Academy of Sciences of Ukraine",
+    btnName: "Natural",
+    description:
+      "The National Museum of Natural History at the National Academy of Sciences of Ukraine is one of the biggest " +
+      "scientific research museums of the type in the world. It was established in 1966 as a unified complex comprising " +
+      "five museums: the Geological, Paleonto- logical, Zoological and Botanical. The National Museum of Natural History " +
+      "is located in the heart of Kyiv, in an antique mansion with expressive and remarkable architecture. " +
+      "The Museum complex itself was built in accordance with the classical approach to museum study. More than 30 " +
+      "thousand speciments which are exhibited in 24 halls occupying 8 thousand square meters illustrate the origin, " +
+      "structure and evolution of the Earth, its plant and animal life, its past and present, the history of the material " +
+      "cul­ture of the tribes and peoples that inhabited the territory of Ukraine. The central place in the museums is " +
+      "occupied by a complex of dioramas: 30 landscape expositions and biogroups. The National Museum of Natural History " +
+      "is headed by Academician Yevhen Shniukov, the National Academy of Sciences of Ukraine.",
+    imageSrc: "images/natural_history.webp",
+    imageAlt: "photo of Natural museum",
+    url: "https://museumkiev.org/",
+  },
+  {
+    id: 4,
+    name: "National Museum of the History of Ukraine",
+    btnName: "Ukraine's history",
+    description:
+      "The National Museum of the History of Ukraine is one of the leading museums of Ukraine, founded in 1904. " +
+      "The museum's funds include more than 800,000 unique monuments of history and culture. Archaeological and " +
+      "numismatic collections are considered especially valuable. Collections of cold and firearms, ethnographies, " +
+      "unique editions of books, glassware and porcelain are no less interesting. The collection of paintings, graphics, " +
+      "and sculptures includes about 10,000 exhibits. There is also a collection of documents of the XV-XXI centuries, " +
+      "Cossack attributes, universals, letters, letters of hetmans and Cossack elders. The Museum of Historical Jewels " +
+      "of Ukraine is a branch that houses unique collections of precious metals and stones. The museum is a real treasury " +
+      "of national historical and cultural relics of the people.",
+    imageSrc: "images/ukraine_history.webp",
+    imageAlt: "photo of Ukraine's history museum",
+    url: "https://nmiu.org/",
+  },
+];
+
+export default museums;
