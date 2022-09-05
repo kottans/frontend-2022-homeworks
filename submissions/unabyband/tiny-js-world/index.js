8b5370ec624eb0a01418683a3ddc3137529cb9c2
unabyband <110461462+unabyband@users.noreply.github.com>
2022-09-05 20:50:24 +0300

    Update file after prettier

--- submissions/unabyband/tiny-js-world/index.js
+++ submissions/unabyband/tiny-js-world/index.js
@@ -1,60 +1,60 @@
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
+   Code repository: https://github.com/unabyband/a-tiny-JS-world
+   Web app: https://unabyband.github.io/a-tiny-JS-world/
    */
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 
-   const inhabitants = [
-      dog = {
-         species: 'dog',
-         name: 'Rex',
-         gender: 'male',
-         legs: 4,
-         hands: 0,
-         saying: 'woof',
-         friends: 'John, Jenny'
-      },
-      cat = {
-         species: 'cat',
-         name: 'Tom',
-         gender: 'male',
-         legs: 4,
-         hands: 0,
-         saying: 'meow',
-         friends: 'Jenny'
-      },
-      woman = {
-         species: 'human',
-         name: 'Jenny',
-         gender: 'female',
-         legs: 2,
-         hands: 2,
-         saying: 'Hi, John!',
-         friends: 'John, Tom'
-      },
-      man = {
-         species: 'human',
-         name: 'John',
-         gender: 'male',
-         legs: 2,
-         hands: 2,
-         saying: 'Hi, Jenny!',
-         friends: 'Jenny, Rex, Tom'
-      },
-      cat_woman = {
-         species: cat.species + '-' + woman.species,
-         name: woman.name,
-         gender: 'no gender',
-         legs: woman.legs,
-         hands: woman.hands,
-         saying: cat.saying,
-         friends: 0
-      }
-   ];
+const inhabitants = [
+  (dog = {
+    species: "dog",
+    name: "Rex",
+    gender: "male",
+    legs: 4,
+    hands: 0,
+    saying: "woof",
+    friends: "John, Jenny",
+  }),
+  (cat = {
+    species: "cat",
+    name: "Tom",
+    gender: "male",
+    legs: 4,
+    hands: 0,
+    saying: "meow",
+    friends: "Jenny",
+  }),
+  (woman = {
+    species: "human",
+    name: "Jenny",
+    gender: "female",
+    legs: 2,
+    hands: 2,
+    saying: "Hi, John!",
+    friends: "John, Tom",
+  }),
+  (man = {
+    species: "human",
+    name: "John",
+    gender: "male",
+    legs: 2,
+    hands: 2,
+    saying: "Hi, Jenny!",
+    friends: "Jenny, Rex, Tom",
+  }),
+  (cat_woman = {
+    species: cat.species + "-" + woman.species,
+    name: woman.name,
+    gender: "no gender",
+    legs: woman.legs,
+    hands: woman.hands,
+    saying: cat.saying,
+    friends: 0,
+  }),
+];
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
@@ -78,9 +78,18 @@
 // var test = man;
 // test.saying = "<h2>Where's the money, Lebowski?</h2>";
 
-function printAll (args) {
-   print([args.species,'<strong>' + args.name + '</strong>', args.gender, args.legs, 
-         args.hands,'<em>' + args.saying + '</em>', args.friends].join('; '));
+function printAll(args) {
+  print(
+    [
+      args.species,
+      "<strong>" + args.name + "</strong>",
+      args.gender,
+      args.legs,
+      args.hands,
+      "<em>" + args.saying + "</em>",
+      args.friends,
+    ].join("; ")
+  );
 }
 
-inhabitants.forEach(element => printAll(element));
+inhabitants.forEach((element) => printAll(element));
