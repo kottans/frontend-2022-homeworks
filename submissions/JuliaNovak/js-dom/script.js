39397b2add55e4f763cd0af0e8a0b3a1ffb73a26
JuliaNovak <julia.novak9@gmail.com>
2022-09-05 12:10:17 +0200

    implement dataset method

--- submissions/JuliaNovak/js-dom/script.js
+++ submissions/JuliaNovak/js-dom/script.js
@@ -1,68 +1,78 @@
 const methods = [
-    {
-        id: 1,
-        type: 'Array.prototype.filter()',
-        description: 'The filter() method creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that pass the test implemented by the provided function.',
-        img: './img/filter.png'
-    },
-    {
-        id: 2,
-        type: 'Array.prototype.find()',
-        description: 'The find() method returns the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned.',
-        img: './img/find.png'
-    },
-    {
-        id: 3,
-        type: 'Array.prototype.forEach()',
-        description: 'The forEach() method executes a provided function once for each array element.',
-        img: './img/forEach.png'
-    },
-    {
-        id: 4,
-        type: 'Array.prototype.map()',
-        description: 'The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.',
-        img: './img/map.png'
-    },
-    {
-        id: 5,
-        type: 'Array.prototype.push()',
-        description: 'The push() method adds one or more elements to the end of an array and returns the new length of the array.',
-        img: './img/push.png'
-    },
-    {
-        id: 6,
-        type: 'Array.prototype.toString()',
-        description: 'The toString() method returns a string representing the specified array and its elements.',
-        img: './img/toString.png'
-    },
-]
+   {
+      id: 1,
+      type: "Array.prototype.filter()",
+      description:
+         "The filter() method creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that pass the test implemented by the provided function.",
+      img: "./img/filter.png",
+   },
+   {
+      id: 2,
+      type: "Array.prototype.find()",
+      description:
+         "The find() method returns the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned.",
+      img: "./img/find.png",
+   },
+   {
+      id: 3,
+      type: "Array.prototype.forEach()",
+      description:
+         "The forEach() method executes a provided function once for each array element.",
+      img: "./img/forEach.png",
+   },
+   {
+      id: 4,
+      type: "Array.prototype.map()",
+      description:
+         "The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.",
+      img: "./img/map.png",
+   },
+   {
+      id: 5,
+      type: "Array.prototype.push()",
+      description:
+         "The push() method adds one or more elements to the end of an array and returns the new length of the array.",
+      img: "./img/push.png",
+   },
+   {
+      id: 6,
+      type: "Array.prototype.toString()",
+      description:
+         "The toString() method returns a string representing the specified array and its elements.",
+      img: "./img/toString.png",
+   },
+];
 
-const methodsList = document.getElementsByClassName('methods-list')[0]
-methods.forEach(el => {
-    const li = document.createElement('li')
-    li.innerText = el.type
-    li.classList.add("methods-list__list-item");
+const methodsList = document.querySelector(".methods-list");
 
-    const img = document.createElement('img')
-    img.classList.add("methods-description__image-example");
-    img.src = el.img
+methods.forEach((el) => {
+   const li = document.createElement("li");
+   li.innerText = el.type;
+   li.setAttribute("data-id", el.id);
+   li.classList.add("methods-list__list-item");
 
-    const title = document.createElement('h4')
-    title.innerText = el.type
-    title.classList.add("title");
-    
-    const description = document.createElement('p')
-    description.innerText = el.description
+   const img = document.createElement("img");
+   img.classList.add("methods-description__image-example");
+   img.src = el.img;
 
-    const methodsDescription = document.querySelector('.methods-description')
-    
-    li.addEventListener('click', () => {
-        methodsDescription.innerText=''
+   const title = document.createElement("h4");
+   title.innerText = el.type;
+   title.classList.add("title");
 
-        methodsDescription.appendChild(title)
-        methodsDescription.appendChild(description)
-        methodsDescription.appendChild(img)
-    })
+   const description = document.createElement("p");
+   description.innerText = el.description;
 
-    methodsList.append(li)
-})
+   const methodsDescription = document.querySelector(".methods-description");
+   const onClick = ({ target }) => {
+      const selectedLi = target.dataset.id;
+      const el = methods.find((method) => method.id == selectedLi);
+      methodsDescription.innerText = "";
+
+      methodsDescription.appendChild(title);
+      methodsDescription.appendChild(description);
+      methodsDescription.appendChild(img);
+   };
+
+   li.addEventListener("click", onClick);
+   methodsList.append(li);
+});
