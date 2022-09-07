05c1692f2ee77ee53523b5441e5352c58ddda569
Volodymyr Franchuk <franchuk.vldr@gmail.com>
2022-09-07 20:14:41 +0300

    Rewroted main print function using Array#map and Array#forEach methods, rename a few variables

--- submissions/franchukv/a-tiny-js-world/index.js
+++ submissions/franchukv/a-tiny-js-world/index.js
@@ -1,15 +1,11 @@
 /* 
    Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
-
    Code repository: https://github.com/franchukv/a-tiny-JS-world
    Web app: https://franchukv.github.io/a-tiny-JS-world/
 */
 
 // ======== OBJECTS DEFINITIONS ========
-
-// Define your objects here
-
 const man = {
    species: 'human',
    gender: 'male',
@@ -18,7 +14,7 @@ const man = {
    hands: 2,
    feets: 2,
    saying: 'Sheikh Ma Shieraki Anni.',
-};
+}
 
 const woman = {
    species: 'human',
@@ -29,7 +25,7 @@ const woman = {
    feets: 2,
    saying: 'Drakaris!',
    friends: `Fell in love with ${man.name}.`,
-};
+}
 
 const dog = {
    species: 'dog',
@@ -40,7 +36,7 @@ const dog = {
    feets: 4,
    saying: 'woof-woof!',
    friends: `Only ${man.name} is friends.`,
-};
+}
 
 const cat = {
    species: 'cat',
@@ -51,7 +47,7 @@ const cat = {
    feets: 4,
    saying: 'Meow, bow to me!',
    friends: 'No friends, only servants!'
-};
+}
 
 const catwoman = {
    species: 'human',
@@ -62,27 +58,20 @@ const catwoman = {
    feets: 2,
    saying: cat.saying,
    friends: `In astral connection with ${cat.name}.`,
-};
-
-// ======== OUTPUT ========
-
-const inhabitants = [man, woman, dog, cat, catwoman];
-
-const objToString = (obj) => {
-   const propNames = Object.getOwnPropertyNames(obj);
-   let propValuesArr = [];
-
-   for (let i = 0; i < propNames.length; i++) {
-      propValuesArr.push(obj[propNames[i]]);
-   }
-
-   return propValuesArr.join('; ');
-};
-
-function greetings(arr, func) {
-   for (let i = 0; i < arr.length; i++) {
-      print(func(arr[i]));
-   }
 }
 
-greetings(inhabitants, objToString);
+// ======== OUTPUT ========
+const inhabitants = [man, woman, dog, cat, catwoman];
+const properties = ['species', 'gender', 'name', 'status', 'hands', 'feets', 'saying', 'friends'];
+
+function objectToString(habitant) {
+   let temporaryArray = [];
+
+   properties.forEach((property) => {
+      temporaryArray.push(habitant[property]);
+   })
+
+   return temporaryArray.join('; ');
+}
+
+inhabitants.map((habitant) => { print(objectToString(habitant)) });
