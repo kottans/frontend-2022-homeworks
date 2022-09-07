6e9097a4e574ce2c11558f31f97aa54ebbbfd2f5
NadiaVorontsova <Vorontsovvaaa@gmail.com>
2022-09-07 21:26:14 +0300

    Some changes after review

--- submissions/NadiaVorontsova/classic-frogger-game/app.js
+++ submissions/NadiaVorontsova/classic-frogger-game/app.js
@@ -1,24 +1,26 @@
-const startPlayerPositionX = 200,
-    startPlayerPositionY = 390,
-    cellWidth = 101,
-    cellHeight = 83,
-    startX = 0,
-    startY = 0,
-    rightWall = 400,
-    bottomWall = 350;
-
+const START_PLAYER_POSITION_X = 200,
+    START_PLAYER_POSITION_Y = 390,
+    CELL_WIDTH = 83,
+    CELL_HEIGHT = 101,
+    START_X = 0,
+    START_Y = 0,
+    RIGHT_WALL = 400,
+    BOTTOM_WALL = 350,
+    CHAR_BOY = 55,
+    ENEMY = 40;
+    
 let countScore = 0;
 const scopeElement = document.createElement('div');
 scopeElement.style.cssText = 'font-size:25px; padding-top:20px; margin-bottom:-20px';
 document.body.appendChild(scopeElement);
 
-const Player = function (x, y) {
+const Player = function () {
     this.resetPlayerPosition();
     this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.update = function () {
-    if (this.y < startY) {
+    if (this.y < START_Y) {
         this.resetPlayerPosition();
         countScore++;
         score();
@@ -30,26 +32,26 @@ Player.prototype.render = function () {
 };
 
 Player.prototype.resetPlayerPosition = function () {
-    this.x = startPlayerPositionX;
-    this.y = startPlayerPositionY;
+    this.x = START_PLAYER_POSITION_X;
+    this.y = START_PLAYER_POSITION_Y;
 }
 
 Player.prototype.handleInput = function (route) {
     switch (route) {
         case 'left':
-            if (this.x > startX)
-                this.x -= cellWidth;
+            if (this.x > START_X)
+                this.x -= CELL_HEIGHT;
             break;
         case 'up':
-            this.y -= cellHeight;
+            this.y -= CELL_WIDTH;
             break;
         case 'right':
-            if (this.x < rightWall)
-                this.x += cellWidth;
+            if (this.x < RIGHT_WALL)
+                this.x += CELL_HEIGHT;
             break;
         case 'down':
-            if (this.y < bottomWall)
-                this.y += cellHeight;
+            if (this.y < BOTTOM_WALL)
+                this.y += CELL_WIDTH;
             break;
         default:
             break;
@@ -58,9 +60,10 @@ Player.prototype.handleInput = function (route) {
 
 const player = new Player();
 
-const Enemy = function (x, y) {
+const Enemy = function (x, y, player) {
     this.x = x;
     this.y = y;
+    this.player = player;
     this.speed = Math.random() * 200 + 80;
     this.sprite = 'images/enemy-bug.png';
 };
@@ -68,10 +71,10 @@ const Enemy = function (x, y) {
 Enemy.prototype.update = function (dt) {
     this.x += this.speed * dt;
     if (this.x > ctx.canvas.width) {
-        this.x = startX;
+        this.x = START_X;
     }
     if (this.checkCollision()) {
-        player.resetPlayerPosition();
+        this.player.resetPlayerPosition();
     }
 };
 
@@ -80,8 +83,8 @@ Enemy.prototype.render = function () {
 };
 
 Enemy.prototype.checkCollision = function () {
-    if (Math.abs(this.x - player.x) < 50 && Math.abs(this.y - player.y) < 50) {
-        player.resetPlayerPosition();
+    if (Math.abs(this.x - this.player.x) < CHAR_BOY && Math.abs(this.y - this.player.y) < CHAR_BOY) {
+        this.player.resetPlayerPosition();
         if (countScore > 0) {
             countScore--;
             score();
@@ -92,11 +95,14 @@ Enemy.prototype.checkCollision = function () {
 const allEnemies = [];
 
 function createEnemies() {
-    const positionY = [60, 140, 220];
-    for (let y of positionY) {
-        const randomPositionX = Math.random() * (-100) - 100;
-        allEnemies.push(new Enemy(randomPositionX, y));
-    }
+    const positionY = [
+        CELL_HEIGHT - ENEMY,
+        CELL_HEIGHT * 1.8 - ENEMY, 
+        CELL_HEIGHT * 2.6 - ENEMY
+    ];
+    positionY.map((y)=>{
+        allEnemies.push(new Enemy(START_X, y, player));
+    });
 };
 
 function score() {
