5bf76ef59d1004716706d0168c2124a9dddeb4fc
vovan-zt <predator_vovan@ukr.net>
2022-09-08 17:53:44 +0300

    updated functionality

--- submissions/vovan-zt/app_friends/script.js
+++ submissions/vovan-zt/app_friends/script.js
@@ -1,171 +1,175 @@
+﻿const url = 'https://randomuser.me/api/?results=20'
 
-window.addEventListener('DOMContentLoaded', () => {
+const myForm = document.querySelector('.form'),
+    cards = document.querySelector('.cards'),
+    resetBtn = document.querySelector('.filter__reset-button'),
+    hamburgerBtn = document.querySelector('.hamburger'),
+    filter = document.querySelector('.filter'),
+    closeBtn = document.querySelector('.filter__close')
 
-    async  function getResponse () {
-        let response = await fetch('https://randomuser.me/api/?results=20');
-        let content = await response.json(); 
+let dataBase = []
+let sortedDataBase = []
 
-        const {results} = content;
-        let dataBase = [...results];
-
-        function render (data) {
-            data.forEach (item => {
-                let card = document.createElement('div')
-                card.classList.add('cards__wrapper');
-              
-                card.innerHTML = `
-                    <div class="cards__wrapper-name">
-                        <h3> ${item.name.first} ${item.name.last}</h3>
-                    </div>
-
-                    <div class="cards__wrapper-photo">
-                        <img src=${item.picture.large} alt="photo">
-                    </div>
-
-                    <div class="cards__wrapper-years">
-                    I have ${item.dob.age} years old
-                    </div>
-
-                    <div class="cards__wrapper-email">
-                        ${item.email}
-                    </div>
-
-                    <div class="cards__wrapper-phone">
-                        ${item.cell}
-                    </div>
-
-                    <div class="cards__wrapper-city">
-                        ${item.location.city}
-                    </div>
-
-                    <hr class="devider">
-
-                    <div class="cards__wrapper-sex">${item.gender}</div>
-                
-                `
-                document.querySelector('.cards').appendChild(card);
-               
-            });
-
-               
-            document.querySelectorAll('.cards__wrapper-sex').forEach(item => {
-                if (item.innerHTML =="female") {
-                    item.classList.add('color_violet');
-                    item.parentNode.firstElementChild.classList.add('bgcolor_violet'); 
-                } 
-            });
-        }
-        render (dataBase);
-
-        let cardsWrapper = document.querySelectorAll('.cards'),
-            ageUp = document.querySelector('.filter__age-up'),
-            ageDown = document.querySelector('.filter__age-down'),
-            ageSearch = document.querySelector('.filter__age-searchTerm'),
-            nameUp = document.querySelector('.filter__name-up'),
-            nameDown = document.querySelector('.filter__name-down'),
-            nameSearch = document.querySelector('.filter__name-searchTerm'),
-            male = document.querySelector('#male'),
-            female = document.querySelector('#female'),
-            allPeople = document.querySelector('#all'),
-            reset = document.querySelector('.filter__reset-button');
-
-        function deleteDataBase () {
-            cardsWrapper.forEach(item => {
-                item.innerHTML = '';
-            }) 
-        }  
-
-        // Сортировка по возрасту
-        function sortByAgeUp(arr) {
-            arr.sort((a, b) => a.dob.age > b.dob.age ? 1 : -1);
-        }
-
-        function sortByAgeDown(arr) {
-            arr.sort((a, b) => a.dob.age < b.dob.age ? 1 : -1);
-        }
-
-        function filterAge(filter, sorting) { 
-            filter.addEventListener('click', e => {
-                e.preventDefault();
-                sorting(dataBase);
-                deleteDataBase();        
-                render(dataBase);
-            });
-        }
-
-        function sortByAge() {
-            ageSearch.addEventListener('input', e => {
-                let target = e.target.value;
-                console.log(target.length)
-                let newarr = dataBase.filter(age => age.dob.age == target);
-                deleteDataBase();     
-                render(newarr);
-                if (target.length == 0) {
-                    render(dataBase);
-                }
-            })
-        }
-
-        filterAge(ageUp, sortByAgeUp);
-        filterAge(ageDown, sortByAgeDown);
-        sortByAge();
-
-
-         // Сортировка по имени
-        function sortByNameUp(arr) {
-            arr.sort((a, b) => a.name.first > b.name.first ? 1 : -1);
-        }
-
-        function sortByNamedDown(arr) {
-            arr.sort((a, b) => a.name.first < b.name.first ? 1 : -1);
-        }
-
-        function sortByName() {
-            nameSearch.addEventListener('input', e => {
-                let target = e.target.value;
-                console.log(target.length)
-                let newarr = dataBase.filter(age => age.name.first.toLowerCase() == target.toLowerCase());
-                deleteDataBase();     
-                render(newarr);
-                if (target.length == 0) {
-                    render(dataBase);
-                }
-            })
-        }
-
-         filterAge(nameUp, sortByNameUp);
-         filterAge(nameDown, sortByNamedDown);
-         sortByName()
-
-
-         // Сортировка по полу
-         function sortBySex(gender) {
-            gender.addEventListener('change', e => {
-                let target = e.target.value;
-                console.log(target)
-                let newarr = dataBase.filter(gender => gender.gender == target);
-                deleteDataBase();     
-                render(newarr);
-                if (target == 'all') {
-                    render(dataBase);
-                }
-            })
-        }
-
-        sortBySex(male);
-        sortBySex(female);
-        sortBySex(allPeople);
-
-
-        // сброс фильтров
-        function resetFilter () {
-            reset.addEventListener('click', e => {
-                e.preventDefault();
-                deleteDataBase();     
-                render(results);
-            });
-        }  
-        resetFilter();
+async function getResponse() {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        const { results } = await data
+        dataBase = [...results]
+        sortedDataBase = [...dataBase]
+        showCards(dataBase)
+    } catch (e) {
+        console.error(e)
     }
-    getResponse ();
-});
+}
+
+function createCards({ picture, name, dob, gender, location, email, cell }) {
+    const cardSection = document.createElement('section')
+    cardSection.classList.add('cards__wrapper')
+
+    let colorGender = ''
+    let bgcolorName = ''
+    if (gender == 'female') {
+        colorGender = 'color_violet'
+        bgcolorName = 'bgcolor_violet'
+    }
+
+    cardSection.innerHTML = `
+    <div class="cards__wrapper-name ${bgcolorName}">
+    <h3> ${name.first} ${name.last}</h3>
+    </div>
+
+    <div class="cards__wrapper-photo">
+      <img src=${picture.large} alt="">
+    </div>
+
+    <div class="cards__wrapper-years">
+    I have ${dob.age} years old
+    </div>
+
+    <div class="cards__wrapper-email">
+    <a href='${email}'>${email}</a>
+    </div>
+
+    <div class="cards__wrapper-phone">
+    <a  href="tel:+ ${cell}"> ${cell}</a>
+    </div>
+
+    <div class="cards__wrapper-city">
+      ${location.city}
+    </div>
+
+    <hr class="devider">
+
+    <div class="cards__wrapper-gender ${colorGender}">${gender}</div>
+
+  `
+    cards.append(cardSection)
+}
+
+function showCards(dataBase) {
+    dataBase.forEach(
+        ({ picture, name, dob, gender, location, email, cell }) => {
+            return createCards({
+                picture,
+                name,
+                dob,
+                gender,
+                location,
+                email,
+                cell,
+            })
+        }
+    )
+}
+
+function searchByInput(sortedDataBase) {
+    if (myForm.searchAge.value) {
+        return inputSearchAge(sortedDataBase)
+    } else if (myForm.searchName.value) {
+        return inputSearchName(sortedDataBase)
+    } else {
+        return sortedDataBase
+    }
+}
+
+function inputSearchAge(dataArray) {
+    return dataArray.filter((card) => {
+        const age = `${card.dob.age}`
+        return age.includes(myForm.searchAge.value)
+    })
+}
+
+function inputSearchName(dataArray) {
+    return dataArray.filter((card) => {
+        const fullName = `${card.name.first} ${card.name.last}`
+        return fullName
+            .toLowerCase()
+            .includes(myForm.searchName.value.toLowerCase())
+    })
+}
+
+const compareAge = (firstUser, secondUser) =>
+    firstUser.dob.age - secondUser.dob.age
+
+const compareName = (firstUser, secondUser) =>
+    firstUser.name.first.toLowerCase() <= secondUser.name.first.toLowerCase()
+        ? -1
+        : 1
+
+function sortingUsers(dataArray) {
+    if (myForm.sorting.value === 'ageUp') {
+        return dataArray.sort((a, b) => compareAge(a, b))
+    } else if (myForm.sorting.value === 'ageDown') {
+        return dataArray.sort((a, b) => compareAge(b, a))
+    }
+
+    if (myForm.sorting.value === 'nameUp') {
+        return dataArray.sort((a, b) => compareName(a, b))
+    } else if (myForm.sorting.value === 'nameDown') {
+        return dataArray.sort((a, b) => compareName(b, a))
+    }
+}
+
+function filterByGender(dataArray) {
+    if (myForm.male.checked) {
+        return dataArray.filter((user) => user.gender === 'male')
+    }
+    if (myForm.female.checked) {
+        return dataArray.filter((user) => user.gender === 'female')
+    }
+    if (myForm.all.checked) {
+        return dataArray
+    }
+}
+
+function resetFilters() {
+    sortedDataBase = [...dataBase]
+    cards.innerHTML = ''
+    myForm.reset()
+    showCards(sortedDataBase)
+}
+
+function changeForm() {
+    sortingUsers(sortedDataBase)
+    const filterGender = filterByGender(sortedDataBase)
+    const searchInput = searchByInput(filterGender)
+
+    cards.innerHTML = ''
+    showCards(searchInput)
+    return searchInput
+}
+
+function toggleFilter() {
+    filter.classList.toggle('filter__active')
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    getResponse()
+
+    hamburgerBtn.addEventListener('click', toggleFilter)
+    closeBtn.addEventListener('click', toggleFilter)
+    myForm.addEventListener('input', changeForm)
+    myForm.addEventListener('submit', (e) => e.preventDefault())
+    resetBtn.addEventListener('click', resetFilters)
+})
