6c60bb1b28fb185e42909bd288600202366fa4f9
Mifaresss <106471161+Mifaresss@users.noreply.github.com>
2022-09-08 19:34:48 +0300

    Added properties: legs and arms

--- представления/Мифаресс/крошечный-js-мир/index.js
+++ представления/Мифаресс/крошечный-js-мир/index.js
@@ -1,17 +1,19 @@
-function Population(name, species, gender, saying) {
+function Population(name, species, gender, legs, hands, saying) {
    this.name = name;
    this.species = species;
    this.gender = gender;
+   this.legs = legs;
+   this.hands = hands;
    this.saying = saying;
 }
 
-const dog = new Population('Baxter', 'dog', 'male', 'Woof-woof!');
-const cat = new Population('Lunya', 'cat', 'female', 'Meeeow!');
-const man = new Population('Alex', 'human', 'male', 'Hello!');
-const women = new Population('Alice', 'human', 'female', 'Hi!');
+const dog = new Population('Baxter', 'dog', 'male', 4, 0, 'Woof-woof!');
+const cat = new Population('Lunya', 'cat', 'female', 4, 0, 'Meeeow!');
+const man = new Population('Alex', 'human', 'male', 2, 2, 'Hello!');
+const women = new Population('Alice', 'human', 'female', 2, 2, 'Hi!');
 
 const inhabitants = [dog, cat, man, women];
-const inhabitantsProperties = ['name', 'species', 'gender', 'saying'];
+const inhabitantsProperties = ['name', 'species', 'gender', 'legs', 'hands', 'saying'];
 
 inhabitants.forEach(inhabitant => {
    print(inhabitantsProperties.map(property => inhabitant[property]).join('; '))
