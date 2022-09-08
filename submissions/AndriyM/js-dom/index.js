e37b514d70ea7d5b3a1013b22d1112027e8e7df8
andreymashko7 <63639567+andreymashko7@users.noreply.github.com>
2022-09-08 23:14:55 +0300

    update index.js, fix variables

--- submissions/AndriyM/js-dom/index.js
+++ submissions/AndriyM/js-dom/index.js
@@ -7,25 +7,29 @@ const links = document.querySelectorAll(".sidebar__link")
 function createCardsMarcup(cards) {
     return cards
         .map(
-            ({ id, url, title }) =>
-                `<div class="card__item" id="${id}">
+            ({
+                id,
+                url,
+                title
+            }) =>
+            `<div class="card__item" id="${id}">
                     <img src="${url}" class="card__img" alt=""/>
                     <p class="card__title">${title}</p>
-                </div>`
+            </div>`
         )
         .join("")
 }
 
-const cardsMarcup = createCardsMarcup(cards)
-rootElement.innerHTML = cardsMarcup
+rootElement.innerHTML = createCardsMarcup(cards)
 const cardRefs = document.querySelectorAll(".card__item")
-
 sideBarContainer.addEventListener("click", onsideBarElementClick)
 
-function onsideBarElementClick({ target }) {
+function onsideBarElementClick({
+    target
+}) {
     links.forEach((link) => link.classList.remove("active"))
     target.classList.add("active")
-    let currentActivetLink = target.dataset.id
+    const currentActivetLink = target.dataset.id
 
     cardRefs.forEach((card) => {
         if (card.classList.contains("visible")) {
