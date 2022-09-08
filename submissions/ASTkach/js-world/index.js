c4eb357ec5aff8a14b68ee0a329b50511dd570f7
ASTkach <anatolii.s.tkach@gmail.com>
2022-09-08 20:52:01 +0300

    Made my code dry

--- submissions/ASTkach/js-world/index.js
+++ submissions/ASTkach/js-world/index.js
@@ -1,12 +1,3 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
-
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
 const dog = {
     species: "dog",
     name: "Elvis",
@@ -48,36 +39,6 @@ const man = {
 };
 
 let jsWorld = [dog, cat, woman, man];
+let properties = ["species", "name", "gender", "legs", "hands", "saying", "friends"];
 
-let result = jsWorld.map((item) => {
-    return item.species + "; " + 
-           item.name + "; " + 
-           item.gender + "; " + 
-           item.legs + "; " + 
-           item.hands + "; " + 
-           item.saying + "; " + 
-           item.friends;
-});
-
-for (let i = 0; i < result.length; i++) {
-    print(`<strong>${result[i]}</strong>`);
-}
-
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+jsWorld.map((item) => print(properties.map((element) => item[element]).join(";")));
