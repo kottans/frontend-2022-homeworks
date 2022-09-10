3e90fdc509d737516322853e1503554cedea84bc
Ihor <107474617+IhorOzerov@users.noreply.github.com>
2022-09-10 23:38:45 +0300

    Updated Frogger app
    fixed: assignment of constants, separation of functions

--- submissions/IhorOzerov/frogger/app.js
+++ submissions/IhorOzerov/frogger/app.js
@@ -1,93 +1,107 @@
-const canvasWidth = 505;
-const refreshTime = 500;
-const yStart = 0;
-const xStart = 0;
+const config = {
+    CANVAS_WIDTH : 505,
+    REFRESH_TIME : 500,
+    Y_START : 0,
+    X_START : 0,
 
-const enemyConst = {
-    refresh: -101,
-    locations: [62, 144, 228],
-    speed: (Math.random() * 200) + 200,
-    sprite: "images/enemy-bug.png"
-}
+    ENEMY_CONST: {
+        refresh: -101,
+        locations: [62, 144, 228],
+        speed: (Math.random() * 200) + 200,
+        sprite: "images/enemy-bug.png"
+    },
 
-const playerConst = {
-    x: 203,
-    y: 390,
-    stepX: 102,
-    stepY: 83,
-    sprite: "images/char-boy.png",
-    height: 60,
-    width: 70,
-    rightBorder: 400
-}
+    PLAYER_CONST: {
+        x: 203,
+        y: 390,
+        stepX: 102,
+        stepY: 83,
+        sprite: "images/char-boy.png",
+        height: 60,
+        width: 70,
+        rightBorder: 400
+    }
+};
 
 const Enemy = function (x, y) {
     this.x = x;
     this.y = y;
-    this.speed = enemyConst.speed;
-    this.sprite = enemyConst.sprite;
+    this.speed = config.ENEMY_CONST.speed;
+    this.sprite = config.ENEMY_CONST.sprite;
 }; 
 
-const allEnemies = [];
+Enemy.prototype.update = function (dt) {
+    this.x += config.ENEMY_CONST.speed * dt;
+    if (this.x > config.CANVAS_WIDTH) {
+        this.x = config.ENEMY_CONST.refresh;
+    };
+    this.checkCollision();
+};
 
-enemyConst.locations.forEach(function(enemyY){
-    const startPoint = Math.random()*canvasWidth;
-    const enemy = new Enemy(startPoint, enemyY, enemyConst.speed);
-    allEnemies.push(enemy);
-});
-
-Enemy.prototype.update = function(dt) {
-    this.x += enemyConst.speed * dt;
-    if(this.x > canvasWidth){
-        this.x = enemyConst.refresh;
-    }
-    if(player.x < this.x + playerConst.width &&
-        player.x + playerConst.width > this.x &&
-        player.y < this.y + playerConst.height &&
-        playerConst.height + player.y > this.y){
-            player.x = playerConst.x;
-            player.y = playerConst.y;
-    }
+Enemy.prototype.checkCollision = function () {
+    
+    const enemyWidth = this.x + config.PLAYER_CONST.width;
+    const enemyHeight = this.y + config.PLAYER_CONST.height;
+    const playerWidth = player.x + config.PLAYER_CONST.width;
+    const playerHeight = player.y + config.PLAYER_CONST.height;
+    
+    if (player.x < enemyWidth &&
+        playerWidth > this.x &&
+        player.y < enemyHeight &&
+        playerHeight > this.y) {
+            player.x = config.PLAYER_CONST.x;
+            player.y = config.PLAYER_CONST.y;
+            }
 };
 
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+const allEnemies = [];
+
+config.ENEMY_CONST.locations.forEach(
+    function (enemyPosition) {
+    const startPoint = Math.random()*config.CANVAS_WIDTH;
+    const enemy = new Enemy(startPoint, enemyPosition, config.ENEMY_CONST.speed);
+    allEnemies.push(enemy);
+});
+
 const Player = function() {
-    this.x = playerConst.x;
-    this.y = playerConst.y;
-    this.sprite = playerConst.sprite;
+    this.x = config.PLAYER_CONST.x;
+    this.y = config.PLAYER_CONST.y;
+    this.sprite = config.PLAYER_CONST.sprite;
 };
 const player = new Player;
 
-Player.prototype.update = function (){ 
-    if(this.y < yStart){
-    setTimeout(function (){
-        player.x = playerConst.x;
-        player.y = playerConst.y;
-    }, refreshTime);
-}} 
+Player.prototype.update = function () {
+    if (this.y < config.Y_START) {
+        setTimeout(function () {
+            player.x = config.PLAYER_CONST.x;
+            player.y = config.PLAYER_CONST.y;
+        }, config.REFRESH_TIME);
+    }
+};
 
-Player.prototype.render = function(){
+Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-} 
+};
 
-Player.prototype.handleInput = function (allowKey){
-    if(allowKey == "left" && this.x > xStart){
-        this.x -= playerConst.stepX;
+Player.prototype.handleInput = function (allowKey) {
+    if (allowKey == "left" && this.x > config.X_START) {
+        this.x -= config.PLAYER_CONST.stepX;
     }
-    if(allowKey == "right" && this.x < playerConst.rightBorder){
-        this.x += playerConst.stepX;
+    if (allowKey == "right" && this.x < config.PLAYER_CONST.rightBorder) {
+        this.x += config.PLAYER_CONST.stepX;
     }
-    if(allowKey == "up" && this.y > yStart){
-        this.y -= playerConst.stepY;
+    if (allowKey == "up" && this.y > config.Y_START) {
+        this.y -= config.PLAYER_CONST.stepY;
     }
-    if(allowKey == "down" && this.y < playerConst.y){
-        this.y += playerConst.stepY;
+    if (allowKey == "down" && this.y < config.PLAYER_CONST.y) {
+        this.y += config.PLAYER_CONST.stepY;
     }
-} 
- 
+}; 
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
