83e9531c9422b71120afc4c48d56b47f2946d4e6
Ann Paiuk <annvseslavskaya@gmail.com>
2022-09-10 18:38:05 +0300

    Add oop exercise task

---
+++ submissions/misshatsker/oop-exercise/index.js
@@ -0,0 +1,53 @@
+class MyInhabitant {
+   constructor(species, name, gender, limbs, saying) {
+      this.species = species;
+      this.name = name;
+      this.gender = gender;
+      this.limbs = limbs;
+      this.saying = saying;
+   }
+
+   getInhabitantInfo() {
+      return `${this.species}; ${this.name}; ${this.gender}; ${this.limbs.join('; ')}; ${this.saying}`;
+   }
+}
+
+class Human extends MyInhabitant {
+   constructor(name, gender, saying) {
+      super('human', name, gender, [2, 2], saying);
+   }
+}
+
+class Animal extends MyInhabitant {
+   constructor(name, gender, saying) {
+      super('animal', name, gender, [4], saying);
+   }
+}
+
+class Dog extends Animal {
+   constructor(name, gender) {
+      super(name, gender, 'Woof-woof!');
+   }
+}
+
+class Cat extends Animal {
+   constructor(name, gender) {
+      super(name, gender, 'Meow-meow!');
+   }
+}
+
+const inhabitants = [
+   new Human('Lilu', 'female', 'Aloha!'),
+   new Human('Sofi', 'female', 'Hi!'),
+   new Human('Mila', 'female', 'What`s up?'),
+   new Human('Bob', 'male', 'Have a nice day!'),
+   new Human('Mike', 'male', 'How are you?'),
+   new Human('Vasya', 'male', 'Hey!'),
+   new Dog('Mira', 'female'),
+   new Dog('Baksik', 'male'),
+   new Cat('Tyson', 'female'),
+   new Cat('Valet', 'male')
+];
+
+inhabitants.forEach((inhabitant) =>
+   print(inhabitant.getInhabitantInfo()));
