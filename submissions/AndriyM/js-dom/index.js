55c6e593ae8605f4e5fe4441d1be1e009c180778
andreymashko7 <63639567+andreymashko7@users.noreply.github.com>
2022-09-11 14:06:21 +0300

    js-dom task (#408)
    * add files for js-dom task
    
    * update index.js, fix variables
    
    * added new line to the end

---
+++ submissions/AndriyM/js-dom/index.js
@@ -0,0 +1,43 @@
+import cards from "./data.js"
+
+const rootElement = document.querySelector(".content")
+const sideBarContainer = document.querySelector(".sidebar__list")
+const links = document.querySelectorAll(".sidebar__link")
+
+function createCardsMarcup(cards) {
+    return cards
+        .map(
+            ({
+                id,
+                url,
+                title
+            }) =>
+            `<div class="card__item" id="${id}">
+                    <img src="${url}" class="card__img" alt=""/>
+                    <p class="card__title">${title}</p>
+            </div>`
+        )
+        .join("")
+}
+
+rootElement.innerHTML = createCardsMarcup(cards)
+const cardRefs = document.querySelectorAll(".card__item")
+sideBarContainer.addEventListener("click", onsideBarElementClick)
+
+function onsideBarElementClick({
+    target
+}) {
+    links.forEach((link) => link.classList.remove("active"))
+    target.classList.add("active")
+    const currentActivetLink = target.dataset.id
+
+    cardRefs.forEach((card) => {
+        if (card.classList.contains("visible")) {
+            card.classList.remove("visible")
+        }
+        if (card.id === currentActivetLink) {
+            card.classList.add("visible")
+        }
+    })
+}
+
