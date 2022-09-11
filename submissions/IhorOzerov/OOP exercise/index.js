23fc8caf213dd1a269f3ed863053e679eac350f4
Ihor <goshus@ukr.net>
2022-09-11 13:51:04 +0300

    first commit of OOP JS-World

---
+++ submissions/IhorOzerov/OOP exercise/index.js
@@ -0,0 +1,75 @@
+class Inhabitant {
+   constructor(creature, name, gender, legs, friends, say) {
+      this.creature = creature
+      this.name = name
+      this.gender = gender
+      this.legs = legs
+      this.friends = friends
+      this.say = say
+   }
+}
+
+class Animal extends Inhabitant{
+   constructor(creature, name, gender, friends, say) {
+      super(creature, name, gender, 4, friends, say);
+   }
+   inhabitantSpeech() {
+      return`Sup, i am a ` + this.creature + `, my name is ` + this.name +
+         `, i am cool ` + this.gender + `, with all of my ` + this.legs +
+         ` legs and my friends ` + this.friends + ` I want ` + this.say
+   }
+}
+
+class Cat extends Animal{
+   constructor(name, gender, friends) {
+      super("Cat", name, gender, friends, "to say Where is my money, Gregory?!");
+   }
+}
+
+class Dog extends Animal{
+   constructor(name, gender, friends) {
+      super("Dog", name, gender, friends, "to say that you are breathtaking!");
+   }
+}
+
+class Human extends Inhabitant{
+   constructor(name, gender, friends, say) {
+      super("Human", name, gender, 2, friends, say);
+      this.hands = 2
+   }
+   inhabitantSpeech() {
+      return `Hi, i am a ` + this.creature + `, my name is ` + this.name +
+         `, i am nice ` + this.gender + `, with my ` + this.hands + ` hands, ` +
+         this.legs + ` legs and my friends ` + this.friends + ` I want ` + this.say
+   }
+}
+   
+class Man extends Human{
+   constructor(name, friends) {
+      super(name, "male", friends, "to Stage 1!"); 
+   }
+}
+
+class Woman extends Human{
+   constructor(name, friends) {
+      super(name, "female", friends, 'to learn React!');
+   }
+}
+
+class CatWoman extends Woman{
+    constructor(name, friends) {
+      super(name, friends);
+      this.say = Object.getOwnPropertyDescriptors(new Cat).say.value
+      this.creature = "Cat-Woman"
+   }
+}
+
+let Gangster = new Cat("Gangster", "male", "Yohanna and Vanessa");
+let Gregory = new Man("Gregory", "Vanessa and Bandit");
+let Yohanna = new Woman("Yohanna", "Gangster and Gregory");
+let Bandit = new Dog('Bandit', 'male', "Gregory and Gangster");
+let Vanessa = new CatWoman("Vanessa", "Gangster and Yohanna");
+
+const members = [Gangster, Bandit, Gregory, Yohanna, Vanessa];
+
+members.map(member => print(member.inhabitantSpeech()));
