4e34a35f7ebd2de50c7b7c39b9dfa94fa94b2498
develabr <develabr@gmail.com>
2022-09-12 15:16:35 +0300

    Memory Pair Game

---
+++ submissions/develabr/memory-pair-game/app.js
@@ -0,0 +1,110 @@
+const cardsArray = [
+  {
+    name: 'angular',
+    img: './img/angular.png',
+  },
+  {
+    name: 'css',
+    img: './img/css.png',
+  },
+  {
+    name: 'html',
+    img: './img/html.png',
+  },
+  {
+    name: 'javasript',
+    img: './img/javascript.png',
+  },
+  {
+    name: 'react',
+    img: './img/react.png',
+  },
+  {
+    name: 'sass',
+    img: './img/sass.png',
+  },
+];
+
+const cardsMix = cardsArray.concat(cardsArray).sort(() => 0.5 - Math.random());
+
+const gameField = document.getElementById('container');
+const grid = document.createElement('section');
+grid.setAttribute('class', 'wrapper');
+gameField.appendChild(grid);
+
+cardsMix.forEach((item) => {
+  const { name, img } = item;
+
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = name;
+
+  const front = document.createElement('div');
+  front.classList.add('front');
+
+  const back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = `url(${img})`;
+  back.style.backgroundColor = '#e2e2e2';
+
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
+});
+
+const match = () => {
+  const selected = document.querySelectorAll('.selected');
+  selected.forEach((card) => {
+    card.classList.add('match');
+  });
+};
+
+let firstGuess = '';
+let secondGuess = '';
+let count = 0;
+let previousTarget = null;
+let delay = 1200;
+
+const resetGuesses = () => {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
+
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach((card) => {
+    card.classList.remove('selected');
+  });
+};
+const wrapper = document.querySelector('.wrapper');
+wrapper.addEventListener('click', (event) => {
+  const clicked = event.target;
+
+  if (
+    clicked.nodeName === 'SECTION' ||
+    clicked === previousTarget ||
+    clicked.parentNode.classList.contains('selected') ||
+    clicked.parentNode.classList.contains('match')
+  ) {
+    return;
+  }
+
+  if (count < 2) {
+    count++;
+    if (count === 1) {
+      firstGuess = clicked.parentNode.dataset.name;
+      clicked.parentNode.classList.add('selected');
+    } else {
+      secondGuess = clicked.parentNode.dataset.name;
+      clicked.parentNode.classList.add('selected');
+    }
+
+    if (firstGuess && secondGuess) {
+      if (firstGuess === secondGuess) {
+        setTimeout(match, delay);
+      }
+      setTimeout(resetGuesses, delay);
+    }
+    previousTarget = clicked;
+  }
+});
