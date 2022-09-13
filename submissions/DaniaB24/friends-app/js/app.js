ed3ddbe6a5157daaf72bac94b62dcff8ac9f57cc
Dania Buriachenko <buriachenko923@gmail.com>
2022-09-13 22:49:18 +0300

    add handle error and refactored code

--- submissions/DaniaB24/friends-app/js/app.js
+++ submissions/DaniaB24/friends-app/js/app.js
@@ -1,24 +1,37 @@
-const cardsOfFriends = document.getElementById("allHumans");
+const cardsOfFriends = document.getElementById("all_humans");
 const searchBar = document.getElementById("search");
 const filterMenu = document.getElementById("filter_menu");
-const URL = "https://randomuser.me/api/?results=10";
-let friends;
 
+let friends;
+let filteredUsers;
 function startApp() {
+  const URL = "https://randomuser.me/api?results=10";
   fetch(URL)
+    .then(handleErrors)
     .then((response) => response.json())
     .then((users) => {
       console.log("ok");
       friends = users.results;
+      filteredUsers = users.results;
       displayUsers(friends);
+      searchFriends(filteredUsers);
       filterMenu.addEventListener("click", (event) => {
-        renderFriends(event, friends);
+        filterSetting(event, friends);
       });
     })
-    .catch((error) => console.log("Authorization failed : " + error.message));
+    .catch((error) => {
+      alert("Please reload, Authorization failed : " + error.message);
+      console.log("Authorization failed : " + error.message);
+    });
+}
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
 }
 
-function renderFriends({ target }, friends) {
+function filterSetting({ target }, friends) {
   const sortedResults = sortFriends(target, friends);
   filterByGender(sortedResults ?? []);
 }
@@ -27,12 +40,12 @@ function displayUsers(friends) {
     .map((item) => {
       return `
       <div class="card_person">
-      <div class="card_container">
-      <img src="${item.picture.large}">
-      <h3 class="card_h3">${item.name.first} ${item.name.last}</h3>
-      <p class="card_age">Age: ${item.dob.age}</p>
-      <p class="card_gender">Gender: ${item.gender}</p>
-      </div>
+         <div class="card_container">
+           <img src="${item.picture.large}">
+           <h3 class="card_h3">${item.name.first} ${item.name.last}</h3>
+           <p class="card_age">Age: ${item.dob.age}</p>
+           <p class="card_gender">Gender: ${item.gender}</p>
+         </div>
       </div>`;
     })
     .join("");
@@ -42,7 +55,7 @@ function displayUsers(friends) {
 function sortByAge(a, b) {
   return a.dob.age > b.dob.age ? 1 : -1;
 }
-function sortByName (a, b) {
+function sortByName(a, b) {
   return a.name.first.localeCompare(b.name.first);
 }
 function sortFriends(target, friends) {
@@ -50,19 +63,16 @@ function sortFriends(target, friends) {
 
   switch (button) {
     case "a-z":
-      friends.sort((a, b) => sortByName(a, b));
+      friends.sort(sortByName);
       break;
     case "z-a":
       friends.sort((a, b) => sortByName(b, a));
       break;
     case "0-9":
       friends.sort((a, b) => sortByAge(a, b));
-
       break;
-
     case "9-0":
       friends.sort((a, b) => sortByAge(b, a));
-
       break;
   }
   return friends;
@@ -74,17 +84,16 @@ function filterByGender(friends) {
   const checkboxes = Array.from(
     document.querySelectorAll(".gender_checkbox:checked")
   );
-  const filteredUsers = friends.filter((item) =>
+  filteredUsers = friends.filter((item) =>
     checkboxes.some((gender) => item.gender === gender.value)
   );
   displayUsers(filteredUsers);
-  searchFriends(filteredUsers);
 }
 
-function searchFriends(filteredUsers) {
+function searchFriends() {
   searchBar.addEventListener("input", (e) => {
     const search = e.target.value.toLowerCase();
-    const searchedFriend = filteredUsers.filter((item) => {
+    let searchedFriend = filteredUsers.filter((item) => {
       const { first: firstName, last: lastName } = item.name;
       return `${firstName} ${lastName}`.toLowerCase().includes(search);
     });
