5f342e5bc452b88885bac575264245820c70014e
Olexiy Dobroskok <a.v.dobroskok.kh@gmail.com>
2022-09-13 20:23:51 +0300

    New Perspective on the Task

--- submissions/OlexiyDobroskok/OOP_exercise/index.js
+++ submissions/OlexiyDobroskok/OOP_exercise/index.js
@@ -1,75 +1,159 @@
-class Resident {
-  constructor(species, name, gender, saying) {
-    this.species = species;
-    this.gender = gender;
-    this.name = name;
-    this.saying = saying;
+const inhabitantDefaultProperties = {
+  species: "No species",
+  name: "No name",
+  gender: "No gender",
+  saying: "Hello",
+  friends: "",
+};
+
+const humanDefaultProperties = {
+  hands: 2,
+  legs: 2,
+};
+
+const petsDefaultProperties = {
+  paws: 4,
+};
+
+class Inhabitant {
+  static worldPopulation = [];
+  constructor({
+    species,
+    name,
+    gender,
+    saying,
+    friends,
+  } = inhabitantDefaultProperties) {
+    this.species = species ?? inhabitantDefaultProperties.species;
+    this.name = name ?? inhabitantDefaultProperties.name;
+    this.gender = gender ?? inhabitantDefaultProperties.gender;
+    this.saying = saying ?? inhabitantDefaultProperties.saying;
+    this.friends = friends ?? inhabitantDefaultProperties.friends;
+    Inhabitant.worldPopulation.push(this);
   }
-  printPresentation() {
-    const properties = [
-      this.species,
-      this.name,
-      this.gender,
-      this.legs,
-      this.paws,
-      this.hands,
-      this.saying,
-      this.friends,
-    ];
-    const activeProperties = properties.filter(
-      (prop) => prop !== undefined && prop !== ""
+
+  addFriend({ name }) {
+    this.friends += name + "; ";
+  }
+
+  introduceYourSelf() {
+    if (this.friends === "") {
+      this.friends = "Looking for friends!";
+    }
+    return `<strong>${this.saying}! My name is ${this.name}. Species: ${this.species}. Gender: ${this.gender}. Friends: ${this.friends} </strong>`;
+  }
+}
+
+class Human extends Inhabitant {
+  constructor({
+    name,
+    gender,
+    saying,
+    legs,
+    hands,
+    friends,
+  } = humanDefaultProperties) {
+    super({ species: "human", name, gender, saying, friends });
+    this.hands = hands ?? humanDefaultProperties.hands;
+    this.legs = legs ?? humanDefaultProperties.legs;
+  }
+  introduceYourSelf() {
+    return (
+      super.introduceYourSelf() +
+      `<strong>I have ${this.hands} hands and ${this.legs} legs!</strong>`
     );
-    print(activeProperties.join("; "));
   }
 }
 
-class Human extends Resident {
-  constructor(name, gender, saying) {
-    super("human", name, gender, saying);
-    this.hands = 2;
-    this.legs = 2;
+class Man extends Human {
+  constructor({ name, saying, friends, legs, hands }) {
+    super({ name, gender: "male", saying, legs, hands, friends });
   }
 }
 
-class Pet extends Resident {
-  constructor(species, name, gender, saying) {
-    super(species, name, gender, saying);
-    this.paws = 4;
+class Woman extends Human {
+  constructor({ name, saying, friends, legs, hands }) {
+    super({ name, gender: "female", saying, legs, hands, friends });
   }
 }
 
-class Cat extends Pet {
-  constructor(name, gender, saying) {
-    super("cat", name, gender, saying);
+class Pets extends Inhabitant {
+  constructor({
+    species,
+    name,
+    gender,
+    paws,
+    saying,
+    friends,
+  } = petsDefaultProperties) {
+    super({ species, name, gender, saying, friends });
+    this.paws = paws ?? petsDefaultProperties.paws;
+  }
+  introduceYourSelf() {
+    return (
+      super.introduceYourSelf() + `<strong>I have ${this.paws} paws!</strong>`
+    );
   }
 }
 
-class Dog extends Pet {
-  constructor(name, gender, saying) {
-    super("dog", name, gender, saying);
+class Cat extends Pets {
+  static meow;
+  constructor({ name, gender, saying = "¡miau miau", friends, paws }) {
+    super({
+      species: "cat",
+      name,
+      gender,
+      paws,
+      saying,
+      friends,
+    });
+    Cat.meow = this.saying;
   }
 }
 
-class CatWoman extends Human {
-  constructor(name, gender, saying) {
-    super(name, gender, saying);
+class Dog extends Pets {
+  static bark;
+  constructor({ name, gender, saying = "¡guau guau", friends, paws }) {
+    super({
+      species: "dog",
+      name,
+      gender,
+      paws,
+      saying,
+      friends,
+    });
+    Dog.bark = this.saying;
   }
 }
 
-const man = new Human("José", "male", "Hola, amigo!");
-const woman = new Human("Martina", "female", "Buenos días!");
-const catwoman = new CatWoman("Nerea", "female");
-const cat = new Cat("Lalo", "female", "¡miau miau!");
-const dog = new Dog("Pako", "male", "¡guau guau!");
-
-man.friends = woman.name;
-woman.friends = [man.name, dog.name];
-catwoman.saying = cat.saying + " muchachos!";
-
-function showATinyWorldResidents(residents) {
-  residents.forEach((resident) => resident.printPresentation());
+class CatWoman extends Woman {
+  constructor({ name, saying = "", friends, legs, hands }) {
+    super({ name, saying, friends, legs, hands });
+    this.saying = Cat.meow + " " + saying;
+  }
 }
 
-const aTinyWorldResidents = [man, woman, cat, dog, catwoman];
+const manJose = new Man({ name: "José", saying: "Hola, amigo" });
+const womanMartina = new Woman({ name: "Martina", saying: "Buenos días" });
+const catLalo = new Cat({ name: "Lalo", gender: "female" });
+const dogPako = new Dog({ name: "Pako", gender: "male" });
+const catwomanNerea = new CatWoman({ name: "Nerea", saying: "muchachos" });
 
-showATinyWorldResidents(aTinyWorldResidents);
+manJose.addFriend(catwomanNerea);
+womanMartina.addFriend(dogPako);
+catwomanNerea.addFriend(catLalo);
+catwomanNerea.addFriend(manJose);
+
+function showWorldPopulation() {
+  const inhabitantNames = Inhabitant.worldPopulation.map(
+    (inhabitant) => inhabitant.name
+  );
+  print(
+    `<strong>Population: ${Inhabitant.worldPopulation.length} inhabitants! It is: ${inhabitantNames}</strong>`
+  );
+  Inhabitant.worldPopulation.forEach((inhabitant) =>
+    print(inhabitant.introduceYourSelf())
+  );
+}
+
+showWorldPopulation();
