ad63029a9e0fc63ba6de504114d3a75e9935af3d
Dmytro <dmytryj.kyzyma@gmail.com>
2022-09-14 15:19:00 +0300

    correct position preloader on page

--- submissions/DmytryjK/task-friends-app/js/modules/fetchCards.js
+++ submissions/DmytryjK/task-friends-app/js/modules/fetchCards.js
@@ -1,11 +1,11 @@
-const preloader = document.querySelector('.preloader');
+const preloader = document.querySelector('.preloader__content');
 
 const getPeopleInfo = async (url) => {
     try {
-        preloader.classList.add('preloader_active');
+        preloader.classList.add('preloader__content_active');
         const res = await fetch(url);
 
-        await preloader.classList.remove('preloader_active');
+        await preloader.classList.remove('preloader__content_active');
         return await res.json();
     } catch (error) {
         return await getPeopleInfo(url);
