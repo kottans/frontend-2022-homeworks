5ab5df6778d93ecd259964d3d1dda7c7e662a238
Vasyl <vasylchaban12@gmail.com>
2022-09-19 01:32:37 +0300

    fixed by recommendations

--- submissions/Conversee12/DOM/script.js
+++ submissions/Conversee12/DOM/script.js
@@ -1,47 +1,55 @@
 const rockBands = [
   {
+    id: "1",
     title: "Nirvana",
     about: "Nirvana was an American grunge band that was formed in Aberdeen, Washington in 1987. Nirvana disbanded after Kurt Cobain committed suicide in 1994. The drummer of the band, David Grohl, went on to start the Foo-Fighters, an alternative rock band. The band was comprised of Kurt Cobain (Vocals, Guitar), Dave Grohl (Drums) and Krist Novoselic (Bass). Nirvana is most well-known for their albums Bleach, Nevermind, In Utero and MTV Unplugged in New York and their songs 'About a Girl', 'Smells Like Teen Spirit', 'Come As You Are', 'Lithium', 'In Bloom', 'Heart-Shaped Box', 'All Apologies' and 'Pennyroyal Tea'. The band is known for being one of the most influential bands of all time. Together with Soundgarden, Pearl Jam and Alice In Chains, the band is part of the Big 4 of Grunge",
     myBestSong: 'Best song in my opinion : "Something in the way"',
     image: "img/nirvana.jpg"
   },
   {
+    id: "2",
     title: "Linkin Park",
     about: "Linkin Park is an American rock band formed in Agoura Hills, California, in 1996. The members currently include Mike Shinoda (vocals, keyboards, rhythm guitar), Brad Delson (guitar), Dave \"Phoenix\" Farrell (bass), Joe Hahn (DJ) and Rob Bourdon (drums). Linkin Park are associated with a mixture of Alternative Rock, Hip Hop and Techno. Some of their influences include Nine Inch Nails, Deftones, The Roots and Aphex Twin. The band rose to international fame with their debut album Hybrid Theory, which was released in 2000. The album was certified diamond by the RIAA in 2005 and multi-platinum in several other countries. Their song ‘In the End’ is considered to be one of the best rock songs of the 21st century. Linkin Park has sold over 68 million albums worldwide and have won two Grammy Awards. They released 7 studio albums, and their most popular songs include \“Numb\”, \“In The End\”, \“What I’ve Done\”, and \“New Divide\”.",
     myBestSong: 'Best song in my opinion : "From the Inside"',
     image: "img/linkinpark.jpg"
   },
   {
+    id: "3",
     title: "Three Days Grace",
     about: "Three Days Grace is a Canadian post-grunge/alternative rock band formed in Norwood, Ontario in 1992 with a line-up consisting of guitarist and lead vocalist Adam Gontier, drummer and backing vocalist Neil Sanderson, and bassist Brad Walst. Their original name was Groundswell until they changed it to their current name in 1997. In 2003, the band's current guitarist Barry Stock was recruited to the band. In 2013 Gontier left the band due to creative differences. He was replaced with Brad's brother Matt Walst.",
     myBestSong: 'Best song in my opinion : "I hate Everyting About You"',
     image: "img/threedays.jpg"
   },
   {
+    id: "4",
     title: "Metallica",
     about: "Metallica is an American Heavy/Thrash Metal band formed in 1981 . The original lineup was James Hetfield (Vocalist and Rhythm guitarist), Dave Mustaine (Lead Guitar), Lars Ulrich (Drums), Ron McGovney (Bassist). The group came into mainstream from their Self-Titled album and the hit single \"Enter Sandman\". With their first two albums, Kill 'Em All and Ride The Lightning reaching underground success, their third effort, Master of Puppets has since become one of the most important heavy metal albums ever, as it became the first thrash metal album to be certified platinum. The title track has since become the band's most played song live.",
     myBestSong: 'Best song in my opinion : "The Unforgiven"',
     image: "img/metallica.jpg"
   },
   {
+    id: "5",
     title: "Guns N' Roses",
     about: "Guns N' Roses is an American hard rock band from Los Angeles formed in 1985. The classic lineup, as signed to Geffen Records in 1986, consisted of vocalist Axl Rose, lead guitarist Slash, rhythm guitarist Izzy Stradlin, bassist Duff McKagan, and drummer Steven Adler. Apart from hard rock they are also associated with heavy metal. They are best known for their album ‘Appetite for Destruction’ and songs like ‘Sweet Child O’ Mine’. Guns N' Roses has been credited with reviving the mainstream popularity of rock music, at a time when popular music was dominated by dance music and glam metal. Its late 1980s and early 1990s years have been described as the period in which the group brought forth a \"hedonistic rebelliousness\" reminiscent of the early Rolling Stones, a reputation that had earned the group the nickname \"the most dangerous band in the world\".",
     myBestSong: 'Best song in my opinion : "Sweet Child O’ Mine"',
     image: "img/guns.jpg"
   },
   {
+    id: "6",
     title: "Foo Fighters",
     about: "Foo Fighters are an American alternative rock band, formed in Seattle in 1994. The band was founded by Nirvana drummer Dave Grohl after the death of Kurt Cobain and the dissolution of his previous band. The band consists of Dave Grohl (lead vocalist, rhythm guitar, main songwriter), Pat Smear (rhythm guitar), Nate Mendel (bass), Taylor Hawkins (drums, backing vocals), Chris Shiflett (lead guitar, backing vocals) and the most recent addition, Rami Jaffee (keyboards). Former members include William Goldsmith (drummer) and Franz Stahl (guitarist).Over the course of the band's career, its albums have won many awards and they have achieved recognition worldwide, with their shows often selling out within minutes. Overall, they've won 26 awards and received 87 nominations in total, including four Grammy Awards for Best Rock Album , more than any other band. As of 2015, the band's eight albums have sold 12 million copies in the U.S. alone.",
     myBestSong: 'Best song in my opinion : "The Pretender"',
     image: "img/foofighters.jpg"
   },
   {
+    id: "7",
     title: "Bullet for My Valentine",
     about: "Bullet for My Valentine are a Welsh metalcore band from Bridgend, formed in 1998. The band is composed of Matthew Tuck (lead vocals, rhythm guitar), Michael Paget (lead guitar, backing vocals), Michael Thomas (drums) and Jamie Mathias (bass guitar). Former members include Nick Crandle and Jason James; both were on bass. They were formed under the name Jeff Killed John and started their music career by covering songs by Metallica and Nirvana. Jeff Killed John recorded six songs which were not released; two of these tracks were reworked later in their career as Bullet for My Valentine. Financial difficulties dictated the name change, which was followed by a change in music direction. In 2002, the band secured a five-album deal with Sony BMG.",
     myBestSong: 'Best song in my opinion : "Your Betrayal"',
     image: "img/valentine.jpg"
   },
   {
+    id: "8",
     title: "Thirty Seconds to Mars",
     about: "Thirty Seconds to Mars (commonly stylized as 30 Seconds to Mars) is an American rock band from Los Angeles, California, formed in 1998. The band consists of brothers Jared Leto (lead vocals, guitar, bass, keyboards) and Shannon Leto (drums, percussion). During the course of its existence, it has undergone various line-up changes. The band's debut album, 30 Seconds to Mars (2002), was produced by Bob Ezrin and released to positive reviews but only to limited commercial success. The band achieved worldwide fame with the release of its second album A Beautiful Lie (2005), which received multiple certifications all over the world. Its next release, This Is War (2009), showed a dramatic evolution in the band's musical style, as it incorporated experimental music as well as eclectic influences. The recording process of the album was marked by a legal dispute with record label EMI that eventually became the subject of the documentary film Artifact (2012).",
     myBestSong: 'Best song in my opinion : "A Beautiful Lie"',
@@ -51,9 +59,10 @@ const rockBands = [
 
 function initButtons(container) {
   return rockBands.forEach((band) => {
-    const { title } = band;
+    const { id, title } = band;
     const button = document.createElement("button");
-    button.classList.add("buttons");
+    button.classList.add("button");
+    button.id = id;
     button.innerText = title;
     button.addEventListener("click", ({ target }) => {
       showMain(target);
@@ -64,7 +73,7 @@ function initButtons(container) {
 
 function showMain(target) {
   const content = rockBands.filter(
-    (band) => target.innerText === band.title
+    (band) => target.id === band.id
   );
   const [{ title, myBestSong, about, image }] = content;
   const bandTitle = document.querySelector(".section__title");
@@ -77,7 +86,6 @@ function showMain(target) {
   bandImage.src = image;
 }
 
-const sideBar = document.querySelector(".sideBar");
-initButtons(sideBar);
+initButtons(document.querySelector(".sideBar"));
 
-showMain({ innerText: rockBands[0].title });
+showMain(rockBands[0]);
