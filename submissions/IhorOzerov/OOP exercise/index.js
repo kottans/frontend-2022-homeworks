ceb7757c32ea16ca00c626bee815290c0030207f
Ihor <107474617+IhorOzerov@users.noreply.github.com>
2022-09-18 22:54:30 +0300

    added new members to list

--- submissions/IhorOzerov/OOP exercise/index.js
+++ submissions/IhorOzerov/OOP exercise/index.js
@@ -19,8 +19,8 @@ class Animal extends Inhabitant{
       super(creature, name, gender, 4, friends, say);
    }
    inhabitantSpeech() {
-      return super.inhabitantSpeech(`Sup, i am a `, `, my name is `, `, i am cool `, `. With all of my `,
-      ` legs and my friends, `, ` I want `) 
+      return super.inhabitantSpeech(`Sup, i am a `, `, my name is `, `, i am cool `, `. With all of my `, ` legs and my friends, `,
+         ` I want `) 
    }
 }
 
@@ -41,9 +41,9 @@ class Human extends Inhabitant{
       super("Human", name, gender, 2, friends, say);
       this.hands = 2
    }
-   inhabitantSpeech(handsText) {
-      return super.inhabitantSpeech(`Hi, i am a `, `, my name is `, `, i am nice `, `. With my ` + this.hands +
-      ` hands, `, ` legs and my friends, `, ` I want `)
+   inhabitantSpeech() {
+      return super.inhabitantSpeech(`Hi, i am a `, `, my name is `, `, i am nice `, `. With my ` + this.hands + ` hands, `,
+         ` legs and my friends, `, ` I want `)
    }
 }
    
@@ -67,12 +67,19 @@ class CatWoman extends Woman{
    }
 }
 
+//, 
 let Gangster = new Cat("Gangster", "male", "Yohanna and Vanessa");
+let Nancy = new Cat("Nancy", "female", "Gangster and Vanessa")
 let Gregory = new Man("Gregory", "Vanessa and Bandit");
-let Yohanna = new Woman("Yohanna", "Gangster and Gregory");
+let Andy = new Man("Andy", "Rosemary and Charlotte");
+let Peter = new Man("Peter", "Nancy and Bandit");
 let Bandit = new Dog('Bandit', 'male', "Gregory and Gangster");
+let Rosemary = new Dog('Rosemary', 'female', "Bandit and Yohanna");
+let Yohanna = new Woman("Yohanna", "Gangster and Gregory");
+let Charlotte = new Woman('Charlotte', 'July and Bandit');
+let July = new Woman("July", "Nance and Charlotte");
 let Vanessa = new CatWoman("Vanessa", "Gangster and Yohanna");
 
-const members = [Gangster, Bandit, Gregory, Yohanna, Vanessa];
+const members = [Gangster, Nancy, Gregory, Andy, Peter, Bandit, Rosemary, Yohanna, Charlotte, July, Vanessa];
 
 members.map(member => print(member.inhabitantSpeech()));
