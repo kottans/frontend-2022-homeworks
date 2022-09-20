6bf0842b164d331cf3a087d6bd42fba310f19e5b
YuraZagor <u.zagorulko@gmail.com>
2022-09-20 18:20:27 +0300

    added ;-s

--- submissions/YuraZagor/memory-game/script.js
+++ submissions/YuraZagor/memory-game/script.js
@@ -1,33 +1,33 @@
 const cardsArr = [{
 		id: 'azazaaza',
 		image: './img/azazaaza.PNG',
-	}, {
+	},{
 		id: 'bear',
 		image: './img/bear.PNG',
-	}, {
+	},{
 		id: 'fox',
 		image: './img/fox.PNG',
-	},	{
+	},{
 		id: 'nebryti-ta-negoleni',
 		image: './img/nebryti-ta-negoleni.PNG',
-	},	{
+	},{
 		id: 'rabbits',
 		image: './img/rabbits.PNG',
-	},	{
+	},{
 		id: 'tiger',
 		image: './img/tiger.PNG',
 	},
 ];
 
-const container = document.querySelector('.container')
-const playAgainEl = document.getElementById('play--again')
-playAgainEl.addEventListener('click', app)
+const container = document.querySelector('.container');
+const playAgainEl = document.getElementById('play--again');
+playAgainEl.addEventListener('click', app);
 
-randomArr = [...cardsArr.concat(cardsArr)]
+randomArr = [...cardsArr.concat(cardsArr)];
 
 function randomizeCards(){
 	return randomArr.sort(function() { return 0.5 - Math.random() })
-}
+};
 
 function gameStart() {
 	randomArr.forEach((card)=>{
@@ -39,64 +39,65 @@ function gameStart() {
 			</div>
 			`))
 	})
-}
+};
 
-let gameArr = []
-let cardElem
-let card1
-let card2
-let count = 0
+let gameArr = [];
+let cardElem;
+let card1;
+let card2;
+let count = 0;
 
 container.onclick = function(event) {
 	cardElem = event.target.closest('.card');
 	if (!cardElem) {
-		return
-	} 
+		return;
+	}; 
 	if (cardElem.className.includes('open')) {
 		return;
-	}
+	};
 	if (cardElem.className.includes('hidden')) {
 		return;
-	} 
-	showCard(cardElem)
-}
+	};
+	showCard(cardElem);
+};
 
 function showCard(cardElem){
 	if (gameArr.length > 1) { 
-		return 
-	}
-	cardElem.classList.add("open")
-	gameArr.push(cardElem)
+		return;
+	};
+	cardElem.classList.add("open");
+	gameArr.push(cardElem);
 	if (gameArr.length > 1) {
-		checkPair()
-	}	
-}
+		checkPair();
+	};	
+};
 
 function checkPair(){
 	card1 = gameArr[0];
 	card2 = gameArr[1];
 	setTimeout(() => {
-		card1.classList.remove("open")
-		card2.classList.remove("open")
+		card1.classList.remove("open");
+		card2.classList.remove("open");
 		if (card1.dataset.definition == card2.dataset.definition) {
-			card1.classList.add("hidden")
-			card2.classList.add("hidden")
-			count ++
-		}
-		gameArr = []	
+			card1.classList.add("hidden");
+			card2.classList.add("hidden");
+			count ++;
+		};
+		gameArr = [];	
 		
 		console.log(count)
 		if (count === 6) {
-			count = 0
-			playAgainEl.classList.remove('hide--message')
-		}
+			count = 0;
+			playAgainEl.classList.remove('hide--message');
+		};
 	}, 700);
-}
+};
 
 function app () {
-	container.innerHTML = ''
-	playAgainEl.classList.add('hide--message')
+	container.innerHTML = '';
+	playAgainEl.classList.add('hide--message');
 	randomizeCards()
 	gameStart()
-}
-app() 
+};
+
+app()
