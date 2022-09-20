c0876b89b12e67e4fa609443b98f282cc9415e83
YuraZagor <u.zagorulko@gmail.com>
2022-09-20 12:06:31 +0300

    added some ;-s

--- submissions/YuraZagor/tiny world pre-oop/index.js
+++ submissions/YuraZagor/tiny world pre-oop/index.js
@@ -1,57 +1,57 @@
 const dog = {
-   species: "dog",
-   name: "JEff",
-   gender: "male",
-   legs: 4,
-   hands: 0,
-   saying: "bow-wwou!",
-   friend: 'everyone',
- };
- const cat = {
-   species: "cat",
-   name: "Kizma",
-   gender: "female",
-   legs: 4,
-   hands: 0,
-   saying: "meaaawurrr!",
-   friend: 0,
- };
- const woman = {
-   species: "human",
-   name: "Maria",
-   gender: "female",
-   legs: 2,
-   hands: 2,
-   saying: "Are U hungry, dear?",
-   friend: 'dog'
- };
- const man = {
-   species: "human",
-   name: "Grumio",
-   gender: "male",
-   legs: 2,
-   hands: 2,
-   saying: "Wanna beer with ice-cream!",
-   friend: "Maria"
- };
- const cat_woman = {
-   species: "catmandu",
-   name: "Anabelle",
-   gender: "female",
-   legs: 2,
-   hands: 2,
-   saying: cat.saying,
-   friend: "Grumio"
- };
+  species: "dog",
+  name: "JEff",
+  gender: "male",
+  legs: 4,
+  hands: 0,
+  saying: "bow-wwou!",
+  friend: 'everyone',
+};
+const cat = {
+  species: "cat",
+  name: "Klizma",
+  gender: "female",
+  legs: 4,
+  hands: 0,
+  saying: "meaaawurrr!",
+  friend: 0,
+};
+const woman = {
+  species: "human",
+  name: "Maria",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  saying: "Are U hungry, dear?",
+  friend: 'dog'
+};
+const man = {
+  species: "human",
+  name: "Grumio",
+  gender: "male",
+  legs: 2,
+  hands: 2,
+  saying: "Wanna beer with ice-cream!",
+  friend: "Maria"
+};
+const cat_woman = {
+  species: "catmandu",
+  name: "Anabelle",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  saying: cat.saying,
+  friend: "Grumio"
+};
 
-const inhabitants = [dog, cat, woman, man, cat_woman,]
-let printArr = []
+const inhabitants = [dog, cat, woman, man, cat_woman,];
+let printArr = [];
 
 for (let i = 0; i< inhabitants.length; i++) {
-   printArr.push([])
-   for (let key in inhabitants[i]){
-      printArr[i].push(inhabitants[i][key])
-   }
-}
+  printArr.push([]);
+  for (let key in inhabitants[i]){
+     printArr[i].push(inhabitants[i][key])
+  };
+};
 
-printArr.forEach( item => print(item.join('; '),'div') )
+printArr.forEach( item => print(item.join('; '),'div') );
