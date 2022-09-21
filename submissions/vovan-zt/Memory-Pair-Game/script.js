bfe25626b0290b0f4bab97c3651cd1d65afe64a9
vovan-zt <predator_vovan@ukr.net>
2022-09-21 14:50:18 +0300

    Edited according to mentor comments

--- submissions/vovan-zt/Memory-Pair-Game/script.js
+++ submissions/vovan-zt/Memory-Pair-Game/script.js
@@ -9,82 +9,77 @@ const cards = [
     { name: 'yamato', image: 'img/18.jpg' },
 ]
 
+const container = document.querySelector('.container')
+const wrapper = document.querySelector('.wrapper')
+const cardsList = document.getElementsByClassName('hide-swap')
+let cardsVisible = []
+let lockWrapper = false
+
 cards.push(...cards)
 
-const sortingCards = (dataBase) => {
-    dataBase.sort(() => {
+const shuffleCards = () => {
+    cards.sort(() => {
         return 0.5 - Math.random()
     })
 }
 
-const container = document.querySelector('.container')
-
 const generateCards = ({ name, image }) => {
     return `
-            <div class ="hide-swap">
-                <div class="c1" data-card='${name}'></div>
-                <img class="c2" src="${image}" alt="${name}">
-            </div>
-        `
+        <div class ="hide-swap">
+            <div class="c1" data-card='${name}'></div>
+            <img class="c2" src="${image}" alt="${name}">
+        </div>
+    `
 }
 
-const showCards = (dataBase) => {
-    sortingCards(cards)
-    const cardsDiv = document.createElement('div')
-    cardsDiv.classList.add('wrapper')
-    cardsDiv.innerHTML = dataBase.reduce(
+const showCards = () => {
+    shuffleCards(cards)
+    wrapper.innerHTML = ''
+    wrapper.innerHTML = cards.reduce(
         (acc, card) => (acc += generateCards(card)),
         ''
     )
-    container.append(cardsDiv)
+    container.append(wrapper)
 }
 
 showCards(cards)
 
-const cardsList = document.querySelectorAll('.hide-swap')
-const wrapper = document.querySelector('.wrapper')
-let cardsVisible = []
-
-const cancelCardActive = () => {
-    cardsList.forEach((card) => {
-        card.classList.remove('active')
-    })
-}
-
-const messageBlock = document.createElement('div')
-messageBlock.classList.add('victory')
-
 const messageVictory = () => {
+    const messageBlock = document.createElement('div')
+    messageBlock.classList.add('victory')
     messageBlock.innerHTML = `
-            <h2 class= "victory__title">Сongratulations you won</h2>
-            <div class= "victory__descr">press reset to start over</div>
-            <button class= "victory__btn">Reset</button>
-
+        <h2 class= "victory__title">Congratulations you won</h2>
+        <div class= "victory__desc">press reset to start over</div>
+        <button class= "victory__btn">Reset</button>
     `
     container.append(messageBlock)
-    resetGame()
+    restartGame(messageBlock)
 }
 
-const resetGame = () => {
+const restartGame = (messageBlock) => {
     const resetBtn = document.querySelector('.victory__btn')
     resetBtn.addEventListener('click', (e) => {
         e.preventDefault()
-        location.reload()
+        showCards(cards)
+        messageBlock.remove()
     })
 }
 
-const resetCards = () => {
-    const cardsLength = document.querySelectorAll('.visible').length
-    if (cardsLength == 12) {
-        setTimeout(() => messageVictory(), 800)
-    }
+const cancelCardActive = () => {
+    setTimeout(() => {
+        Array.from(cardsList).forEach((card) => {
+            card.classList.remove('active')
+            lockWrapper = false
+        })
+    }, 800)
+    lockWrapper = true
 }
 
 const changeCard = () => {
     let count = 0
     if (cardsVisible[0] == cardsVisible[1]) {
         cardsVisible = []
-        cardsList.forEach((item) => {
+        Array.from(cardsList).forEach((item) => {
             if (item.classList.contains('active')) {
                 count++
             }
@@ -93,7 +88,7 @@ const changeCard = () => {
             cardsVisible = []
             cancelCardActive()
         } else {
-            cardsList.forEach((item) => {
+            Array.from(cardsList).forEach((item) => {
                 if (item.classList.contains('active')) {
                     item.classList.add('visible')
                 }
@@ -102,24 +97,33 @@ const changeCard = () => {
     } else {
         cardsVisible = []
     }
-    setTimeout(() => cancelCardActive(), 600)
-    resetCards()
+    cancelCardActive()
+}
+
+const checkCardsVisibleLength = () => {
+    if (cardsVisible.length === 2) {
+        changeCard()
+    } else {
+        setTimeout(() => (lockWrapper = false), 300)
+    }
 }
 
 const showContent = ({ target }) => {
-    if (
-        !target.parentElement.classList.contains('container') &&
-        !target.classList.contains('hide-swap')
-    ) {
-        target.parentElement.classList.toggle('active')
-        if (target.parentElement.classList.contains('active')) {
-            cardsVisible.push(target.dataset.card)
-        } else {
-            cardsVisible.pop()
-        }
+    if (lockWrapper) return
+
+    if (target.closest('.hide-swap') && !target.classList.contains('c2')) {
+        target.closest('.hide-swap').classList.add('active')
+        cardsVisible.push(target.dataset.card)
+        lockWrapper = true
+        checkCardsVisibleLength()
     }
-    if (cardsVisible.length == 2) {
-        changeCard()
+    checkForWin()
+}
+
+const checkForWin = () => {
+    const cardsLength = document.querySelectorAll('.visible').length
+    if (cardsLength == 12) {
+        setTimeout(() => messageVictory(), 1200)
     }
 }
 
