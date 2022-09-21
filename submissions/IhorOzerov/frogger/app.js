f59e7332abfad5f9603149525f2a905858f21c37
Ihor <107474617+IhorOzerov@users.noreply.github.com>
2022-09-21 12:02:47 +0300

    updated methods
    improved refer to global variable 'player' in Enemy's and Player's methods

--- submissions/IhorOzerov/frogger/app.js
+++ submissions/IhorOzerov/frogger/app.js
@@ -1,8 +1,8 @@
 const config = {
-    CANVAS_WIDTH : 505,
-    REFRESH_TIME : 500,
-    Y_START : 0,
-    X_START : 0,
+    CANVAS_WIDTH: 505,
+    REFRESH_TIME: 500,
+    Y_START: 0,
+    X_START: 0,
 
     ENEMY_CONST: {
         refresh: -101,
@@ -23,12 +23,13 @@ const config = {
     }
 };
 
-const Enemy = function (x, y) {
+const Enemy = function (x, y, player) {
     this.x = x;
     this.y = y;
+    this.player = player;
     this.speed = config.ENEMY_CONST.speed;
     this.sprite = config.ENEMY_CONST.sprite;
-}; 
+};
 
 Enemy.prototype.update = function (dt) {
     this.x += config.ENEMY_CONST.speed * dt;
@@ -39,46 +40,38 @@ Enemy.prototype.update = function (dt) {
 };
 
 Enemy.prototype.checkCollision = function () {
-    
+
     const enemyWidth = this.x + config.PLAYER_CONST.width;
     const enemyHeight = this.y + config.PLAYER_CONST.height;
-    const playerWidth = player.x + config.PLAYER_CONST.width;
-    const playerHeight = player.y + config.PLAYER_CONST.height;
-    
-    if (player.x < enemyWidth &&
+    const playerWidth = this.player.x + config.PLAYER_CONST.width;
+    const playerHeight = this.player.y + config.PLAYER_CONST.height;
+
+    if (this.player.x < enemyWidth &&
         playerWidth > this.x &&
-        player.y < enemyHeight &&
+        this.player.y < enemyHeight &&
         playerHeight > this.y) {
-            player.x = config.PLAYER_CONST.x;
-            player.y = config.PLAYER_CONST.y;
-            }
+        this.player.x = config.PLAYER_CONST.x;
+        this.player.y = config.PLAYER_CONST.y;
+    }
 };
 
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-const allEnemies = [];
-
-config.ENEMY_CONST.locations.forEach(
-    function (enemyPosition) {
-    const startPoint = Math.random()*config.CANVAS_WIDTH;
-    const enemy = new Enemy(startPoint, enemyPosition, config.ENEMY_CONST.speed);
-    allEnemies.push(enemy);
-});
-
-const Player = function() {
+const Player = function () {
     this.x = config.PLAYER_CONST.x;
     this.y = config.PLAYER_CONST.y;
     this.sprite = config.PLAYER_CONST.sprite;
 };
+
 const player = new Player;
 
 Player.prototype.update = function () {
     if (this.y < config.Y_START) {
-        setTimeout(function () {
-            player.x = config.PLAYER_CONST.x;
-            player.y = config.PLAYER_CONST.y;
+        setTimeout(() => {
+            this.x = config.PLAYER_CONST.x;
+            this.y = config.PLAYER_CONST.y;
         }, config.REFRESH_TIME);
     }
 };
@@ -100,7 +93,16 @@ Player.prototype.handleInput = function (allowKey) {
     if (allowKey == "down" && this.y < config.PLAYER_CONST.y) {
         this.y += config.PLAYER_CONST.stepY;
     }
-}; 
+};
+  
+const allEnemies = [];
+
+config.ENEMY_CONST.locations.forEach(
+    function (enemyPosition) {
+        const startPoint = Math.random() * config.CANVAS_WIDTH;
+        const enemy = new Enemy(startPoint, enemyPosition, player);
+        allEnemies.push(enemy);
+    });
 
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
@@ -111,3 +113,4 @@ document.addEventListener('keyup', function(e) {
     };
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
