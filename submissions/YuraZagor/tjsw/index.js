3683bb992900f29125c37316b20afb454c40a1bb
YuraZagor <u.zagorulko@gmail.com>
2022-09-21 22:57:05 +0300

    stable vertion

--- submissions/YuraZagor/tjsw/index.js
+++ submissions/YuraZagor/tjsw/index.js
@@ -1,77 +1,115 @@
-const inhabitants = [];
+const manNamesArr = ['Vasil', 'Yurij', 'Zahar', 'Taras', 'Olexiy', 'Oleg', 'Mykola', 'Sashko', 'Sergij', 'Bohdan']
+const womanNamesArr = ['Hanna', 'Helena', 'Vasilisa', 'Yulya', 'Oksana', 'Olena', 'Olga', 'Myroslava', 'Svitlana', 'Bohdana']
+const animalNamesArr = ['Joy', 'Happy', 'Lucky', 'Puffy']
 
 let catSay = 'meau';
 let dogSay = 'woufff';
-let manSay = 'Wanna_beer_with_ice-cream!';
-let womanSay = 'Are_U_hungry,_dear?';
+let humanSay = 'Glory to Ukraine!';
 
-let womanName = 'Maria';
-let manName = 'Grumio';
-let dogName = 'Happy';
-let catName = 'Lucky';
+function randomSelect(arr){
+  return arr[Math.floor(Math.random()*arr.length)]
+}
+let manName = randomSelect(manNamesArr)
+let womanName = randomSelect(womanNamesArr)
+let animalName = randomSelect(animalNamesArr)
 
+const inhabitants = [
+  { species: 'human', 
+    gender: 'male',
+    name: 'Vasil',
+    saying: 'Slava natsiyi',
+    friend: 'Hanna',
+    legs: 2,
+    hands: 2,
+  },
+  { species: 'human', 
+    gender: 'female',
+    name: 'Hanna',
+    saying: 'Smert vorogam',
+    friend: 'Vasil',
+    legs: 2,
+    hands: 2,
+  },
+];
 
-class Inhabitant {
-  constructor(species, gender, name, saying, friend){
+class Inhabitant {  
+  constructor(species, name, saying, friend, gender){
     this.species=species;
-    this.gender=gender;
     this.name=name;
-    this.saying=saying;
+    this.saying = saying
     this.friend=friend;
+    this.gender=gender;
   }
-  populate(){
-    const { species, name, gender, saying, friend } = this
-    const printArr = [species, name, gender, saying, friend]
-    inhabitants.push(this);
-    print(printArr.join('; '),'div');
-  };
 };
 
 class Animal extends Inhabitant{
-  constructor( species, gender, name = dogName, saying = dogSay, friend = womanName, paws=4, tail=1 ){
-    super(species, gender, name, saying, friend);
+  constructor( species, name, saying, friend, gender, paws, tail ){
+    super(species, name, saying, friend, gender);
     this.paws=paws;
     this.tail=tail;
   };
   populate(){
-    const { species, gender, name, saying, friend, paws, tail} = this
-    const printArr = [species, gender, name, saying, friend, paws, tail]
+    const { species, name, saying, friend, gender, paws, tail} = this
+    const inhabitantProperties = [species, name, saying, friend, gender, paws, tail]
+    print(inhabitantProperties.join('; '),'div');
     inhabitants.push(this);
-    print(printArr.join('; '),'div');
   };
 };
 
+class Dog extends Animal{
+  constructor( name=randomSelect(animalNamesArr), friend=randomSelect(inhabitants).name, gender='male' ){
+    super('dog', name, dogSay, friend, gender, 4, 1);
+  };
+};
+class Cat extends Animal{
+  constructor( name=randomSelect(animalNamesArr), friend=randomSelect(inhabitants).name, gender='female' ){
+    super('cat', name, catSay, friend, gender, 4, 1);
+  };
+
+};
+
 class Human extends Inhabitant{
-  constructor( species, gender, name = manName, saying = manSay, friend=dogName, legs=2, hands=2 ){
-    super(species, gender, name, saying, friend);
+  constructor( name=randomSelect(womanNamesArr), friend=randomSelect(inhabitants).name, gender='female', legs=2, hands=2){
+    super('human', name, humanSay, friend, gender);
     this.legs=legs;
     this.hands=hands;
   };
   populate(){
     const { species, gender, name, saying, friend, legs, hands} = this
-    const printArr = [species, gender, name, saying, friend, legs, hands]
+    const inhabitantProperties = [species, gender, name, saying, friend, legs, hands]
+    print(inhabitantProperties.join('; '),'div');
     inhabitants.push(this);
-    print(printArr.join('; '),'div');
   };
 };
 
-function creation(species, gender, name, saying, friend, legsPaws, handsTail){
-  species.toLowerCase() === 'human' 
-  ? (new Human(species, gender, name, saying, friend, legsPaws, handsTail)).populate()
-  : (new Animal(species, gender, name, saying, friend, legsPaws, handsTail)).populate()
+class CatWoman extends Human{
+  constructor( name=randomSelect(womanNamesArr), friend=randomSelect(inhabitants).name, ){
+    super(name, catSay, friend, 'female', 2, 2);
+  };
 };
 
-function multipleCreation(num, species, gender, name, saying, friend, legsPaws, handsTail){
+function creation(className, name, friend, gender){
+  className.toLowerCase() === 'human' 
+  ? (new Human(name, friend, gender)).populate()
+  : className.toLowerCase() === 'catwoman' 
+  ? (new CatWoman(name, friend)).populate()
+  : className.toLowerCase() === 'cat' 
+  ? (new Cat(name, friend, gender)).populate()
+  : (new Dog(name, friend, gender)).populate()
+};
+
+function multipleCreation(num, species){
   for (let i = 0; i<num; i++){
-    creation(species, gender, name, saying, friend, legsPaws, handsTail)
+    creation(species)
   };  
 };
-
-creation('human', 'female', womanName, womanSay, catName);
-creation('human', 'male', manName, manSay, dogName);
-creation('dog', 'male', dogName, dogSay, [womanName, manName, dogName].join(', '));
-creation('cat', 'female', catName, catSay, womanName);
-multipleCreation(4, 'human', 'male')
+const vasya = new Human ('Vasya', womanName, 'male' )
+vasya.populate()
+creation('human', womanName, animalName, 'male' );
+creation('human', randomSelect(womanNamesArr), animalName, 'female' );
+creation('dog', animalName, 'male', animalName);
+creation('cat', animalName, 'female', animalName);
+multipleCreation(4, 'human' )
 
 
 const inputElement = document.createElement('form');
@@ -101,42 +139,6 @@ inputElement.innerHTML =
       <option value="dog">dog</option>
     </select>
   </section>
-
-  <section class="gender">
-    <label for="gender">boy or girl shall I choose? </label>
-    <select name="gender" id="gender">
-      <option value="male">male</option>
-      <option value="female">female</option>
-    </select>
-  </section>
-
-  <section class="naming">
-    <label for="naming">So let's come along with a name  </label>
-    <textarea id="naming" name="naming" rows="1" cols="15" placeholder = 'name'></textarea>
-  </section>
-
-  <section class="saying-selection">
-    <label for="saying">Seems that kinda inhabitants would say</label>
-    <input list="sayings" id="saying" name="saying" placeholder = 'saying'>
-    <datalist id="sayings">
-      <option value=${catSay}></option>
-      <option value=${dogSay}></option>
-      <option value=${manSay}></option>
-      <option value=${womanSay}></option>
-
-    </datalist>
-  </section>
-
-  <section class="friend">
-  <label for="friend">They are best friends with </label>
-    <select name="friend" id="friend">
-    <option value=${manName}>${manName}</option>
-    <option value=${womanName}>${womanName}</option>
-    <option value=${dogName}>${dogName}</option>
-    <option value=${catName}>${catName}</option>
-    </select>
-  </section>
-
     
   <section class="submission">
     <input type='submit' value='Create!'>
@@ -149,11 +151,7 @@ function useFormValue(event) {
    
   num = document.getElementById('num').value;
   species = document.getElementById('species').value;
-  gender = document.getElementById('gender').value;
-  friend = document.getElementById('friend').value;
 
-  document.getElementById('naming').value ? name = document.getElementById('naming').value : name ='Joy';
-  document.getElementById('saying').value ? saying = document.getElementById('saying').value : saying = "Glory to Ukraine";
-
-  multipleCreation(num, species, gender, name, saying, friend);
+  multipleCreation(num, species);
 };  
+
