f52c4e01192b86e3ae96a7a2d468c515fe5ff54b
YuraZagor <u.zagorulko@gmail.com>
2022-09-21 12:40:32 +0300

    destructuring aplied

--- submissions/YuraZagor/tjsw/index.js
+++ submissions/YuraZagor/tjsw/index.js
@@ -10,11 +10,74 @@ let manName = 'Grumio';
 let dogName = 'Happy';
 let catName = 'Lucky';
 
+
+class Inhabitant {
+  constructor(species, gender, name, saying, friend){
+    this.species=species;
+    this.gender=gender;
+    this.name=name;
+    this.saying=saying;
+    this.friend=friend;
+  }
+  populate(){
+    const { species, name, gender, saying, friend } = this
+    const printArr = [species, name, gender, saying, friend]
+    inhabitants.push(this);
+    print(printArr.join('; '),'div');
+  };
+};
+
+class Animal extends Inhabitant{
+  constructor( species, gender, name = dogName, saying = dogSay, friend = womanName, paws=4, tail=1 ){
+    super(species, gender, name, saying, friend);
+    this.paws=paws;
+    this.tail=tail;
+  };
+  populate(){
+    const { species, gender, name, saying, friend, paws, tail} = this
+    const printArr = [species, gender, name, saying, friend, paws, tail]
+    inhabitants.push(this);
+    print(printArr.join('; '),'div');
+  };
+};
+
+class Human extends Inhabitant{
+  constructor( species, gender, name = manName, saying = manSay, friend=dogName, legs=2, hands=2 ){
+    super(species, gender, name, saying, friend);
+    this.legs=legs;
+    this.hands=hands;
+  };
+  populate(){
+    const { species, gender, name, saying, friend, legs, hands} = this
+    const printArr = [species, gender, name, saying, friend, legs, hands]
+    inhabitants.push(this);
+    print(printArr.join('; '),'div');
+  };
+};
+
+function creation(species, gender, name, saying, friend, legsPaws, handsTail){
+  species.toLowerCase() === 'human' 
+  ? (new Human(species, gender, name, saying, friend, legsPaws, handsTail)).populate()
+  : (new Animal(species, gender, name, saying, friend, legsPaws, handsTail)).populate()
+};
+
+function multipleCreation(num, species, gender, name, saying, friend, legsPaws, handsTail){
+  for (let i = 0; i<num; i++){
+    creation(species, gender, name, saying, friend, legsPaws, handsTail)
+  };  
+};
+
+creation('human', 'female', womanName, womanSay, catName);
+creation('human', 'male', manName, manSay, dogName);
+creation('dog', 'male', dogName, dogSay, [womanName, manName, dogName].join(', '));
+creation('cat', 'female', catName, catSay, womanName);
+multipleCreation(4, 'human', 'male')
+
+
 const inputElement = document.createElement('form');
 inputElement.classList.add('input--form');
 document.getElementById('main').appendChild(inputElement);
 inputElement.id = 'form';
-
 inputElement.innerHTML = 
 `
   <h3>Let's populate this tiny World!!!</h3>
@@ -32,19 +95,19 @@ inputElement.innerHTML =
 
   <section class="species">
     <label for="species">It will be </label>
-	  <select name="species" id="species">
+    <select name="species" id="species">
       <option value="human">Human</option>
       <option value="cat">Cat</option>
       <option value="dog">dog</option>
-	  </select>
+    </select>
   </section>
 
   <section class="gender">
     <label for="gender">boy or girl shall I choose? </label>
-	  <select name="gender" id="gender">
+    <select name="gender" id="gender">
       <option value="male">male</option>
       <option value="female">female</option>
-	  </select>
+    </select>
   </section>
 
   <section class="naming">
@@ -66,17 +129,17 @@ inputElement.innerHTML =
 
   <section class="friend">
   <label for="friend">They are best friends with </label>
-	  <select name="friend" id="friend">
+    <select name="friend" id="friend">
     <option value=${manName}>${manName}</option>
     <option value=${womanName}>${womanName}</option>
     <option value=${dogName}>${dogName}</option>
     <option value=${catName}>${catName}</option>
-	  </select>
+    </select>
   </section>
 
     
   <section class="submission">
-	  <input type='submit' value='Create!'>
+    <input type='submit' value='Create!'>
   </section>
   `
 const form = document.getElementById('form');
@@ -94,60 +157,3 @@ function useFormValue(event) {
 
   multipleCreation(num, species, gender, name, saying, friend);
 };  
-
-class Inhabitant {
-  constructor(species, gender, name, saying, friend){
-    this.species=species;
-    this.gender=gender;
-    this.name=name;
-    this.saying=saying;
-    this.friend=friend;
-  }
-  depict(){
-    return `${this.species}; ${this.name}; ${this.gender}; ${this.saying}; ${this.friend}`
-  };
-  populate(){
-    inhabitants.push(this);
-    print(this.depict(),'div');
-  };
-};
-
-class Animal extends Inhabitant{
-  constructor( species, gender, name = dogName, saying = dogSay, friend = womanName, paws=4, tail=1 ){
-    super(species, gender, name, saying, friend);
-    this.paws=paws;
-    this.tail=tail;
-  };
-  depict(){
-    return `${this.species}; ${this.name}; ${this.gender}; ${this.paws}; ${this.tail}; ${this.saying}; ${this.friend}`
-  };
-};
-
-class Human extends Inhabitant{
-  constructor( species, gender, name = manName, saying = manSay, friend=dogName, legs=2, hands=2 ){
-    super(species, gender, name, saying, friend);
-    this.legs=legs;
-    this.hands=hands;
-  };
-  depict(){
-    return `${this.species}; ${this.name}; ${this.gender}; ${this.legs}; ${this.hands}; ${this.saying}; ${this.friend}`
-  };
-};
-
-function creation(species, gender, name, saying, friend, legsPaws, handsTail){
-  species.toLowerCase() === 'human' 
-  ? (new Human(species, gender, name, saying, friend, legsPaws, handsTail)).populate()
-  : (new Animal(species, gender, name, saying, friend, legsPaws, handsTail)).populate()
-};
-
-function multipleCreation(num, species, gender, name, saying, friend, legsPaws, handsTail){
-  for (let i = 0; i<num; i++){
-    creation(species, gender, name, saying, friend, legsPaws, handsTail)
-  };  
-};
-
-creation('human', 'female', womanName, womanSay, catName);
-creation('human', 'male', manName, manSay, dogName);
-creation('dog', 'male', dogName, dogSay, [womanName, manName, dogName].join(', '));
-creation('cat', 'female', catName, catSay, womanName);
-multipleCreation(4, 'human', 'male')
