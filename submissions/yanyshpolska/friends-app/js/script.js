48cacca8d97c0ea2d079fa2e26f9ca0bfa7ed43a
Yanyshpolska <acya2002@gmail.com>
2022-09-21 17:02:45 +0300

    reworked according to common advices

--- submissions/yanyshpolska/friends-app/js/script.js
+++ submissions/yanyshpolska/friends-app/js/script.js
@@ -2,10 +2,7 @@
 
 const usersArea = document.querySelector(".friends");
 const friendsForm = document.querySelector(".options");
-friendsForm.addEventListener("change", doFiltering);
-
-const searchNameFilter = document.querySelector(".options__search");
-searchNameFilter.addEventListener("input", doFiltering);
+friendsForm.addEventListener("input", doFiltering);
 
 let initialFriendsSet = [];
 let resultingFriendsSet = [];
@@ -30,78 +27,65 @@ function initial() {
 function showFriends(friendsArray) {
   usersArea.innerHTML = "";
   for (let user of friendsArray) {
-    const userCard = `<div class="user"> 
+    const userCard = `<article class="user"> 
 	<div class="user__photo" style="
 		  background: url(${user.picture.large}) 0 0/cover no-repeat;
 		"></div>
-		 <span class="user__name">${user.name.last} ${user.name.first}</span>
+		 <h3 class="user__name">${user.name.last} ${user.name.first}</h3>
 		 <span class="user__info">${user.location.city}</span>
 		 <span class="user__info">${user.gender}, ${user.dob.age}</span>
-	</div>`;
+	</article>`;
     usersArea.innerHTML += userCard;
   }
 }
 
 function doFiltering() {
-  const isMale = document.querySelector("#male");
-  const isFemale = document.querySelector("#female");
-  const isAll = document.querySelector("#gender-all");
+  const gender = document.querySelector('input[name="gender"]:checked').value;
 
-  switch (true) {
-    case isMale.checked:
-      resultingFriendsSet = filterGender(initialFriendsSet, isMale.id);
-      break;
-    case isFemale.checked:
-      resultingFriendsSet = filterGender(initialFriendsSet, isFemale.id);
-      break;
-    case isAll.checked:
-      resultingFriendsSet = initialFriendsSet;
-      break;
-    default:
-      break;
-  }
+  gender === "all"
+    ? (resultingFriendsSet = initialFriendsSet)
+    : (resultingFriendsSet = filterGender(initialFriendsSet, gender));
+
   resultingFriendsSet = searchNameinArray(resultingFriendsSet);
   sortingArray(resultingFriendsSet);
 }
 
 function sortingArray() {
-  const ageIncrease = document.querySelector("#age-increase");
-  const ageDecrease = document.querySelector("#age-decrease");
-  const nameAtoZ = document.querySelector("#name-AtoZ");
-  const nameZtoA = document.querySelector("#name-ZtoA");
-  switch (true) {
-    case nameAtoZ.checked:
-      showFriends(sortNameAtoZ(resultingFriendsSet));
-      break;
-    case nameZtoA.checked:
-      showFriends(sortNameZtoA(resultingFriendsSet));
-      break;
-    case ageIncrease.checked:
-      showFriends(sortAgeIncrease(resultingFriendsSet));
-      break;
-    case ageDecrease.checked:
-      showFriends(sortAgeDecrease(resultingFriendsSet));
-      break;
-  }
+  const sortingCondition = document.querySelector(
+    'input[name="sorting"]:checked'
+  );
+
+  sortingCondition.dataset.info === "age"
+    ? showFriends(sortByAge(resultingFriendsSet, sortingCondition.value))
+    : showFriends(sortByName(resultingFriendsSet, sortingCondition.value));
 }
 
-function sortAgeIncrease(friendsArray) {
-  return friendsArray.sort((a, b) => a.dob.age - b.dob.age);
+const compareName = (firstFriend, secondFriend) => {
+  return firstFriend.name.last.localeCompare(secondFriend.name.last);
+};
+const compareAge = (firstFriend, secondFriend) => {
+  return firstFriend.dob.age - secondFriend.dob.age;
+};
+
+function sortByAge(friendsArray, value) {
+  return value === "ascending"
+    ? friendsArray.sort(compareAge)
+    : friendsArray.sort((a, b) => compareAge(b, a));
 }
-function sortAgeDecrease(friendsArray) {
-  return friendsArray.sort((a, b) => b.dob.age - a.dob.age);
+
+function sortByName(friendsArray, value) {
+  return value === "ascending"
+    ? friendsArray.sort(compareName)
+    : friendsArray.sort((a, b) => compareName(b, a));
 }
-function sortNameAtoZ(friendsArray) {
-  return friendsArray.sort((a, b) => a.name.last.localeCompare(b.name.last));
-}
-function sortNameZtoA(friendsArray) {
-  return friendsArray.sort((a, b) => b.name.last.localeCompare(a.name.last));
-}
+
 function filterGender(friendsArray, gender) {
   return friendsArray.filter((user) => user.gender === gender);
 }
 
 function searchNameinArray(friendsArray) {
+  const searchNameFilter = document.querySelector(".options__search");
+
   return friendsArray.filter(
     (user) =>
       user.name.last
