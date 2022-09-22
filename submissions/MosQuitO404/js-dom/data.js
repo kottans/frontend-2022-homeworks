c73584d721ad266dad2c5b2c54af7fdb55b42538
Vitaliy Zuyenko <v.v.zuenko@gmail.com>
2022-09-22 21:12:45 +0300

    Bug fixes

--- submissions/MosQuitO404/js-dom/data.js
+++ submissions/MosQuitO404/js-dom/data.js
@@ -2,8 +2,10 @@ const alfaRomeo = {
     teamTitle: 'alfa-romeo',
     firstPilotName: 'Valtteri Bottas',
     secondPilotName: 'Zhou Guanyu',
-    firstPilotFlagImage: './teams_img/flags/finland.gif', 
+    firstPilotFlagImage: './teams_img/flags/finland.gif',
+    firstPilotCountry: 'finland',
     secondPilotFlagImage: './teams_img/flags/china.jpg',
+    secondPilotCountry: 'china',
     firstPilotImage: './teams_img/teams_pilots/alfa-romeo/Valtteri-Bottas.jpg',
     secondPilotImage: './teams_img/teams_pilots/alfa-romeo/Zhou-Guanyu.jpg',
     teamInfo: '<h3>In profile</h3><p>The name Alfa Romeo boasts Formula 1 connections dating back to the championship`s inception in 1950. Fast forward to the 21st century and Italian flare combines with Swiss sensibilities in a new era for the team formerly known as Sauber.</p><p>Having enjoyed considerable success in world sportscars, where he helped nurture the emerging talents of future F1 stars Michael Schumacher and Heinz-Harald Frentzen, Peter Sauber guided his eponymous squad into F1 in 1993.</p><p>The team has since established itself as a mainstay of the grid, becoming race winners under BMW`s brief ownership, and developing a well-earned reputation not only for producing competitive cars, but also for developing young drivers.</p>'
@@ -13,7 +15,9 @@ const alphaTauri = {
     firstPilotName: 'Pierre Gasly',
     secondPilotName: 'Yuki Tsunoda',
     firstPilotFlagImage: './teams_img/flags/france.gif',
+    firstPilotCountry: 'france',
     secondPilotFlagImage: './teams_img/flags/japan.jpg',
+    secondPilotCountry: 'japan',
     firstPilotImage: './teams_img/teams_pilots/alphatauri/Pierre-Gasly.jpg',
     secondPilotImage: './teams_img/teams_pilots/alphatauri/Yuki-Tsunoda.jpg',
     teamInfo: '<h3>In profile</h3><p>Established in 2006 as a squad in which young drivers from Red Bull`s prodigious talent pool could cut their F1 teeth, AlphaTauri - originally named Toro Rosso - were formed from the ashes of the plucky Minardi team. Sebastian Vettel gave validity to the approach almost immediately, delivering a fairy-tale win in 2008, before going on to enjoy world championship success with parent team Red Bull Racing. Today the ethos of nurturing talent still holds true, though the Italian squad are no longer simply a "B team" but a constructor in their own right...</p>' 
@@ -23,7 +27,9 @@ const alpine = {
     firstPilotName: 'Fernando Alonso',
     secondPilotName: 'Esteban Ocon',
     firstPilotFlagImage: './teams_img/flags/spain.jpg',
+    firstPilotCountry: 'spain',
     secondPilotFlagImage: './teams_img/flags/france.gif',
+    secondPilotCountry: 'france',
     firstPilotImage: './teams_img/teams_pilots/alpine/Fernando-Alonso.jpg',
     secondPilotImage: './teams_img/teams_pilots/alpine/Esteban-Ocon.jpg',
     teamInfo: '<h3>In profile</h3><p>Alpine may be a relatively new name to Formula 1, but Renault`s famous sportscar arm has plenty of motorsport heritage. The 2021 rebrand of the team marked the next step in Renault`s F1 revival, begun in 2016 with the takeover of the then-Lotus squad. Already race winners in their new guise, regular podiums and a tilt at the title must be their next target...</p>' 
@@ -33,7 +39,9 @@ const astonMartin = {
     firstPilotName: 'Sebastian Vettel',
     secondPilotName: 'Lance Stroll',
     firstPilotFlagImage: './teams_img/flags/germany.jpg',
+    firstPilotCountry: 'germany',
     secondPilotFlagImage: './teams_img/flags/canada.jpg',
+    secondPilotCountry: 'canada',
     firstPilotImage: './teams_img/teams_pilots/aston-martin/Sebastian-Vettel.jpg',
     secondPilotImage: './teams_img/teams_pilots/aston-martin/Lance-Stroll.jpg',
     teamInfo: '<h3>In profile</h3><p>Aston Martin`s original Formula 1 foray - over half a century ago - lasted just five races. This time, though, it`s serious. This F1 squad are no strangers to success, having won in their original guise of Jordan and most recently as Racing Point in 2020. Renowned for their ability to punch above their weight, and now with a four-time champion leading their driver line-up, Aston Martin are very much a team to watch...</p>' 
@@ -43,7 +51,9 @@ const ferrari = {
     firstPilotName: 'Charles Leclerc',
     secondPilotName: 'Carlos Sainz',
     firstPilotFlagImage: './teams_img/flags/monaco.jpg',
+    firstPilotCountry: 'monaco',
     secondPilotFlagImage: './teams_img/flags/spain.jpg',
+    secondPilotCountry: 'spain',
     firstPilotImage: './teams_img/teams_pilots/ferrari/Charles-Leclerc.jpg',
     secondPilotImage: './teams_img/teams_pilots/ferrari/Carlos-Sainz.jpg',
     teamInfo: '<h3>In profile</h3><p>For many, Ferrari and Formula 1 racing have become inseparable. The only team to have competed in every season since the world championship began, the Prancing Horse has grown from the humble dream of founder Enzo Ferrari to become one of the most iconic and recognised brands in the world. Success came quickly through the likes of Alberto Ascari and John Surtees, and continued - in amongst leaner times - with Niki Lauda in the 1970s and then Michael Schumacher in the 2000s, when Ferrari claimed a then unprecedented five consecutive title doubles, securing their status as the most successful and decorated team in F1 history...</p>' 
@@ -53,7 +63,9 @@ const haas = {
     firstPilotName: 'Mick Schumacher',
     secondPilotName: 'Kevin Magnussen',
     firstPilotFlagImage: './teams_img/flags/germany.jpg',
+    firstPilotCountry: 'germany',
     secondPilotFlagImage: './teams_img/flags/denmark.jpg',
+    secondPilotCountry: 'denmark',
     firstPilotImage: './teams_img/teams_pilots/haas/Mick-Schumacher.jpg',
     secondPilotImage: './teams_img/teams_pilots/haas/Kevin-Magnussen.jpg',
     teamInfo: '<h3>In profile</h3><p>The youngest team on the grid, Haas made their highly impressive debut in 2016, and in the process became the first all-American-led F1 squad in three decades. Founded by industrialist Gene Haas, they are based in the United States on the same Kannapolis, North Carolina facility as his championship-winning NASCAR Sprint Cup Series team, Stewart-Haas Racing. The Ferrari-powered team, led by the charismatic Guenther Steiner, also have a UK factory in Banbury...</p>' 
@@ -63,7 +75,9 @@ const mcLaren = {
     firstPilotName: 'Daniel Ricciardo',
     secondPilotName: 'Lando Norris',
     firstPilotFlagImage: './teams_img/flags/australia.jpg',
+    firstPilotCountry: 'australia',
     secondPilotFlagImage: './teams_img/flags/gb.jpg',
+    secondPilotCountry: 'great britain',
     firstPilotImage: './teams_img/teams_pilots/mclaren/Daniel-Ricciardo.jpg',
     secondPilotImage: './teams_img/teams_pilots/mclaren/Lando-Norris.jpg',
     teamInfo: '<h3>In profile</h3><p>Since entering the sport in 1966 under the guidance and restless endeavour of eponymous founder Bruce, McLaren`s success has been nothing short of breathtaking. Five glittering decades have yielded countless victories, pole positions and podiums, not to mention eight constructors` championships. What`s more, some of the sport`s greatest drivers made their names with the team, including Emerson Fittipaldi, Ayrton Senna, Mika Hakkinen and Lewis Hamilton...</p>'
@@ -73,7 +87,9 @@ const mercedes = {
     firstPilotName: 'Lewis Hamilton',
     secondPilotName: 'George Russell',
     firstPilotFlagImage: './teams_img/flags/gb.jpg',
+    firstPilotCountry: 'great britain',
     secondPilotFlagImage: './teams_img/flags/gb.jpg',
+    secondPilotCountry: 'great britain',
     firstPilotImage: './teams_img/teams_pilots/mercedes/Lewis-Hamilton.jpg',
     secondPilotImage: './teams_img/teams_pilots/mercedes/George-Russell.jpg',
     teamInfo: '<h3>In profile</h3><p>Mercedes` modern F1 revival started with the creation of a works squad for 2010 - the platform for a meteoric rise up the Grand Prix order. The team generated huge excitement from the off with the sensational return of Michael Schumacher, but headlines soon followed on track: three podiums in their debut season, all via Nico Rosberg - who then claimed a breakthrough pole/victory double at China in 2012. The following season he was paired with Lewis Hamilton, the duo going on to stage some epic title battles as the Silver Arrows swept all before them to become one of the most dominant forces of the modern F1 era. And with Hamilton now partnered by steely Finn Valtteri Bottas, Mercedes remain very much the team to beat...</p>'
@@ -83,7 +99,9 @@ const redBull = {
     firstPilotName: 'Max Verstappen',
     secondPilotName: 'Sergio Perez',
     firstPilotFlagImage: './teams_img/flags/netherlands.gif',
+    firstPilotCountry: 'netherlands',
     secondPilotFlagImage: './teams_img/flags/mexico.gif',
+    secondPilotCountry: 'mexico',
     firstPilotImage: './teams_img/teams_pilots/red-bull/Max-Verstappen.jpg',
     secondPilotImage: './teams_img/teams_pilots/red-bull/Sergio-Perez.jpg',
     teamInfo: '<h3>In profile</h3><p>Red Bull were no strangers to F1 - as sponsors - prior to formally entering as a works team in 2004. Nonetheless, the scale of their success over the following decade was staggering. After a first podium in 2006, the team hit their stride in 2009, claiming six victories and second in the constructors` standings. Over the next four seasons they were a tour de force, claiming consecutive title doubles between 2010 and 2013, with Sebastian Vettel emerging as the sport`s youngest quadruple champion. Now their hopes of recapturing that glory lie with an equally exciting talent - one named Max Verstappen...</p>'
@@ -93,14 +111,12 @@ const williams = {
     firstPilotName: 'Nicholas Latifi',
     secondPilotName: 'Alexander Albon',
     firstPilotFlagImage: './teams_img/flags/canada.jpg',
+    firstPilotCountry: 'canada',
     secondPilotFlagImage: './teams_img/flags/thailand.png',
+    secondPilotCountry: 'thailand',
     firstPilotImage: './teams_img/teams_pilots/villiams/Nicholas-Latifi.jpg',
     secondPilotImage: './teams_img/teams_pilots/villiams/Alexander-Albon.jpg',
     teamInfo: '<h3>In profile</h3><p>Driven on by the brilliance and passion of the late Sir Frank Williams, Williams grew from humble beginnings to become a Formula 1 behemoth, unrivalled by all except Ferrari and McLaren in terms of enduring success. Over the past four decades the team has racked up Grand Prix wins and championship glory, and in the process nurtured some of the greatest talents in the sport, both in and out the cockpit. Now, following the Williams family`s decision to step aside after the sale of the team to Dorilton Capital, a new era begins...</p>'
 }
 
-export
-const teamsData = [alfaRomeo, alphaTauri, alpine, astonMartin, ferrari, haas, mcLaren, mercedes, redBull, williams];
-
-export
-const teamsProperties = ['teamTitle', 'firstPilotName', 'secondPilotName', 'firstPilotFlagImage', 'secondPilotFlagImage', 'firstPilotImage', 'secondPilotImage', 'teamInfo'];
+export const teamsData = [alfaRomeo, alphaTauri, alpine, astonMartin, ferrari, haas, mcLaren, mercedes, redBull, williams];
--- submissions/MosQuitO404/js-dom/index.js
+++ submissions/MosQuitO404/js-dom/index.js
@@ -1,4 +1,4 @@
-import { teamsData, teamsProperties } from "./data.js";
+import { teamsData } from "./data.js";
 
 const hamb = document.querySelector('#hamb');
 const popup = document.querySelector('#popup');
@@ -23,31 +23,23 @@ function renderPopup() {
     popup.append(popupMenu);
 }
 
-// Teams creation
+// Teams HTML creation
 
-class Team {
-    constructor(teamTitle, firstPilotName, secondPilotName, firstPilotFlagImage, secondPilotFlagImage, firstPilotImage, secondPilotImage, teamInfo) {
-        this.teamTitle = teamTitle;
-        this.firstPilotName = firstPilotName;
-        this.secondPilotName = secondPilotName;
-        this.firstPilotFlagImage = firstPilotFlagImage;
-        this.secondPilotFlagImage = secondPilotFlagImage;
-        this.firstPilotImage = firstPilotImage;
-        this.secondPilotImage = secondPilotImage;
-        this.teamInfo = teamInfo;
+class TeamHTML {
+    constructor(team) {
         this.html = `<div class="pilots-container">
                         <div class="pilot-wrapper">
-                            <span class="pilot-name"><h3>${this.firstPilotName}</h3></span>
-                            <img class="flag-img" src="${this.firstPilotFlagImage}" alt="">
-                            <span class="pilot-img-wrapper"><img class="pilot-img" src="${this.firstPilotImage}" alt=""></span>
+                            <span class="pilot-name"><h3>${team.firstPilotName}</h3></span>
+                            <img class="flag-img" src="${team.firstPilotFlagImage}" alt="${team.firstPilotCountry} flag">
+                            <span class="pilot-img-wrapper"><img class="pilot-img" src="${team.firstPilotImage}" alt="first pilot ${team.firstPilotName}"></span>
                         </div>
                         <div class="pilot-wrapper">
-                            <span class="pilot-name"><h3>${this.secondPilotName}</h3></span>
-                            <img class="flag-img" src="${this.secondPilotFlagImage}" alt="">
-                            <span class="pilot-img-wrapper"><img class="pilot-img" src="${this.secondPilotImage}" alt=""></span>
+                            <span class="pilot-name"><h3>${team.secondPilotName}</h3></span>
+                            <img class="flag-img" src="${team.secondPilotFlagImage}" alt="${team.secondPilotCountry}">
+                            <span class="pilot-img-wrapper"><img class="pilot-img" src="${team.secondPilotImage}" alt="second pilot ${team.secondPilotName}"></span>
                         </div>
                     </div>
-                    <div class="team-info-container">${this.teamInfo}</div>`;
+                    <div class="team-info-container">${team.teamInfo}</div>`;
     }
 
     get htmlRender() {
@@ -64,16 +56,17 @@ function generateTeamHandler(e) {
     e.preventDefault();
     const parent = e.target.closest('.menu-team');
     if (parent) {
-        const actionTeam = teamsData.filter(team => team['teamTitle'] === parent.dataset.action);
-        new Team(...generatePropertiesValue(...actionTeam, teamsProperties)).htmlRender;
+        const actionTeam = teamsData.find(team => {
+            if (team.teamTitle == parent.dataset.action) {
+                return team;
+            }
+        });
+        
+        new TeamHTML(actionTeam).htmlRender;
     }
     if (popup.classList.contains('open')) {
         setTimeout(hambClose, 500);
     }
-}
-
-function generatePropertiesValue(team, teamsProperties) { 
-    return teamsProperties.map(property => team[property]);
 }
 
 function hambClose() { 
