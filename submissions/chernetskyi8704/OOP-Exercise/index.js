dd10755090ccd41406f74dac0e222f89c6bb03ee
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2022-09-24 11:48:53 +0300

    Update PR to pre-OOP version.

--- submissions/chernetskyi8704/OOP-Exercise/index.js
+++ submissions/chernetskyi8704/OOP-Exercise/index.js
@@ -1,199 +1,42 @@
-class Inhabitant {
-  constructor(Species, Name, Gender, Saying) {
-    this.Species = Species;
-    this.Name = Name;
-    this.Gender = Gender;
-    this.Saying = Saying;
-  }
+const dog = {
+  species: "dog",
+  legs: 4,
+  hands: 0,
+  name: "Molly",
+  gender: "female",
+  saying: "WOOF-WOOF!",
+};
 
-  addBoldAndRedSemicolon(string) {
-    return string.bold().fontcolor("red");
-  }
+const cat = {
+  species: "cat",
+  legs: 4,
+  hands: 0,
+  name: "Whiskey",
+  gender: "male",
+  saying: "Meow!",
+};
 
-  addRedBoldCollon(string) {
-    return string.bold().fontcolor("red");
-  }
+const woman = {
+  species: "human",
+  legs: 2,
+  hands: 2,
+  name: "Kate",
+  gender: "female",
+  saying: "Have a good day!",
+};
 
-  makeStringRedAndBold(string) {
-    return string.bold().fontcolor("Red");
-  }
+const man = {
+  species: "human",
+  legs: 2,
+  hands: 2,
+  name: "Dimitrij",
+  gender: "male",
+  saying: "Nice to meet you!",
+};
 
-  makeStringBold(string) {
-    return string.bold();
-  }
-
-  saying() {
-    return ["Saying"].map(
-      (propertyOfTheInhabitant) =>
-        ` ${
-          this.makeStringRedAndBold(propertyOfTheInhabitant) +
-          this.addRedBoldCollon(":")
-        } ${this.makeStringBold(this.Saying)}` +
-        this.addBoldAndRedSemicolon(";")
-    );
-  }
-
-  displayData() {
-    return (
-      ["Species", "Name", "Gender"]
-        .map(
-          (propertyOfTheInhabitant) =>
-            `${
-              this.makeStringRedAndBold(propertyOfTheInhabitant) +
-              this.addRedBoldCollon(":")
-            } ${this[propertyOfTheInhabitant].bold()}`
-        )
-        .join(this.addBoldAndRedSemicolon("; ")) +
-      this.addBoldAndRedSemicolon(";")
-    );
-  }
-}
-class Human extends Inhabitant {
-  constructor(Species, Name, Gender, Saying) {
-    super(Species, Name, Gender, Saying);
-    this.Hands = "2";
-    this.Legs = "2";
-  }
-
-  displayData() {
-    return (
-      `${super.displayData()} ` +
-      ["Hands", "Legs"]
-        .map(
-          (propertyOfTheInhabitant) =>
-            `${
-              this.makeStringRedAndBold(propertyOfTheInhabitant) +
-              this.addRedBoldCollon(":")
-            } ${this[propertyOfTheInhabitant].bold()}`
-        )
-        .join(this.addBoldAndRedSemicolon(";")) +
-      this.addBoldAndRedSemicolon("; ") +
-      this.saying()
-    );
-  }
-}
-
-class Animal extends Inhabitant {}
-
-class Man extends Human {
-  constructor(Name, Gender, Saying) {
-    super("Man🤵", Name, Gender, Saying);
-  }
-}
-
-class Woman extends Human {
-  constructor(Name, Gender, Saying) {
-    super("Woman👩", Name, Gender, Saying);
-  }
-}
-
-class Dog extends Animal {
-  constructor(Name, Gender, Saying) {
-    super("Dog🐶", Name, Gender, Saying);
-    this.Paws = "4";
-    this.Tail = "1";
-  }
-
-  displayData() {
-    return (
-      `${super.displayData()} ` +
-      ["Paws", "Tail"]
-        .map(
-          (propertyOfTheInhabitant) =>
-            `${
-              this.makeStringRedAndBold(propertyOfTheInhabitant) +
-              this.addRedBoldCollon(":")
-            } ${this[propertyOfTheInhabitant].bold()}`
-        )
-        .join(this.addBoldAndRedSemicolon("; ")) +
-      this.addBoldAndRedSemicolon(";") +
-      this.saying()
-    );
-  }
-}
-
-class Cat extends Animal {
-  constructor(Name, Gender, Saying) {
-    super("Cat🐱", Name, Gender, Saying);
-    this.Paws = "4";
-    this.Tail = "1";
-  }
-
-  displayData() {
-    return (
-      `${super.displayData()} ` +
-      ["Paws", "Tail"]
-        .map(
-          (propertyOfTheInhabitant) =>
-            `${
-              this.makeStringRedAndBold(propertyOfTheInhabitant) +
-              this.addRedBoldCollon(":")
-            } ${this[propertyOfTheInhabitant].bold()}`
-        )
-        .join(this.addBoldAndRedSemicolon("; ")) +
-      this.addBoldAndRedSemicolon(";") +
-      this.saying()
-    );
-  }
-}
-
-class Hamster extends Animal {
-  constructor(Name, Gender, Saying) {
-    super("Hamster🐹", Name, Gender, Saying);
-    this.Paws = "4";
-    this.Tail = "1";
-  }
-
-  displayData() {
-    return (
-      `${super.displayData()} ` +
-      ["Paws", "Tail"]
-        .map(
-          (propertyOfTheInhabitant) =>
-            `${
-              this.makeStringRedAndBold(propertyOfTheInhabitant) +
-              this.addRedBoldCollon(":")
-            } ${this[propertyOfTheInhabitant].bold()}`
-        )
-        .join(this.addBoldAndRedSemicolon("; ")) +
-      this.addBoldAndRedSemicolon(";") +
-      this.saying()
-    );
-  }
-}
-
-class Snake extends Animal {
-  constructor(Name, Gender, Saying) {
-    super("Snake🐍", Name, Gender, Saying);
-    this.Tail = "1";
-  }
-  displayData() {
-    return (
-      `${super.displayData()} ` +
-      ["Tail"]
-        .map(
-          (propertyOfTheInhabitant) =>
-            `${
-              this.makeStringRedAndBold(propertyOfTheInhabitant) +
-              this.addRedBoldCollon(":")
-            } ${this[propertyOfTheInhabitant].bold()}`
-        )
-        .join(this.addBoldAndRedSemicolon("; ")) +
-      this.addBoldAndRedSemicolon(";") +
-      this.saying()
-    );
-  }
-}
-
-const man = new Man("Dimitrij", "Male", "Nice to meet you👋");
-const woman = new Woman("Kate", "Female", "Have a good day🤗");
-const dog = new Dog("Molly", "Female", "Bring me more food🍗 WOOF-woof!");
-const cat = new Cat("Whiskey", "Male", "Meeow... I'm tired of Molly😑");
-const hamster = new Hamster("Zhuzha", "Female", "Here is so cold🤧");
-const snake = new Snake("Nagini", "Female", "Ssssssss");
-
-const allInhabitants = [man, woman, dog, cat, hamster, snake];
+const allInhabitants = [dog, cat, woman, man];
+const keys = ["species", "legs", "hands", "name", "gender", "saying"];
 
 allInhabitants.map((inhabitant) => {
-  print(inhabitant.displayData());
+  print(keys.map((key) => inhabitant[key]).join("; "));
 });
