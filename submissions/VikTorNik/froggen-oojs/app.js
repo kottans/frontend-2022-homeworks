4cf9786a1eeedb5f1c91b29e0286ed9cdd66c3e1
V_Pro <C:\Users\Viktor_Dell\AppData\Roaming\The Bat!>
2022-09-26 23:06:30 +0300

    Fixed bug after review

--- submissions/VikTorNik/froggen-oojs/app.js
+++ submissions/VikTorNik/froggen-oojs/app.js
@@ -1,72 +1,85 @@
 const GAME_CONF = {
-    OffsetX : 101,
-    OffsetY : 83,
-    overlap : 0.8,
-    collumn : 5,
-    countWinner : 0,
-    countСheckmate : 0,
+    OffsetX: 101,
+    OffsetY: 83,
+    overlap: 0.8,
+    collumn: 5,
+    countWinner: 0,
+    countСheckmate: 0,
 };
 
 const PLAYER_CONF = {
     startX: 202,
     startY: 405,
+    offsetMinX: 0,
+    offsetMaxX: 400,
+    offsetMinY: 0,
+    offsetMaxY: 400,
+    sprite: "images/char-boy.png",
 };
 
 const ENEMY_CONF = {
     startY: GAME_CONF.OffsetY / 4,
     speed: 50,
-    maximumCount: 5,    
+    maximumCount: 5,
+    sprite: 'images/enemy-bug.png',
 };
 
-let allEnemies = [];
-
-let Enemy = function (x, y, speed) {
-    this.sprite = 'images/enemy-bug.png';
+const Elementary = function (x, y, sprite) {
     this.x = x;
     this.y = y;
-    this.speed = ENEMY_CONF.speed * (1 + Math.random() * 4);
-};
+    this.sprite = sprite;
+}
+
+const Enemy = function (x, y, sprite, speed, player) {
+    Elementary.call(this, x, y, sprite);
+    this.speed = speed;
+    this.player = player;
+}
+// Enemy.prototype = Object.create(Elementary.prototype);
+// Enemy.prototype.constructor = Enemy;
 
 Enemy.prototype.update = function (dt) {
     this.x += this.speed * dt;
     this.x > GAME_CONF.OffsetX * GAME_CONF.collumn ? this.x = -GAME_CONF.OffsetX : false;
     this.collision();
-    this.winner();
 };
 
 Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-function createEnems() {
-    allEnemies = [...Array(Math.floor((3 + Math.random() * (ENEMY_CONF.maximumCount - 2))))].map((_, i) =>
-        i = new Enemy(-GAME_CONF.OffsetX, GAME_CONF.OffsetY * Math.floor(Math.random() * 3 + 1) - ENEMY_CONF.startY, ENEMY_CONF.speed));
-};
-
-createEnems();
-
 Enemy.prototype.collision = function () {
-    ((player.x < this.x + GAME_CONF.OffsetX * GAME_CONF.overlap) &&
-        (player.x > this.x - GAME_CONF.OffsetX * GAME_CONF.overlap) &&
-        (player.y < this.y + GAME_CONF.OffsetY * GAME_CONF.overlap) &&
-        (player.y > this.y - GAME_CONF.OffsetY * GAME_CONF.overlap)) ? (player.x = PLAYER_CONF.startX, player.y = PLAYER_CONF.startY, createEnems(), GAME_CONF.countСheckmate += 1) : false;
+    ((this.player.x < this.x + GAME_CONF.OffsetX * GAME_CONF.overlap) &&
+        (this.player.x > this.x - GAME_CONF.OffsetX * GAME_CONF.overlap) &&
+        (this.player.y < this.y + GAME_CONF.OffsetY * GAME_CONF.overlap) &&
+        (this.player.y > this.y - GAME_CONF.OffsetY * GAME_CONF.overlap)) ?
+        (this.player.x = PLAYER_CONF.startX,
+            this.player.y = PLAYER_CONF.startY,
+            GAME_CONF.countСheckmate += 1)
+        : false;
 };
 
-Enemy.prototype.winner = function () {
-    player.y < 0 ? (player.x = PLAYER_CONF.startX, player.y = PLAYER_CONF.startY, createEnems(), GAME_CONF.countWinner += 1) : false;
-};
-
-let Player = function (x = PLAYER_CONF.startX, y = PLAYER_CONF.startY) {
-    this.sprite = "images/char-boy.png";
-    this.x = x;
-    this.y = y;
+const Player = function (x, y, sprite) {
+    Elementary.call(this, x, y, sprite);
 }
 
 Player.prototype.update = function () {
-    this.offsetMinX = 0;
-    this.offsetMaxX = 400;
-    this.offsetMinY = 0;
-    this.offsetMaxY = 400;
+    this.offsetMinX = PLAYER_CONF.offsetMinX;
+    this.offsetMaxX = PLAYER_CONF.offsetMaxX;
+    this.offsetMinY = PLAYER_CONF.offsetMinY;
+    this.offsetMaxY = PLAYER_CONF.offsetMaxY;
+    this.winner();
+};
+
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    this.countWinnerСheckmate();
+};
+
+Player.prototype.winner = function () {
+    this.y < 0
+        ? (this.x = PLAYER_CONF.startX, this.y = PLAYER_CONF.startY, GAME_CONF.countWinner += 1)
+        : false;
 };
 
 Player.prototype.countWinnerСheckmate = function () {
@@ -82,17 +95,6 @@ Player.prototype.countWinnerСheckmate = function () {
     ctx.fillText(GAME_CONF.countСheckmate, Math.floor(GAME_CONF.OffsetX * 3.5), 117);
 };
 
-Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    this.countWinnerСheckmate();
-};
-
-let player = new Player();
-
-document.addEventListener('keyup', e => {
-    player.handleInput(e.code);
-});
-
 Player.prototype.handleInput = function (keyPress) {
     keyPress == 'ArrowLeft' && this.x > this.offsetMinX ?
         this.x -= GAME_CONF.OffsetX : false;
@@ -103,4 +105,18 @@ Player.prototype.handleInput = function (keyPress) {
     keyPress == 'ArrowDown' && this.y < this.offsetMaxY ?
         this.y += GAME_CONF.OffsetY : false;
 }
+
+const player = new Player(PLAYER_CONF.startX, PLAYER_CONF.startY, PLAYER_CONF.sprite);
+
+const allEnemies = [...Array(Math.floor((3 + Math.random() * (ENEMY_CONF.maximumCount - 2))))].map((_, enemy) =>
+    enemy = new Enemy(-GAME_CONF.OffsetX,
+        GAME_CONF.OffsetY * Math.floor(Math.random() * 3 + 1) - ENEMY_CONF.startY,
+        ENEMY_CONF.sprite,
+        ENEMY_CONF.speed * (1 + Math.random() * 4),
+        player)
+);
+
+document.addEventListener('keyup', event => {
+    player.handleInput(event.code);
+});
 
