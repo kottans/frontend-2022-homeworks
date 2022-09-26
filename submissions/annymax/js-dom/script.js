66a8dec5f21c538384467fae6a6c24b7a7766d41
Anna Lapko <annaaleks986@gmail.com>
2022-09-26 21:52:17 +0300

    fixed CSS media queries for no-repeat background-image and opening menu with tab

--- submissions/annymax/js-dom/script.js
+++ submissions/annymax/js-dom/script.js
@@ -57,7 +57,7 @@ const basketPlayers = [
   (born August 23, 1978, Philadelphia, Pennsylvania, U.S.—died January 26, 2020, Calabasas, California), 
   American professional basketball player, who helped lead the Los Angeles Lakers of the National 
   Basketball
-   Association(NBA) to five championships (2000–02 and 2009–10).`,
+   Association (NBA) to five championships (2000–02 and 2009–10).`,
   },
   {
     id: "6",
@@ -72,15 +72,17 @@ const basketPlayers = [
   },
 ];
 
-const button = document.querySelector(".image-ball");
+const button = document.querySelector(".button");
 const menu = document.querySelector(".menu");
 const content = document.querySelector(".content-wrapper");
 const siteTitle = document.querySelector(".site-title");
+const firstListItem = document.querySelector(".focus-el");
 
 button.addEventListener("click", function () {
   content.innerHTML = "";
   menu.classList.toggle("menu-active");
   siteTitle.classList.toggle("hidden");
+  firstListItem.focus();
 });
 
 const bodyElement = document.body;
