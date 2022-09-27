086b1d50e9fad7fdd1b0b555fba61ae2a2274320
vovan-zt <predator_vovan@ukr.net>
2022-09-27 23:03:08 +0300

    fix OOP/SOLID compliant.

--- submissions/vovan-zt/TiniWorld2/index.js
+++ submissions/vovan-zt/TiniWorld2/index.js
@@ -1,42 +1,58 @@
 class WorldInhabitant {
-   constructor(species, name, gender, saying) {
-      this.species = species;
-      this.name = name;
-      this.gender = gender;
-      this.saying = saying;
-      this.prop = [species, name, gender, saying];
-   }
+  constructor(species, name, gender, saying) {
+    this.species = species;
+    this.name = name;
+    this.gender = gender;
+    this.saying = saying;
+    this.prop = [species, name, gender, saying];
+  }
 
-   getMeaning() {
-      return this.prop.map(meaning => meaning);
-   }    
+  getMeaning() {
+    return this.prop.map((meaning) => meaning).join("; ");
+  }
+}
+class Human extends WorldInhabitant {
+  constructor(name, gender, saying, friends = null) {
+    super("human", name, gender, saying);
+    this.legs = 2;
+    this.hands = 2;
+    this.friends = friends;
+    this.prop = [...this.prop, this.legs, this.hands, this.friends];
+  }
+}
+class Animal extends WorldInhabitant {
+  constructor(species, name, gender, saying) {
+    super(species, name, gender, saying);
+    this.paws = 4;
+    this.prop = [...this.prop, this.paws];
+  }
+}
+class Dog extends Animal {
+  constructor(name, gender) {
+    super("dog", name, gender, "woof!");
+  }
+}
+class Cat extends Animal {
+  constructor(name, gender) {
+    super("cat", name, gender, "meow!");
+  }
+}
+class CatWoman extends Human {
+  constructor(name, saying) {
+    super(name, "woman", saying);
+  }
 }
 
-class Human extends WorldInhabitant { 
-   constructor(species, name, gender, saying, legs = 2, hands = 2, friends = '') {
-      super(species, name, gender, saying);
-      this.legs = legs;
-      this.hands = hands;
-      this.prop = [species, name, gender, saying, legs, hands, friends];
-   }
-}
+const man = new Human("Jon", "male", "I want to sleep!");
 
-class Animal extends WorldInhabitant { 
-   constructor(species, name, gender, saying, paws = 4) {
-      super(species, name, gender, saying);
-      this.paws = paws;
-      this.prop = [species, name, gender, saying, paws];
-   }
-}
+const woman = new Human("Lena", "female", "Hello!", man.name);
 
- const man = new Human('human', 'Jon', 'male', 'I want to sleep');
+const cat = new Cat("Luna", "female");
 
- const woman = new Human('human', 'Lena', 'female', 'Hello', 2, 2, man.name );
- 
- const cat = new Animal('cat', 'Luna', 'female','murrr');
-  
- const dog = new Animal('dog','Rasti','male','woof-woof');
+const dog = new Dog("Rasti", "male");
 
- const catWoman = new Human('catwoman','Selina', 'female', cat.saying);
+const catWoman = new CatWoman("Selina", cat.saying);
 
- [man, woman, cat, dog, catWoman].forEach(inhabitant => print(inhabitant.getMeaning().join('; ')))
+[man, woman, cat, dog, catWoman].forEach((inhabitant) =>
+  print(inhabitant.getMeaning())
+);
