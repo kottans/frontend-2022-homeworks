c7dba79ebb1d05ec6866dad632bd5310095255b2
Yuriy Kozak <zoumzgame@gmail.com>
2022-09-27 17:48:07 +0300

    Make selectors witout objects, fix shuffle method, change method for to method forEach, understand difference between null and undefinde

--- submissions/Levi123/memory-pair-game/script.js
+++ submissions/Levi123/memory-pair-game/script.js
@@ -1,17 +1,15 @@
-const SELECTORS = {
-    flipContainer : document.querySelector('.flip-container'),
-    content : document.querySelector('.main-content__playing-field'),
-    textContent: document.querySelector('.main-content'),
-    timer : document.querySelector('.seconds'),
-    flipCounter: document.querySelector('.flips'),
-}
+const flipContainer = document.querySelector('.flip-container');
+const content = document.querySelector('.main-content__playing-field');
+const textContent = document.querySelector('.main-content');
+const timer = document.querySelector('.seconds');
+const flipCounter = document.querySelector('.flips');
 
 const STATE = {
     totalFlips : 0,
     totalTime : 0,
     flipElementIdArray : [],
-    cardOne : undefined,
-    cardTwo : undefined,
+    cardOne : null,
+    cardTwo : null,
     statusGame : false,
     loop: null,
     combinationCounter: 6,
@@ -51,28 +49,30 @@ const generateGame = () => {
         },
     ]
 
-
-    function shuffle(arrayToShuffle) {
-        for(var j, x, i = arrayToShuffle.length; i; j = parseInt(Math.random() * i), x = arrayToShuffle[--i], arrayToShuffle[i] = arrayToShuffle[j], arrayToShuffle[j] = x);
-        return arrayToShuffle;
-    };
-
-    const generateElementForBoard = shuffle([...allElementsForGame, ...allElementsForGame]);
-    console.log(generateElementForBoard);
-
-    for (let i of generateElementForBoard){
-        let list = `
-            <div class="main-content__element flipper">
-                <div class="front">
-                    <img src="img/card.png" alt="" class="flip-card-img" data-id=${i.id}>
-                </div>
-                <div class="back">
-                    <img src=${i.images} alt="" class="flip-card-back-img">
-                </div>
-            </div>
-        `
-    SELECTORS.content.innerHTML += list;
+    const shuffleArray = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          const temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
+        return array;
     }
+
+    const generateElementForBoard = shuffleArray([...allElementsForGame, ...allElementsForGame]);
+
+    generateElementForBoard.forEach((elementForRender) => {
+        content.innerHTML += `
+        <div class="main-content__element flipper">
+            <div class="front">
+                <img src="img/card.png" alt="" class="flip-card-img" data-id=${elementForRender.id}>
+            </div>
+            <div class="back">
+                <img src=${elementForRender.images} alt="" class="flip-card-back-img">
+            </div>
+        </div>
+    `
+    })
 }       
 
 generateGame();
@@ -81,14 +81,14 @@ const startGame = () => {
     STATE.statusGame = true;
     STATE.loop = setInterval(() => {
         STATE.totalTime++
-        SELECTORS.timer.innerText = ` ${STATE.totalTime} sec`
+        timer.innerText = ` ${STATE.totalTime} sec`
     }, 1000)
 }
 
 const flipCard = (target) => {
     STATE.totalFlips++;
     target.classList.add('rotate_card');
-    SELECTORS.flipCounter.innerText = ` ${STATE.totalFlips}`;
+    flipCounter.innerText = ` ${STATE.totalFlips}`;
 }
 
 const checkCard = (target, parentElement) => {
@@ -111,7 +111,7 @@ const checkCard = (target, parentElement) => {
         setTimeout(() => {
             STATE.cardOne.innerHTML='';
             STATE.cardTwo.innerHTML='';
-            STATE.cardOne = undefined;
+            STATE.cardOne = null;
             STATE.isInteractive = true;
         }, 1000)
     }
@@ -122,7 +122,7 @@ const checkCard = (target, parentElement) => {
         setTimeout(() => {
             STATE.cardOne.classList.remove('rotate_card');
             STATE.cardTwo.classList.remove('rotate_card');
-            STATE.cardOne = undefined;
+            STATE.cardOne = null;
             STATE.isInteractive = true;
         }, 1000)
     }
@@ -132,7 +132,7 @@ const checkCard = (target, parentElement) => {
     }
 }
  
-SELECTORS.flipContainer.addEventListener('click', function (event) {
+flipContainer.addEventListener('click', function (event) {
     const eventTarget = event.target;
     const eventParent = eventTarget.parentElement;
     if (event.target.closest('.front') && STATE.isInteractive){
