9d0570cc21e999b8417ce24b1e3fbfb8c6af62ec
m.sofiichuk <koyap@ukr.net>
2022-09-27 20:34:23 +0300

    moved legs prop and declaration method

--- submissions/sofiichuk/oop-exercise/index.js
+++ submissions/sofiichuk/oop-exercise/index.js
@@ -1,47 +1,58 @@
 class Animal {
-    constructor(name, gender, saying) {
+    constructor(name, gender, saying, legs) {
         this.name = name;
         this.gender = gender;
         this.saying = saying;
+        this.legs = legs;
+    }
+
+    declareAnimal() {
+        return [this.name, this.gender, this.saying, this.legs].join(";");
     }
 }
 
 class Human extends Animal {
     constructor(name, gender, saying, legs = 2, arms = 2) {
-        super(name, gender, saying);
-        this.legs = legs;
+        super(name, gender, saying, legs);
         this.arms = arms;
+    }
+
+    declareGenus() {
+        return [this.constructor.name, super.declareAnimal(), this.arms].join(";");
     }
 }
 
 class Pet extends Animal {
     constructor(name, gender, saying, legs = 4) {
-        super(name, gender, saying);
-        this.legs = legs;
+        super(name, gender, saying, legs);
+    }
+
+    declareGenus() {
+        return [this.constructor.name, super.declareAnimal()].join(";");
     }
 }
 
 class Man extends Human {
     constructor(name, gender = 'male', saying = 'hi', legs, arms) {
-        super(name, gender, saying, legs, arms);
+        super(name, gender, saying, legs, arms)
     }
 }
 
 class Woman extends Human {
     constructor(name, gender = 'female', saying = 'hello', legs, arms) {
-        super(name, gender, saying, legs, arms);
+        super(name, gender, saying, legs, arms)
     }
 }
 
 class Dog extends Pet {
     constructor(name, gender, saying = 'woof', legs) {
-        super(name, gender, saying, legs);
+        super(name, gender, saying, legs)
     }
 }
 
 class Cat extends Pet {
     constructor(name, gender, saying = 'meow', legs) {
-        super(name, gender, saying, legs);
+        super(name, gender, saying, legs)
     }
 }
 
@@ -60,6 +71,5 @@ const Kitty = new Cat('Kitty', 'female');
 const Tiger = new Cat('Tiger', 'male');
 
 const creatures = [Tom, Dick, Harry, Polly, Molly, Mary, Buddy, Betty, Kitty, Tiger];
-const creaturePropsList = creatures.map(props => `${Object.values(props).join(";")}`)
-    .join('\n\n');
+const creaturePropsList = creatures.map(one => one.declareGenus()).join('\n\n');
 print(creaturePropsList);
