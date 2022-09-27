97a9edc41c10d1988438ecb0efce7938b9557ba2
vovan-zt <predator_vovan@ukr.net>
2022-09-27 19:46:34 +0300

    fix comments

--- submissions/vovan-zt/Memory-Pair-Game/script.js
+++ submissions/vovan-zt/Memory-Pair-Game/script.js
@@ -1,132 +1,128 @@
-﻿"use strict";
+﻿'use strict'
 
 const cards = [
-  {name: "asuma", image: "img/13.jpg"},
-  {name: "hinata", image: "img/14.jpg"},
-  {name: "naruto", image: "img/15.jpg"},
-  {name: "gaara", image: "img/16.jpg"},
-  {name: "kakashi", image: "img/17.jpg"},
-  {name: "yamato", image: "img/18.jpg"},
-];
+    { name: 'asuma', image: 'img/13.jpg' },
+    { name: 'hinata', image: 'img/14.jpg' },
+    { name: 'naruto', image: 'img/15.jpg' },
+    { name: 'gaara', image: 'img/16.jpg' },
+    { name: 'kakashi', image: 'img/17.jpg' },
+    { name: 'yamato', image: 'img/18.jpg' },
+]
 
-const container = document.querySelector(".container");
-const wrapper = document.querySelector(".wrapper");
-const cardsList = document.getElementsByClassName("hide-swap");
-let cardsVisible = [];
-let lockWrapper = false;
+const container = document.querySelector('.container')
+const wrapper = document.querySelector('.wrapper')
+const cardsList = document.getElementsByClassName('card')
+let cardsVisible = []
+let isWrapperLocked = false
 
-cards.push(...cards);
+cards.push(...cards)
 
 const shuffleCards = () => {
-  cards.sort(() => {
-    return 0.5 - Math.random();
-  });
-};
+    cards.sort(() => {
+        return 0.5 - Math.random()
+    })
+}
 
-const generateCards = ({name, image}) => {
-  return `
-        <div class ="hide-swap">
-            <div class="c1" data-card='${name}'></div>
-            <img class="c2" src="${image}" alt="${name}">
+const generateCards = ({ name, image }) => {
+    return `
+        <div class ="card">
+            <div class="card__front-side" data-card='${name}'></div>
+            <img class="card__back-side" src="${image}" alt="${name}">
         </div>
-    `;
-};
+    `
+}
 
 const showCards = () => {
-  shuffleCards(cards);
-  wrapper.innerHTML = "";
-  wrapper.innerHTML = cards.reduce(
-    (acc, card) => (acc += generateCards(card)),
-    ""
-  );
-  container.append(wrapper);
-};
+    shuffleCards(cards)
+    wrapper.innerHTML = ''
+    wrapper.innerHTML = cards.reduce(
+        (acc, card) => (acc += generateCards(card)),
+        ''
+    )
+    container.append(wrapper)
+}
 
-showCards(cards);
+showCards(cards)
 
 const messageVictory = () => {
-  const messageBlock = document.createElement("div");
-  messageBlock.classList.add("victory");
-  messageBlock.innerHTML = `
-        <h2 class= "victory__title">Congratulation you won</h2>
+    const messageBlock = document.createElement('div')
+    messageBlock.classList.add('victory')
+    messageBlock.innerHTML = `
+        <h2 class= "victory__title">Сongratulation you won</h2>
         <div class= "victory__desc">press reset to start over</div>
         <button class= "victory__btn">Reset</button>
-    `;
-  container.append(messageBlock);
-  restartGame(messageBlock);
-};
+    `
+    container.append(messageBlock)
+    restartGame(messageBlock)
+}
 
 const restartGame = (messageBlock) => {
-  const resetBtn = document.querySelector(".victory__btn");
-  resetBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    showCards(cards);
-    messageBlock.remove();
-  });
-};
+    const resetBtn = document.querySelector('.victory__btn')
+    resetBtn.addEventListener('click', () => {
+        showCards(cards)
+        messageBlock.remove()
+    })
+}
 
 const cancelCardActive = () => {
-  setTimeout(() => {
-    Array.from(cardsList).forEach((card) => {
-      card.classList.remove("active");
-      lockWrapper = false;
-    });
-  }, 800);
-  lockWrapper = true;
-};
+    setTimeout(() => {
+        Array.from(cardsList).forEach((card) => {
+            card.classList.remove('active')
+        })
+        isWrapperLocked = false
+    }, 800)
+    isWrapperLocked = true
+}
 
 const changeCard = () => {
-  let count = 0;
-  if (cardsVisible[0] == cardsVisible[1]) {
-    cardsVisible = [];
-    Array.from(cardsList).forEach((item) => {
-      if (item.classList.contains("active")) {
-        count++;
-      }
-    });
-    if (count < 2) {
-      cardsVisible = [];
-      cancelCardActive();
-    } else {
-      Array.from(cardsList).forEach((item) => {
-        if (item.classList.contains("active")) {
-          item.classList.add("visible");
+    const activeCards = document.querySelectorAll('.active')
+    if (cardsVisible[0] == cardsVisible[1]) {
+        cardsVisible = []
+        if (activeCards.length < 2) {
+            cardsVisible = []
+            cancelCardActive()
+        } else {
+            activeCards.forEach((item) => {
+                if (item.classList.contains('active')) {
+                    item.classList.add('visible')
+                }
+            })
         }
-      });
+    } else {
+        cardsVisible = []
     }
-  } else {
-    cardsVisible = [];
-  }
-  cancelCardActive();
-};
+    cancelCardActive()
+}
 
 const checkCardsVisibleLength = () => {
-  if (cardsVisible.length === 2) {
-    changeCard();
-  } else {
-    setTimeout(() => (lockWrapper = false), 300);
-  }
-};
+    if (cardsVisible.length === 2) {
+        changeCard()
+    } else {
+        setTimeout(() => (isWrapperLocked = false), 300)
+    }
+}
 
-const showContent = ({target}) => {
-  if (lockWrapper) return;
-
-  if (target.closest(".hide-swap") && !target.classList.contains("c2")) {
-    target.closest(".hide-swap").classList.add("active");
-    cardsVisible.push(target.dataset.card);
-    lockWrapper = true;
-    checkCardsVisibleLength();
-  }
-  checkForWin();
-};
+const showContent = ({ target }) => {
+    if (isWrapperLocked) return
+    if (
+        target.closest('.card') &&
+        !target.classList.contains('card__back-side')
+    ) {
+        target.closest('.card').classList.add('active')
+        cardsVisible.push(target.dataset.card)
+        isWrapperLocked = true
+        checkCardsVisibleLength()
+    }
+    checkForWin()
+}
 
 const checkForWin = () => {
-  const cardsLength = document.querySelectorAll(".visible").length;
-  if (cardsLength == 12) {
-    setTimeout(() => messageVictory(), 1200);
-  }
-};
+    const cardsLength = document.querySelectorAll('.visible').length
+    if (cardsLength == 12) {
+        setTimeout(() => messageVictory(), 1200)
+    }
+}
 
-window.addEventListener("DOMContentLoaded", () => {
-  wrapper.addEventListener("click", showContent);
-});
+window.addEventListener('DOMContentLoaded', () => {
+    wrapper.addEventListener('click', showContent)
+})
