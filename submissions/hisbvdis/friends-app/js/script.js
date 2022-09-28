9d03382d8c5564dd8095c8c2ac84747744fcd5ee
hisbvdis <hisbvdis@gmail.com>
2022-09-28 16:37:50 +0300

    URL updating function

--- submissions/hisbvdis/friends-app/js/script.js
+++ submissions/hisbvdis/friends-app/js/script.js
@@ -17,27 +17,27 @@ filtersFormElem.gender.value = genderValue;
 
 searchElem.addEventListener("input", (evt) => {
   searchValue = evt.target.value;
-  urlSearchParams.search = searchValue;
+  urlSearchParams.set("search", searchValue);
   renderUserList();
 });
 
 sortElem.addEventListener("change", (evt) => {
   sortValue = evt.target.value;
-  urlSearchParams.sort = sortValue;
+  urlSearchParams.set("sort", sortValue);
   renderUserList();
 });
 
 filtersFormElem.addEventListener("change", (evt) => {
   if (evt.target.name !== "gender") return;
   genderValue = evt.target.value;
-  urlSearchParams.gender = genderValue;
+  urlSearchParams.set("gender", genderValue);
   renderUserList();
 })
 
 renderUserList(true);
 
 function renderUserList(isFirstRender = false) {
-  if (isFirstRender) {
+  if (!isFirstRender) {
     updateURL();
   }
   
@@ -109,9 +109,15 @@ function sortArray(arr, method) {
 
 
 function updateURL() {
-  const searchParams = Object.entries(urlSearchParams)
-    .map(([key, value]) => `${key}=${value}`)
-    .join("&");
+  // const searchParams = Object.keys(urlSearchParams)
+  //   .map(([key, value]) => `${key}=${value}`)
+  //   .join("&");
 
-  history.replaceState(null, null, `?${searchParams}`);
+  let urlString = "?";
+  for (let [key, value] of urlSearchParams.entries()) {
+    urlString += `&${key}=${value}`;
+  }
+  
+
+  history.replaceState(null, null, urlString);
 }
