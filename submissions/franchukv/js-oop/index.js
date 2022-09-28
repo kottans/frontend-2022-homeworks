0076e3868cd93691759ae129339efd5983519125
Volodymyr Franchuk <franchuk.vldr@gmail.com>
2022-09-28 13:24:34 +0300

    rewrote app

--- submissions/franchukv/js-oop/index.js
+++ submissions/franchukv/js-oop/index.js
@@ -1,8 +1,7 @@
 /* 
    Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
-
-   Code repository: https://github.com/franchukv/a-tiny-JS-world/
+   Code repository: https://github.com/franchukv/a-tiny-JS-world
    Web app: https://franchukv.github.io/a-tiny-JS-world/
 */
 
@@ -14,46 +13,41 @@ class Inhabitants {
       this.name = name;
       this.status = status;
       this.saying = saying;
-      this.friends = friends;
-      this.properties = ['species', 'gender', 'name', 'status'];
+      this.friends = friends ? friends : `no friends`;
+      this.properties = ['species', 'gender', 'name', 'status', 'saying', 'friends',]
    }
 
-   objToString() {
+   inhabinantPropetriesToString() {
       return this.properties.map((property) => this[property]).join('; ');
    }
-
-   toMakeFriends() {
-      return this.properties.push('friends');
-   }
-
 }
 
 class Human extends Inhabitants {
-   constructor(species, gender, name, status, saying, friends) {
-      super(species, gender, name, status, saying, friends);
+   constructor(gender, name, status, saying, friends) {
+      super('human', gender, name, status, saying, friends);
+      this.legs = 2;
       this.hands = 2;
-      this.feets = 2;
-      this.properties = [...this.properties, 'hands', 'feets', 'saying'];
+      this.properties = [...this.properties, 'hands', 'legs'];
+   }
+}
+
+class Man extends Human {
+   constructor(name, status, saying, friends) {
+      super('male', name, status, saying, friends);
+   }
+}
+
+class Woman extends Human {
+   constructor(name, status, saying, friends) {
+      super('female', name, status, saying, friends);
    }
 }
 
 class Animal extends Inhabitants {
    constructor(species, gender, name, status, saying, friends) {
       super(species, gender, name, status, saying, friends);
-      this.feets = 4;
-      this.properties = [...this.properties, 'feets', 'saying'];
-   }
-}
-
-class Man extends Human {
-   constructor(name, status, saying, friends) {
-      super('human', 'male', name, status, saying, friends);
-   }
-}
-
-class Woman extends Human {
-   constructor(name, status, saying, friends) {
-      super('human', 'female', name, status, saying, friends);
+      this.paws = 4;
+      this.properties = [...this.properties, 'paws'];
    }
 }
 
@@ -69,19 +63,19 @@ class Cat extends Animal {
    }
 }
 
-const man = new Man('Drogo', 'Khal', 'Sheikh Ma Shieraki Anni.');
-const woman = new Woman('Daenerys', 'Daenerys of House Targaryen, First with Her Name, Breaker of Chains and Mother of Dragons', 'Drakaris!', `Fell in love with ${man.name}.`);
-const dog = new Dog('male', 'Sharik', "Yard's terrier", 'woof-woof!', `Only ${man.name} is friends.`);
-const cat = new Cat('female', 'Bastet', "Pharaoh's cat", 'Meow, bow to me!', 'No friends, only servants!');
-const catwoman = new Woman('Selina Kyle', 'Superhero', cat.saying, `In astral connection with ${cat.name}.`);
+const man = new Man('Drogo', 'Khal', 'Sheikh Ma Shieraki Anni');
+const woman = new Woman('Daenerys', 'Daenerys of House Targaryen, First with Her Name, Breaker of Chains and Mother of Dragons', 'Drakaris', `Fell in love with ${man.name}`);
+const dog = new Dog('male', 'Sharik', "Yard's terrier", 'woof-woof', `Only ${man.name} is friends`);
+const cat = new Cat('female', 'Bastet', "Pharaoh's cat", 'Meow, bow to me', 'No friends, only servants');
+const catwoman = new Woman('Selina Kyle', 'Superhero', cat.saying, `In astral connection with ${cat.name}`);
 
 // ======== OUTPUT ========
 const allInhabitants = [
-   man.objToString(),
-   woman.objToString(woman.toMakeFriends()),
-   dog.objToString(dog.toMakeFriends()),
-   cat.objToString(cat.toMakeFriends()),
-   catwoman.objToString(catwoman.toMakeFriends()),
+   man.inhabinantPropetriesToString(),
+   woman.inhabinantPropetriesToString(),
+   dog.inhabinantPropetriesToString(),
+   cat.inhabinantPropetriesToString(),
+   catwoman.inhabinantPropetriesToString(),
 ];
 
 allInhabitants.forEach((inhabitant) => print(inhabitant));
