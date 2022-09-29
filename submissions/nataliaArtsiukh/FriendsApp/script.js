4466767c6faaf15a979aa9f5c2c24089d8f80527
Natalia Artsiukh <nataliaprocessmail@gmail.com>
2022-09-29 10:47:38 +0300

    Code refactoring and interface improvements
    Added pointer cursor to form buttons. Corrected english words. Added a fixed height to the footer so that when changing the quote and automatically resizing the footer, it does not cover the lower cards. The functions of creating and rendering cards are combined into one. Elements are now created using insertAdjacentHTML. I connected filters and sorts so that they can work simultaneously (added a function to apply filters and call it every time I change the settings). Changed the order of functions. Now I define settings using the value property, and not through classes.

--- submissions/nataliaArtsiukh/FriendsApp/script.js
+++ submissions/nataliaArtsiukh/FriendsApp/script.js
@@ -2,6 +2,9 @@ const board = document.querySelector('.main-section');
 const genderFilter = document.querySelector('.gender-filter');
 const sortFriends = document.querySelector('.sort');
 const nameFilter = document.querySelector('.form-input');
+let nameFilterValue = '';
+let genderFilterValue = '';
+let sortValue = '';
 const friends = [];
 const quotations = [
     'A dear friend from afar flies in for a minute - and you don\'t have a cake!', 
@@ -12,107 +15,90 @@ const quotations = [
     'At what idiot fair did you find your friend?'
 ]; 
 
-const createCard = function({name: {first, last}, email, dob: {age}, phone, picture: {large}}) {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    board.append(card); 
-    const photo = document.createElement('img');
-    photo.setAttribute('src', large);
-    photo.setAttribute('alt', `Photo of ${first} ${last}`);
-    photo.classList.add('card-img');
-    card.append(photo);
-    const name = document.createElement('h2');
-    name.classList.add('name');
-    name.textContent = `${first} ${last}`;
-    card.append(name);
-    const personAge = document.createElement('p');
-    personAge.classList.add('age');
-    personAge.textContent = `${age} years.`;
-    card.append(personAge);
-    const phoneNumber = document.createElement('p');
-    phoneNumber.classList.add('contacts');
-    phoneNumber.textContent = phone;
-    card.append(phoneNumber);
-    const emailAddress = document.createElement('p');
-    emailAddress.classList.add('contacts');
-    emailAddress.textContent = email;
-    card.append(emailAddress);
-}
-
 const getFriends = async function() {
-    const response = await fetch('https://randomuser.me/api/?results=100&inc=gender,name,email,picture,dob,phone&noinfo');
-    const {results:people} = await response.json();
-    friends.push(...people); 
-    drawCards(friends);
+    try {
+        const response = await fetch('https://randomuser.me/api/?results=100&inc=gender,name,email,picture,dob,phone&noinfo');
+        const {results:people} = await response.json();
+        friends.push(...people); 
+        drawCards(friends);
+    } catch (error) {
+        alert('Try again later.');
+    } 
 }
 
 const drawCards = function(people) {
     board.innerHTML = '';
-    people.forEach(friend => createCard(friend));
+    const cards = people.map(({name: {first, last}, email, dob: {age}, phone, picture: {large}}) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        card.insertAdjacentHTML('beforeend', `
+            <img src="${large}" alt="Photo of ${first} ${last}" class="card-img">
+            <h2 class="name">${first} ${last}</h2>
+            <p class="age">${age} years.</p>
+            <p class="contacts">${phone}</p>
+            <p class="contacts">${email}</p>
+        `);
+        return card;
+    });
+    board.append(...cards);
+}
+
+const filterByName = function(friends, inputText) {
+    if (inputText) {
+        return friends.filter(({name:{first, last}}) => 
+            first.toLowerCase().startsWith(inputText) || last.toLowerCase().startsWith(inputText) || `${first} ${last}`.toLowerCase().startsWith(inputText)
+        )
+    }
+    return friends;
 }
 
 const filterByGender = function(friends, sex) {
-    return friends.filter(({gender}) => gender == sex);
+    if (sex) {
+        return friends.filter(({gender}) => gender == sex);
+    } 
+    return friends;
 }
 
-genderFilter.addEventListener('click', function(e) {
-    let filteredFriends = friends;
-    const genderLabel = e.target.closest('.form-label');
-    if (genderLabel) {
-        if (genderLabel.classList.contains('male')) {
-            filteredFriends = filterByGender(friends, 'male');
-        } else if (genderLabel.classList.contains('female')) {
-            filteredFriends = filterByGender(friends, 'female')
-        } 
-        drawCards(filteredFriends);
-    }
-})
-
-const sortByAge = function(friends, ageSortDirection) {
-    if (ageSortDirection == 'from-yang') {
-        return friends.sort((firstFriend, secondFriend) => firstFriend.dob.age - secondFriend.dob.age)
-    } else if (ageSortDirection == 'from-old') {
-        return friends.sort((firstFriend, secondFriend) => secondFriend.dob.age - firstFriend.dob.age)
-    }
-}
-
-const sortByAlphabet = function(friends, alphabetSortDirection) {
-    if (alphabetSortDirection == 'foreward') {
+const sort = function(friends, sortDirection) {
+    if (sortDirection == 'ascending') {
         return friends.sort(({name: {first: firsName, last: firstSurname}}, {name: {first: secondName, last: secondSurname}}) => 
             `${firsName} ${firstSurname}`.localeCompare(`${secondName} ${secondSurname}`))
-    } else if (alphabetSortDirection == 'backward') {
+    } else if (sortDirection == 'descending') {
         return friends.sort(({name: {first: firsName, last: firstSurname}}, {name: {first: secondName, last: secondSurname}}) => 
         `${secondName} ${secondSurname}`.localeCompare(`${firsName} ${firstSurname}`))
+    } else if (sortDirection == 'from-young') {
+        return friends.sort((firstFriend, secondFriend) => firstFriend.dob.age - secondFriend.dob.age)
+    } else if (sortDirection == 'from-old') {
+        return friends.sort((firstFriend, secondFriend) => secondFriend.dob.age - firstFriend.dob.age)
+    } else {
+        return friends;
     }
 }
 
-sortFriends.addEventListener('click', function(e) {
-    let sortedFriends = friends;
-    const sortLabel = e.target.closest('.form-label');
-    if (sortLabel) {
-        if (sortLabel.classList.contains('from-yang')) {
-            sortedFriends = sortByAge(friends, 'from-yang');
-        } else if (sortLabel.classList.contains('from-old')) {
-            sortedFriends = sortByAge(friends, 'from-old');
-        } else if (sortLabel.classList.contains('foreward')) {
-            sortedFriends = sortByAlphabet(friends, 'foreward');
-        } else if (sortLabel.classList.contains('backward')) {
-            sortedFriends = sortByAlphabet(friends, 'backward');
-        }
-        drawCards(sortedFriends)
-    }
-})
-
-const filterByName = function(friends, inputText) {
-    return friends.filter(({name:{first, last}}) => 
-        first.toLowerCase().startsWith(inputText) || last.toLowerCase().startsWith(inputText) || `${first} ${last}`.toLowerCase().startsWith(inputText)
-    )
+const applyFilters = function() {
+    let filteredFriends = friends;
+    filteredFriends = filterByName(filteredFriends, nameFilterValue);
+    filteredFriends = filterByGender(filteredFriends, genderFilterValue);
+    filteredFriends = sort(filteredFriends, sortValue);
+    return filteredFriends;
 }
 
 nameFilter.addEventListener('input', function(e) {
-    let filteredFriends = friends;
-    const inputText = e.target.value;
-    filteredFriends = filterByName(friends, inputText.toLowerCase());
+    nameFilterValue = e.target.value.toLowerCase();
+    let filteredFriends = applyFilters();
+    drawCards(filteredFriends);
+})
+
+genderFilter.addEventListener('input', function(e) {
+    genderFilterValue = e.target.value;
+    let filteredFriends = applyFilters();
+    drawCards(filteredFriends);
+})
+
+sortFriends.addEventListener('input', function(e) {
+    sortValue = e.target.value;
+    let filteredFriends = applyFilters();
     drawCards(filteredFriends);
 })
 
