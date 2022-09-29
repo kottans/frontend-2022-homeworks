ba655a57b154bce0a6702b70db10aec2091d3bcc
vovan-zt <predator_vovan@ukr.net>
2022-09-29 17:02:21 +0300

    update code

--- submissions/vovan-zt/TiniWorld2/index.js
+++ submissions/vovan-zt/TiniWorld2/index.js
@@ -1,58 +1,84 @@
 class WorldInhabitant {
-  constructor(species, name, gender, saying) {
-    this.species = species;
-    this.name = name;
-    this.gender = gender;
-    this.saying = saying;
-    this.prop = [species, name, gender, saying];
-  }
+	constructor(species, name, gender, saying) {
+		this.species = species;
+		this.name = name;
+		this.gender = gender;
+		this.saying = saying;
+	}
 
-  getMeaning() {
-    return this.prop.map((meaning) => meaning).join("; ");
-  }
+	getMeaning() {
+		return ["species", "name", "gender", "saying"]
+			.map((property) => {
+				if (property === "species") {
+					return this[property].toUpperCase();
+				}
+				return this[property];
+			})
+			.join("; ");
+	}
 }
 class Human extends WorldInhabitant {
-  constructor(name, gender, saying, friends = null) {
-    super("human", name, gender, saying);
-    this.legs = 2;
-    this.hands = 2;
-    this.friends = friends;
-    this.prop = [...this.prop, this.legs, this.hands, this.friends];
-  }
+	constructor(name, gender, saying, friends) {
+		super("human", name, gender, saying);
+		this.legs = 2;
+		this.hands = 2;
+		this.friends = friends;
+	}
+
+	getMeaning() {
+		return `${super.getMeaning()};I have ${this.legs} legs and ${
+			this.hands
+		} hands; ${this.friends ? this.friends.name : "no friends"}`;
+	}
+}
+class Man extends Human {
+	constructor(name, saying, friends) {
+		super(name, "man", saying, friends);
+	}
+}
+class Woman extends Human {
+	constructor(name, saying, friends) {
+		super(name, "woman", saying, friends);
+	}
+}
+class CatWoman extends Woman {
+	constructor(name) {
+		super(name);
+		this.species = "CatWoman";
+		this.saying = cat.saying;
+	}
 }
 class Animal extends WorldInhabitant {
-  constructor(species, name, gender, saying) {
-    super(species, name, gender, saying);
-    this.paws = 4;
-    this.prop = [...this.prop, this.paws];
-  }
+	constructor(species, name, gender, saying) {
+		super(species, name, gender, saying);
+		this.paws = 4;
+	}
+
+	getMeaning() {
+		return `${super.getMeaning()}; animal has ${this.paws} paws`;
+	}
 }
 class Dog extends Animal {
-  constructor(name, gender) {
-    super("dog", name, gender, "woof!");
-  }
+	constructor(name, gender) {
+		super("dog", name, gender, "woof!");
+	}
 }
 class Cat extends Animal {
-  constructor(name, gender) {
-    super("cat", name, gender, "meow!");
-  }
-}
-class CatWoman extends Human {
-  constructor(name, saying) {
-    super(name, "woman", saying);
-  }
+	constructor(name, gender) {
+		super("cat", name, gender, "meow!");
+	}
 }
 
-const man = new Human("Jon", "male", "I want to sleep!");
+const woman = new Woman("Lena", "Hello!");
 
-const woman = new Human("Lena", "female", "Hello!", man.name);
+const man = new Man("Jon", "I want to sleep!", woman);
 
 const cat = new Cat("Luna", "female");
 
 const dog = new Dog("Rasti", "male");
 
-const catWoman = new CatWoman("Selina", cat.saying);
+const catWoman = new CatWoman("Selina");
 
-[man, woman, cat, dog, catWoman].forEach((inhabitant) =>
-  print(inhabitant.getMeaning())
+[man, woman, catWoman, cat, dog].forEach((inhabitant) =>
+	print(inhabitant.getMeaning())
 );
