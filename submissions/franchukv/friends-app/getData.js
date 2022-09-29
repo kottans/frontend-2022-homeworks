67d98be87eec519770fd6e3f2572c3fbe367aa12
Volodymyr Franchuk <franchuk.vldr@gmail.com>
2022-09-29 12:14:20 +0300

    now filter have '_filter-lock' class when don't have users

--- submissions/franchukv/friends-app/getData.js
+++ submissions/franchukv/friends-app/getData.js
@@ -4,6 +4,7 @@ import loadingPlaceholder from './placeholder.js';
 
 async function getUsersData() {
     loadingPlaceholder();
+    document.querySelector('.sidebar').classList.add('_filter-lock');
 
     try {
         const response = await fetch(`https://randomuser.me/api/?results=${limitOfUsers}&seed=1&nat=ua`);
@@ -20,6 +21,8 @@ async function getUsersData() {
             <div>`;
 
         users.innerHTML = endWarning;
+    } finally {
+        document.querySelector('.sidebar').classList.remove('_filter-lock');
     }
 }
 
