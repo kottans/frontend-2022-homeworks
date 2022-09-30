64515ddb74cdb6426ef773e66e1e02a134c24106
Yuliia <yuliiadd@gmail.com>
2022-09-30 15:32:23 +0300

    fixed error with no-result animation

--- submissions/YuliiaDyka/friends-app/main.js
+++ submissions/YuliiaDyka/friends-app/main.js
@@ -201,13 +201,16 @@ function searchName() {
         filtredUsers = filterBySex(filterByAge(allUsers));
         filtredUsers = filtredUsers.filter(user => filterByName(user, filters.name));
         if (filtredUsers.length) {
+            console.log(filtredUsers.length);
             header.classList.remove('no-results');
             renderCards(filtredUsers);
         } else {
+            console.log(filtredUsers.length);
             cardsSection.innerHTML = "";
             header.classList.add('no-results');
         }
     } else {
+        header.classList.remove('no-results');
         filtredUsers = filterBySex(filterByAge(allUsers));
         renderCards(filtredUsers);
     }
