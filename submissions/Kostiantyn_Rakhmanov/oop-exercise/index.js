23de64b64e4bdd39062b2f1764cdab0203fb7dba
Kostiantyn Rakhmanov <rahmanoff@gmail.com>
2022-09-30 13:18:30 +0300

    Deleted: comments

--- submissions/Kostiantyn_Rakhmanov/oop-exercise/index.js
+++ submissions/Kostiantyn_Rakhmanov/oop-exercise/index.js
@@ -1,8 +1,5 @@
 import { print } from './js/lib.js';
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-
 class Inhabitant {
    constructor(species, name, gender, saying) {
       this.species = species;
@@ -72,25 +69,6 @@ const toby = new Dog ('Toby', 'male', 'woof-woof');
 const alice = new Cat ('Alice', 'female', 'meow');
 const mary = new Woman ('Mary', 'Hi, John!');
 const john = new Man ('John', 'Hi, Mary!');
-
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
 
 const inhabitants = [toby, alice, mary, john];
 
