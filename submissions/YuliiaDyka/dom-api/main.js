392dd754e691617a8bee796f4b97c9de7930b8cb
Yuliia Dyka <98177848+Yuliiadd@users.noreply.github.com>
2022-09-30 21:42:00 +0300

    added DOM-API homework (#360)
    * added DOM-API homework
    
    * added empty strings in js files
    
    * refactor: added mobile version
    
    * refactor: fixed page background
    
    * refactor: update calcProgress with alert
    
    * modifided dataObj and moved css rules to style.css
    
    * modified dataObj and render function
    
    * fixed coments for re-review

---
+++ submissions/YuliiaDyka/dom-api/main.js
@@ -0,0 +1,114 @@
+'use strict'
+
+import dataObj from "./base.js";
+
+const title = document.querySelector('.title__chapter');
+const nav_items = document.querySelectorAll('.nav__item');
+const nav = document.querySelector('.nav');
+const btn = document.querySelector('.btn');
+const buttonsDiv = document.querySelector('.buttons');
+const btn__home = document.querySelector('.home__btn');
+const checkboxes = document.querySelectorAll('.checkbox');
+const progressDig = document.querySelector('.progress__dig');
+const progressLine = document.querySelector('.progress');
+const totalProgress = 3;
+const defaultTitle = 'Welcome to Kottans!';
+const defaultId = 15;
+
+let currentTitle = title.textContent;
+let progressMax = checkboxes.length;
+ 
+// event listeners
+
+nav.addEventListener("click", getCurrrentContent);
+btn__home.addEventListener('click', goToHomePage);
+
+// functions declaration
+
+function getCurrrentContent(e) {
+    nav_items.forEach(item => { item.classList.remove('selected')}); 
+    let target = e.target;
+    let id;
+    switch (target.tagName) {
+        case "LI": 
+        target.classList.add('selected');
+        id = target.firstChild.id;
+        currentTitle = e.target.innerText;
+        changePage(currentTitle, id);
+        break; 
+
+        case "A":
+        target.parentElement.classList.add('selected'); 
+        id = target.id;
+        currentTitle = e.target.innerText;
+        changePage(currentTitle, id);
+        break; 
+
+        case "INPUT":
+        getCurrentProgress();
+        break; 
+    };
+};
+
+function getCurrentProgress() {
+    let currentProgress = 0;  
+    checkboxes.forEach(item => { if (item.checked) { currentProgress++;}});
+    progressDig.textContent = Math.floor((currentProgress / progressMax * 100)) + "%";
+    progressLine.style.width = Math.floor((currentProgress / progressMax * 100)) + "%";  
+    if (currentProgress == totalProgress) {
+        alert("Вітаннячка, котан! Го на Стейдж 1)))")
+    };
+};
+
+function changePage(currentTitle, id) {
+const title = document.querySelector('.title__chapter');
+const description = document.querySelector('.description');
+
+const newTitle = document.createElement("h4");
+newTitle.classList.add('title__chapter');
+newTitle.textContent = currentTitle;
+title.replaceWith(newTitle);
+
+const newDesc = renderPage(id);
+description.replaceWith(newDesc);
+
+btn.classList.remove('btn');
+btn.classList.add('hidden');
+buttonsDiv.classList.remove('buttons');
+buttonsDiv.classList.add('buttons__grid');
+};
+
+function renderPage(id) {
+    const desc = document.createElement('div');
+    desc.classList.add('description');
+    const taskList = document.createElement("ol");
+    taskList.classList.add('task');
+
+    const [{ taskExesice }]= dataObj.filter(task => task.id == id);
+    const taskExesiceList = taskExesice.map(task => {
+        const { taskText, url, urlText } = task;
+        return`
+                <li class="task__item">
+                    ${taskText ? taskText : ""}
+                    ${url ? `<a class="link tomato" href="${url}" target="_blank">${urlText}</a>` : ""}
+                </li>
+            `;
+    }).join('');    
+
+    taskList.innerHTML = taskExesiceList;
+    desc.append(taskList);
+    return desc;
+}
+
+function goToHomePage() {
+    nav_items.forEach(item => {
+        item.classList.remove('selected');
+    }); 
+    changePage(defaultTitle, defaultId);
+
+    document.querySelector('.task__item').classList.add('default');
+    btn.classList.remove('hidden');
+    btn.classList.add('btn');
+    buttonsDiv.classList.remove('buttons__grid');
+    buttonsDiv.classList.add('buttons');
+};
