3fc23ce0442f717c8683b5b7f0532a1877daf2f2
unabyband <110461462+unabyband@users.noreply.github.com>
2022-10-01 16:07:35 +0300

    Applied prettier to script file

--- submissions/unabyband/memory-pair-game/script.js
+++ submissions/unabyband/memory-pair-game/script.js
@@ -1,4 +1,4 @@
-const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll(".memory-card");
 
 let hasFlippedCard = false;
 let lockBoard = false;
@@ -8,69 +8,70 @@ let matchedPairs = 0;
 shuffleCards();
 
 function flipCard() {
-    if (lockBoard) return;
-    if (this === firstCard) return;
-    this.classList.add('flip');
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.add("flip");
 
-    if (!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
-    } else {
-        hasFlippedCard = false;
-        secondCard = this;
-        checkForMatch();
-    }
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
+    checkForMatch();
+  }
 }
 
-function checkForMatch () {
-    let isMatch = firstCard.id === secondCard.id;
-    isMatch ? disableCards() : unflipCards();
+function checkForMatch() {
+  let isMatch = firstCard.id === secondCard.id;
+  isMatch ? disableCards() : unflipCards();
 }
 
-function disableCards () {
-    setTimeout(() => {
-    firstCard.classList.add('hidden');
-    secondCard.classList.add('hidden');   
-    },500);
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard); 
-    matchedPairs += 1;
-    checkMatchedPairs();
+function disableCards() {
+  setTimeout(() => {
+    firstCard.classList.add("hidden");
+    secondCard.classList.add("hidden");
+  }, 500);
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  matchedPairs += 1;
+  checkMatchedPairs();
 }
 
-function unflipCards () {
-    lockBoard = true;
-    setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
     lockBoard = false;
-    }, 1000); 
+  }, 1000);
 }
 
-function shuffleCards () {
-    cards.forEach(card => {
-        let randomPos = Math.floor(Math.random() * 16);
-        card.style.order = randomPos;
-    });
+function shuffleCards() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 16);
+    card.style.order = randomPos;
+  });
 }
 
-function printMatchedPairs () {
-    document.querySelector('.wins-counter').innerHTML = `You have ${matchedPairs} matched pairs.`;
+function printMatchedPairs() {
+  document.querySelector(
+    ".wins-counter"
+  ).innerHTML = `You have ${matchedPairs} matched pairs.`;
 }
 
-function checkMatchedPairs () {
-    if (matchedPairs === 8) {
-        setTimeout(() => {
-        alert('YOU WON, CONGRATULATIONS!');
-        window.location.reload();
-        return;    
-        },750);
-      
-    } else {
-        setTimeout(() => {
-        printMatchedPairs(); 
-        },750);
-    }
+function checkMatchedPairs() {
+  if (matchedPairs === 8) {
+    setTimeout(() => {
+      alert("YOU WON, CONGRATULATIONS!");
+      window.location.reload();
+      return;
+    }, 750);
+  } else {
+    setTimeout(() => {
+      printMatchedPairs();
+    }, 750);
+  }
 }
-cards.forEach(card => card.addEventListener('click', flipCard));
+cards.forEach((card) => card.addEventListener("click", flipCard));
 
