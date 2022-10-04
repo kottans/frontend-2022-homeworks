a1ce4cc27913eb005758895f17b6817d86b05d37
Kostiantyn Rakhmanov <rahmanoff@gmail.com>
2022-10-04 21:14:15 +0300

    Edited: collision and keys Input

--- submissions/Kostiantyn_Rakhmanov/frogger/app.js
+++ submissions/Kostiantyn_Rakhmanov/frogger/app.js
@@ -1,8 +1,7 @@
-// Enemies our player must avoid
 const CANVAS = {
     start: 0,
-    rows: 5,
-    columns: 4,
+    rows: 6,
+    columns: 5,
     section: {
         width: 100,
         height: 80,
@@ -14,7 +13,7 @@ const CANVAS_HEIGHT = CANVAS.section.height * CANVAS.rows;
 
 const PLAYER = {
     startCoordX: 200,
-    startCoordY: 375,
+    startCoordY: 400,
     startLocationShift: 60,
     startDelay: 777,
 };
@@ -36,97 +35,83 @@ const ENEMY = {
 };
 
 let Enemy = function (x, y, speed, player) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
     this.x = x;
     this.y = y;
-    this.speed = speed;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.speed = speed;
     this.player = player;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-
 Enemy.prototype.update = function (dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x += this.speed * dt;
-
     if (this.x > ENEMY.coordX.max) {
         this.x = ENEMY.coordX.min;
         this.speed = ENEMY.speed.min + Math.floor(Math.random() * ENEMY.speed.max);
     }
-    if (player.x < this.x + CANVAS.section.height &&
-        player.x + CANVAS.section.height > this.x &&
+    if (player.x < this.x + CANVAS.section.width &&
+        player.x + CANVAS.section.width > this.x &&
         player.y < this.y + PLAYER.startLocationShift &&
         PLAYER.startLocationShift + player.y > this.y) {
-        player.x = PLAYER.startCoordX.x;
-        player.y = PLAYER.startCoordY.y;
+        player.x = PLAYER.startCoordX;
+        player.y = PLAYER.startCoordY;
     }
+
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 let Player = function (x, y) {
-    this.sprite = 'images/char-cat-girl.png';
     this.x = x;
     this.y = y;
+    this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.update = function () {
-
 };
 
 Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function (keyPress) {
-    if (keyPress == 'left' && this.x > CANVAS.columns) {
-        this.x -= CANVAS.section.width;
-    };
-    if (keyPress == 'right' && this.x < CANVAS_WIDTH) {
-        this.x += CANVAS.section.width;
-    };
-    if (keyPress == 'up' && this.y > CANVAS.start) {
-        this.y -= CANVAS.section.height;
-    };
-    if (keyPress == 'down' && this.y < CANVAS_WIDTH - PLAYER.startLocationShift) {
-        this.y += CANVAS.section.height;
-    };
-    if (this.y < CANVAS.start) {
-        setTimeout(() => {
-            player.x = PLAYER.startCoordX;
-            player.y = PLAYER.startCoordY;
-        }, PLAYER.startDelay);
+Player.prototype.handleInput = function (key) {
+    switch (key) {
+        case 'up':
+            this.y -= CANVAS.section.height;
+            if (this.y < CANVAS.start) {
+                this.y = PLAYER.startCoordY;
+            }
+            break;
+        case 'down':
+            this.y += CANVAS.section.height;
+            if (this.y >= CANVAS_HEIGHT) {
+                this.y = CANVAS_HEIGHT - CANVAS.section.height;
+            }
+            break;
+        case 'left':
+            this.x -= CANVAS.section.width;
+            if (this.x < CANVAS.section.width) {
+                this.x = CANVAS.start;
+            }
+            break;
+        case 'right':
+            this.x += CANVAS.section.width;
+            if (this.x >= CANVAS_WIDTH) {
+                this.x = CANVAS_WIDTH - CANVAS.section.width;
+            }
+            break;
     };
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-let allEnemies = [];
+let player = new Player(PLAYER.startCoordX, PLAYER.startCoordY);
 
+const allEnemies = [];
 ENEMY.locationsY.forEach(function (locationY) {
-    let enemy = new Enemy(CANVAS.start, locationY, ENEMY.speed.max, PLAYER);
+    let enemy = new Enemy(CANVAS.start, locationY, ENEMY.speed.min, player);
     allEnemies.push(enemy);
 });
 
-// Place the player object in a variable called player
-
-let player = new Player(PLAYER.startCoordX, PLAYER.startCoordY);
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
@@ -134,5 +119,6 @@ document.addEventListener('keyup', function (e) {
         39: 'right',
         40: 'down'
     };
+
     player.handleInput(allowedKeys[e.keyCode]);
 });
