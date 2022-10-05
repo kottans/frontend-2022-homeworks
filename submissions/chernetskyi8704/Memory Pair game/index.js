1a25f43f77e2303472ec5667ea7b302b14d4ba8f
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2022-10-05 15:23:00 +0300

    Have made refactored my code.

--- submissions/chernetskyi8704/Memory Pair game/index.js
+++ submissions/chernetskyi8704/Memory Pair game/index.js
@@ -1,8 +1,8 @@
 "use strict";
 const mainListItems = document.querySelector(".main__list-items");
 const startButton = document.querySelector(".start__game-button");
-const startGameBlock = document.querySelector(".start__game");
-const restartGameBlock = document.querySelector(".restart__game");
+const blockOfTheStartGameButton = document.querySelector(".start__game");
+const blockOfTheRestartGameButton = document.querySelector(".restart__game");
 const restartButton = document.querySelector(".restart__game-button");
 let opened = [];
 let flippedCards = 0;
@@ -21,11 +21,11 @@ const addClassForItem = (item, className) => item.classList.add(className);
 const removeClassForItem = (item, className) =>
   item.classList.remove(className);
 
-const addClassForEachItem = (arr, className) =>
-  arr.forEach((item) => item.classList.add(className));
+const addClassForEachItem = (array, className) =>
+  array.forEach((item) => item.classList.add(className));
 
-const removeClassForEachItem = (arr, className) =>
-  arr.forEach((item) => item.classList.remove(className));
+const removeClassForEachItem = (array, className) =>
+  array.forEach((item) => item.classList.remove(className));
 
 const shuffleAray = (array) => {
   for (
@@ -41,7 +41,7 @@ const shuffleAray = (array) => {
 const allGameCards = [...allImages, ...allImages];
 shuffleAray(allGameCards);
 
-const displayTheCards = () => {
+const displayAllCards = () => {
   allGameCards.map((card) => {
     const html = `<li class="list__item ">
     <div class="front">
@@ -54,7 +54,7 @@ const displayTheCards = () => {
     mainListItems.insertAdjacentHTML("beforeend", html);
   });
 };
-displayTheCards(allGameCards);
+displayAllCards(allGameCards);
 
 const allCards = document.querySelectorAll(".list__item");
 addClassForEachItem(allCards, "_hide");
@@ -70,7 +70,7 @@ const compareTwoCards = function () {
     let ifOpenedCardsAreSame =
       opened[0].lastElementChild.firstElementChild.src ===
       opened[1].lastElementChild.firstElementChild.src;
-    ifOpenedCardsAreSame ? sameCard() : diffCard();
+    ifOpenedCardsAreSame ? addMoreOpacity() : turnOverTheOpenCards();
   }
 };
 
@@ -79,15 +79,15 @@ const ifAllCardsAreFlipped = () => {
     const allFlippedCards = document.querySelectorAll(".list__item");
     opened = [];
     setTimeout(function () {
-      addClassForItem(restartGameBlock, "animated");
-      removeClassForItem(restartGameBlock, "_hide");
+      addClassForItem(blockOfTheRestartGameButton, "animated");
+      removeClassForItem(blockOfTheRestartGameButton, "_hide");
       removeClassForItem(mainListItems, "animated");
       allFlippedCards.forEach((card) => card.classList.add("_hide"));
     }, 2000);
   }
 };
 
-const beforeGameStart = () => {
+const playTheAnimationBeforeStartingTheGame = () => {
   const allCards = document.querySelectorAll(".list__item");
   addClassForEachItem(allCards, "_flip");
 
@@ -96,14 +96,14 @@ const beforeGameStart = () => {
   }, 2000);
 };
 
-const sameCard = () => {
+const addMoreOpacity = () => {
   opened.forEach((card) => card.lastElementChild.classList.add("_moreOpacity"));
   flippedCards += 2;
   opened = [];
   ifAllCardsAreFlipped();
 };
 
-const diffCard = () =>
+const turnOverTheOpenCards = () =>
   setTimeout(() => {
     removeClassForEachItem(opened, "_flip");
     opened = [];
@@ -112,16 +112,16 @@ const diffCard = () =>
 startButton.addEventListener("click", () => {
   addClassForItem(mainListItems, "animated");
   removeClassForEachItem(allCards, "_hide");
-  addClassForItem(startGameBlock, "_hide");
-  beforeGameStart();
+  addClassForItem(blockOfTheStartGameButton, "_hide");
+  playTheAnimationBeforeStartingTheGame();
 });
 
 restartButton.addEventListener("click", () => {
-  removeClassForItem(restartGameBlock, "animated");
-  addClassForItem(restartGameBlock, "_hide");
+  removeClassForItem(blockOfTheRestartGameButton, "animated");
+  addClassForItem(blockOfTheRestartGameButton, "_hide");
   addClassForItem(mainListItems, "animated");
-  displayTheCards(allGameCards);
-  beforeGameStart();
+  displayAllCards(allGameCards);
+  playTheAnimationBeforeStartingTheGame();
   flippedCards = 0;
 });
 
