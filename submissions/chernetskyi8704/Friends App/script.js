50b7f1e2842206ad9c94d7a1140d5944d17e7a62
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2022-10-05 12:17:09 +0300

    The design has been changed. Fixed bugs, validator errors and added media queries.

--- submissions/chernetskyi8704/Friends App/script.js
+++ submissions/chernetskyi8704/Friends App/script.js
@@ -1,14 +1,10 @@
-const usersField = document.querySelector(".users");
-
+const filterBody = document.querySelector(".main__filters");
+const burgerImage = document.querySelector(".burger_img");
+const listOfUsers = document.querySelector(".users");
 const allFilters = document.querySelector(".main__filters");
-const resetBtn = document.querySelector(".resetBtn");
-
-const genderChoosenAll = document.querySelector('input[value="all"]');
-const genderChoosenMale = document.querySelector('input[value="male"]');
-const genderChoosenFemale = document.querySelector('input[value="female"]');
-
+const filterButtons = document.querySelector(".filter__buttons");
+const closeBurgerButton = document.querySelector(".close");
 const searchInput = document.querySelector('input[name="search"]');
-
 const usersURL =
   "https://randomuser.me/api/?results=16&nat=ua&inc=gender,name,location,picture,dob&seed=1 noinfo";
 
@@ -20,7 +16,6 @@ const getUsersData = async (url) => {
     const response = await fetch(url);
     const data = await response.json();
     allUsers = [...data.results];
-    copyOfAllUsers = [...allUsers];
     displayAllUsers(allUsers);
   } catch (err) {
     console.log("Error");
@@ -28,11 +23,11 @@ const getUsersData = async (url) => {
 };
 getUsersData(usersURL);
 
-const displayAllUsers = (arrOfUsers) => {
-  usersField.innerHTML = "";
-  arrOfUsers.map((user) => {
+const displayAllUsers = (usersArray) => {
+  listOfUsers.innerHTML = "";
+  usersArray.map((user) => {
     const html = ` <li class="user">
-      <div class="user_photo">
+      <div class="user_photo-wrapper">
         <img src=${user.picture.large} alt="" class="user_img" />
       </div>
       <div class="user_description">
@@ -54,108 +49,181 @@ const displayAllUsers = (arrOfUsers) => {
         <img src="img/email.png" alt="" class="img_manipulate" />
       </div>
     </li>`;
-    usersField.insertAdjacentHTML("beforeend", html);
+    listOfUsers.insertAdjacentHTML("beforeend", html);
   });
 };
 
-let rules = ["", false, false, false, false];
+let sortingRules = {
+  filterByGenderIsChoosed: "all",
+  ageByDescendingIsChecked: false,
+  ageByIncreasingIsChecked: false,
+  nameByDescendingIsChecked: false,
+  nameByIncreasingIsChecked: false,
+};
 
-function filterUsers(target) {
-  if (genderChoosenMale.checked) {
-    rules[0] = "male";
-  }
-  if (genderChoosenFemale.checked) {
-    rules[0] = "female";
-  }
-  if (genderChoosenAll.checked) {
-    rules[0] = "all";
-  }
-  if (target.value === "0-9" && target.checked) {
-    rules[1] = true;
-    rules[2] = false;
-    rules[3] = false;
-    rules[4] = false;
-  }
-  if (target.value === "9-0" && target.checked) {
-    rules[2] = true;
-    rules[1] = false;
-    rules[3] = false;
-    rules[4] = false;
-  }
-  if (target.value === "A-z" && target.checked) {
-    rules[3] = true;
-    rules[4] = false;
-    rules[1] = false;
-    rules[2] = false;
-  }
-  if (target.value === "Z-a" && target.checked) {
-    rules[4] = true;
-    rules[3] = false;
-    rules[1] = false;
-    rules[2] = false;
-  }
+const defineSortingRules = (target) => {
+  switch (target.value) {
+    case "male":
+      sortingRules.filterByGenderIsChoosed = "male";
+      break;
 
-  if (rules[1] || rules[2]) {
-    displayAllUsers(sortByAge(filterByGender(copyOfAllUsers, rules), rules));
-  } else if (rules[3] || rules[4]) {
-    displayAllUsers(
-      sortByAlphabets(filterByGender(copyOfAllUsers, rules), rules)
-    );
-  } else if (
-    rules[0] === "all" ||
-    rules[0] === "male" ||
-    rules[0] === "female"
-  ) {
-    displayAllUsers(filterByGender(copyOfAllUsers, rules));
-  }
-}
+    case "female":
+      sortingRules.filterByGenderIsChoosed = "female";
+      break;
 
-function filterByGender(arr, rules) {
-  if (rules[0] === "male" || rules[0] === "female") {
-    arr = arr.filter((user) => user.gender === rules[0]);
-    return arr;
-  } else if (rules[0] === "all") {
-    return arr;
-  }
-}
+    case "all":
+      sortingRules.filterByGenderIsChoosed = "all";
+      break;
 
-function sortByAge(arr, rules) {
-  if (rules[1]) {
-    arr.sort((a, b) => a.dob.age - b.dob.age);
-  } else {
-    arr.sort((a, b) => b.dob.age - a.dob.age);
-  }
-  return arr;
-}
+    case "0-9":
+      sortingRules.ageByIncreasingIsChecked = true;
+      sortingRules.ageByDescendingIsChecked = false;
+      sortingRules.nameByIncreasingIsChecked = false;
+      sortingRules.nameByDescendingIsChecked = false;
+      break;
 
-function sortByAlphabets(arr, rules) {
-  if (rules[3]) {
-    arr.sort((a, b) => (a.name.first > b.name.first ? 1 : -1));
-  } else {
-    arr.sort((a, b) => (a.name.first > b.name.first ? -1 : 1));
-  }
-  return arr;
-}
+    case "9-0":
+      sortingRules.ageByDescendingIsChecked = true;
+      sortingRules.ageByIncreasingIsChecked = false;
+      sortingRules.nameByIncreasingIsChecked = false;
+      sortingRules.nameByDescendingIsChecked = false;
+      break;
 
-allFilters.addEventListener("input", function (e) {
-  const target = e.target;
+    case "A-z":
+      sortingRules.nameByIncreasingIsChecked = true;
+      sortingRules.nameByDescendingIsChecked = false;
+      sortingRules.ageByIncreasingIsChecked = false;
+      sortingRules.ageByDescendingIsChecked = false;
+      break;
+
+    case "Z-a":
+      sortingRules.nameByDescendingIsChecked = true;
+      sortingRules.nameByIncreasingIsChecked = false;
+      sortingRules.ageByIncreasingIsChecked = false;
+      sortingRules.ageByDescendingIsChecked = false;
+      break;
+  }
+};
+
+const dispalySortedUsers = function (target) {
+  copyOfAllUsers = [...allUsers];
+  defineSortingRules(target);
 
   if (searchInput.value !== "") {
-    const searchUser = copyOfAllUsers.filter(
+    const findUser = copyOfAllUsers.filter(
       (user) =>
         user.name.first
           .toLowerCase()
           .includes(searchInput.value.toLowerCase()) ||
         user.name.last.toLowerCase().includes(searchInput.value.toLowerCase())
     );
-    displayAllUsers(searchUser);
+    if (
+      sortingRules.ageByIncreasingIsChecked ||
+      sortingRules.ageByDescendingIsChecked
+    ) {
+      displayAllUsers(
+        sortByAge(filterByGender(findUser, sortingRules), sortingRules)
+      );
+    } else if (
+      sortingRules.nameByIncreasingIsChecked ||
+      sortingRules.nameByDescendingIsChecked
+    ) {
+      displayAllUsers(
+        sortByAlphabets(filterByGender(findUser, sortingRules), sortingRules)
+      );
+    } else {
+      displayAllUsers(filterByGender(findUser, sortingRules));
+    }
+  } else if (
+    sortingRules.ageByIncreasingIsChecked ||
+    sortingRules.ageByDescendingIsChecked
+  ) {
+    displayAllUsers(
+      sortByAge(filterByGender(copyOfAllUsers, sortingRules), sortingRules)
+    );
+  } else if (
+    sortingRules.nameByIncreasingIsChecked ||
+    sortingRules.nameByDescendingIsChecked
+  ) {
+    displayAllUsers(
+      sortByAlphabets(
+        filterByGender(copyOfAllUsers, sortingRules),
+        sortingRules
+      )
+    );
+  } else if (sortingRules.filterByGenderIsChoosed) {
+    displayAllUsers(filterByGender(copyOfAllUsers, sortingRules));
+  }
+};
+
+const filterByGender = (arrayOfUsers, sortingRules) => {
+  if (
+    sortingRules.filterByGenderIsChoosed === "male" ||
+    sortingRules.filterByGenderIsChoosed === "female"
+  ) {
+    arrayOfUsers = arrayOfUsers.filter(
+      (user) => user.gender === sortingRules.filterByGenderIsChoosed
+    );
+    return arrayOfUsers;
+  } else if (sortingRules.filterByGenderIsChoosed === "all") {
+    return arrayOfUsers;
+  }
+};
+
+const sortByAge = (arrayOfUsers, sortingRules) => {
+  if (sortingRules.ageByIncreasingIsChecked) {
+    arrayOfUsers.sort(
+      (firstUser, secondUser) => firstUser.dob.age - secondUser.dob.age
+    );
   } else {
-    filterUsers(target);
+    arrayOfUsers.sort(
+      (firstUser, secondUser) => secondUser.dob.age - firstUser.dob.age
+    );
+  }
+  return arrayOfUsers;
+};
+
+const sortByAlphabets = (arrayOfUsers, sortingRules) => {
+  if (sortingRules.nameByIncreasingIsChecked) {
+    arrayOfUsers.sort((firstUser, secondUser) =>
+      firstUser.name.first > secondUser.name.first ? 1 : -1
+    );
+  } else {
+    arrayOfUsers.sort((firstUser, secondUser) =>
+      firstUser.name.first > secondUser.name.first ? -1 : 1
+    );
+  }
+  return arrayOfUsers;
+};
+
+allFilters.addEventListener("input", function (e) {
+  const target = e.target;
+  dispalySortedUsers(target);
+});
+
+filterButtons.addEventListener("click", function (e) {
+  const target = e.target;
+  if (target.classList.contains("reset")) {
+    displayAllUsers(allUsers);
+    sortingRules = {
+      filterByGenderIsChoosed: "all",
+      ageByIncreasingIsChecked: false,
+      ageByDescendingIsChecked: false,
+      nameByIncreasingIsChecked: false,
+      nameByDescendingIsChecked: false,
+    };
+    searchInput.value = "";
+  } else if (target.classList.contains("close")) {
+    document.body.classList.remove("_lock");
+    burgerImage.classList.remove("_active");
+    filterBody.classList.remove("_active");
+    closeBurgerButton.classList.add("hidden");
   }
 });
 
-resetBtn.addEventListener("click", function () {
-  displayAllUsers(allUsers);
-  rules = ["", false, false, false, false];
-  searchInput.value = "";
+burgerImage.addEventListener("click", (e) => {
+  document.body.classList.add("_lock");
+  burgerImage.classList.add("_active");
+  filterBody.classList.add("_active");
+  closeBurgerButton.classList.remove("hidden");
 });
