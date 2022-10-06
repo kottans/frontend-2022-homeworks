231eb3ab99fc8ab8f4fdc701371158bca01c9299
m.sofiichuk <koyap@ukr.net>
2022-10-06 18:13:11 +0300

    removed redundant classes

--- submissions/sofiichuk/oop-exercise/index.js
+++ submissions/sofiichuk/oop-exercise/index.js
@@ -5,64 +5,40 @@ class Animal {
         this.saying = saying;
         this.legs = legs;
     }
-
     declareAnimal() {
-        return [this.name, this.gender, this.saying, this.legs].join(";");
+        return [this.constructor.name, this.name, this.gender, this.saying, this.legs].join(";");
     }
 }
 
 class Human extends Animal {
-    constructor(name, gender, saying, legs = 2, arms = 2) {
+    constructor(name, gender, saying = 'hello', legs = 2, arms = 2) {
         super(name, gender, saying, legs);
         this.arms = arms;
     }
-
-    declareGenus() {
-        return [this.constructor.name, super.declareAnimal(), this.arms].join(";");
+    declareAnimal() {
+        return [super.declareAnimal(), this.arms].join(";");
     }
 }
 
-class Pet extends Animal {
-    constructor(name, gender, saying, legs = 4) {
-        super(name, gender, saying, legs);
-    }
-
-    declareGenus() {
-        return [this.constructor.name, super.declareAnimal()].join(";");
-    }
-}
-
-class Man extends Human {
-    constructor(name, gender = 'male', saying = 'hi', legs, arms) {
-        super(name, gender, saying, legs, arms)
-    }
-}
-
-class Woman extends Human {
-    constructor(name, gender = 'female', saying = 'hello', legs, arms) {
-        super(name, gender, saying, legs, arms)
-    }
-}
-
-class Dog extends Pet {
-    constructor(name, gender, saying = 'woof', legs) {
+class Dog extends Animal {
+    constructor(name, gender, saying = 'woof', legs = 4) {
         super(name, gender, saying, legs)
     }
 }
 
-class Cat extends Pet {
-    constructor(name, gender, saying = 'meow', legs) {
+class Cat extends Animal {
+    constructor(name, gender, saying = 'meow', legs = 4) {
         super(name, gender, saying, legs)
     }
 }
 
-const Tom = new Man('Tom');
-const Dick = new Man('Dick');
-const Harry = new Man('Harry');
+const Tom = new Human('Tom', 'male');
+const Dick = new Human('Dick', 'male');
+const Harry = new Human('Harry', 'male');
 
-const Polly = new Woman('Polly');
-const Molly = new Woman('Molly');
-const Mary = new Woman('Mary');
+const Polly = new Human('Polly', 'female');
+const Molly = new Human('Molly', 'female');
+const Mary = new Human('Mary', 'female');
 
 const Buddy = new Dog('Buddy', 'male');
 const Betty = new Dog('Betty', 'female');
@@ -71,5 +47,5 @@ const Kitty = new Cat('Kitty', 'female');
 const Tiger = new Cat('Tiger', 'male');
 
 const creatures = [Tom, Dick, Harry, Polly, Molly, Mary, Buddy, Betty, Kitty, Tiger];
-const creaturePropsList = creatures.map(one => one.declareGenus()).join('\n\n');
-print(creaturePropsList);
+const creaturePropsList = creatures.map(one => one.declareAnimal()).join('\n\n');
+console.log(creaturePropsList);
