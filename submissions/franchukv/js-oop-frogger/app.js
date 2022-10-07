ceb54fd75878a99dc66dbe99d329f22834100535
Volodymyr Franchuk <franchuk.vldr@gmail.com>
2022-10-07 15:02:09 +0300

    Yeap, rewrote the app again :D
    1. Rewrote game engine for visual demonstration of new features (I'm not sure is it okay or not for this task).
    2. Added simple functionality to choose size of game desk.
    3. Now all math calculating by a computer.
    4. Enemies added automatically into game based on game desk size.

--- submissions/franchukv/js-oop-frogger/app.js
+++ submissions/franchukv/js-oop-frogger/app.js
@@ -1,43 +1,38 @@
-const CANVAS_HEIGHT = 606;
-const CANVAS_WIDTH = 505;
+window.gameRowsNumber = prompt('How many rows do you want to play?', 6);
+window.gameColsNumber = prompt('How many columns do you want to play?', 5);
+
 const TILE_HEIGHT = 83;
 const TILE_WIDTH = 101;
+const ROWS_NUMBER = window.gameRowsNumber;
+const COLS_NUMBER = window.gameColsNumber;
+const CANVAS_HEIGHT = TILE_HEIGHT * ROWS_NUMBER;
+const CANVAS_WIDTH = TILE_WIDTH * COLS_NUMBER;
 
 const PLAYER_CONF = {
     size: {
         height: 40,
-        width: 40
+        width: 40,
     },
     position: {
-        x: 202,
-        y: 380
-    }
+        x: COLS_NUMBER % 2 != 0 ? TILE_WIDTH * Math.floor(COLS_NUMBER / 2) : CANVAS_WIDTH - TILE_WIDTH,
+        y: TILE_HEIGHT * (ROWS_NUMBER - 1.5),
+    },
 };
 
 const ENEMIES_CONF = {
     size: {
         height: 40,
-        width: 80
+        width: 80,
     },
-    enemies: [{
-        x: -80,
-        y: 55,
-        speed: 280
-    },
-    {
-        x: -80,
-        y: 135,
-        speed: 150
-    },
-    {
-        x: -80,
-        y: 220,
-        speed: 200
-    }]
+    enemiesNumber: ROWS_NUMBER - 3,
+    firstEnemyPositionY: 55,
+    minSpeed: 100,
+    maxSpeed: 300,
+    enemies: [],
 };
 
 const Player = function ({ position, size }) {
-    this.sprite = 'images/char-boy.png';
+    this.sprite = "images/char-boy.png";
     this.x = position.x;
     this.y = position.y;
     this.height = size.height;
@@ -51,16 +46,16 @@ Player.prototype.resetPlayerPosition = function ({ position }) {
 
 Player.prototype.handleInput = function (key) {
     switch (key) {
-        case 'up':
+        case "up":
             if (this.y > 0) this.y -= TILE_HEIGHT;
             break;
-        case 'down':
-            if (this.y <= CANVAS_HEIGHT - TILE_HEIGHT * 3) this.y += TILE_HEIGHT;
+        case "down":
+            if (this.y <= CANVAS_HEIGHT - TILE_HEIGHT * 2) this.y += TILE_HEIGHT;
             break;
-        case 'left':
+        case "left":
             if (this.x > 0) this.x -= TILE_WIDTH;
             break;
-        case 'right':
+        case "right":
             if (this.x < CANVAS_WIDTH - TILE_WIDTH) this.x += TILE_WIDTH;
             break;
         default:
@@ -70,7 +65,9 @@ Player.prototype.handleInput = function (key) {
 
 Player.prototype.update = function () {
     if (this.y < 0) {
-        setTimeout(() => { this.resetPlayerPosition(PLAYER_CONF); }, 200);
+        setTimeout(() => {
+            this.resetPlayerPosition(PLAYER_CONF);
+        }, 200);
     }
 };
 
@@ -78,13 +75,13 @@ Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-const Enemy = function ({ size }, enemyProperties, player) {
-    this.sprite = 'images/enemy-bug.png';
+const Enemy = function (size, enemyStartPositionX, enemyProperty, player) {
+    this.sprite = "images/enemy-bug.png";
     this.height = size.height;
     this.width = size.width;
-    this.x = enemyProperties.x;
-    this.y = enemyProperties.y;
-    this.speed = enemyProperties.speed;
+    this.x = enemyStartPositionX;
+    this.y = enemyProperty.y;
+    this.speed = enemyProperty.speed;
     this.player = player;
 };
 
@@ -95,7 +92,6 @@ Enemy.prototype.checkCollision = function () {
         this.player.x + this.player.width > this.x &&
         this.player.x < this.x + this.width
     ) {
-
         this.player.resetPlayerPosition(PLAYER_CONF);
     }
 };
@@ -104,7 +100,7 @@ Enemy.prototype.update = function (dt) {
     this.checkCollision();
 
     if (this.x >= CANVAS_WIDTH) {
-        this.x = -80;
+        this.x = -TILE_WIDTH;
     }
 
     this.x += this.speed * dt;
@@ -114,22 +110,35 @@ Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-function addEnemiesToInitArray() {
-    ENEMIES_CONF.enemies.forEach((enemy) => {
-        allEnemies.push(new Enemy(ENEMIES_CONF, enemy, player));
+const createEnemies = (
+    { enemiesNumber, firstEnemyPositionY, minSpeed, maxSpeed },
+    tileHeight,
+    tileWidth
+) => {
+    for (let i = 0; i < enemiesNumber; i++) {
+        ENEMIES_CONF.enemies.push({});
+        ENEMIES_CONF.enemies[i].y = firstEnemyPositionY + tileHeight * i;
+        ENEMIES_CONF.enemies[i].speed =
+            Math.random() * (maxSpeed - minSpeed) + minSpeed;
+    }
+
+    ENEMIES_CONF.enemies.forEach((enemyProperty) => {
+        allEnemies.push(
+            new Enemy(ENEMIES_CONF.size, -tileWidth, enemyProperty, player)
+        );
     });
-}
+};
 
 const player = new Player(PLAYER_CONF);
 const allEnemies = [];
-addEnemiesToInitArray();
+createEnemies(ENEMIES_CONF, TILE_HEIGHT, TILE_WIDTH);
 
-document.addEventListener('keyup', function (e) {
+document.addEventListener("keyup", function (e) {
     var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down",
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
