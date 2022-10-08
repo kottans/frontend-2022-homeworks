6c26b27e177faf2677be15d3230c42401e4e35ec
develabr <develabr@gmail.com>
2022-10-08 18:36:21 +0300

    Add switcher between dark and light theme

--- submissions/develabr/memory-pair-game/app.js
+++ submissions/develabr/memory-pair-game/app.js
@@ -118,3 +118,49 @@ wrapper.addEventListener('click', (event) => {
     previousTarget = clicked;
   }
 });
+
+window.addEventListener('load', windowLoad);
+function windowLoad() {
+  const htmlBlock = document.documentElement;
+  const saveUserTheme = localStorage.getItem('user-theme');
+  let userTheme;
+  if (window.matchMedia) {
+    userTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light';
+  }
+  window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', (e) => {
+      !saveUserTheme ? changeTheme() : null;
+    });
+
+  const themeButton = document.querySelector('.theme');
+  if (themeButton) {
+    themeButton.addEventListener('click', function (e) {
+      changeTheme(true);
+    });
+  }
+
+  function setThemeClass() {
+    if (saveUserTheme) {
+      htmlBlock.classList.add(saveUserTheme);
+    } else {
+      htmlBlock.classList.add(userTheme);
+    }
+  }
+  setThemeClass();
+
+  function changeTheme(saveTheme = false) {
+    let currentTheme = htmlBlock.classList.contains('light') ? 'light' : 'dark';
+    let newTheme;
+    if (currentTheme === 'light') {
+      newTheme = 'dark';
+    } else if (currentTheme === 'dark') {
+      newTheme = 'light';
+    }
+    htmlBlock.classList.remove(currentTheme);
+    htmlBlock.classList.add(newTheme);
+    saveTheme ? localStorage.setItem('user-theme', newTheme) : null;
+  }
+}
