866e5aa5bd4917ec54ccfa9f4c80db756bfeae57
andreymashko7 <63639567+andreymashko7@users.noreply.github.com>
2022-10-09 19:39:15 +0300

    fixed DRY problem

--- submissions/AndriyM/oop_exercise_tiny_world/index.js
+++ submissions/AndriyM/oop_exercise_tiny_world/index.js
@@ -1,35 +1,36 @@
+const keys = ["species", "name", "gender", "saying", "legs"];
 class Inhabitant {
-    constructor(species, name, gender, legs, saying) {
-        this.species = species;
-        this.name = name;
-        this.legs = legs;
-        this.gender = gender;
-        this.saying = saying;
-    }
-    inhabitantShowProps() {
-        return `${this.species}; ${this.name}; ${this.gender}; ${this.legs}; ${this.saying}`;
-    }
+	constructor(species, name, gender, legs, saying) {
+		this.species = species;
+		this.name = name;
+		this.legs = legs;
+		this.gender = gender;
+		this.saying = saying;
+	}
+	inhabitantShowProps() {
+		return keys.map((key) => this[key]).join("; ");
+	}
 }
 class Human extends Inhabitant {
-    constructor(name, gender, saying, legs = 2, hands = 2) {
-        super("human", name, gender, legs, saying);
-        this.hands = hands;
-    }
-    inhabitantShowProps() {
-        return `${super.inhabitantShowProps()}; ${this.hands}`;
-    }
+	constructor(name, gender, saying, legs = 2, hands = 2) {
+		super("human", name, gender, legs, saying);
+		this.hands = hands;
+	}
+	inhabitantShowProps() {
+		return `${super.inhabitantShowProps()}; ${this.hands}`;
+	}
 }
 
 class Dog extends Inhabitant {
-    constructor(name, gender, saying, legs = 4) {
-        super("dog", name, gender, legs, saying);
-    }
+	constructor(name, gender, saying, legs = 4) {
+		super("dog", name, gender, legs, saying);
+	}
 }
 
 class Cat extends Inhabitant {
-    constructor(name, gender, saying, legs = 4) {
-        super("cat", name, gender, legs, saying);
-    }
+	constructor(name, gender, saying, legs = 4) {
+		super("cat", name, gender, legs, saying);
+	}
 }
 
 const dog = new Dog("Rex", "male", "bow-wow!");
@@ -38,5 +39,5 @@ const man = new Human("Joe", "male", "My name is Joe Black");
 const woman = new Human("Jessica", "female", "Hi guys");
 
 [dog, cat, man, woman].forEach((inhabitant) =>
-    print(inhabitant.inhabitantShowProps())
+	print(inhabitant.inhabitantShowProps())
 );
