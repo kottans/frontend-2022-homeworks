afd427060f7f9312d48a111f904fc13563237dbb
D1telo <bratherden@gmail.com>
2022-10-11 12:56:51 +0300

    Update all file self check

--- submissions/D1telo/a-tiny-js-world/index.js
+++ submissions/D1telo/a-tiny-js-world/index.js
@@ -2,8 +2,8 @@ import { print } from "./js/lib.js";
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
+   Code repository: https://github.com/D1telo/a-tiny-JS-world/blob/master/index.js
+   Web app: https://d1telo.github.io/tiny-js/
    */
 
 // ======== OBJECTS DEFINITIONS ========
@@ -57,21 +57,3 @@ const qualityPersonage = ['species', 'name', 'gender', 'legs', 'hands', 'voice']
 
 speciesAll.map(personage => print(qualityPersonage.map(quality => personage[quality]).join(' - ')));
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
