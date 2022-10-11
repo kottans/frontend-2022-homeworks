67fafb2824a7679cb7331697649be563952f99a5
Andrii Konovalov <andy.smokk@gmail.com>
2022-10-11 15:49:51 +0200

    fix legs property

--- submissions/andysmokk/oop-exercise/index.js
+++ submissions/andysmokk/oop-exercise/index.js
@@ -9,9 +9,10 @@
 // Define your objects here
 
 class Inhabitant {
-  constructor(species, name, gender, saying, friends) {
+  constructor(species, name, legs, gender, saying, friends) {
     this.species = species;
     this.name = name;
+    this.legs = legs;
     this.gender = gender;
     this.saying = saying;
     this.friends = friends;
@@ -21,6 +22,7 @@ class Inhabitant {
     const propertiesInhabitant = [
       this.species,
       this.name,
+      this.legs,
       this.gender,
       this.saying,
       this.friends,
@@ -39,35 +41,31 @@ class Inhabitant {
 }
 
 class Human extends Inhabitant {
-  constructor(species, name, gender, saying, friends) {
-    super(species, name, gender, saying, friends);
+  constructor(species, name, legs, gender, saying, friends) {
+    super(species, name, legs, gender, saying, friends);
     this.hands = 2;
-    this.legs = 2;
   }
 
   showPropertiesInhabitant() {
     const propertiesInhabitant = super.showPropertiesInhabitant();
-    propertiesInhabitant.splice(2, 0, this.hands, this.legs);
+    propertiesInhabitant.splice(2, 0, this.hands);
     return propertiesInhabitant;
   }
 }
 
 class NotHuman extends Inhabitant {
-  constructor(species, name, gender, saying, friends) {
-    super(species, name, gender, saying, friends);
-    this.legs = 4;
+  constructor(species, name, legs, gender, saying, friends) {
+    super(species, name, legs, gender, saying, friends);
   }
 
   showPropertiesInhabitant() {
-    const propertiesInhabitant = super.showPropertiesInhabitant();
-    propertiesInhabitant.splice(2, 0, this.legs);
-    return propertiesInhabitant;
+    return super.showPropertiesInhabitant();
   }
 }
 
 class SuperHuman extends Human {
-  constructor(species, name, gender, saying, friends, superPower) {
-    super(species, name, gender, saying, friends);
+  constructor(species, name, legs, gender, saying, friends, superPower) {
+    super(species, name, legs, gender, saying, friends);
     this.superPower = superPower;
   }
 
@@ -78,17 +76,20 @@ class SuperHuman extends Human {
   }
 }
 
-const man = new Human("human", "Momo", "male", "I am Momo", ["Kamila", "Boba"]);
-const woman = new Human("human", "Kamila", "female", "I am Kamila", [
+const man = new Human("human", "Momo", 2, "male", "I am Momo", [
+  "Kamila",
+  "Boba",
+]);
+const woman = new Human("human", "Kamila", 2, "female", "I am Kamila", [
   "Momo",
   "Boba",
   "Chico",
 ]);
-const cat = new NotHuman("cat", "Chico", "male", "Meow-meow", [
+const cat = new NotHuman("cat", "Chico", 4, "male", "Meow-meow", [
   "Kamila",
   "Kity",
 ]);
-const dog = new NotHuman("dog", "Boba", "male", "Woof-woof", [
+const dog = new NotHuman("dog", "Boba", 4, "male", "Woof-woof", [
   "Kamila",
   "Momo",
   "Charly",
@@ -96,6 +97,7 @@ const dog = new NotHuman("dog", "Boba", "male", "Woof-woof", [
 const catWoman = new SuperHuman(
   "cat-woman",
   "Kity",
+  2,
   "female",
   cat.saying + ", i love Batman",
   ["Chico"],
@@ -104,6 +106,7 @@ const catWoman = new SuperHuman(
 const batMan = new SuperHuman(
   "bat-man",
   "Charly",
+  2,
   "male",
   "I don't love Catwoman",
   ["Boba"],
