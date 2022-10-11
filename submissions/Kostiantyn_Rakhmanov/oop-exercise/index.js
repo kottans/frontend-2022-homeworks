1fc7fffe15a9bd877f6fa3133fffd5b3d63a1192
Kostiantyn Rakhmanov <rahmanoff@gmail.com>
2022-10-11 11:43:39 +0300

    Added: legs to Inhabitant

--- submissions/Kostiantyn_Rakhmanov/oop-exercise/index.js
+++ submissions/Kostiantyn_Rakhmanov/oop-exercise/index.js
@@ -1,74 +1,65 @@
 import { print } from './js/lib.js';
 
 class Inhabitant {
-   constructor(species, name, gender, saying) {
+   constructor(species, name, gender, saying, legs) {
       this.species = species;
       this.name = name;
       this.gender = gender;
       this.saying = saying;
+      this.legs = legs;
    }
    printOut() {
-      return ['species', 'name', 'gender', 'saying'].map(
+      return ['species', 'name', 'gender', 'saying', 'legs',].map(
          (prop) => `${prop}: ${this[prop]}`
       ).join('; ');
    }
 }
 
 class Animal extends Inhabitant {
-   constructor (species, name, gender, saying) {
-      super (species, name, gender, saying);
+   constructor(species, name, gender, legs, saying) {
+      super(species, name, gender, legs, saying);
       this.legs = 4;
-   }
-   printOut() {
-      return [
-         super.printOut(),
-         ...['legs'].map((prop) => `${prop}: ${this[prop]}`),
-      ].join('; ');
    }
 }
 
 class Dog extends Animal {
-   constructor (name, gender, saying) {
-      super ('dog', name, gender, saying);
+   constructor(name, gender, saying) {
+      super('dog', name, gender, saying);
    }
 }
 
 class Cat extends Animal {
-   constructor (name, gender, saying) {
-      super ('cat', name, gender, saying);
+   constructor(name, gender, saying) {
+      super('cat', name, gender, saying);
    }
 }
 
 class Human extends Inhabitant {
-   constructor (name, gender, saying) {
-      super('human', name, gender, saying);
+   constructor(name, gender, saying, legs = 2) {
+      super('human', name, gender, saying, legs);
       this.hands = 2;
-      this.legs = 2;
    }
    printOut() {
-      return [
-         super.printOut(),
-         ...['hands','legs'].map((prop) => `${prop}: ${this[prop]}`),
-      ].join('; ');
+      return `${super.printOut()}; hands: ${this.hands}`;
    }
 }
 
 class Woman extends Human {
-   constructor (name, saying) {
-      super (name, 'female', saying);
+   constructor(name, saying) {
+      super(name, 'female', saying);
    }
 }
 
 class Man extends Human {
-   constructor (name, saying) {
-      super (name, 'male', saying);
+   constructor(name, saying) {
+      super(name, 'male', saying);
    }
 }
 
-const toby = new Dog ('Toby', 'male', 'woof-woof');
-const alice = new Cat ('Alice', 'female', 'meow');
-const mary = new Woman ('Mary', 'Hi, John!');
-const john = new Man ('John', 'Hi, Mary!');
+const toby = new Dog('Toby', 'male', 'woof-woof');
+const alice = new Cat('Alice', 'female', 'meow');
+const mary = new Woman('Mary', 'Hi, John!');
+const john = new Man('John', 'Hi, Mary!');
 
 const inhabitants = [toby, alice, mary, john];
 
