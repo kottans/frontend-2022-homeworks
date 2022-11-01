22ca6ee63ca10c54e36d482dbd1ffd02ad3a0d6c
Andriy <63639567+andreymashko7@users.noreply.github.com>
2022-11-01 15:20:11 +0200

    made changes according to the comments

--- submissions/AndriyM/friends-app/index.js
+++ submissions/AndriyM/friends-app/index.js
@@ -1,171 +1,200 @@
-const rootRef = document.querySelector(".main__cards");
-const bodyRef = document.querySelector("body");
-const form = document.querySelector(".main__form");
-let filterAndSortAtr, friends, filteredOrSort;
-let copyFriends = [];
+const refs = {
+	bodyWrapper: document.querySelector("body"),
+	cardsWrapper: document.querySelector(".main__cards"),
+	formWrapper: document.querySelector(".main__form")
+};
+
+const formRefs = {
+	form: document.forms.form,
+	input: document.forms.form.nameInput,
+	button: document.querySelector(".form__clear"),
+	sort: document.querySelector(".form__sort"),
+	filter: document.querySelector(".form__gender")
+};
+
+const modalRefs = {
+	open: document.querySelector(".form-filter"),
+	overlay: document.querySelector(".overlay"),
+	title: document.querySelector(".filter-text")
+};
 
 async function fetchUsers() {
 	const response = await fetch(
-		`https://randomuser.me/api/?results=2500&noinfo`
+		`https://randomuser.me/api/?results=1000&noinfo`
 	);
 	return await response.json();
 }
 
+let friends = [];
+
 try {
-	friends = await fetchUsers();
-	copyFriends = [...friends.results];
+	const users = await fetchUsers();
+	friends = [...users.results];
 } catch (error) {
 	console.log(error.message);
+	refs.bodyWrapper.innerHTML = `<div class='notification error'>
+        Whoops, something went wrong.Please try again later.</div>`;
 }
 
-let sliceFriends = [];
+let copyUsers = [...friends];
 
-function sliceBestMatch(arrOfFriends) {
-	sliceFriends.length = 0;
-	for (let i = 0; i < arrOfFriends.length; i++) {
-		if (i === 8) {
-			break;
-		}
-		sliceFriends.push(arrOfFriends[i]);
-	}
-	return sliceFriends;
-}
-
-let defaultFriends = sliceBestMatch(copyFriends);
-
-function renderCardsMarkup(friends) {
-	return friends
+function createUsersMarkup(users) {
+	return users
 		.map(
 			({ email, cell, name, picture, dob }) =>
 				`<div class="card">
-                    <div>
-                        <img
-                        class="card__img"
-	                    src=${picture.large}
-	                    alt=""
-	                    />
+        <div>
+                    <img
+                    class="card__img"
+                    src=${picture.large}
+                    alt=""
+                    />
                     </div> 
-                        <div class="card__description">
-		                <p class="card__title">${name.first} ${name.last}</p>
-		                <p class="card__email">${email}</p>
-		                <p class="card__phone">${cell}</p>
-                        <span class="card__age">${dob.age}</span>
-	                </div>
-                </div>`
+                    <div class="card__description">
+                    <p class="card__title">${name.first} ${name.last}</p>
+                    <p class="card__email">${email}</p>
+                    <p class="card__phone">${cell}</p>
+                    <span class="card__age">${dob.age}</span>
+                    </div>
+                    </div>`
 		)
 		.join("");
 }
 
-rootRef.innerHTML = renderCardsMarkup(defaultFriends);
-
-function getSortFriends(friends, filterAndSortAtr) {
-	const copyForSort = friends.slice();
-	return copyForSort.sort((a, b) => {
-		switch (filterAndSortAtr) {
-			case "ascending-age":
-				return a.dob.age - b.dob.age;
-			case "descending-age":
-				return b.dob.age - a.dob.age;
-			case "descending-name":
-				return b.name.first.localeCompare(a.name.first);
-			case "ascending-name":
-				return a.name.first.localeCompare(b.name.first);
-			default:
-				return friends;
-		}
-	});
+function sliceBestMatch(users) {
+	const sliceUsers = users.slice(0, 8);
+	return sliceUsers;
 }
 
-function getfilterByGender(friends, filterAndSortAtr) {
-	return friends.filter((friend) => {
-		switch (filterAndSortAtr) {
-			case "all":
-				return friends;
-			case "male":
-				return friend.gender === "male";
-			case "female":
-				return friend.gender === "female";
-			default:
-				return friends;
-		}
-	});
+function handleSubmit(event) {
+	event.preventDefault();
 }
 
-const formSort = document.querySelector(".form__sort");
-const formGender = document.querySelector(".form__gender");
-formSort.addEventListener("click", handleClickForm);
-formGender.addEventListener("click", handleClickForm);
+formRefs.form.addEventListener("submit", handleSubmit);
+
+function renderUsersCards(users) {
+	users.length
+		? (refs.cardsWrapper.innerHTML = createUsersMarkup(
+				sliceBestMatch(users)
+		  ))
+		: (refs.cardsWrapper.innerHTML =
+				"<div class='notification'>No users found</div>");
+}
+
+renderUsersCards(copyUsers);
+
+let filteredByName = [];
+
+function searchByName(event) {
+	const inputText = event.target.value.toLowerCase();
+
+	filteredByName = copyUsers.filter((friend) => {
+		if (
+			friend.name.first.toLowerCase().includes(inputText) ||
+			friend.name.last.toLowerCase().includes(inputText)
+		) {
+			return true;
+		}
+	});
+
+	renderUsersCards(filteredByName);
+
+	if (!inputText) {
+		clearFilters();
+	}
+}
+
+formRefs.input.addEventListener("input", searchByName);
 
 function handleClickForm({ target }) {
-	filterAndSortAtr = target.dataset.sort;
 	if (target.nodeName !== "IMG" && target.nodeName !== "INPUT") {
 		return;
 	}
 
-	if (
-		filterAndSortAtr === "all" ||
-		filterAndSortAtr === "male" ||
-		filterAndSortAtr === "female"
-	) {
-		filteredOrSort = getfilterByGender(copyFriends, filterAndSortAtr);
-	} else {
-		mainForm.nameRadio[0].checked = true;
-		filteredOrSort = getSortFriends(copyFriends, filterAndSortAtr);
+	if (target.dataset.filter) {
+		const filtertAttr = target.dataset.filter;
+		filteredByName.length
+			? (copyUsers = [...filteredByName])
+			: (copyUsers = [...friends]);
+		copyUsers = getfilterByGender(copyUsers, filtertAttr);
+	}
+	if (target.dataset.sort) {
+		const sortAttr = target.dataset.sort;
+		copyUsers = getSortFriends(copyUsers, sortAttr);
 	}
 
-	mainFormInput.value = "";
-	rootRef.innerHTML = renderCardsMarkup(sliceBestMatch(filteredOrSort));
+	renderUsersCards(copyUsers);
 }
 
-const btnRef = document.querySelector(".form__clear");
-btnRef.addEventListener("click", clearFilters);
+formRefs.form.addEventListener("click", handleClickForm);
+
+function getSortFriends(users, sortFilterAttr) {
+	switch (sortFilterAttr) {
+		case "ascending-age":
+			users.sort((a, b) => a.dob.age - b.dob.age);
+			break;
+		case "descending-age":
+			users.sort((a, b) => b.dob.age - a.dob.age);
+			break;
+		case "descending-name":
+			users.sort((a, b) => b.name.first.localeCompare(a.name.first));
+			break;
+		case "ascending-name":
+			users.sort((a, b) => a.name.first.localeCompare(b.name.first));
+		default:
+			return users;
+	}
+	return users;
+}
+
+function getfilterByGender(users, sortFilterAttr) {
+	switch (sortFilterAttr) {
+		case "male":
+		case "female":
+			copyUsers = users.filter((user) => user.gender === sortFilterAttr);
+			break;
+		case "all":
+			users;
+			break;
+		default:
+			users;
+			break;
+	}
+	return copyUsers;
+}
 
 function clearFilters() {
-	mainForm.nameRadio[0].checked = true;
-	mainFormInput.value = "";
-	rootRef.innerHTML = renderCardsMarkup(sliceBestMatch(copyFriends));
+	formRefs.form.nameRadio[0].checked = true;
+	formRefs.input.value = "";
+	filteredByName.length = 0;
+	copyUsers = [...friends];
+	renderUsersCards(copyUsers);
 }
 
-const mainForm = document.forms.form;
-const mainFormInput = mainForm.nameInput;
-mainFormInput.addEventListener("input", findByName);
-
-function findByName(event) {
-	const inputText = event.target.value.toLowerCase();
-	const friendsByName = copyFriends.filter((friend) =>
-		friend.name.first.toLowerCase().includes(inputText)
-	);
-	if (!friendsByName.length) {
-		rootRef.innerHTML = "<div class='notification'>No friends found</div>";
-		return;
-	}
-	mainForm.nameRadio[0].checked = true;
-	rootRef.innerHTML = renderCardsMarkup(sliceBestMatch(friendsByName));
-}
-
-const filterRef = document.querySelector(".form-filter");
-const backDropRef = document.querySelector(".overlay");
-backDropRef.addEventListener("click", onbackdropClick);
-filterRef.addEventListener("click", toggleModal);
+formRefs.button.addEventListener("click", clearFilters);
 
 function onbackdropClick(event) {
 	if (event.currentTarget === event.target) {
-		backDropRef.classList.toggle("is-hidden");
-		form.classList.toggle("show");
-		filterRef.textContent = "open filter";
+		refs.formWrapper.classList.toggle("show");
+		modalRefs.overlay.classList.toggle("is-hidden");
+		modalRefs.title.textContent = "open filter";
 	}
 }
+
+modalRefs.overlay.addEventListener("click", onbackdropClick);
 
 function toggleModal({ target }) {
 	if (target.nodeName !== "DIV") {
 		return;
 	}
 
-	form.classList.toggle("show");
-	backDropRef.classList.toggle("is-hidden");
-	filterRef.textContent = "close filter";
+	refs.formWrapper.classList.toggle("show");
+	modalRefs.overlay.classList.toggle("is-hidden");
+	modalRefs.title.textContent = "close filter";
 
-	if (!form.classList.contains("show")) {
-		filterRef.textContent = "open filter";
+	if (!refs.formWrapper.classList.contains("show")) {
+		modalRefs.title.textContent = "open filter";
 	}
 }
+
+modalRefs.open.addEventListener("click", toggleModal);
