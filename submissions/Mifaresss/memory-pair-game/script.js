aa5878da280d5d9c76f9fb84dad3576821b8b62e
Mifaresss <9803211@gmail.com>
2023-02-24 21:56:07 +0200

    Updated app based on new architecture (using state)

--- submissions/Mifaresss/memory-pair-game/script.js
+++ submissions/Mifaresss/memory-pair-game/script.js
@@ -1,98 +1,83 @@
 const buttonSwitch = document.querySelector('.switcher-theme__input');
-buttonSwitch.addEventListener('click', toggleThemeClass);
+buttonSwitch.addEventListener('click', toggleClass);
 
-function toggleThemeClass({ currentTarget }) {
-	const inputStatus = currentTarget.checked;
-	if (inputStatus) {
-		document.body.classList.add('dark-theme');
-	} else {
-		document.body.classList.remove('dark-theme');
-	}
-	localStorage.setItem('Theme', inputStatus);
+function toggleClass({ currentTarget }) {
+  const inputStatus = currentTarget.checked;
+  if (inputStatus) {
+    document.body.classList.add('dark-theme');
+  } else {
+    document.body.classList.remove('dark-theme');
+  }
+  localStorage.setItem('Theme', inputStatus);
 }
 function savesThemeSelection() {
-	if (localStorage.getItem('Theme') == 'true') {
-		document.body.classList.add('dark-theme');
-		buttonSwitch.checked = 'true';
-	}
+  if (localStorage.getItem('Theme') == 'true') {
+    document.body.classList.add('dark-theme');
+    buttonSwitch.checked = 'true';
+  }
 }
 savesThemeSelection();
 
+const cards = document.querySelector('.cards');
 
-const cards = document.querySelectorAll('.cards__item');
-
-let hasFlippedCards = false;
-let firstCard;
-let secondCard;
-let lockBoard = false;
-
-function flipsAndIdentifiesCards() {
-	if (lockBoard) return;
-	if (this === firstCard) return;
-
-	this.classList.add('flip');
-
-	if (!hasFlippedCards) {
-		hasFlippedCards = true;
-		firstCard = this;
-		return;
-	}
-
-	hasFlippedCards = false;
-	secondCard = this;
-
-	checkMatch();
-	resetGame();
+function shuffleCards(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
-function checkMatch() {
-	if (firstCard.dataset.number == secondCard.dataset.number) {
-		hidesPairedCards();
-	} else {
-		flipsUnpairedCards();
-	}
+const cardsList = Array.from(cards.children);
+shuffleCards(cardsList);
+cardsList.forEach((card, index) => (card.style.order = index));
+
+cards.addEventListener('click', handleCardClick);
+
+const state = {
+  flippedCards: [],
+  hiddenCards: [],
+};
+
+function canFlipCard(card) {
+  return card.classList.contains('flip') || card.classList.contains('cards') || card.classList.contains('cards__img');
 }
 
-function hidesPairedCards() {
-	firstCard.classList.add('hidden');
-	secondCard.classList.add('hidden');
-	resetBoard();
+function flipCard(card) {
+  card.classList.add('flip');
+  state.flippedCards.push(card);
 }
 
-function flipsUnpairedCards() {
-	lockBoard = true;
-	setTimeout(() => {
-		firstCard.classList.remove('flip')
-		secondCard.classList.remove('flip')
-		resetBoard();
-	}, 1100)
+function checkMatch(firstCard, secondCard) {
+  if (firstCard.dataset.number === secondCard.dataset.number) {
+    firstCard.classList.add('hidden');
+    secondCard.classList.add('hidden');
+    state.hiddenCards.push(firstCard, secondCard);
+    resetGame();
+  } else {
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+    }, 500);
+  }
 }
 
-function resetBoard() {
-	[firstCard, secondCard] = [null, null];
-	[hasFlippedCards, lockBoard] = [false, false];
+function handleCardClick({ target }) {
+  if (canFlipCard(target)) return;
+  flipCard(target);
+
+  if (state.flippedCards.length === 2) {
+    const [firstCard, secondCard] = state.flippedCards;
+    checkMatch(firstCard, secondCard);
+    state.flippedCards = [];
+  }
 }
 
 function resetGame() {
-	const cardsWidthClassHidden = [];
-	cards.forEach(card => {
-		if (card.classList.contains('hidden')) {
-			cardsWidthClassHidden.push(1);
-		}
-	})
-	if (cardsWidthClassHidden.length === 16) {
-		setTimeout(() => {
-			alert('Поздравляю, Вы прошли игру!');
-			location.reload();
-		}, 1000)
-	}
+  if (state.hiddenCards.length === 16) {
+    setTimeout(() => {
+      alert('Поздравляю, Вы прошли игру!');
+      location.reload();
+    }, 1000);
+  }
 }
-
-function cardsShuffling(card) {
-	card.style.order = Math.floor(Math.random() * 16);
-}
-
-cards.forEach(card => {
-	card.addEventListener('click', flipsAndIdentifiesCards);
-	cardsShuffling(card);
-})
