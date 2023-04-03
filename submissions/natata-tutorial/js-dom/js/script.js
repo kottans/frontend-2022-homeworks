95d1105bc7c36bd95dcaeeeea4077ea54115cf72
natata-tutorial <natata.politko@gmail.com>
2023-04-03 04:28:19 +0300

    fix: event delegation, content shift

--- submissions/natata-tutorial/js-dom/js/script.js
+++ submissions/natata-tutorial/js-dom/js/script.js
@@ -1,26 +1,69 @@
-"use strict"
+"use strict";
 
 import contentObj from "./content.js";
 
-const contentTitle = document.querySelector('.content__title');
-const contentText = document.querySelector('.content__text');
-const contentPicture = document.querySelector('.content__picture');
+document.addEventListener("DOMContentLoaded", createSidebarList);
 
-const sideNav = document.querySelector('.sidenav');
-const sideNavElements = sideNav.querySelectorAll('.sidenav__item');
-const burgerButton = document.querySelector('.burger__button');
+function createSidebarList() {
 
-sideNavElements.forEach(el => {
-   el.addEventListener('click', function (event) {
+   const sideNav = document.querySelector(".sidenav");
+   const list = document.createElement("ul");
+   list.classList.add("sidenav__list");
+
+   const listHtml = contentObj
+      .map((item) => {
+         return `<li>
+         <a href="#" class="sidenav__item" 
+         id="${item.id}" > ${item.styleTitle}
+         </a>
+         </li>`;
+      })
+      .join("");
+   list.innerHTML = listHtml;
+   sideNav.append(list);
+   sideNav.addEventListener("click", handleSidebarItemClick);
+
+}
+
+function handleSidebarItemClick(event) {
+
+   const burgerButton = document.querySelector(".burger__button");
+   const sideNavList = document.querySelector(".sidenav__list");
+   if (!sideNavList) return;
+
+   if (event.target.closest(".sidenav__item")) {
+      const currentItem = event.target.closest(".sidenav__item");
+
+      if (!currentItem.classList.contains("active")) {
+         const activeItem = sideNavList.querySelector(".active");
+         if (activeItem) activeItem.classList.remove("active");
+         currentItem.classList.add("active");
+         displayContent(currentItem.id);
+      }
+
       event.preventDefault();
       burgerButton.click();
-      const contentId = this.id;
-      const newContent = contentObj.filter(el => el.id == contentId);
-      newContent.forEach(el => {
-         contentTitle.innerHTML = el.styleTitle;
-         contentText.innerHTML = el.styleDescription;
-         contentPicture.src = el.stylePicture;
-      });
-   })
-})
+   }
 
+}
+
+function displayContent(contentId) {
+
+   const newContent = contentObj.find((el) => el.id === contentId);
+   if (!newContent) return;
+
+   const contentSection = document.querySelector(".content__seciton");
+   if (!contentSection) return;
+
+   contentSection.innerHTML = `
+      <h2 class="content__title">${newContent.styleTitle}</h2>
+      <img
+         class="content__picture"
+         src="${newContent.stylePicture}"
+         alt="${newContent.styleTitle}"
+         />
+      <div class="content__text">
+      ${newContent.styleDescription}
+      </div>`;
+
+}
